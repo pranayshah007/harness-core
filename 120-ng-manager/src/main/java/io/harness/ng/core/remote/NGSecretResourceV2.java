430657e234a92b9bbdc2d14c4d3fbc95c03783ce
Tejas Tiwari <tejas.tiwari@harness.io>
2022-03-30 21:55:44 +0530

    [PL-22385]: Resolved bugs

--- 120-ng-manager/src/main/java/io/harness/ng/core/remote/NGSecretResourceV2.java
+++ 120-ng-manager/src/main/java/io/harness/ng/core/remote/NGSecretResourceV2.java
@@ -68,8 +68,6 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.io.InputStream;
 import java.lang.reflect.Field;
-import java.net.URISyntaxException;
-import java.security.InvalidKeyException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
