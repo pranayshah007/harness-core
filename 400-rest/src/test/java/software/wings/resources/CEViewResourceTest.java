e5bc1ea8107208bdd30274242370b2504d276f78
Prashant Sharma <prashant.sharma@harness.io>
2022-06-06 08:25:17 +0530

    feat: [CDS-0]: codeformat fix for develop

--- 400-rest/src/test/java/software/wings/resources/CEViewResourceTest.java
+++ 400-rest/src/test/java/software/wings/resources/CEViewResourceTest.java
@@ -9,12 +9,12 @@ package software.wings.resources;
 
 import static io.harness.rule.OwnerRule.NIKUNJ;
 
-import static org.mockito.Matchers.anyBoolean;
 import static software.wings.graphql.datafetcher.billing.CloudBillingHelper.unified;
 
 import static java.lang.String.format;
 import static javax.ws.rs.client.Entity.entity;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
