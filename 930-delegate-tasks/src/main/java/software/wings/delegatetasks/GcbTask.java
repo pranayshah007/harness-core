fa42b2704bbd6f5ebb96dd6f977cd5139fdd6305
Deepak Patankar <deepak.patankar@harness.io>
2022-06-06 22:22:17 +0530

    [PL-38203]: Fix the issue when gcb task is not working with private repo

--- 930-delegate-tasks/src/main/java/software/wings/delegatetasks/GcbTask.java
+++ 930-delegate-tasks/src/main/java/software/wings/delegatetasks/GcbTask.java
@@ -190,7 +190,9 @@ public class GcbTask extends AbstractDelegateRunnableTask {
   }
 
   String fetchSpecFromGit(final @NotNull GcbTaskParams params) {
-    encryptionService.decrypt(params.getGitConfig(), params.getEncryptedDataDetails(), false);
+    if(params.getGitConfig() != null) {
+      encryptionService.decrypt(params.getGitConfig(), params.getEncryptedDataDetails(), false);
+    }
     final GcbRemoteBuildSpec repositorySpec =
         requireNonNull(requireNonNull(params.getGcbOptions(), "gcbOptions should not be null").getRepositorySpec(),
             "repositorySpec should not be null");
