c4bd47dbb59282d29be4cbc34cba600f86d705ff
arpit-harness <arpit.jain@harness.io>
2022-06-15 10:10:27 +0530

    [CVS-10740]: Added migration to add enabled in SLI and SLO

--- 300-cv-nextgen/src/main/java/io/harness/cvng/migration/list/AddEnabledFlagToSLISLOMigration.java
+++ 300-cv-nextgen/src/main/java/io/harness/cvng/migration/list/AddEnabledFlagToSLISLOMigration.java
@@ -10,12 +10,16 @@ package io.harness.cvng.migration.list;
 import io.harness.cvng.migration.CVNGMigration;
 import io.harness.cvng.migration.beans.ChecklistItem;
 import io.harness.cvng.servicelevelobjective.entities.ServiceLevelIndicator;
+import io.harness.cvng.servicelevelobjective.entities.ServiceLevelIndicator.ServiceLevelIndicatorKeys;
+import io.harness.cvng.servicelevelobjective.entities.ServiceLevelObjective;
+import io.harness.cvng.servicelevelobjective.entities.ServiceLevelObjective.ServiceLevelObjectiveKeys;
 import io.harness.persistence.HIterator;
 import io.harness.persistence.HPersistence;
 
 import com.google.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import org.mongodb.morphia.query.Query;
+import org.mongodb.morphia.query.UpdateResults;
 
 @Slf4j
 public class AddEnabledFlagToSLISLOMigration implements CVNGMigration {
@@ -27,6 +31,25 @@ public class AddEnabledFlagToSLISLOMigration implements CVNGMigration {
     Query<ServiceLevelIndicator> serviceLevelIndicatorQuery = hPersistence.createQuery(ServiceLevelIndicator.class);
     try (HIterator<ServiceLevelIndicator> sliIterator = new HIterator<>(serviceLevelIndicatorQuery.fetch())) {
       while (sliIterator.hasNext()) {
+        ServiceLevelIndicator serviceLevelIndicator = sliIterator.next();
+        UpdateResults updateResults = hPersistence.update(serviceLevelIndicator,
+            hPersistence.createUpdateOperations(ServiceLevelIndicator.class)
+                .set(ServiceLevelIndicatorKeys.enabled, true));
+        log.info("Updated for SLI {}, {}, Update Result {}", serviceLevelIndicator.getProjectIdentifier(),
+            serviceLevelIndicator.getIdentifier(), updateResults);
+      }
+    }
+
+    log.info("Begin migration for updating SL0 with enabled flag");
+    Query<ServiceLevelObjective> serviceLevelObjectiveQuery = hPersistence.createQuery(ServiceLevelObjective.class);
+    try (HIterator<ServiceLevelObjective> sloIterator = new HIterator<>(serviceLevelObjectiveQuery.fetch())) {
+      while (sloIterator.hasNext()) {
+        ServiceLevelObjective serviceLevelObjective = sloIterator.next();
+        UpdateResults updateResults = hPersistence.update(serviceLevelObjective,
+            hPersistence.createUpdateOperations(ServiceLevelObjective.class)
+                .set(ServiceLevelObjectiveKeys.enabled, true));
+        log.info("Updated for SLO {}, {}, Update Result {}", serviceLevelObjective.getProjectIdentifier(),
+            serviceLevelObjective.getIdentifier(), updateResults);
       }
     }
   }
