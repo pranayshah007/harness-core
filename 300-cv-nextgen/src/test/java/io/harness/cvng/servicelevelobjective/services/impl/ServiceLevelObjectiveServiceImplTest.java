8da1161f9bf6da81fb07b014e104629e2948cbcb
Naveen Shukla <105041588+naveen-harness@users.noreply.github.com>
2022-06-15 14:25:54 +0530

    feat: [SRM-10617]: Slo service create update and delete audit events hotfix (#34134) (#34343)
    * [SRM-10615]: Slo service create update and delete audit events (#34134)
    
    * [SRM-10588]: Add event classes for monitored service audit trail events
    
    * [SRM-10588]: Address comments
    
    * [SRM-10588]: Fix issues
    
    * [SRM-10588]: Fix code lints
    
    * [SRM-10588]: Outbox integration
    
    * [SRM-10588]: Integrate create audit event
    
    * [SRM-10588]: Configuration restored to default
    
    * [SRM-10588]: Address review comments comments.
    
    * [SRM-10588]: Fix unit tests.
    
    * [SRM-10588]: Fix codestyle
    
    * [SRM-10615]: Integrate update, delete and toggle events with audit service.
    
    * [SRM-10615]: Add unit-tests, add yaml difference.
    
    * [SRM-10615]: Fix uni-tests, add complete yaml diff (old and new)
    
    * [SRM-10615]: Add more tests.
    
    * [SRM-10615]: Fix code smells.
    
    * [SRM-10617]: Audit trail for SLO CUD events.
    
    * [SRM-10615]: Add unit-tests.
    
    * [SRM-10617]: Refactor unit-tests.
    
    * [SRM-10617]: Fix code smells.
    
    * [SRM-10617]: Fix code smells.
    
    * [SRM-10617]: Fix code smells.
    
    * [SRM-10617]: Fix unit-tests.
    
    * Update ServiceLevelObjectiveServiceImplTest.java

--- 300-cv-nextgen/src/test/java/io/harness/cvng/servicelevelobjective/services/impl/ServiceLevelObjectiveServiceImplTest.java
+++ 300-cv-nextgen/src/test/java/io/harness/cvng/servicelevelobjective/services/impl/ServiceLevelObjectiveServiceImplTest.java
@@ -13,6 +13,7 @@ import static io.harness.cvng.servicelevelobjective.entities.SLIRecord.SLIState.
 import static io.harness.rule.OwnerRule.ABHIJITH;
 import static io.harness.rule.OwnerRule.DEEPAK_CHHIKARA;
 import static io.harness.rule.OwnerRule.KAPIL;
+import static io.harness.rule.OwnerRule.NAVEEN;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -39,6 +40,9 @@ import io.harness.cvng.core.beans.params.logsFilterParams.SLILogsFilter;
 import io.harness.cvng.core.services.api.CVNGLogService;
 import io.harness.cvng.core.services.api.VerificationTaskService;
 import io.harness.cvng.core.services.api.monitoredService.MonitoredServiceService;
+import io.harness.cvng.events.servicelevelobjective.ServiceLevelObjectiveCreateEvent;
+import io.harness.cvng.events.servicelevelobjective.ServiceLevelObjectiveDeleteEvent;
+import io.harness.cvng.events.servicelevelobjective.ServiceLevelObjectiveUpdateEvent;
 import io.harness.cvng.notification.beans.ErrorBudgetRemainingMinutesConditionSpec;
 import io.harness.cvng.notification.beans.NotificationRuleCondition;
 import io.harness.cvng.notification.beans.NotificationRuleConditionType;
@@ -52,6 +56,7 @@ import io.harness.cvng.notification.entities.SLONotificationRule.SLOErrorBudgetR
 import io.harness.cvng.notification.entities.SLONotificationRule.SLONotificationRuleCondition;
 import io.harness.cvng.notification.services.api.NotificationRuleService;
 import io.harness.cvng.notification.utils.NotificationRuleCommonUtils;
+import io.harness.cvng.outbox.CVServiceOutboxEventHandler;
 import io.harness.cvng.servicelevelobjective.SLORiskCountResponse;
 import io.harness.cvng.servicelevelobjective.beans.DayOfWeek;
 import io.harness.cvng.servicelevelobjective.beans.ErrorBudgetRisk;
@@ -90,6 +95,9 @@ import io.harness.cvng.statemachine.entities.AnalysisOrchestrator.AnalysisOrches
 import io.harness.exception.InvalidRequestException;
 import io.harness.ng.beans.PageResponse;
 import io.harness.notification.notificationclient.NotificationResultWithoutStatus;
+import io.harness.outbox.OutboxEvent;
+import io.harness.outbox.api.OutboxService;
+import io.harness.outbox.filter.OutboxEventFilter;
 import io.harness.persistence.HPersistence;
 import io.harness.rest.RestResponse;
 import io.harness.rule.Owner;
@@ -135,6 +143,8 @@ public class ServiceLevelObjectiveServiceImplTest extends CvNextGenTestBase {
   @Inject NotificationRuleCommonUtils notificationRuleCommonUtils;
   @Mock(answer = Answers.RETURNS_DEEP_STUBS) AccountClient accountClient;
   @Inject private SLIRecordServiceImpl sliRecordService;
+  @Inject private OutboxService outboxService;
+  @Inject CVServiceOutboxEventHandler cvServiceOutboxEventHandler;
   String accountId;
   String orgIdentifier;
   String projectIdentifier;
@@ -1122,6 +1132,48 @@ public class ServiceLevelObjectiveServiceImplTest extends CvNextGenTestBase {
     createMonitoredService();
     assertThatThrownBy(() -> serviceLevelObjectiveService.create(projectParams, sloDTO))
         .hasMessage("NotificationRule with identifier rule is of type MONITORED_SERVICE and cannot be added into SLO");
+  }
+
+  @Test
+  @Owner(developers = NAVEEN)
+  @Category(UnitTests.class)
+  public void testCreate_ServiceLevelObjectiveCreateAuditEvent() {
+    ServiceLevelObjectiveDTO sloDTO = createSLOBuilder();
+    createMonitoredService();
+    serviceLevelObjectiveService.create(projectParams, sloDTO);
+    List<OutboxEvent> outboxEvents = outboxService.list(OutboxEventFilter.builder().maximumEventsPolled(10).build());
+    OutboxEvent outboxEvent = outboxEvents.get(outboxEvents.size() - 1);
+    assertThat(outboxEvent.getEventType()).isEqualTo(ServiceLevelObjectiveCreateEvent.builder().build().getEventType());
+  }
+
+  @Test
+  @Owner(developers = NAVEEN)
+  @Category(UnitTests.class)
+  public void testUpdate_ServiceLevelObjectiveUpdateAuditEvent() {
+    ServiceLevelObjectiveDTO sloDTO = createSLOBuilder();
+    createMonitoredService();
+    ServiceLevelObjectiveResponse serviceLevelObjectiveResponse =
+        serviceLevelObjectiveService.create(projectParams, sloDTO);
+    assertThat(serviceLevelObjectiveResponse.getServiceLevelObjectiveDTO()).isEqualTo(sloDTO);
+    sloDTO.setDescription("newDescription");
+    serviceLevelObjectiveService.update(projectParams, sloDTO.getIdentifier(), sloDTO);
+    List<OutboxEvent> outboxEvents = outboxService.list(OutboxEventFilter.builder().maximumEventsPolled(10).build());
+    OutboxEvent outboxEvent = outboxEvents.get(outboxEvents.size() - 1);
+    assertThat(outboxEvent.getEventType()).isEqualTo(ServiceLevelObjectiveUpdateEvent.builder().build().getEventType());
+  }
+
+  @Test
+  @Owner(developers = NAVEEN)
+  @Category(UnitTests.class)
+  public void testDelete_ServiceLevelObjectiveDeleteAuditEvent() {
+    ServiceLevelObjectiveDTO sloDTO = createSLOBuilder();
+    createMonitoredService();
+    serviceLevelObjectiveService.create(projectParams, sloDTO);
+    boolean isDeleted = serviceLevelObjectiveService.delete(projectParams, sloDTO.getIdentifier());
+    assertThat(isDeleted).isEqualTo(true);
+    List<OutboxEvent> outboxEvents = outboxService.list(OutboxEventFilter.builder().maximumEventsPolled(10).build());
+    OutboxEvent outboxEvent = outboxEvents.get(outboxEvents.size() - 1);
+    assertThat(outboxEvent.getEventType()).isEqualTo(ServiceLevelObjectiveDeleteEvent.builder().build().getEventType());
   }
 
   @Test
