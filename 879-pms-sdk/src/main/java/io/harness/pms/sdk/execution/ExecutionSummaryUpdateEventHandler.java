d5cd837de4110d9b1fa4319dcefb771acd72b4af
Brijesh Dhakar <brijesh.dhakar@harness.io>
2022-06-15 21:18:20 +0530

    minor fix

--- 879-pms-sdk/src/main/java/io/harness/pms/sdk/execution/ExecutionSummaryUpdateEventHandler.java
+++ 879-pms-sdk/src/main/java/io/harness/pms/sdk/execution/ExecutionSummaryUpdateEventHandler.java
@@ -57,13 +57,7 @@ public class ExecutionSummaryUpdateEventHandler implements OrchestrationEventHan
             .setPlanExecutionId(ambiance.getPlanExecutionId())
             .setNodeExecutionId(AmbianceUtils.obtainCurrentLevel(ambiance).getRuntimeId());
     Optional<Level> stageLevel = AmbianceUtils.getStageLevelFromAmbiance(ambiance);
-    if (stageLevel.isPresent()) {
-      if (stageLevel.get().hasStrategyMetadata()) {
-        executionSummaryUpdateRequest.setNodeUuid(stageLevel.get().getRuntimeId());
-      } else {
-        executionSummaryUpdateRequest.setNodeUuid(stageLevel.get().getSetupId());
-      }
-    }
+    stageLevel.ifPresent(value -> executionSummaryUpdateRequest.setNodeUuid(value.getSetupId()));
     String pipelineInfoJson = RecastOrchestrationUtils.toJson(
         executionSummaryModuleInfoProvider.getPipelineLevelModuleInfo(orchestrationEvent));
     if (EmptyPredicate.isNotEmpty(pipelineInfoJson)) {
