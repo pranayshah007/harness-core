75dfc9d56b3dcca02714a85fd346884d216a7cbd
Shubhanshu Verma <shubhanshu.verma@harness.io>
2022-06-16 13:21:11 +0530

    [CCM-7761]: fixed notification client method

--- 930-ng-core-clients/src/main/java/io/harness/notifications/NotificationResourceClient.java
+++ 930-ng-core-clients/src/main/java/io/harness/notifications/NotificationResourceClient.java
@@ -3,9 +3,10 @@ package io.harness.notifications;
 import io.harness.NGCommonEntityConstants;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.ng.core.dto.ResponseDTO;
 import io.harness.notification.channeldetails.NotificationChannel;
 import io.harness.notification.notificationclient.NotificationResult;
-import io.harness.rest.RestResponse;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
@@ -16,7 +17,7 @@ public interface NotificationResourceClient {
   String BASE_API = "notification";
 
   @POST(BASE_API)
-  Call<RestResponse<NotificationResult>> sendNotification(
+  Call<ResponseDTO<NotificationResult>> sendNotification(
       @Query(value = NGCommonEntityConstants.ACCOUNT_KEY) String accountIdentifier,
       @Body NotificationChannel notificationChannel);
 }
