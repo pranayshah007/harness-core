124f8b1dbd09c6a9433741ec277f123c9b23d3b7
Jenny James <jenny.james@harness.io>
2022-06-20 19:54:44 -0700

    fix UnrecognizedPropertyException

--- 920-delegate-agent-beans/src/main/java/io/harness/beans/DelegateHeartbeatResponse.java
+++ 920-delegate-agent-beans/src/main/java/io/harness/beans/DelegateHeartbeatResponse.java
@@ -7,7 +7,6 @@
 
 package io.harness.beans;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
