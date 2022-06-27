dcc118ff94e0c524e8157779ee28b7e4ba2acc61
Abhinav Singh <abhinav.singh@harness.io>
2022-06-27 21:39:31 +0530

    [feat]: [PL-25582]: NG Ldap settings CRUD changes (#34610)
    * [feat]: [PL-25915]: group name search in NG Ldap
    
    * [feat]: [PL-25915]: ldap search for group names in NG
    
    Signed-off-by: Prateek Barapatre <prateek.barapatre@harness.io>
    
    * [feat]: [PL-25915]: group name search in NG Ldap checkstyle
    
    * [feat]: [PL-25915]: few minor changes
    
    Signed-off-by: Prateek Barapatre <prateek.barapatre@harness.io>
    
    * [feat]: [PL-25582]: NG ldap CRUD ops
    
    Signed-off-by: Prateek Barapatre <prateek.barapatre@harness.io>
    
    * [feat]: [PL-25582]: minor updates
    
    Signed-off-by: Prateek Barapatre <prateek.barapatre@harness.io>
    
    * [feat]: [PL-25915]: NG Ldap settings CRUD and group name search
    
    * [feat]: [PL-25915]: NG Ldap settings CRUD and group name search
    
    * [feat]: [PL-25915]: LDAPSetting schema
    
    * [feat]: [PL-25582]: review comments
    
    Signed-off-by: Prateek Barapatre <prateek.barapatre@harness.io>
    
    * Update module-dependency.hash
    
    * Update module-dependency.hash

--- 120-ng-manager/modules/ldap/src/main/java/io/harness/ldap/service/impl/NGLdapServiceImpl.java
+++ 120-ng-manager/modules/ldap/src/main/java/io/harness/ldap/service/impl/NGLdapServiceImpl.java
@@ -3,7 +3,9 @@ package io.harness.ldap.service.impl;
 import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 import static io.harness.delegate.beans.NgSetupFields.NG;
 import static io.harness.delegate.beans.NgSetupFields.OWNER;
+import static io.harness.remote.client.RestClientUtils.getResponse;
 
+import static software.wings.beans.TaskType.NG_LDAP_SEARCH_GROUPS;
 import static software.wings.beans.TaskType.NG_LDAP_TEST_CONN_SETTINGS;
 
 import io.harness.annotations.dev.HarnessTeam;
@@ -13,24 +15,34 @@ import io.harness.delegate.beans.DelegateResponseData;
 import io.harness.delegate.beans.ErrorNotifyResponseData;
 import io.harness.delegate.beans.RemoteMethodReturnValueData;
 import io.harness.delegate.beans.TaskData;
+import io.harness.delegate.beans.ldap.LdapSettingsWithEncryptedDataDetail;
 import io.harness.delegate.beans.ldap.NGLdapDelegateTaskParameters;
 import io.harness.delegate.beans.ldap.NGLdapDelegateTaskResponse;
+import io.harness.delegate.beans.ldap.NGLdapGroupSearchTaskParameters;
+import io.harness.delegate.beans.ldap.NGLdapGroupSearchTaskResponse;
+import io.harness.delegate.task.TaskParameters;
 import io.harness.delegate.utils.TaskSetupAbstractionHelper;
 import io.harness.exception.InvalidRequestException;
 import io.harness.ldap.service.NGLdapService;
+import io.harness.ng.authenticationsettings.remote.AuthSettingsManagerClient;
+import io.harness.rest.RestResponse;
 import io.harness.service.DelegateGrpcClientWrapper;
 
 import software.wings.beans.TaskType;
+import software.wings.beans.dto.LdapSettings;
+import software.wings.beans.sso.LdapGroupResponse;
 import software.wings.beans.sso.LdapTestResponse;
 import software.wings.service.impl.ldap.LdapDelegateException;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.time.Duration;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import retrofit2.Call;
 
 @AllArgsConstructor(onConstructor = @__({ @Inject }))
 @Singleton
@@ -38,6 +50,7 @@ import lombok.extern.slf4j.Slf4j;
 @OwnedBy(HarnessTeam.PL)
 public class NGLdapServiceImpl implements NGLdapService {
   public static final String UNKNOWN_RESPONSE_FROM_DELEGATE = "Unknown Response from delegate";
+  private final AuthSettingsManagerClient managerClient;
   private final DelegateGrpcClientWrapper delegateService;
   private final TaskSetupAbstractionHelper taskSetupAbstractionHelper;
   @Override
@@ -54,8 +67,37 @@ public class NGLdapServiceImpl implements NGLdapService {
     return delegateTaskResponse.getLdapTestResponse();
   }
 
+  @Override
+  public Collection<LdapGroupResponse> searchLdapGroupsByName(
+      String accountIdentifier, String orgIdentifier, String projectIdentifier, String ldapId, String name) {
+    Call<RestResponse<LdapSettingsWithEncryptedDataDetail>> settingsWithEncryptedDataDetails =
+        managerClient.getLdapSettingsWithEncryptedDataDetails(accountIdentifier);
+    if (null == settingsWithEncryptedDataDetails) {
+      log.warn(
+          "Failed to get ldap settings with encrypted data detail from manager for account: {}", accountIdentifier);
+      throw new InvalidRequestException("Failed to get LDAPSettings with encrypted data detail for the request");
+    }
+    LdapSettingsWithEncryptedDataDetail settingsWithEncryptedDataDetail = getResponse(settingsWithEncryptedDataDetails);
+    LdapSettings ldapSettings = settingsWithEncryptedDataDetail.getLdapSettings();
+
+    NGLdapGroupSearchTaskParameters parameters =
+        NGLdapGroupSearchTaskParameters.builder()
+            .ldapSettings(ldapSettings)
+            .encryptedDataDetail(settingsWithEncryptedDataDetail.getEncryptedDataDetail())
+            .name(name)
+            .build();
+
+    DelegateResponseData delegateResponseData =
+        getDelegateResponseData(accountIdentifier, orgIdentifier, projectIdentifier, parameters, NG_LDAP_SEARCH_GROUPS);
+
+    NGLdapGroupSearchTaskResponse groupSearchResponse = (NGLdapGroupSearchTaskResponse) delegateResponseData;
+    log.info("Delegate response for searchLdapGroupsByName returned groups whose size: {}",
+        groupSearchResponse.getLdapListGroupsResponses().size());
+    return groupSearchResponse.getLdapListGroupsResponses();
+  }
+
   private DelegateResponseData getDelegateResponseData(String accountIdentifier, String orgIdentifier,
-      String projectIdentifier, NGLdapDelegateTaskParameters parameters, TaskType taskType) {
+      String projectIdentifier, TaskParameters parameters, TaskType taskType) {
     DelegateTaskRequest delegateTaskRequest =
         DelegateTaskRequest.builder()
             .taskType(taskType.name())
