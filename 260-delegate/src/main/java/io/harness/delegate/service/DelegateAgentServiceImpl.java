703e0d9cdbe52db0eb580fca618db8b4e635f64f
Raghvendra Singh <raghu@harness.io>
2022-06-29 15:38:37 -0700

    [fix]: [DEL-4390]: Job queuing -- Start 500 jobs on a delegate at once and overwhelms it
    This reverts commit 00f058750b693b8602c3902389ffcf0206bbe1d4.

--- 260-delegate/src/main/java/io/harness/delegate/service/DelegateAgentServiceImpl.java
+++ 260-delegate/src/main/java/io/harness/delegate/service/DelegateAgentServiceImpl.java
@@ -318,6 +318,9 @@ public class DelegateAgentServiceImpl implements DelegateAgentService {
   private final String delegateDescription = System.getenv().get("DELEGATE_DESCRIPTION");
   private final boolean delegateNg = isNotBlank(System.getenv().get("DELEGATE_SESSION_IDENTIFIER"))
       || (isNotBlank(System.getenv().get("NEXT_GEN")) && Boolean.parseBoolean(System.getenv().get("NEXT_GEN")));
+  private final int delegateTaskLimit = isNotBlank(System.getenv().get("DELEGATE_TASKS_LIMIT"))
+      ? Integer.parseInt(System.getenv().get("DELEGATE_TASKS_LIMIT"))
+      : 0;
   public static final String JAVA_VERSION = "java.version";
   private final double RESOURCE_USAGE_THRESHOLD = 0.75;
 
@@ -1925,6 +1928,16 @@ public class DelegateAgentServiceImpl implements DelegateAgentService {
           return;
         }
 
+        int perpetualTaskCount = 0;
+        if (perpetualTaskWorker != null) {
+          perpetualTaskCount = perpetualTaskWorker.getCurrentlyExecutingPerpetualTasksCount().intValue();
+        }
+
+        if (delegateTaskLimit > 0 && (currentlyExecutingFutures.size() + perpetualTaskCount) >= delegateTaskLimit) {
+          log.info(
+              "Delegate reached Delegate Size Task Limit of {}. It will not acquire this time.", delegateTaskLimit);
+          return;
+        }
         currentlyAcquiringTasks.add(delegateTaskId);
 
         log.debug("Try to acquire DelegateTask - accountId: {}", accountId);
