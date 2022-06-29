9cfe7e9de028ff631cffda3456adbd9b18bba783
Sahil Hindwani <sahil.hindwani@harness.io>
2022-06-29 17:20:47 +0530

    [PIE-4294]: utility for expanding execution yaml for CI

--- 878-ng-common-utilities/src/main/java/io/harness/steps/matrix/StrategyHelper.java
+++ 878-ng-common-utilities/src/main/java/io/harness/steps/matrix/StrategyHelper.java
@@ -1,5 +1,6 @@
 package io.harness.steps.matrix;
 
+import io.harness.exception.InvalidRequestException;
 import io.harness.plancreator.execution.ExecutionWrapperConfig;
 import io.harness.plancreator.steps.ParallelStepElementConfig;
 import io.harness.plancreator.steps.StepGroupElementConfig;
@@ -9,6 +10,7 @@ import io.harness.pms.yaml.YamlUtils;
 import io.harness.yaml.utils.JsonPipelineUtils;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -39,39 +41,46 @@ public class StrategyHelper {
     return forLoopStrategyConfigService.expandJsonNode(strategyConfig, nodeWithStrategy);
   }
 
-  public List<ExecutionWrapperConfig> expandExecutionWrapperConfig(ExecutionWrapperConfig executionWrapperConfig)
-      throws IOException {
-    if (executionWrapperConfig.getStep() != null) {
-      List<JsonNode> expandedJsonNodes = expandJsonNodes(executionWrapperConfig.getStep());
-      return expandedJsonNodes.stream()
-          .map(node -> ExecutionWrapperConfig.builder().step(node).uuid(executionWrapperConfig.getUuid()).build())
-          .collect(Collectors.toList());
-    }
-    if (executionWrapperConfig.getParallel() != null) {
-      ParallelStepElementConfig parallelStepElementConfig =
-          YamlUtils.read(executionWrapperConfig.getParallel().toString(), ParallelStepElementConfig.class);
-      List<ExecutionWrapperConfig> executionWrapperConfigs = new ArrayList<>();
-      for (ExecutionWrapperConfig config : parallelStepElementConfig.getSections()) {
-        executionWrapperConfigs.addAll(expandExecutionWrapperConfig(config));
+  public List<ExecutionWrapperConfig> expandExecutionWrapperConfig(ExecutionWrapperConfig executionWrapperConfig) {
+    try {
+      if (executionWrapperConfig.getStep() != null) {
+        List<JsonNode> expandedJsonNodes = expandJsonNodes(executionWrapperConfig.getStep());
+        return expandedJsonNodes.stream()
+            .map(node -> ExecutionWrapperConfig.builder().step(node).uuid(executionWrapperConfig.getUuid()).build())
+            .collect(Collectors.toList());
       }
-      parallelStepElementConfig.setSections(executionWrapperConfigs);
-      JsonNode parallelNode = JsonPipelineUtils.asTree(parallelStepElementConfig);
-      return Lists.newArrayList(ExecutionWrapperConfig.builder().parallel(parallelNode).build());
-    }
-    if (executionWrapperConfig.getStepGroup() != null) {
-      StepGroupElementConfig stepGroupElementConfig =
-          YamlUtils.read(executionWrapperConfig.getStepGroup().toString(), StepGroupElementConfig.class);
-      List<ExecutionWrapperConfig> executionWrapperConfigs = new ArrayList<>();
-      for (ExecutionWrapperConfig config : stepGroupElementConfig.getSteps()) {
-        executionWrapperConfigs.addAll(expandExecutionWrapperConfig(config));
+      if (executionWrapperConfig.getParallel() != null) {
+        ParallelStepElementConfig parallelStepElementConfig =
+            YamlUtils.read(executionWrapperConfig.getParallel().toString(), ParallelStepElementConfig.class);
+        List<ExecutionWrapperConfig> executionWrapperConfigs = new ArrayList<>();
+        for (ExecutionWrapperConfig config : parallelStepElementConfig.getSections()) {
+          executionWrapperConfigs.addAll(expandExecutionWrapperConfig(config));
+        }
+        parallelStepElementConfig.setSections(executionWrapperConfigs);
+        ArrayNode arrayNode = JsonPipelineUtils.getMapper().createArrayNode();
+        for (ExecutionWrapperConfig config : executionWrapperConfigs) {
+          arrayNode.add(JsonPipelineUtils.asTree(config));
+        }
+        return Lists.newArrayList(ExecutionWrapperConfig.builder().parallel(arrayNode).build());
       }
-      stepGroupElementConfig.setSteps(executionWrapperConfigs);
-      JsonNode stepGroupNode = JsonPipelineUtils.asTree(stepGroupElementConfig);
-      List<JsonNode> expandedJsonNodes = expandJsonNodes(stepGroupNode);
-      return expandedJsonNodes.stream()
-          .map(node -> ExecutionWrapperConfig.builder().stepGroup(node).uuid(executionWrapperConfig.getUuid()).build())
-          .collect(Collectors.toList());
+      if (executionWrapperConfig.getStepGroup() != null) {
+        StepGroupElementConfig stepGroupElementConfig =
+            YamlUtils.read(executionWrapperConfig.getStepGroup().toString(), StepGroupElementConfig.class);
+        List<ExecutionWrapperConfig> executionWrapperConfigs = new ArrayList<>();
+        for (ExecutionWrapperConfig config : stepGroupElementConfig.getSteps()) {
+          executionWrapperConfigs.addAll(expandExecutionWrapperConfig(config));
+        }
+        stepGroupElementConfig.setSteps(executionWrapperConfigs);
+        JsonNode stepGroupNode = JsonPipelineUtils.asTree(stepGroupElementConfig);
+        List<JsonNode> expandedJsonNodes = expandJsonNodes(stepGroupNode);
+        return expandedJsonNodes.stream()
+            .map(
+                node -> ExecutionWrapperConfig.builder().stepGroup(node).uuid(executionWrapperConfig.getUuid()).build())
+            .collect(Collectors.toList());
+      }
+      return Lists.newArrayList(executionWrapperConfig);
+    } catch (Exception ex) {
+      throw new InvalidRequestException("Unable to expand yaml for a execution element with strategy");
     }
-    return Lists.newArrayList(executionWrapperConfig);
   }
 }
