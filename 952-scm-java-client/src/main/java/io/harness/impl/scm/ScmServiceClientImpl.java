c166cc8b7553a59564565bb18fb6788ddf5d90f5
Mohit Garg <mohit.garg@harness.io>
2022-07-05 18:42:24 +0530

    fix: [PL-26275]: List branches addAll() operation fix (#35006)
    * [PL-26275]: fixing list addAll() issue in branch listing
    
    * [PL-26275]: increment build count
    
    * [PL-26275]: increment build count

--- 952-scm-java-client/src/main/java/io/harness/impl/scm/ScmServiceClientImpl.java
+++ 952-scm-java-client/src/main/java/io/harness/impl/scm/ScmServiceClientImpl.java
@@ -334,7 +334,9 @@ public class ScmServiceClientImpl implements ScmServiceClient {
     final Provider provider = scmGitProviderMapper.mapToSCMGitProvider(scmConnector);
     ListBranchesResponse listBranchesResponse = null;
     int branchCount = listBranchesWithDefaultResponse.getBranchesCount();
-    List<String> branchesList = listBranchesWithDefaultResponse.getBranchesList();
+    List<String> branchesList = new ArrayList<>();
+
+    branchesList.addAll(new ArrayList<>(listBranchesWithDefaultResponse.getBranchesList()));
     int pageNumber = listBranchesWithDefaultResponse.getPagination().getNext();
     while (pageNumber != 0 && branchCount <= pageSize) {
       ListBranchesRequest listBranchesRequest = ListBranchesRequest.newBuilder()
@@ -350,7 +352,7 @@ public class ScmServiceClientImpl implements ScmServiceClient {
             .setError(listBranchesResponse.getError())
             .build();
       }
-      branchesList.addAll(listBranchesResponse.getBranchesList());
+      branchesList.addAll(new ArrayList<>(listBranchesResponse.getBranchesList()));
       pageNumber = listBranchesResponse.getPagination().getNext();
       branchCount += listBranchesResponse.getBranchesCount();
     }
