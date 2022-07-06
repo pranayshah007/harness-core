02888fd11c79b157262a8c8548404d4bb01015f7
Harsh Jain <harsh.jain@harness.io>
2022-07-06 11:50:35 -0700

    feat: [CI-4730]: move execution code to new module

--- 312-ci-manager/contracts/api/src/main/java/io/harness/cimanager/executionconfig/api/CIExecutionConfigResource.java
+++ 312-ci-manager/contracts/api/src/main/java/io/harness/cimanager/executionconfig/api/CIExecutionConfigResource.java
@@ -5,7 +5,7 @@
  * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
  */
 
-package io.harness.app.resources;
+package io.harness.cimanager.executionconfig.api;
 
 import static io.harness.account.accesscontrol.AccountAccessControlPermissions.EDIT_ACCOUNT_PERMISSION;
 import static io.harness.account.accesscontrol.AccountAccessControlPermissions.VIEW_ACCOUNT_PERMISSION;
@@ -13,20 +13,18 @@ import static io.harness.annotations.dev.HarnessTeam.CI;
 
 import io.harness.NGCommonEntityConstants;
 import io.harness.accesscontrol.NGAccessControlCheck;
+import io.harness.accesscontrol.commons.exceptions.AccessDeniedErrorDTO;
 import io.harness.account.accesscontrol.ResourceTypes;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.beans.sweepingoutputs.StageInfraDetails.Type;
 import io.harness.ci.beans.entities.CIExecutionImages;
 import io.harness.ci.config.Operation;
-import io.harness.ci.execution.CIExecutionConfigService;
 import io.harness.ci.execution.DeprecatedImageInfo;
-import io.harness.cimanager.commons.exceptions.AccessDeniedErrorDTO;
 import io.harness.ng.core.dto.ErrorDTO;
 import io.harness.ng.core.dto.FailureDTO;
 import io.harness.ng.core.dto.ResponseDTO;
 import io.harness.security.annotations.NextGenManagerAuth;
 
-import com.google.inject.Inject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -46,14 +44,12 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import lombok.AllArgsConstructor;
 
 @OwnedBy(CI)
 @Api("/execution-config")
 @Path("/execution-config")
 @Produces({"application/json"})
 @Consumes({"application/json"})
-@AllArgsConstructor(onConstructor = @__({ @Inject }))
 @NextGenManagerAuth
 @ApiResponses(value =
     {
@@ -79,9 +75,7 @@ import lombok.AllArgsConstructor;
       @Content(mediaType = "application/json", schema = @Schema(implementation = AccessDeniedErrorDTO.class))
       , @Content(mediaType = "application/yaml", schema = @Schema(implementation = AccessDeniedErrorDTO.class))
     })
-public class CIExecutionConfigResource {
-  @Inject CIExecutionConfigService configService;
-
+public interface CIExecutionConfigResource {
   @POST
   @Path("/update-config")
   @ApiOperation(value = "Update execution config", nickname = "updateExecutionConfig")
@@ -89,14 +83,11 @@ public class CIExecutionConfigResource {
   Operation(operationId = "updateExecutionConfig", summary = "Override execution Config for CI builds",
       responses = { @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "True or False") })
   @NGAccessControlCheck(resourceType = ResourceTypes.ACCOUNT, permission = EDIT_ACCOUNT_PERMISSION)
-  public ResponseDTO<Boolean>
+  ResponseDTO<Boolean>
   updateExecutionConfig(@NotNull @QueryParam(NGCommonEntityConstants.INFRA) Type infra,
       @NotNull @QueryParam(NGCommonEntityConstants.ACCOUNT_KEY) String accountIdentifier,
       @RequestBody(required = true,
-          description = "Details of the Update Operations") @NotNull @Valid List<Operation> operations) {
-    return ResponseDTO.newResponse(configService.updateCIContainerTags(accountIdentifier, operations, infra));
-  }
-
+          description = "Details of the Update Operations") @NotNull @Valid List<Operation> operations);
   @POST
   @Path("/reset-config")
   @ApiOperation(value = "Reset execution config", nickname = "resetExecutionConfig")
@@ -104,13 +95,11 @@ public class CIExecutionConfigResource {
   Operation(operationId = "resetExecutionConfig", summary = "Reset execution Config for CI builds",
       responses = { @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "True or False") })
   @NGAccessControlCheck(resourceType = ResourceTypes.ACCOUNT, permission = EDIT_ACCOUNT_PERMISSION)
-  public ResponseDTO<Boolean>
+  ResponseDTO<Boolean>
   resetExecutionConfig(@NotNull @QueryParam(NGCommonEntityConstants.INFRA) Type infra,
       @NotNull @QueryParam(NGCommonEntityConstants.ACCOUNT_KEY) String accountIdentifier,
-      @RequestBody(required = true,
-          description = "Details of the Reset Operations") @NotNull @Valid List<Operation> operations) {
-    return ResponseDTO.newResponse(configService.resetCIContainerTags(accountIdentifier, operations, infra));
-  }
+      @RequestBody(
+          required = true, description = "Details of the Reset Operations") @NotNull @Valid List<Operation> operations);
 
   @DELETE
   @Path("/")
@@ -119,11 +108,8 @@ public class CIExecutionConfigResource {
       summary = "Delete Execution Config for CI builds and use default configs",
       responses = { @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "True or False") })
   @NGAccessControlCheck(resourceType = ResourceTypes.ACCOUNT, permission = EDIT_ACCOUNT_PERMISSION)
-  public ResponseDTO<Boolean>
-  deleteExecutionConfig(@NotNull @QueryParam(NGCommonEntityConstants.ACCOUNT_KEY) String accountIdentifier) {
-    return ResponseDTO.newResponse(configService.deleteCIExecutionConfig(accountIdentifier));
-  }
-
+  ResponseDTO<Boolean>
+  deleteExecutionConfig(@NotNull @QueryParam(NGCommonEntityConstants.ACCOUNT_KEY) String accountIdentifier);
   @GET
   @Path("/")
   @ApiOperation(value = "Get execution config", nickname = "getExecutionConfig")
@@ -131,11 +117,8 @@ public class CIExecutionConfigResource {
   @io.swagger.v3.oas.annotations.Operation(operationId = "getExecutionConfig", summary = "Get deprecated tags for CI",
       responses =
       { @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "List of deprecated tags for CI builds") })
-  public ResponseDTO<List<DeprecatedImageInfo>>
-  getExecutionConfig(@NotNull @QueryParam(NGCommonEntityConstants.ACCOUNT_KEY) String accountIdentifier) {
-    return ResponseDTO.newResponse(configService.getDeprecatedTags(accountIdentifier));
-  }
-
+  ResponseDTO<List<DeprecatedImageInfo>>
+  getExecutionConfig(@NotNull @QueryParam(NGCommonEntityConstants.ACCOUNT_KEY) String accountIdentifier);
   @GET
   @Path("/get-customer-config")
   @ApiOperation(value = "Get customer's execution config", nickname = "getCustomerConfig")
@@ -147,14 +130,10 @@ public class CIExecutionConfigResource {
         @io.swagger.v3.oas.annotations.responses.
         ApiResponse(description = "List of overridden images and versions used in CI Builds")
       })
-  public ResponseDTO<CIExecutionImages>
+  ResponseDTO<CIExecutionImages>
   getCustomerConfig(@NotNull @QueryParam(NGCommonEntityConstants.INFRA) Type infra,
       @NotNull @QueryParam(NGCommonEntityConstants.OVERRIDES_ONLY) @DefaultValue("true") boolean overridesOnly,
-      @NotNull @QueryParam(NGCommonEntityConstants.ACCOUNT_KEY) String accountIdentifier) {
-    CIExecutionImages ciExecutionImages = configService.getCustomerConfig(accountIdentifier, infra, overridesOnly);
-    return ResponseDTO.newResponse(ciExecutionImages);
-  }
-
+      @NotNull @QueryParam(NGCommonEntityConstants.ACCOUNT_KEY) String accountIdentifier);
   @GET
   @Path("/get-default-config")
   @ApiOperation(value = "Get default execution config", nickname = "getDefaultConfig")
@@ -166,8 +145,6 @@ public class CIExecutionConfigResource {
         ApiResponse(description = "List of default images and versions used in CI Builds")
       })
   @NGAccessControlCheck(resourceType = ResourceTypes.ACCOUNT, permission = VIEW_ACCOUNT_PERMISSION)
-  public ResponseDTO<CIExecutionImages>
-  getDefaultConfig(@NotNull @QueryParam(NGCommonEntityConstants.INFRA) Type infra) {
-    return ResponseDTO.newResponse(configService.getDefaultConfig(infra));
-  }
+  ResponseDTO<CIExecutionImages>
+  getDefaultConfig(@NotNull @QueryParam(NGCommonEntityConstants.INFRA) Type infra);
 }
