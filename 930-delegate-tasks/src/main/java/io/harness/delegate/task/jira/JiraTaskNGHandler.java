ad38523879ae491dfa721b5689625fe855ddff7f
Lucas Sales <lucas.sales@harness.io>
2022-07-09 09:23:07 -0300

    fixing checkstyle

--- 930-delegate-tasks/src/main/java/io/harness/delegate/task/jira/JiraTaskNGHandler.java
+++ 930-delegate-tasks/src/main/java/io/harness/delegate/task/jira/JiraTaskNGHandler.java
@@ -27,7 +27,6 @@ import com.google.inject.Singleton;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 
 @OwnedBy(CDC)
