48c3d314a04d6482a3b2b1a4aa503d135a89689e
Shreyas Singh <shreyas.singh@harness.io>
2022-07-19 11:22:42 +0530

    [PL-19821]: Adding dto files

---
+++ 878-ng-common-utilities/src/main/java/io/harness/engine/expressions/ShellScriptBaseDTO.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.engine.expressions;
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.EXTERNAL_PROPERTY;
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
+
+import io.harness.ng.core.template.TemplateEntityConstants;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.TypeAlias;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@TypeAlias("ShellScriptBaseDTO")
+public class ShellScriptBaseDTO {
+  // TYPE
+  @JsonProperty("type") String type = TemplateEntityConstants.SCRIPT;
+
+  @NotNull
+  @JsonProperty("spec")
+  @JsonTypeInfo(use = NAME, property = "type", include = EXTERNAL_PROPERTY, visible = true)
+  ShellScriptSpec shellScriptSpec;
+}
---
+++ 878-ng-common-utilities/src/main/java/io/harness/engine/expressions/ShellScriptSourceWrapper.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.engine.expressions;
+
+import static io.harness.annotations.dev.HarnessTeam.PL;
+
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.EXTERNAL_PROPERTY;
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
+
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.pms.yaml.YamlNode;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import io.swagger.annotations.ApiModelProperty;
+import javax.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@OwnedBy(PL)
+public class ShellScriptSourceWrapper {
+  @JsonProperty(YamlNode.UUID_FIELD_NAME)
+  @Getter(onMethod_ = { @ApiModelProperty(hidden = true) })
+  @ApiModelProperty(hidden = true)
+  String uuid;
+  @NotNull String type = "Inline";
+
+  @NotNull
+  @JsonTypeInfo(use = NAME, property = "type", include = EXTERNAL_PROPERTY, visible = true)
+  ShellScriptBaseSource spec;
+}
