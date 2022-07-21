e3ea88240365632321c3c3ba2a4c7fd624637969
Trunapushpa <trunapushpa.surkar@harness.io>
2022-07-21 15:39:21 +0530

    fix: [CCM-8275]: tesy

--- 450-ce-views/src/main/java/io/harness/ccm/views/service/impl/PerspectiveAnomalyServiceImpl.java
+++ 450-ce-views/src/main/java/io/harness/ccm/views/service/impl/PerspectiveAnomalyServiceImpl.java
@@ -75,11 +75,9 @@ public class PerspectiveAnomalyServiceImpl implements PerspectiveAnomalyService 
 //
 //    List<AnomalyData> anomalyData = new ArrayList<>();
 //    anomalies.forEach(anomaly -> anomalyData.add(AnomalyUtils.buildAnomalyData(anomaly)));
-    AnomalyData anomaly = AnomalyData.builder().id("_id_").time(1657800397000L).anomalyRelativeTime("3hrs back").actualAmount(1.1).expectedAmount(2.2).anomalousSpend(4.4).anomalousSpendPercentage(4.0).resourceInfo("resourceInfo").resourceName("resourceName").entity(EntityInfo.builder().field("field").clusterName("cnma").clusterId("cid").namespace("namesp").workloadName("wname").workloadType("wtype").gcpProjectId("gcpPId").gcpProduct("gcpProd").gcpSKUId("gcpSkuId").gcpSKUDescription("gcpSKUDesc").awsUsageAccountId("awsUsageAcId").awsServiceCode("awsServCode").awsInstancetype("awSSInnstacneT").awsUsageType("awsUsa").azureSubscriptionGuid("awsSubI").azureResourceGroup("acre").azureMeterCategory("ametere").azureServiceName("ljkfd").azureInstanceId("insta").build()).details("details").status("status").statusRelativeTime("statusRelativeTime").comment("comment").cloudProvider("cloudProvicer").anomalyScore(1.1).userFeedback(AnomalyFeedback.TRUE_ANOMALY).build();
-    ArrayList<AnomalyData> anomalies = new ArrayList<>();
-    anomalies.add(anomaly);
-    anomalies.add(anomaly);
-    return anomalies;
+    List<AnomalyData> anomalyData = Collections.singletonList(AnomalyData.builder().id("_id_").time(1657800397000L).anomalyRelativeTime("3hrs back").actualAmount(1.1).expectedAmount(2.2).anomalousSpend(4.4).anomalousSpendPercentage(4.0).resourceInfo("resourceInfo").resourceName("resourceName").entity(EntityInfo.builder().field("field").clusterName("cnma").clusterId("cid").namespace("namesp").workloadName("wname").workloadType("wtype").gcpProjectId("gcpPId").gcpProduct("gcpProd").gcpSKUId("gcpSkuId").gcpSKUDescription("gcpSKUDesc").awsUsageAccountId("awsUsageAcId").awsServiceCode("awsServCode").awsInstancetype("awSSInnstacneT").awsUsageType("awsUsa").azureSubscriptionGuid("awsSubI").azureResourceGroup("acre").azureMeterCategory("ametere").azureServiceName("ljkfd").azureInstanceId("insta").build()).details("details").status("status").statusRelativeTime("statusRelativeTime").comment("comment").cloudProvider("cloudProvicer").anomalyScore(1.1).userFeedback(AnomalyFeedback.TRUE_ANOMALY).build());
+
+    return anomalyData;
   }
 
   private PerspectiveQueryDTO getDefaultPerspectiveQuery() {
