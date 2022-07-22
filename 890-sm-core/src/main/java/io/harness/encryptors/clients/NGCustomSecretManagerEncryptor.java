6d457286cf1cc991f0bf152589ce13da399e7c24
Shreyas Singh <shreyas.singh@harness.io>
2022-07-22 12:26:54 +0530

    [PL-19821]: adding shell script task handler ng

--- 890-sm-core/src/main/java/io/harness/encryptors/clients/NGCustomSecretManagerEncryptor.java
+++ 890-sm-core/src/main/java/io/harness/encryptors/clients/NGCustomSecretManagerEncryptor.java
@@ -7,18 +7,49 @@
 
 package io.harness.encryptors.clients;
 
+import static io.harness.data.structure.EmptyPredicate.isEmpty;
 import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
+import static io.harness.eraro.ErrorCode.SECRET_MANAGEMENT_ERROR;
+import static io.harness.exception.WingsException.USER;
+import static io.harness.logging.CommandExecutionStatus.SUCCESS;
+import static io.harness.threading.Morpheus.sleep;
 
+import static software.wings.service.impl.security.customsecretsmanager.CustomSecretsManagerValidationUtils.buildShellScriptParameters;
+
+import static java.time.Duration.ofMillis;
+
+import io.harness.concurrent.HTimeLimiter;
+import io.harness.delegate.command.CommandExecutionResult;
 import io.harness.encryptors.CustomEncryptor;
+import io.harness.exception.CommandExecutionException;
+import io.harness.exception.SecretManagementDelegateException;
 import io.harness.security.encryption.EncryptedDataParams;
 import io.harness.security.encryption.EncryptedRecord;
 import io.harness.security.encryption.EncryptedRecordData;
 import io.harness.security.encryption.EncryptionConfig;
+import io.harness.shell.ShellExecutionData;
 
+import software.wings.beans.delegation.ShellScriptParameters;
+import software.wings.delegatetasks.ShellScriptTaskHandler;
+import software.wings.security.encryption.secretsmanagerconfigs.CustomSecretsManagerConfig;
+
+import com.google.common.util.concurrent.TimeLimiter;
+import com.google.inject.Inject;
+import java.time.Duration;
 import java.util.Set;
 
 public class NGCustomSecretManagerEncryptor implements CustomEncryptor {
-  // use ng taks
+  private final NGShellScriptTaskHandler ngShellScriptTaskHandler;
+
+  private static final String OUTPUT_VARIABLE = "secret";
+  private final TimeLimiter timeLimiter;
+
+  @Inject
+  public NGCustomSecretManagerEncryptor(TimeLimiter timeLimiter, NGShellScriptTaskHandler ngShellScriptTaskHandler) {
+    this.timeLimiter = timeLimiter;
+    this.ngShellScriptTaskHandler = ngShellScriptTaskHandler;
+  }
+
   @Override
   public boolean validateReference(
       String accountId, Set<EncryptedDataParams> params, EncryptionConfig encryptionConfig) {
@@ -28,6 +59,41 @@ public class NGCustomSecretManagerEncryptor implements CustomEncryptor {
 
   @Override
   public char[] fetchSecretValue(String accountId, EncryptedRecord encryptedRecord, EncryptionConfig encryptionConfig) {
-    return new char[0];
+    CustomSecretsManagerConfig customSecretsManagerConfig = (CustomSecretsManagerConfig) encryptionConfig;
+    final int NUM_OF_RETRIES = 3;
+    int failedAttempts = 0;
+    while (true) {
+      try {
+        return HTimeLimiter.callInterruptible21(timeLimiter, Duration.ofSeconds(20),
+            () -> fetchSecretValueInternal(encryptedRecord, customSecretsManagerConfig));
+      } catch (SecretManagementDelegateException e) {
+        throw e;
+      } catch (Exception e) {
+        failedAttempts++;
+        if (failedAttempts == NUM_OF_RETRIES) {
+          String message = "Failed to decrypt " + encryptedRecord.getName() + " after " + NUM_OF_RETRIES + " retries";
+          throw new SecretManagementDelegateException(SECRET_MANAGEMENT_ERROR, message, e, USER);
+        }
+        sleep(ofMillis(1000));
+      }
+    }
+  }
+
+  private char[] fetchSecretValueInternal(
+      EncryptedRecord encryptedRecord, CustomSecretsManagerConfig customSecretsManagerConfig) {
+    ShellScriptParameters shellScriptParameters = buildShellScriptParameters(customSecretsManagerConfig);
+    CommandExecutionResult commandExecutionResult = shellScriptTaskHandler.handle(shellScriptParameters);
+    if (commandExecutionResult.getStatus() != SUCCESS) {
+      String errorMessage = "Could not retrieve secret with the given parameters due to error in shell script.";
+      throw new CommandExecutionException(errorMessage);
+    }
+    ShellExecutionData shellExecutionData = (ShellExecutionData) commandExecutionResult.getCommandExecutionData();
+    String result = shellExecutionData.getSweepingOutputEnvVariables().get(OUTPUT_VARIABLE);
+    if (isEmpty(result) || result.equals("null")) {
+      String errorMessage = "Empty or null value returned by custom shell script for the given secret: "
+          + encryptedRecord.getName() + ", for accountId: " + customSecretsManagerConfig.getAccountId();
+      throw new SecretManagementDelegateException(SECRET_MANAGEMENT_ERROR, errorMessage, USER);
+    }
+    return result.toCharArray();
   }
 }
