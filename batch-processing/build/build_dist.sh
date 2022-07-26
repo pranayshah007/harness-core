b7d00fe279ab7609c0c4f31b8e6217f1b91e3798
Hitesh Aringa <hitesh.aringa@harness.io>
2022-07-26 17:46:11 +0530

    [CCM-7946]: fix

--- batch-processing/build/build_dist.sh
+++ batch-processing/build/build_dist.sh
@@ -37,5 +37,7 @@ cp ../../batch-processing/build/container/Dockerfile-batch-processing-jenkins-k8
 cp ../../batch-processing/build/container/Dockerfile-batch-processing-jenkins-k8-gcr-openjdk ./Dockerfile-gcr
 cp ../../batch-processing/build/container/Dockerfile-batch-processing-cie-jdk ./Dockerfile-batch-processing-cie-jdk
 cp -r ../../batch-processing/build/container/scripts/ .
+cp -r ../../batch-processing/build/container/scripts/inject-onprem-apm-bins-into-dockerimage.sh .
+cp -r ../../batch-processing/build/container/scripts/inject-saas-apm-bins-into-dockerimage.sh .
 
 cd ../..
