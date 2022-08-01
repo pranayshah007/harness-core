5c0d4223bf4d9d40f89b6f360aee701da9b4fec9
Brijesh Dhakar <brijesh.dhakar@harness.io>
2022-08-01 15:40:37 +0530

    [fix]: [PIE-4660]: Fix NPE. Adding stageSchema one only when its namespace is present in shcmea (#35891)
    * [fix]: [PIE-4660]: Adding stageSchema one only when its namespace is present in shcmea
    
    * increasing build number

--- pipeline-service/service/src/main/java/io/harness/pms/pipeline/service/yamlschema/PmsYamlSchemaHelper.java
+++ pipeline-service/service/src/main/java/io/harness/pms/pipeline/service/yamlschema/PmsYamlSchemaHelper.java
@@ -14,6 +14,7 @@ import io.harness.EntityType;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.beans.FeatureName;
+import io.harness.data.structure.EmptyPredicate;
 import io.harness.jackson.JsonNodeUtils;
 import io.harness.plancreator.stages.parallel.ParallelStageElementConfig;
 import io.harness.plancreator.stages.stage.StageElementConfig;
@@ -242,6 +243,9 @@ public class PmsYamlSchemaHelper {
       List<YamlSchemaWithDetails> stageSchemaWithDetails =
           allSchemaDetails.stream()
               .filter(o -> o.getYamlSchemaMetadata().getYamlGroup().getGroup().equals(StepCategory.STAGE.name()))
+              .filter(o
+                  -> EmptyPredicate.isEmpty(o.getYamlSchemaMetadata().getNamespace())
+                      || pipelineDefinitions.get(o.getYamlSchemaMetadata().getNamespace()) != null)
               .collect(Collectors.toList());
 
       YamlSchemaUtils.addOneOfInStageElementWrapperConfig(pipelineDefinitions, stageSchemaWithDetails);
