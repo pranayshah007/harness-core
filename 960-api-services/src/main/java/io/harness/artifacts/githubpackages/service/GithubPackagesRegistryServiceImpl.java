4bf22f4735f09fda3bb590764b41950e0b506cbe
Ved Tambat <ved.atultambat@harness.io>
2022-08-05 17:57:06 +0530

    [CDS-25409]: registry service impl

--- 960-api-services/src/main/java/io/harness/artifacts/githubpackages/service/GithubPackagesRegistryServiceImpl.java
+++ 960-api-services/src/main/java/io/harness/artifacts/githubpackages/service/GithubPackagesRegistryServiceImpl.java
@@ -7,15 +7,44 @@
 
 package io.harness.artifacts.githubpackages.service;
 
-import io.harness.artifacts.beans.BuildDetailsInternal;
+import static java.util.stream.Collectors.toList;
+
 import io.harness.artifacts.githubpackages.beans.GithubPackagesInternalConfig;
+import io.harness.exception.ArtifactServerException;
+import io.harness.exception.ExceptionUtils;
+import io.harness.exception.NestedExceptionUtils;
+import io.harness.exception.WingsException;
+import io.harness.exception.runtime.GithubPackagesServerRuntimeException;
+
+import software.wings.common.BuildDetailsComparatorAscending;
+import software.wings.helpers.ext.jenkins.BuildDetails;
 
 import java.util.List;
 
 public class GithubPackagesRegistryServiceImpl implements GithubPackagesRegistryService {
   @Override
-  public List<BuildDetailsInternal> getBuilds(
-      GithubPackagesInternalConfig githubPackagesInternalConfig, String packageName, int maxNoOfTagsPerImage) {
+  public List<BuildDetails> getBuilds(
+      GithubPackagesInternalConfig githubPackagesInternalConfig, String packageName, int maxNoOfVersionsPerPackage) {
+    List<BuildDetails> buildDetails;
+    try {
+      if (githubPackagesInternalConfig.hasCredentials()) {
+        buildDetails = getBuildDetails(githubPackagesInternalConfig, packageName);
+      } else {
+        buildDetails = null;
+      }
+    } catch (GithubPackagesServerRuntimeException ex) {
+      throw ex;
+    } catch (Exception e) {
+      throw NestedExceptionUtils.hintWithExplanationException("Could not fetch vesions for the package",
+          "Check if the package exists and if the permissions are scoped for the authenticated user",
+          new ArtifactServerException(ExceptionUtils.getMessage(e), e, WingsException.USER));
+    }
+
+    return buildDetails.stream().sorted(new BuildDetailsComparatorAscending()).collect(toList());
+  }
+
+  private List<BuildDetails> getBuildDetails(
+      GithubPackagesInternalConfig githubPackagesInternalConfig, String packageName) {
     return null;
   }
 }
