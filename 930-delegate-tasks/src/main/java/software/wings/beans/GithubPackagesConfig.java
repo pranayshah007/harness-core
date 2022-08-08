953de022fa9548594e71dcc067d3bdf912c5ae1a
Ved Tambat <ved.atultambat@harness.io>
2022-08-09 01:25:08 +0530

    [CDS-25409]: compile check-2

--- 930-delegate-tasks/src/main/java/software/wings/beans/GithubPackagesConfig.java
+++ 930-delegate-tasks/src/main/java/software/wings/beans/GithubPackagesConfig.java
@@ -8,48 +8,67 @@
 package software.wings.beans;
 
 import static io.harness.annotations.dev.HarnessTeam.CDC;
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 
 import io.harness.annotations.dev.HarnessModule;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.annotations.dev.TargetModule;
-import io.harness.delegate.beans.executioncapability.ExecutionCapability;
-import io.harness.delegate.beans.executioncapability.ExecutionCapabilityDemander;
-import io.harness.delegate.task.TaskParameters;
-import io.harness.expression.ExpressionEvaluator;
+import io.harness.encryption.Encrypted;
 
 import software.wings.annotation.EncryptableSetting;
 import software.wings.beans.config.ArtifactSourceable;
-import software.wings.settings.SettingValue;
+import software.wings.jersey.JsonViews;
+import software.wings.settings.SettingVariableTypes;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonView;
+import com.github.reinert.jjschema.Attributes;
+import com.github.reinert.jjschema.SchemaIgnore;
 import java.util.List;
+import java.util.Objects;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.validator.constraints.NotEmpty;
 
 @OwnedBy(CDC)
 @JsonTypeName("GITHUB_PACKAGES")
 @Data
 @Builder
+@ToString(exclude = "password")
 @EqualsAndHashCode(callSuper = false)
 @TargetModule(HarnessModule._957_CG_BEANS)
-public class GithubPackagesConfig extends SettingValue
-    implements EncryptableSetting, ArtifactSourceable, TaskParameters, ExecutionCapabilityDemander {
-  @Override
-  public List<ExecutionCapability> fetchRequiredExecutionCapabilities(ExpressionEvaluator maskingEvaluator) {
-    return null;
-  }
+public class GithubPackagesConfig implements EncryptableSetting, ArtifactSourceable {
+  @Attributes(title = "Github Packages URL", required = true) @NotEmpty private String githubPackagesUrl;
+  @Attributes(title = "Username") private String username;
+  @Attributes(title = "Password") @Encrypted(fieldName = "password") private char[] password;
+  private List<String> delegateSelectors;
+  @SchemaIgnore @NotEmpty private String accountId;
 
-  @Override
-  public String fetchResourceCategory() {
-    return null;
-  }
+  @JsonView(JsonViews.Internal.class) @SchemaIgnore private String encryptedPassword;
+
+  private boolean skipValidation;
 
   @Override
   public String getAccountId() {
-    return null;
+    return accountId;
   }
 
   @Override
   public void setAccountId(String accountId) {}
+
+  @SchemaIgnore
+  public boolean hasCredentials() {
+    return isNotEmpty(username);
+  }
+
+  public String getUsername() {
+    return Objects.isNull(username) ? "" : username;
+  }
+
+  @Override
+  public SettingVariableTypes getSettingType() {
+    return null;
+  }
 }
