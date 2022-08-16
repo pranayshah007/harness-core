019f99c112137527a683bb1a0a71f980c2d2ca10
svmav <shubhanshu.verma@harness.io>
2022-08-16 15:56:06 +0530

    [CCM-8560]: Added CCM admin role migration to migration list

--- access-control/service/src/main/java/io/harness/accesscontrol/commons/migration/AccessControlMongoBackgroundMigrationDetails.java
+++ access-control/service/src/main/java/io/harness/accesscontrol/commons/migration/AccessControlMongoBackgroundMigrationDetails.java
@@ -11,11 +11,7 @@ import static io.harness.annotations.dev.HarnessTeam.PL;
 
 import io.harness.accesscontrol.acl.migration.ACLAddBooleanFieldsMigration;
 import io.harness.accesscontrol.resources.resourcegroups.migration.MultipleManagedResourceGroupMigration;
-import io.harness.accesscontrol.roleassignments.migration.AccountBasicRoleAssignmentAdditionMigration;
-import io.harness.accesscontrol.roleassignments.migration.RoleAssignmentPrincipalScopeLevelMigration;
-import io.harness.accesscontrol.roleassignments.migration.RoleAssignmentResourceGroupMigration;
-import io.harness.accesscontrol.roleassignments.migration.RoleAssignmentScopeAdditionMigration;
-import io.harness.accesscontrol.roleassignments.migration.SuperAdminViaAllResourcesMigration;
+import io.harness.accesscontrol.roleassignments.migration.*;
 import io.harness.accesscontrol.scopes.harness.migration.ScopeMigration;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.migration.MigrationDetails;
@@ -54,6 +50,7 @@ public class AccessControlMongoBackgroundMigrationDetails implements MigrationDe
         .add(Pair.of(11, NoopMigration.class))
         .add(Pair.of(12, ACLAddBooleanFieldsMigration.class))
         .add(Pair.of(13, AccountBasicRoleAssignmentAdditionMigration.class))
+        .add(Pair.of(14, CCMAdminRoleAssignmentAdditionMigration.class))
         .build();
   }
 }
