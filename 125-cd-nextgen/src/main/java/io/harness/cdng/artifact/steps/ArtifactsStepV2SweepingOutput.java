0106fb9aad49613e920722a4900f7d689472c02e
Yogesh Chauhan <yogesh.chauhan@harness.io>
2022-08-17 23:08:05 +0530

    feat: [CDS-0]: Running pipeline with artifact primart

---
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/artifact/steps/ArtifactsStepV2SweepingOutput.java
@@ -0,0 +1,27 @@
+package io.harness.cdng.artifact.steps;
+
+import io.harness.annotation.RecasterAlias;
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.cdng.artifact.bean.ArtifactConfig;
+import io.harness.pms.sdk.core.data.ExecutionSweepingOutput;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.springframework.data.annotation.TypeAlias;
+
+@Builder
+@Value
+@EqualsAndHashCode(callSuper = false)
+@OwnedBy(HarnessTeam.CI)
+@TypeAlias("ArtifactsStepV2SweepingOutput")
+@JsonTypeName("artifactsStepV2SweepingOutput")
+@RecasterAlias("io.harness.cdng.artifact.steps.ArtifactsStepV2SweepingOutput")
+public class ArtifactsStepV2SweepingOutput implements ExecutionSweepingOutput {
+  String primaryArtifactTaskId;
+  @Builder.Default Map<String, ArtifactConfig> artifactConfigMap = new HashMap<>();
+}
