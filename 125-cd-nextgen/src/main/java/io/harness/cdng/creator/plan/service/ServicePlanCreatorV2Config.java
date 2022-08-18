79ba40b02472968a10c95bca1bfceaaa60198703
Yogesh Chauhan <yogesh.chauhan@harness.io>
2022-08-18 15:22:37 +0530

    feat: [CDS-0]: checkstyle & code format

--- 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/service/ServicePlanCreatorV2Config.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/service/ServicePlanCreatorV2Config.java
@@ -1,16 +1,12 @@
 package io.harness.cdng.creator.plan.service;
 
-import static io.harness.yaml.schema.beans.SupportedPossibleFieldTypes.runtime;
-
 import io.harness.cdng.service.beans.ServiceDefinition;
 import io.harness.cdng.service.beans.ServiceUseFromStageV2;
 import io.harness.pms.yaml.ParameterField;
-import io.harness.yaml.YamlSchemaTypes;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import java.util.Map;
-import javax.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
