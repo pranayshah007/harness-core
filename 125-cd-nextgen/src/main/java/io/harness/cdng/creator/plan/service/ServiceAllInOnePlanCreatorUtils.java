ad24ca77fd6c24e55aed1d4252812f27d1dac4fc
Yogesh Chauhan <yogesh.chauhan@harness.io>
2022-08-20 02:09:44 +0530

    feat: [CDS-41772]: Fix name for ArtifactsPlanNode

--- 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/service/ServiceAllInOnePlanCreatorUtils.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/service/ServiceAllInOnePlanCreatorUtils.java
@@ -81,7 +81,7 @@ public class ServiceAllInOnePlanCreatorUtils {
         PlanNode.builder()
             .uuid("artifacts-" + UUIDGenerator.generateUuid())
             .stepType(ArtifactsStepV2.STEP_TYPE)
-            .name(PlanCreatorConstants.SERVICE_NODE_NAME)
+            .name(PlanCreatorConstants.ARTIFACT_NODE_NAME)
             .identifier(YamlTypes.ARTIFACT_LIST_CONFIG)
             .stepParameters(new EmptyStepParameters())
             .facilitatorObtainment(
