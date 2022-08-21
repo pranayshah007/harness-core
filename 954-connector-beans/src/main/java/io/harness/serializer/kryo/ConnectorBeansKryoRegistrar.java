e234a5b418f00ed2bff0fade8a1552f03858489d
Shreyas Singh <shreyas.singh@harness.io>
2022-08-21 09:14:30 +0530

    [PL-19821]: added correct kry registrations

--- 954-connector-beans/src/main/java/io/harness/serializer/kryo/ConnectorBeansKryoRegistrar.java
+++ 954-connector-beans/src/main/java/io/harness/serializer/kryo/ConnectorBeansKryoRegistrar.java
@@ -145,6 +145,7 @@ import io.harness.delegate.beans.connector.scm.azurerepo.AzureRepoApiAccessDTO;
 import io.harness.delegate.beans.connector.scm.azurerepo.AzureRepoApiAccessSpecDTO;
 import io.harness.delegate.beans.connector.scm.azurerepo.AzureRepoApiAccessType;
 import io.harness.delegate.beans.connector.scm.azurerepo.AzureRepoAuthenticationDTO;
+import io.harness.delegate.beans.connector.scm.azurerepo.AzureRepoConnectionTypeDTO;
 import io.harness.delegate.beans.connector.scm.azurerepo.AzureRepoConnectorDTO;
 import io.harness.delegate.beans.connector.scm.azurerepo.AzureRepoCredentialsDTO;
 import io.harness.delegate.beans.connector.scm.azurerepo.AzureRepoHttpAuthenticationType;
@@ -406,6 +407,7 @@ public class ConnectorBeansKryoRegistrar implements KryoRegistrar {
     kryo.register(OciHelmAuthType.class, 29133);
     kryo.register(OciHelmConnectorDTO.class, 29134);
     kryo.register(OciHelmUsernamePasswordDTO.class, 29135);
+    kryo.register(AzureRepoConnectionTypeDTO.class, 19854);
     kryo.register(CustomSecretManagerConnectorDTO.class, 19875);
     kryo.register(TemplateLinkConfig.class, 19877);
   }
