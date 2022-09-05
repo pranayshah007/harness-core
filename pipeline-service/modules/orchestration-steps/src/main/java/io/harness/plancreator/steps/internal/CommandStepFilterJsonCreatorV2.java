1e5bc66654dfa527b134d4e3d7ead06e57d3f117
Vladimir Peric <vladimir.peric@harness.io>
2022-09-05 16:23:04 +0200

    [feat]: [CDS-42313]: enum const renamed

--- pipeline-service/modules/orchestration-steps/src/main/java/io/harness/plancreator/steps/internal/CommandStepFilterJsonCreatorV2.java
+++ pipeline-service/modules/orchestration-steps/src/main/java/io/harness/plancreator/steps/internal/CommandStepFilterJsonCreatorV2.java
@@ -2,20 +2,28 @@ package io.harness.plancreator.steps.internal;
 
 import io.harness.filters.GenericStepPMSFilterJsonCreatorV2;
 import io.harness.plancreator.steps.AbstractStepNode;
+import io.harness.pms.exception.runtime.InvalidYamlRuntimeException;
 import io.harness.pms.filter.creation.FilterCreationResponse;
 import io.harness.pms.sdk.core.filter.creation.beans.FilterCreationContext;
 
 import com.google.common.collect.Sets;
+import io.harness.pms.yaml.YamlUtils;
+import io.harness.steps.StepSpecTypeConstants;
+
 import java.util.Set;
+
+import static java.lang.String.format;
 
 public class CommandStepFilterJsonCreatorV2 extends GenericStepPMSFilterJsonCreatorV2 {
   @Override
   public Set<String> getSupportedStepTypes() {
-    return Sets.newHashSet("COMMAND", "command", "Command");
+    return Sets.newHashSet(StepSpecTypeConstants.COMMAND, "COMMAND", "command", "Command", "step", "Deployment");
   }
 
   @Override
   public FilterCreationResponse handleNode(FilterCreationContext filterCreationContext, AbstractStepNode yamlField) {
-    return super.handleNode(filterCreationContext, yamlField);
+    //return super.handleNode(filterCreationContext, yamlField);
+    throw new InvalidYamlRuntimeException(
+            format("Command Step filter failed."));
   }
 }
