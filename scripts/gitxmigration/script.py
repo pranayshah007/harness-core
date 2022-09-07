28c30b85f65811958db76b2ca11873230cb3e6e4
Mohit Garg <mohit.garg@harness.io>
2022-09-08 02:40:17 +0530

    [PL-0]: added gitx migration script

---
+++ scripts/gitxmigration/script.py
@@ -0,0 +1,196 @@
+# Copyright 2022 Harness Inc. All rights reserved.
+# Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+# that can be found in the licenses directory at the root of this repository, also available at
+# https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+
+from pymongo import MongoClient
+from enum import Enum
+import requests
+
+INPUT_ACCOUNT_ID = "kmpySmUISimoRrJL6NL73w"
+INPUT_ORG_ID = "default"
+INPUT_PROJECT_ID = "_sept_9"
+LOCAL_API_KEY = "pat.kmpySmUISimoRrJL6NL73w.62a7a3ee66425e616acf6629.ccm4qJhiI42DcOwRtGE3"
+
+PMS_DB_NAME = "pms-harness"
+NG_MANAGER_DB_NAME = "ng-harness"
+
+STORE_TYPE_REMOTE = "REMOTE"
+
+class DBKeys(Enum):
+    ACCOUNT_ID = "accountId"
+    ACCOUNT_IDENTIFIER = "accountIdentifier"
+    ORG_IDENTIFIER = "orgIdentifier"
+    PROJECT_IDENTIFIER = "projectIdentifier"
+    IDENTIFIER = "identifier"
+    REPO = "repo"
+    GIT_CONNECTOR_REF = "gitConnectorRef"
+    IS_FROM_DEFAULT_BRANCH = "isFromDefaultBranch"
+    FILE_PATH = "filePath"
+    ROOT_FOLDER = "rootFolder"
+    STORE_TYPE = "storeType"
+    CONNECTOR_REF = "connectorRef"
+    YAML_GIT_CONFIG_REF = "yamlGitConfigRef"
+    BRANCH = "branch"
+    OBJECT_ID_OF_YAML = "objectIdOfYaml"
+    YAML = "yaml"
+
+
+yaml_git_config_list = []
+
+class YamlGitConfig:
+    def __init__(self, identifier, repo_url, connector_ref):
+        self.identifier = identifier
+        self.repo_url = repo_url
+        self.connector_ref = connector_ref
+
+    def __repr__(self):
+        return "%s | %s | %s" % (self.identifier, self.repo_url, self.connector_ref)
+
+#############################################################################
+
+def get_repo_from_repo_url(repo_url):
+    delimiter_index = repo_url.rfind("/")
+    return repo_url[delimiter_index+1:]
+
+
+def setup_mongo_client():
+    global mongo_client
+    global pms_db
+    global ng_manager_db
+    mongo_client = MongoClient(host="localhost", port=27017)
+    pms_db = mongo_client.get_database(PMS_DB_NAME)
+    ng_manager_db = mongo_client.get_database(NG_MANAGER_DB_NAME)
+
+
+def prepare_yaml_git_config_list():
+    ng_manager_db = mongo_client.get_database(NG_MANAGER_DB_NAME)
+    yaml_git_config_collection = ng_manager_db.yamlGitConfigs
+
+    records = yaml_git_config_collection.find({
+        DBKeys.ACCOUNT_ID.value: INPUT_ACCOUNT_ID,
+        DBKeys.ORG_IDENTIFIER.value: INPUT_ORG_ID,
+        DBKeys.PROJECT_IDENTIFIER.value: INPUT_PROJECT_ID
+    })
+
+    for record in records:
+        identifier = record.get(DBKeys.IDENTIFIER.value)
+        yaml_git_config_list.append(YamlGitConfig(identifier, record.get(DBKeys.REPO.value), record.get(DBKeys.GIT_CONNECTOR_REF.value)))
+
+    print(yaml_git_config_list)
+
+
+def migrate_records(collection, yaml_git_config):
+    query = {
+        DBKeys.ACCOUNT_ID.value: INPUT_ACCOUNT_ID,
+        DBKeys.ORG_IDENTIFIER.value: INPUT_ORG_ID,
+        DBKeys.PROJECT_IDENTIFIER.value: INPUT_PROJECT_ID,
+        DBKeys.YAML_GIT_CONFIG_REF.value: yaml_git_config.identifier,
+    }
+    records = collection.find(query)
+
+    for record in records:
+        print(record)
+        old_file_path = record.get(DBKeys.FILE_PATH.value)
+        old_root_folder = record.get(DBKeys.ROOT_FOLDER.value)
+        # remove starting slash from root folder
+
+        record[DBKeys.REPO.value] = get_repo_from_repo_url(yaml_git_config.repo_url)
+        record[DBKeys.STORE_TYPE.value] = STORE_TYPE_REMOTE
+        record[DBKeys.CONNECTOR_REF.value] = yaml_git_config.connector_ref
+        record[DBKeys.FILE_PATH.value] = old_root_folder[1:] + old_file_path
+
+        collection.update_one({"_id": record.get("_id")}, {"$set": record}, upsert=False)
+
+    collection.update_many(query, {"$unset": {
+                                    DBKeys.ROOT_FOLDER.value: 1,
+                                    DBKeys.YAML_GIT_CONFIG_REF.value: 1,
+                                    DBKeys.BRANCH.value: 1,
+                                    DBKeys.IS_FROM_DEFAULT_BRANCH.value: 1,
+                                    DBKeys.OBJECT_ID_OF_YAML.value: 1,
+                                    DBKeys.YAML.value: 1
+                                    }
+                              })
+
+
+def migrate_records_from_inline_to_remote():
+    for yaml_git_config in yaml_git_config_list:
+        print(yaml_git_config)
+        migrate_records(pms_db.pipelinesPMS, yaml_git_config)
+        migrate_records(pms_db.inputSetsPMS, yaml_git_config)
+
+
+def disable_git_sync_via_api():
+    url = "https://localhost:8181/ng/api/git-sync/disable?accountIdentifier=%s&orgIdentifier=%s&projectIdentifier=%s" % (INPUT_ACCOUNT_ID, INPUT_ORG_ID, INPUT_PROJECT_ID)
+    headers = {'x-api-key': LOCAL_API_KEY}
+    resp = requests.put(url, headers=headers, verify=False)
+    print(resp.status_code)
+
+
+def delete_non_default_pipelines_input_sets():
+    pipeline_collection = pms_db.pipelinesPMS
+    input_sets_collection = pms_db.inputSetsPMS
+    query_condition = {
+        DBKeys.ACCOUNT_ID.value: INPUT_ACCOUNT_ID,
+        DBKeys.ORG_IDENTIFIER.value: INPUT_ORG_ID,
+        DBKeys.PROJECT_IDENTIFIER.value: INPUT_PROJECT_ID,
+        DBKeys.IS_FROM_DEFAULT_BRANCH.value: False
+    }
+    pipeline_collection.delete_many(query_condition)
+    input_sets_collection.delete_many(query_condition)
+
+
+def delete_non_default_connectors():
+    connectors_collection = ng_manager_db.connectors
+    query_condition = {
+        DBKeys.ACCOUNT_IDENTIFIER.value: INPUT_ACCOUNT_ID,
+        DBKeys.ORG_IDENTIFIER.value: INPUT_ORG_ID,
+        DBKeys.PROJECT_IDENTIFIER.value: INPUT_PROJECT_ID,
+        DBKeys.IS_FROM_DEFAULT_BRANCH.value: False
+    }
+    connectors_collection.delete_many(query_condition)
+
+
+def delete_non_default_branch_entities():
+    delete_non_default_pipelines_input_sets()
+    delete_non_default_connectors()
+
+
+def enable_gitx_via_api():
+    url = "https://localhost:8181/ng/api/git-sync-settings/git-simplification?accountIdentifier=%s&orgIdentifier=%s&projectIdentifier=%s" % (
+    INPUT_ACCOUNT_ID, INPUT_ORG_ID, INPUT_PROJECT_ID)
+    headers = {'x-api-key': LOCAL_API_KEY}
+    resp = requests.post(url, headers=headers, verify=False)
+    print(resp.status_code)
+
+
+def cleanup_connector_entities():
+    collection = ng_manager_db.connectors
+    for yaml_git_config in yaml_git_config_list:
+        query = {
+            DBKeys.ACCOUNT_IDENTIFIER.value: INPUT_ACCOUNT_ID,
+            DBKeys.ORG_IDENTIFIER.value: INPUT_ORG_ID,
+            DBKeys.PROJECT_IDENTIFIER.value: INPUT_PROJECT_ID,
+            DBKeys.YAML_GIT_CONFIG_REF.value: yaml_git_config.identifier,
+        }
+
+        collection.update_many(query, {"$unset": {
+                    DBKeys.ROOT_FOLDER.value: 1,
+                    DBKeys.YAML_GIT_CONFIG_REF.value: 1,
+                    DBKeys.BRANCH.value: 1,
+                    DBKeys.IS_FROM_DEFAULT_BRANCH.value: 1,
+                    DBKeys.OBJECT_ID_OF_YAML.value: 1,
+                    DBKeys.YAML.value: 1,
+                    DBKeys.FILE_PATH.value: 1
+                }
+                })
+
+
+if __name__ == "__main__":
+    setup_mongo_client()
+    prepare_yaml_git_config_list()
+    disable_git_sync_via_api()
+    delete_non_default_branch_entities()
+    enable_gitx_via_api()
+    migrate_records_from_inline_to_remote()
+    cleanup_connector_entities()
