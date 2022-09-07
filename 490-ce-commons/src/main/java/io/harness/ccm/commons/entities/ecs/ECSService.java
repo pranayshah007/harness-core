3e1f75ffa420c87f057a338cdd9fc18a647f0e3a
Trunapushpa <trunapushpa.surkar@harness.io>
2022-09-07 11:31:17 +0530

    fix: [CCM-8906]: annotations fix

--- 490-ce-commons/src/main/java/io/harness/ccm/commons/entities/ecs/ECSService.java
+++ 490-ce-commons/src/main/java/io/harness/ccm/commons/entities/ecs/ECSService.java
@@ -10,7 +10,7 @@ package io.harness.ccm.commons.entities.ecs;
 import static io.harness.annotations.dev.HarnessTeam.CE;
 
 import com.amazonaws.services.ecs.model.LaunchType;
-import io.harness.annotation.StoreIn;
+import io.harness.annotations.StoreIn;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.ccm.commons.beans.Resource;
 import io.harness.mongo.index.CompoundMongoIndex;
