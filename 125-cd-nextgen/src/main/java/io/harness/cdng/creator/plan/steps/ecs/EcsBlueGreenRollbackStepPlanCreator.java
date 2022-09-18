f383b7dce90d7b7c93b5d1b6c2a8c9190815886c
Pragyesh Mishra <pragyesh.mishra@harness.io>
2022-09-19 00:02:26 +0530

    ut fix

--- 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/steps/ecs/EcsBlueGreenRollbackStepPlanCreator.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/steps/ecs/EcsBlueGreenRollbackStepPlanCreator.java
@@ -1,6 +1,8 @@
 package io.harness.cdng.creator.plan.steps.ecs;
 
-import com.google.common.collect.Sets;
+import static io.harness.cdng.visitor.YamlTypes.ECS_BLUE_GREEN_CREATE_SERVICE;
+import static io.harness.cdng.visitor.YamlTypes.ECS_BLUE_GREEN_SWAP_TARGET_GROUPS;
+
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.cdng.creator.plan.steps.CDPMSStepPlanCreatorV2;
@@ -12,38 +14,37 @@ import io.harness.pms.sdk.core.plan.creation.beans.PlanCreationContext;
 import io.harness.pms.sdk.core.plan.creation.beans.PlanCreationResponse;
 import io.harness.pms.sdk.core.steps.io.StepParameters;
 
+import com.google.common.collect.Sets;
 import java.util.Set;
-
-import static io.harness.cdng.visitor.YamlTypes.ECS_BLUE_GREEN_CREATE_SERVICE;
-import static io.harness.cdng.visitor.YamlTypes.ECS_BLUE_GREEN_SWAP_TARGET_GROUPS;
 
 @OwnedBy(HarnessTeam.CDP)
 public class EcsBlueGreenRollbackStepPlanCreator extends CDPMSStepPlanCreatorV2<EcsBlueGreenRollbackStepNode> {
-    @Override
-    public Set<String> getSupportedStepTypes() {
-        return Sets.newHashSet(StepSpecTypeConstants.ECS_BLUE_GREEN_ROLLBACK);
-    }
+  @Override
+  public Set<String> getSupportedStepTypes() {
+    return Sets.newHashSet(StepSpecTypeConstants.ECS_BLUE_GREEN_ROLLBACK);
+  }
 
-    @Override
-    public Class<EcsBlueGreenRollbackStepNode> getFieldClass() {
-        return EcsBlueGreenRollbackStepNode.class;
-    }
+  @Override
+  public Class<EcsBlueGreenRollbackStepNode> getFieldClass() {
+    return EcsBlueGreenRollbackStepNode.class;
+  }
 
-    @Override
-    public PlanCreationResponse createPlanForField(PlanCreationContext ctx, EcsBlueGreenRollbackStepNode stepElement) {
-        return super.createPlanForField(ctx, stepElement);
-    }
+  @Override
+  public PlanCreationResponse createPlanForField(PlanCreationContext ctx, EcsBlueGreenRollbackStepNode stepElement) {
+    return super.createPlanForField(ctx, stepElement);
+  }
 
-    @Override
-    protected StepParameters getStepParameters(PlanCreationContext ctx, EcsBlueGreenRollbackStepNode stepElement) {
-        final StepParameters stepParameters = super.getStepParameters(ctx, stepElement);
+  @Override
+  protected StepParameters getStepParameters(PlanCreationContext ctx, EcsBlueGreenRollbackStepNode stepElement) {
+    final StepParameters stepParameters = super.getStepParameters(ctx, stepElement);
 
-        String ecsBlueGreenCreateServiceFnq = getExecutionStepFqn(ctx.getCurrentField(), ECS_BLUE_GREEN_CREATE_SERVICE);
-        String ecsBlueGreenSwapTargetGroupsFnq = getExecutionStepFqn(ctx.getCurrentField(), ECS_BLUE_GREEN_SWAP_TARGET_GROUPS);
-        EcsBlueGreenRollbackStepParameters ecsBlueGreenRollbackStepParameters =
-                (EcsBlueGreenRollbackStepParameters) ((StepElementParameters) stepParameters).getSpec();
-        ecsBlueGreenRollbackStepParameters.setEcsBlueGreenCreateServiceFnq(ecsBlueGreenCreateServiceFnq);
-        ecsBlueGreenRollbackStepParameters.setEcsBlueGreenSwapTargetGroupsFnq(ecsBlueGreenSwapTargetGroupsFnq);
-        return stepParameters;
-    }
+    String ecsBlueGreenCreateServiceFnq = getExecutionStepFqn(ctx.getCurrentField(), ECS_BLUE_GREEN_CREATE_SERVICE);
+    String ecsBlueGreenSwapTargetGroupsFnq =
+        getExecutionStepFqn(ctx.getCurrentField(), ECS_BLUE_GREEN_SWAP_TARGET_GROUPS);
+    EcsBlueGreenRollbackStepParameters ecsBlueGreenRollbackStepParameters =
+        (EcsBlueGreenRollbackStepParameters) ((StepElementParameters) stepParameters).getSpec();
+    ecsBlueGreenRollbackStepParameters.setEcsBlueGreenCreateServiceFnq(ecsBlueGreenCreateServiceFnq);
+    ecsBlueGreenRollbackStepParameters.setEcsBlueGreenSwapTargetGroupsFnq(ecsBlueGreenSwapTargetGroupsFnq);
+    return stepParameters;
+  }
 }
