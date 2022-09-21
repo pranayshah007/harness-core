8ea45b7b7b08ff4ef60ee6449f693f29d5170580
GabeAboy <gabriel.aboy@harness.io>
2022-09-21 15:23:12 -0600

    rm unused

--- 125-cd-nextgen/src/main/java/io/harness/cdng/gitops/CreatePRBaseStepInfo.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/gitops/CreatePRBaseStepInfo.java
@@ -23,7 +23,6 @@ import io.harness.yaml.YamlSchemaTypes;
 
 import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
-import java.util.Map;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
