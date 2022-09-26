b59792582d59432040b08e0f3c0e144474e28c89
Shreyas Singh <shreyas.singh@harness.io>
2022-09-26 12:33:28 +0530

    [PL-27908]: removing unused import

--- 954-connector-beans/src/main/java/io/harness/delegate/beans/connector/customsecretmanager/CustomSecretManagerConnectorDTO.java
+++ 954-connector-beans/src/main/java/io/harness/delegate/beans/connector/customsecretmanager/CustomSecretManagerConnectorDTO.java
@@ -13,7 +13,6 @@ import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.beans.DecryptableEntity;
 import io.harness.connector.DelegateSelectable;
-import io.harness.connector.ManagerExecutable;
 import io.harness.delegate.beans.connector.ConnectorConfigDTO;
 import io.harness.encryption.SecretRefData;
 import io.harness.encryption.SecretReference;
