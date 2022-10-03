25e27c5f960e731a52f454eddc8996ad7acf4525
Xingchi Jin <xingchi.jin@harness.io>
2022-10-03 15:03:09 -0700

    fix package name

--- delegate/agent/src/main/java/io/harness/delegate/DelegateAgent.java
+++ delegate/agent/src/main/java/io/harness/delegate/DelegateAgent.java
@@ -5,7 +5,7 @@
  * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
  */
 
-package delegate.src.main.java.io.harness.agent;
+package io.harness.delegate.agent;
 
 import lombok.extern.slf4j.Slf4j;
 
---
+++ delegate/runner/src/main/java/io/harness/runner/DelegateRunner.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.runner;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DelegateRunner {
+    public static void main(String[] args) {
+        log.info("Delegate runner started");
+    }
+}
