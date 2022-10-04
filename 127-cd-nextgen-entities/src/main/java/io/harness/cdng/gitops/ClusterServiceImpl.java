41f4065a79224abe68ecfec94e63b7f6538ae20a
Rohit Karelia <rohit.karelia@harness.io>
2022-10-03 23:41:36 -0700

    feat: [CDS-44143]: Implement batch undelete api (#38308)
    * feat: [CDS-44143]: Implement batch undelete api  (#38299)
    
    * feat: [CDS-44143]: Implement batch undelete api for unlinking multiple clusters from environments
    
    * feat: [CDS-44143]: Remove overloaded delete method
    
    * feat: [CDS-44143]: Update unit tests
    
    * feat: [CDS-44143]: Fix format
    
    * feat: [CDS-44143]: Update validation to allow unlinking all
    
    * feat: [CDS-44143]: Update error message
    
    (cherry picked from commit 59691a02b809313c456de7629cb032c287426149)
    
    * feat: [CDS-44143]: Update build.properties
    
    * feat: [CDS-44143]: Don't call gitopsservice for delete
    
    * feat: [CDS-44143]: Don't call gitopsservice for delete
    
    * feat: [CDS-44143]: Fix checkstyle

--- 127-cd-nextgen-entities/src/main/java/io/harness/cdng/gitops/ClusterServiceImpl.java
+++ 127-cd-nextgen-entities/src/main/java/io/harness/cdng/gitops/ClusterServiceImpl.java
@@ -84,6 +84,14 @@ public class ClusterServiceImpl implements ClusterService {
   }
 
   @Override
+  public long bulkDelete(
+      List<Cluster> entities, String accountId, String orgIdentifier, String projectIdentifier, String envRef) {
+    List<String> clusterRefs = entities.stream().map(c -> c.getClusterRef()).collect(Collectors.toList());
+    Criteria criteria = getClusterEqualityCriteria(accountId, orgIdentifier, projectIdentifier, envRef, clusterRefs);
+    return clusterRepository.bulkDelete(criteria);
+  }
+
+  @Override
   public boolean delete(String accountId, String orgIdentifier, String projectIdentifier, String envIdentifier,
       String clusterRef, ScopeLevel scopeLevel) {
     checkArgument(isNotEmpty(accountId), "accountId must be present");
@@ -108,6 +116,19 @@ public class ClusterServiceImpl implements ClusterService {
     Criteria criteria = getClusterEqualityCriteriaForAllEnv(
         accountId, orgIdentifier, projectIdentifier, getScopedClusterRef(scope, clusterRef));
     return clusterRepository.delete(criteria);
+  }
+
+  @Override
+  public long deleteAllFromEnvAndReturnCount(
+      String accountId, String orgIdentifier, String projectIdentifier, String envIdentifier) {
+    checkArgument(isNotEmpty(accountId), "accountId must be present");
+    checkArgument(isNotEmpty(orgIdentifier), "org identifier must be present");
+    checkArgument(isNotEmpty(projectIdentifier), "project identifier must be present");
+    checkArgument(isNotEmpty(envIdentifier), "environment identifier must be present");
+
+    Criteria criteria = getClusterEqCriteriaForAllClusters(accountId, orgIdentifier, projectIdentifier, envIdentifier);
+    DeleteResult delete = clusterRepository.delete(criteria);
+    return delete.getDeletedCount();
   }
 
   @Override
@@ -218,6 +239,20 @@ public class ClusterServiceImpl implements ClusterService {
         .is(projectId)
         .and(ClusterKeys.envRef)
         .is(envIdentifier);
+  }
+
+  private Criteria getClusterEqualityCriteria(
+      String accountId, String orgId, String projectId, String envRefs, Collection<String> clusterRefs) {
+    return where(ClusterKeys.accountId)
+        .is(accountId)
+        .and(ClusterKeys.orgIdentifier)
+        .is(orgId)
+        .and(ClusterKeys.projectIdentifier)
+        .is(projectId)
+        .and(ClusterKeys.envRef)
+        .is(envRefs)
+        .and(ClusterKeys.clusterRef)
+        .in(clusterRefs);
   }
 
   private String getDuplicateExistsErrorMessage(
