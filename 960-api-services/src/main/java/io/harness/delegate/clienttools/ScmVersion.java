2d96419b67957c9f868c3d816ff2cc72460759d8
Aman Singh <aman.singh@harness.io>
2022-10-04 17:30:49 +0530

    fix: [CI-5657]: Update scm version for all environments

--- 960-api-services/src/main/java/io/harness/delegate/clienttools/ScmVersion.java
+++ 960-api-services/src/main/java/io/harness/delegate/clienttools/ScmVersion.java
@@ -16,7 +16,7 @@ import org.apache.commons.lang3.StringUtils;
 @Getter
 @Slf4j
 public enum ScmVersion implements ClientToolVersion {
-  DEFAULT("e3ee71bd");
+  DEFAULT("e3ee71b");
 
   private final String version;
 
--- on-prem/dockerless_pov_installer_generator/generate_installer.sh
+++ on-prem/dockerless_pov_installer_generator/generate_installer.sh
@@ -205,7 +205,7 @@ function setupDelegateJars() {
 
     done
 
-    for version in e3ee71bd; do
+    for version in e3ee71b; do
 
       echo "Copying scm ${version} binaries for ${platform}"
 
