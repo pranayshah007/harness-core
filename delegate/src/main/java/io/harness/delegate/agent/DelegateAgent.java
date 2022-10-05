01ebee0e155c862398d4e910c77b8c03df45301d
Marko Milic <marko.milic@harness.io>
2022-10-05 18:52:08 +0200

    [DEL-4946]: Delegate agent sends task reposne to core

--- delegate/src/main/java/io/harness/delegate/agent/DelegateAgent.java
+++ delegate/src/main/java/io/harness/delegate/agent/DelegateAgent.java
@@ -7,38 +7,58 @@
 
 package io.harness.delegate.agent;
 
+import io.harness.delegate.agent.client.delegate.DelegateCoreClient;
+import io.harness.delegate.agent.client.delegate.DelegateCoreClientFactory;
+import io.harness.delegate.agent.servicediscovery.ServiceDiscovery;
+import io.harness.delegate.beans.DelegateTaskResponse;
+import io.harness.serializer.YamlUtils;
+
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeoutException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.exec.LogOutputStream;
 import org.zeroturnaround.exec.ProcessExecutor;
 import org.zeroturnaround.exec.ProcessResult;
+import org.zeroturnaround.exec.listener.ShutdownHookProcessDestroyer;
 
 @Slf4j
 public class DelegateAgent {
   // TODO(gauravnanda): Parametrize this.
-  private static final String DELEGATE_RUNNER_JAR_PATH = "/Users/gauravnanda/test-jar/TaskRunner.jar";
-  private static final int TIMEOUT_MINUTES = 15;
+  private static final String DELEGATE_RUNNER_JAR_PATH = "/opt/harness/task.jar";
+  private static final String DELEGATE_NAME = System.getenv("DELEGATE_NAME");
+  private static final String ACCOUNT_ID = System.getenv("ACCOUNT_ID");
+  private final static DelegateCoreClient delegateCoreClient =
+      DelegateCoreClientFactory.createDelegateCoreClient(ServiceDiscovery.getDelegateServiceEndpoint(DELEGATE_NAME));
 
-  public static void main(String[] args) throws IOException, InterruptedException, TimeoutException {
-    log.info("Delegate agent started");
+  public static void main(final String[] args) throws IOException, InterruptedException, TimeoutException {
+    try {
+      log.info("Delegate agent started");
 
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      // TODO(gauravnanda): Reply back to delegate if not already replied to delegate. We will probably need to build
-      // some state tracking about if a reply has already been sent back.
-      log.debug("Shutdown hook triggered");
-    }));
+      Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+        // TODO(gauravnanda): Reply back to delegate if not already replied to delegate. We will probably need to build
+        // some state tracking about if a reply has already been sent back.
+        log.debug("Shutdown hook triggered");
+      }));
 
-    ProcessResult runnerExecutionResult = runDelegateRunner(DELEGATE_RUNNER_JAR_PATH);
-    // TODO(gauravnanda): Verify the return type from the runner.
-    // TODO(gauravnanda): Add logic to call delegate endpoint to return result.
-    if (runnerExecutionResult.getExitValue() == 0) {
-      log.info(runnerExecutionResult.outputUTF8());
-    } else {
-      log.error(runnerExecutionResult.outputUTF8());
+      final ProcessResult runnerExecutionResult = runDelegateRunner(DELEGATE_RUNNER_JAR_PATH);
+      // TODO(gauravnanda): Verify the return type from the runner.
+      // TODO(gauravnanda): Add logic to call delegate endpoint to return result.
+      if (runnerExecutionResult.getExitValue() == 0) {
+        log.info(runnerExecutionResult.outputUTF8());
+      } else {
+        log.error(runnerExecutionResult.outputUTF8());
+      }
+
+      //TODO: We can also use process output rather than writing and reading from file.
+      final String outputYaml = Files.readString(Paths.get("/etc/output/result.yaml"));
+      final DelegateTaskResponse taskResponse = new YamlUtils().read(outputYaml, DelegateTaskResponse.class);
+
+      delegateCoreClient.taskResponse(ACCOUNT_ID, taskResponse);
+    } catch (final Exception e) {
+      log.error("Something failed!!!", e);
     }
-
-    //
   }
 
   /**
@@ -49,12 +69,24 @@ public class DelegateAgent {
    * @throws InterruptedException
    * @throws TimeoutException
    */
-  private static ProcessResult runDelegateRunner(String runnerJarPath)
+  private static ProcessResult runDelegateRunner(final String runnerJarPath)
       throws IOException, InterruptedException, TimeoutException {
     final ProcessExecutor processExecutor = new ProcessExecutor()
-                                                .timeout(TIMEOUT_MINUTES, TimeUnit.MINUTES)
                                                 .command("java", "-jar", runnerJarPath)
-                                                .readOutput(true);
+                                                .addDestroyer(ShutdownHookProcessDestroyer.INSTANCE)
+                                                .readOutput(true)
+                                                .redirectErrorAlsoTo(new LogOutputStream() {
+                                                  @Override
+                                                  protected void processLine(final String s, final int i) {
+                                                    log.error(s);
+                                                  }
+                                                })
+                                                .redirectOutputAlsoTo(new LogOutputStream() {
+                                                  @Override
+                                                  protected void processLine(final String s, final int i) {
+                                                    log.info(s);
+                                                  }
+                                                });
 
     return processExecutor.execute();
   }
