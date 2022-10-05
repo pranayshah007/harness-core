f7f2130eab2deec5804b402820607e3d0b0e5c26
Marko Milic <marko.milic@harness.io>
2022-10-05 23:24:15 +0200

    [DEL-4946]: Change package name and fix build files

--- delegate/src/main/java/io/harness/delegate/taskagent/client/delegate/DelegateCoreClientFactory.java
+++ delegate/src/main/java/io/harness/delegate/taskagent/client/delegate/DelegateCoreClientFactory.java
@@ -5,9 +5,9 @@
  * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
  */
 
-package io.harness.delegate.agent.client.delegate;
+package io.harness.delegate.taskagent.client.delegate;
 
-import io.harness.delegate.agent.servicediscovery.ServiceEndpoint;
+import io.harness.delegate.taskagent.servicediscovery.ServiceEndpoint;
 import io.harness.exception.SslContextBuilderException;
 import io.harness.network.Http;
 import io.harness.network.NoopHostnameVerifier;
--- delegate/src/main/java/io/harness/delegate/taskagent/servicediscovery/ServiceDiscovery.java
+++ delegate/src/main/java/io/harness/delegate/taskagent/servicediscovery/ServiceDiscovery.java
@@ -5,7 +5,7 @@
  * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
  */
 
-package io.harness.delegate.agent.servicediscovery;
+package io.harness.delegate.taskagent.servicediscovery;
 
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
