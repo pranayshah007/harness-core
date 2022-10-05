23a52f08b757e5bebd7a77194d09fa5c116ad3f7
Shreyas Singh <shreyas.singh@harness.io>
2022-10-05 16:51:04 +0530

    [PL-28744]: adding validation for custom secret manager connector dto.

--- 954-connector-beans/src/main/java/io/harness/delegate/beans/connector/customsecretmanager/CustomSecretManagerConnectorDTO.java
+++ 954-connector-beans/src/main/java/io/harness/delegate/beans/connector/customsecretmanager/CustomSecretManagerConnectorDTO.java
@@ -15,6 +15,9 @@ import io.harness.connector.DelegateSelectable;
 import io.harness.delegate.beans.connector.ConnectorConfigDTO;
 import io.harness.encryption.SecretRefData;
 import io.harness.encryption.SecretReference;
+import io.harness.eraro.ErrorCode;
+import io.harness.exception.InvalidRequestException;
+import io.harness.exception.WingsException;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -22,6 +25,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import javax.validation.constraints.NotNull;
@@ -62,5 +66,47 @@ public class CustomSecretManagerConnectorDTO extends ConnectorConfigDTO implemen
   @Override
   public List<DecryptableEntity> getDecryptableEntities() {
     return Collections.singletonList(this);
+  }
+
+  @Override
+  public void validate() {
+    if (onDelegate) {
+      boolean throwException = false;
+      StringBuilder errorMessageBuilder =
+          new StringBuilder("Target machine information should be absent if execution is on delegate. Found ");
+      if (host != null) {
+        throwException = true;
+        errorMessageBuilder.append(String.format("host = %s ", host));
+      }
+      if (connectorRef != null) {
+        throwException = true;
+        errorMessageBuilder.append(String.format("connector ref = %s ", connectorRef));
+      }
+      if (workingDirectory != null) {
+        throwException = true;
+        errorMessageBuilder.append(String.format("working directory = %s ", workingDirectory));
+      }
+      if (throwException) {
+        throw new InvalidRequestException(
+            errorMessageBuilder.toString(), ErrorCode.INVALID_REQUEST, WingsException.USER);
+      }
+    } else {
+      List<String> targetMachineNullFields = new LinkedList<>();
+      if (host == null) {
+        targetMachineNullFields.add("host");
+      }
+      if (workingDirectory == null) {
+        targetMachineNullFields.add("working directory");
+      }
+      if (connectorRef == null) {
+        targetMachineNullFields.add("connector ref");
+      }
+      String errorMessage =
+          "Target machine information should be present if execution is not on delegate. But the following values are absent. "
+          + targetMachineNullFields;
+      if (targetMachineNullFields.size() > 0) {
+        throw new InvalidRequestException(errorMessage, ErrorCode.INVALID_REQUEST, WingsException.USER);
+      }
+    }
   }
 }
