4c20c41e4cfa8beb850a854ef0c30e0f70abae34
Mankrit Singh <mankrit.singh@harness.io>
2022-10-07 11:01:56 +0530

    [PL-28499]: Renaming

--- pipeline-service/service/src/main/java/io/harness/pms/plan/execution/api/ExecutionsApiImpl.java
+++ pipeline-service/service/src/main/java/io/harness/pms/plan/execution/api/ExecutionsApiImpl.java
@@ -9,36 +9,39 @@ import javax.ws.rs.core.Response;
 
 public class ExecutionsApiImpl implements ExecutionsApi {
   @Override
-  public Response executePipeline(
-      PipelineExecuteRequestBody pipelineExecuteRequestBody, String s, String s1, String s2, String s3, String s4) {
+  public Response executePipeline(PipelineExecuteRequestBody requestBody, String org, String project, String pipeline,
+      String account, String branchGitX) {
     return null;
   }
 
   @Override
-  public Response getExecutionDetails(String s, String s1, String s2, String s3) {
+  public Response getExecutionDetails(String org, String project, String execution, String account) {
     return null;
   }
 
   @Override
-  public Response getExecutionDetailsGraph(String s, String s1, String s2, String s3, String s4, Boolean aBoolean) {
+  public Response getExecutionDetailsGraph(
+      String org, String project, String execution, String account, String stageNode, Boolean fullGraph) {
     return null;
   }
 
   @Override
-  public Response getRuntimeTemplate(String s, String s1, String s2, String s3, List list, String s4) {
+  public Response getRuntimeTemplate(
+      String org, String project, String pipeline, String account, List<String> stageIds, String branchGitX) {
     return null;
   }
 
   @Override
-  public Response listExecutions(String s, String s1, String s2, Integer integer, Integer integer1, String s3,
-      String s4, String s5, List<String> list, String s6, List<String> list1, Boolean aBoolean, String s7, String s8,
-      String s9, String s10, String s11, List<String> list2, List<String> list3, List<String> list4, String s12) {
+  public Response listExecutions(String org, String project, String account, Integer page, Integer limit,
+      String searchTerm, String sort, String order, String pipelineId, String filterId, List<String> status,
+      Boolean myDeployments, String repositoryCI, String branchCI, String tagCI, String prSourceCI, String prTargetCI,
+      List<String> services, List<String> envs, List<String> infras, String branchGitX) {
     return null;
   }
 
   @Override
   public Response registerInterrupt(
-      InterruptRequestBody interruptRequestBody, String s, String s1, String s2, String s3) {
+      InterruptRequestBody interruptRequestBody, String org, String project, String execution, String account) {
     return null;
   }
 }
