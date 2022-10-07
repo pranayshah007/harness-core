1cd9490a42557110e54aed896110a9576ea410fe
Jenny James <jenny.james@harness.io>
2022-10-07 10:41:35 -0700

    fix unit test

--- 400-rest/src/main/java/io/harness/cvng/perpetualtask/CVDataCollectionTaskServiceImpl.java
+++ 400-rest/src/main/java/io/harness/cvng/perpetualtask/CVDataCollectionTaskServiceImpl.java
@@ -268,6 +268,12 @@ public class CVDataCollectionTaskServiceImpl implements CVDataCollectionTaskServ
         return CVNGPerpetualTaskUnassignedReason.MULTIPLE_FAILED_PERPETUAL_TASK;
       case VALIDATION_TASK_FAILED:
         return CVNGPerpetualTaskUnassignedReason.VALIDATION_TASK_FAILED;
+      case PT_TASK_FAILED:
+        return CVNGPerpetualTaskUnassignedReason.PT_TASK_FAILED;
+      case TASK_EXPIRED:
+        return CVNGPerpetualTaskUnassignedReason.TASK_EXPIRED;
+      case TASK_VALIDATION_FAILED:
+        return CVNGPerpetualTaskUnassignedReason.TASK_VALIDATION_FAILED;
       default:
         throw new UnknownEnumTypeException("Task Unassigned Reason", String.valueOf(perpetualTaskUnassignedReason));
     }
