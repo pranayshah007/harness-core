a362d280d9b159f3218837d730ad5754d8f6ca74
Mankrit Singh <mankrit.singh@harness.io>
2022-10-07 11:14:42 +0530

    [PL-28499]: ACS annotations

--- pipeline-service/service/src/main/java/io/harness/pms/plan/execution/api/ExecutionsApiImpl.java
+++ pipeline-service/service/src/main/java/io/harness/pms/plan/execution/api/ExecutionsApiImpl.java
@@ -1,36 +1,44 @@
 package io.harness.pms.plan.execution.api;
 
+import io.harness.accesscontrol.NGAccessControlCheck;
+import io.harness.pms.rbac.PipelineRbacPermissions;
 import io.harness.spec.server.pipeline.ExecutionsApi;
 import io.harness.spec.server.pipeline.model.InterruptRequestBody;
 import io.harness.spec.server.pipeline.model.PipelineExecuteRequestBody;
 
+import java.util.List;
 import javax.ws.rs.core.Response;
 
 public class ExecutionsApiImpl implements ExecutionsApi {
   @Override
+  @NGAccessControlCheck(resourceType = "PIPELINE", permission = PipelineRbacPermissions.PIPELINE_EXECUTE)
   public Response executePipeline(PipelineExecuteRequestBody requestBody, String org, String project, String pipeline,
       String account, String branchGitX) {
     return null;
   }
 
   @Override
+  @NGAccessControlCheck(resourceType = "PIPELINE", permission = PipelineRbacPermissions.PIPELINE_VIEW)
   public Response getExecutionDetails(String org, String project, String execution, String account) {
     return null;
   }
 
   @Override
+  @NGAccessControlCheck(resourceType = "PIPELINE", permission = PipelineRbacPermissions.PIPELINE_VIEW)
   public Response getExecutionDetailsGraph(
       String org, String project, String execution, String account, String stageNode, Boolean fullGraph) {
     return null;
   }
 
   @Override
+  @NGAccessControlCheck(resourceType = "PIPELINE", permission = PipelineRbacPermissions.PIPELINE_VIEW)
   public Response getRuntimeTemplate(
       String org, String project, String pipeline, String account, List<String> stageIds, String branchGitX) {
     return null;
   }
 
   @Override
+  @NGAccessControlCheck(resourceType = "PIPELINE", permission = PipelineRbacPermissions.PIPELINE_VIEW)
   public Response listExecutions(String org, String project, String account, Integer page, Integer limit,
       String searchTerm, String sort, String order, String pipelineId, String filterId, List<String> status,
       Boolean myDeployments, String repositoryCI, String branchCI, String tagCI, String prSourceCI, String prTargetCI,
