449193388b18cb1cf48dc9d456c29ce01a9fa899
Sahiba Khan <sahiba.khan@harness.io>
2022-10-11 23:21:47 +0530

    [CCM-9038]: edited enity value, edited schema and added indexes

--- ce-nextgen/service/src/main/java/io/harness/ccm/remote/resources/policies/PolicyStore.java
+++ ce-nextgen/service/src/main/java/io/harness/ccm/remote/resources/policies/PolicyStore.java
@@ -17,18 +17,14 @@ import io.harness.persistence.UuidAware;
 import com.google.common.collect.ImmutableList;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
-import javax.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.FieldNameConstants;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
-import org.hibernate.validator.constraints.NotBlank;
 import org.mongodb.morphia.annotations.Entity;
 import org.mongodb.morphia.annotations.Id;
-
-import java.util.List;
 
 @Data
 @Builder
@@ -36,52 +32,62 @@ import java.util.List;
 @FieldNameConstants(innerTypeName = "PolicyId")
 @JsonIgnoreProperties(ignoreUnknown = true)
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@Entity(value = "PolicyStore", noClassnameStored = true)
+@Entity(value = "governancePolicy", noClassnameStored = true)
 @Schema(description = "This object will contain the complete definition of a Cloud Cost Policies")
 
 public final class PolicyStore implements PersistentEntity, UuidAware, CreatedAtAware, UpdatedAtAware, AccountAccess,
-        CreatedByAware, UpdatedByAware {
+                                          CreatedByAware, UpdatedByAware {
+  public static List<MongoIndex> mongoIndexes() {
+    return ImmutableList.<MongoIndex>builder()
+        .add(CompoundMongoIndex.builder()
+                 .name("policy")
+                 .field(PolicyId.uuid)
+                 .field(PolicyId.accountId)
+                 .field(PolicyId.orgIdentifier)
+                 .field(PolicyId.projectIdentifier)
+                 .field(PolicyId.cloudProvider)
+                 .build())
+        .add(CompoundMongoIndex.builder().name("sort1").field(PolicyId.lastUpdatedAt).build())
+        .add(CompoundMongoIndex.builder().name("sort2").field(PolicyId.createdAt).build())
+        .build();
+  }
+  @Id @Schema(description = "unique id") String uuid;
+  @Schema(description = "account id") String accountId;
+  @Schema(description = "Identifier") String name;
+  @Schema(description = NGCommonEntityConstants.RESOURCE) String resource;
+  @Schema(description = NGCommonEntityConstants.DESCRIPTION) String description;
+  @Schema(description = NGCommonEntityConstants.POLICY) String policyYaml;
+  @Schema(description = NGCommonEntityConstants.TAGS) List<String> tags;
+  @Schema(description = NGCommonEntityConstants.ORG_PARAM_MESSAGE) String orgIdentifier;
+  @Schema(description = NGCommonEntityConstants.PROJECT_PARAM_MESSAGE) String projectIdentifier;
+  @Schema(description = "cloudProvider") String cloudProvider;
+  @Schema(description = "versionLabel") String versionLabel;
+  @Schema(description = "isStablePolicy") String isStablePolicy;
+  @Schema(description = "versionLabel") String isOOTBPolicy;
+  @Schema(description = NGCommonEntityConstants.CREATED_AT_MESSAGE) long createdAt;
+  @Schema(description = NGCommonEntityConstants.UPDATED_AT_MESSAGE) long lastUpdatedAt;
+  @Schema(description = "created by") private EmbeddedUser createdBy;
+  @Schema(description = "updated by") private EmbeddedUser lastUpdatedBy;
 
-    public static List<MongoIndex> mongoIndexes() {
-        return ImmutableList.<MongoIndex>builder()
-                .add(CompoundMongoIndex.builder().name("uuid").field(PolicyId.uuid).build())
-                .build();
-    }
-    @Id @Schema(description = "unique id") String uuid;
-    @Schema(description = "account id") String accountId;
-    @Schema(description = "Identifier") String name;
-    @Schema(description = NGCommonEntityConstants.RESOURCE) String resource;
-    @Schema(description = NGCommonEntityConstants.DESCRIPTION) String description;
-    @Schema(description = NGCommonEntityConstants.POLICY) String policy;
-    @Schema(description = NGCommonEntityConstants.TAGS) String tags;
-    @Schema(description = "ERROR") String Error;
-    @Schema(description = NGCommonEntityConstants.ORG_PARAM_MESSAGE) String orgIdentifier;
-    @Schema(description = NGCommonEntityConstants.PROJECT_PARAM_MESSAGE) String projectIdentifier;
-
-    @Schema(description = NGCommonEntityConstants.CREATED_AT_MESSAGE) long createdAt;
-    @Schema(description = NGCommonEntityConstants.UPDATED_AT_MESSAGE) long lastUpdatedAt;
-    @Schema(description = "created by") private EmbeddedUser createdBy;
-    @Schema(description = "updated by") private EmbeddedUser lastUpdatedBy;
-
-
-    public PolicyStore toDTO() {
-      return PolicyStore.builder()
-          .uuid(getUuid())
-          .accountId(getAccountId())
-          .name(getName())
-          .resource(getResource())
-          .Error(getError())
-          .description(getDescription())
-          .policy(getPolicy())
-          .tags(getTags())
-          .orgIdentifier(getOrgIdentifier())
-          .projectIdentifier(getProjectIdentifier())
-          .createdAt(getCreatedAt())
-          .lastUpdatedAt(getLastUpdatedAt())
-          .createdBy(getCreatedBy())
-          .lastUpdatedBy(getLastUpdatedBy())
-          .build();
-    }
-
-
+  public PolicyStore toDTO() {
+    return PolicyStore.builder()
+        .uuid(getUuid())
+        .accountId(getAccountId())
+        .name(getName())
+        .resource(getResource())
+        .description(getDescription())
+        .policyYaml(getPolicyYaml())
+        .cloudProvider(getCloudProvider())
+        .versionLabel(getVersionLabel())
+        .isStablePolicy(getIsStablePolicy())
+        .isOOTBPolicy(getIsOOTBPolicy())
+        .tags(getTags())
+        .orgIdentifier(getOrgIdentifier())
+        .projectIdentifier(getProjectIdentifier())
+        .createdAt(getCreatedAt())
+        .lastUpdatedAt(getLastUpdatedAt())
+        .createdBy(getCreatedBy())
+        .lastUpdatedBy(getLastUpdatedBy())
+        .build();
+  }
 }
