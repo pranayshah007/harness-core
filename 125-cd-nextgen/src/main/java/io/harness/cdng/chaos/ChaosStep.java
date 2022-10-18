09fe4175be646deee13dd7449bceaa7afef18b3b
Prashant Pal <prashant.pal@harness.io>
2022-10-18 11:16:41 +0530

    feat: [CHAOS-294]: Added rbac and rollback for chaos step

--- 125-cd-nextgen/src/main/java/io/harness/cdng/chaos/ChaosStep.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/chaos/ChaosStep.java
@@ -8,6 +8,7 @@ import io.harness.chaos.client.remote.ChaosHttpClient;
 import io.harness.eraro.Level;
 import io.harness.executions.steps.StepSpecTypeConstants;
 import io.harness.plancreator.steps.common.StepElementParameters;
+import io.harness.plancreator.steps.common.rollback.AsyncExecutableWithRollbackAndRbac;
 import io.harness.pms.contracts.ambiance.Ambiance;
 import io.harness.pms.contracts.execution.AsyncExecutableResponse;
 import io.harness.pms.contracts.execution.Status;
@@ -17,8 +18,6 @@ import io.harness.pms.contracts.execution.failure.FailureType;
 import io.harness.pms.contracts.steps.StepCategory;
 import io.harness.pms.contracts.steps.StepType;
 import io.harness.pms.execution.utils.AmbianceUtils;
-import io.harness.pms.sdk.core.steps.executables.AsyncExecutable;
-import io.harness.pms.sdk.core.steps.io.PassThroughData;
 import io.harness.pms.sdk.core.steps.io.StepInputPackage;
 import io.harness.pms.sdk.core.steps.io.StepResponse;
 import io.harness.pms.sdk.core.steps.io.StepResponse.StepOutcome;
@@ -32,7 +31,7 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class ChaosStep implements AsyncExecutable<StepElementParameters> {
+public class ChaosStep extends AsyncExecutableWithRollbackAndRbac {
   public static final StepType STEP_TYPE =
       StepType.newBuilder().setType(StepSpecTypeConstants.CHAOS_STEP).setStepCategory(StepCategory.STEP).build();
 
@@ -47,8 +46,13 @@ public class ChaosStep implements AsyncExecutable<StepElementParameters> {
   }
 
   @Override
-  public AsyncExecutableResponse executeAsync(Ambiance ambiance, StepElementParameters stepParameters,
-      StepInputPackage inputPackage, PassThroughData passThroughData) {
+  public void validateResources(Ambiance ambiance, StepElementParameters stepParameters) {
+    // TODO add RBAC checks here if required
+  }
+
+  @Override
+  public AsyncExecutableResponse executeAsyncAfterRbac(
+      Ambiance ambiance, StepElementParameters stepParameters, StepInputPackage inputPackage) {
     ChaosStepParameters params = (ChaosStepParameters) stepParameters.getSpec();
     String callbackId = triggerWorkflow(ambiance, params);
     log.info("Triggered chaos experiment with ref: {}, workflowRunId: {}", params.getExperimentRef(), callbackId);
