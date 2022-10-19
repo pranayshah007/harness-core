e714c29c967913545a0ca55e9d0cf4151c44c8f0
Shreyas Singh <shreyas.singh@harness.io>
2022-10-19 15:55:58 +0530

    [feat]:[PL-29111]: add endpoint to return GCP Region list

--- 120-ng-manager/src/main/java/io/harness/ng/SecretManagerConnectorServiceImpl.java
+++ 120-ng-manager/src/main/java/io/harness/ng/SecretManagerConnectorServiceImpl.java
@@ -399,4 +399,9 @@ public class SecretManagerConnectorServiceImpl implements ConnectorService {
       String accountId, String orgIdentifier, String projectIdentifier, List<String> connectorIdentifiers) {
     return defaultConnectorService.getAttributes(accountId, orgIdentifier, projectIdentifier, connectorIdentifiers);
   }
+
+  @Override
+  public List<String> getGcpRegions() {
+    return defaultConnectorService.getGcpRegions();
+  }
 }
