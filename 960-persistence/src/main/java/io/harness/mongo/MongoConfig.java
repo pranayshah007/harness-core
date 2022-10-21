a447fdcba79e01c0a30cdae2bfbead499ef4b0ad
Hitesh Aringa <hitesh.aringa@harness.io>
2022-10-21 10:19:12 +0530

    [feat]: [CCM-9349]: Fetching if Node/Pod is part of Azure virtual node (#39084)
    * [CCM-9349]: build ++
    
    * [feat]: [CCM-9349]: Fetching if Node/Pod is part of Azure virtual node. (#39022)
    
    * [CCM-9349]: fetching if Node/Pod is part of Azure virtual node. We need to skip for billing that node
    
    * [CCM-9349]: Azure VM pricing
    
    * [CCM-9349]: Azure VM pricing
    
    * [CCM-9349]: Azure VM pricing
    
    Co-authored-by: Hitesh Aringa <hitesh.aringa@harness.io>
    
    Co-authored-by: Abhinav Kumar Singh <abhinav.singh3@harness.io>

--- 960-persistence/src/main/java/io/harness/mongo/MongoConfig.java
+++ 960-persistence/src/main/java/io/harness/mongo/MongoConfig.java
@@ -108,7 +108,7 @@ public class MongoConfig {
 
   private AnalyticNodeMongoConfig analyticNodeConfig = AnalyticNodeMongoConfig.builder().build();
 
-  @NotEmpty private int maxOperationTimeInMillis = 300000;
+  @NotEmpty private int maxOperationTimeInMillis = 1800000;
 
   @JsonIgnore
   public ReadPreference getReadPreference() {
