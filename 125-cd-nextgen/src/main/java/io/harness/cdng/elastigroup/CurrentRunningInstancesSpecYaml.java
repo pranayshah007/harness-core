dba3473b99d823dafc1fef92784a2b82c151a44c
Piyush Bhuwalka <piyush.bhuwalka@harness.io>
2022-10-25 10:39:46 +0530

    [CDS-43508]: 2nd cherrypick

---
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/elastigroup/CurrentRunningInstancesSpecYaml.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.cdng.elastigroup;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import io.harness.annotation.RecasterAlias;
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.TypeAlias;
+
+import javax.validation.constraints.NotNull;
+
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.EXTERNAL_PROPERTY;
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
+
+@OwnedBy(HarnessTeam.CDP)
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@JsonTypeName(InstancesSpecTypeConstants.CURRENT_RUNNING)
+@TypeAlias("currentRuningInstancesSpecYaml")
+@RecasterAlias("io.harness.cdng.elastigroup.CurrentRunningInstancesSpecYaml")
+public class CurrentRunningInstancesSpecYaml extends InstancesSpecAbstractYaml {
+  @JsonProperty("type")
+  @NotNull
+  CurrentRunningInstancesSpecYaml.StepType type =
+          StepType.ElastigroupSetup;
+  @JsonProperty("spec")
+  @JsonTypeInfo(use = NAME, property = "type", include = EXTERNAL_PROPERTY, visible = true)
+  CurrentRunningInstancesSpec currentRunningInstancesSpec;
+  @Override
+  public String getType() {
+    return InstancesSpecTypeConstants.CURRENT_RUNNING;
+  }
+
+  enum StepType {
+    ElastigroupSetup(InstancesSpecTypeConstants.CURRENT_RUNNING);
+    @Getter String name;
+    StepType(String name) {
+      this.name = name;
+    }
+  }
+}
