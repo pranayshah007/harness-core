a48412424ecf83f8b1d96addb69b92f9a083e340
Piyush Bhuwalka <piyush.bhuwalka@harness.io>
2022-10-29 18:37:39 +0530

    [CDS-43508]: some wrong changes fixed

--- 970-ng-commons/src/main/java/io/harness/common/EntityYamlRootNames.java
+++ 970-ng-commons/src/main/java/io/harness/common/EntityYamlRootNames.java
@@ -132,7 +132,7 @@ public class EntityYamlRootNames {
   public static final String AZURE_ROLLBACK_ARM_RESOURCE_STEP = "AzureARMRollback";
   public static final String ECS_RUN_TASK = "EcsRunTask";
   public static final String ECS_BLUE_GREEN_CREATE_SERVICE = "EcsBlueGreenCreateService";
-  public static final String ELASTIGROUP_SERVICE = "ElastigroupService";
+  public static final String ELASTIGROUP_SETUP = "ElastigroupSetup";
   public static final String ECS_BLUE_GREEN_SWAP_TARGET_GROUPS = "EcsBlueGreenSwapTargetGroups";
   public static final String ECS_BLUE_GREEN_ROLLBACK = "EcsBlueGreenRollback";
   public static final String WAIT_STEP = "Wait";
