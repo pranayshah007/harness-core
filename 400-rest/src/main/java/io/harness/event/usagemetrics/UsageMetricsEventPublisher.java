922124ba0753fa0c7f8c8fa97d5779b2bc542675
Abhishek <abhishek.abhishek@harness.io>
2022-10-31 15:38:53 +0530

    removed unused import

--- 400-rest/src/main/java/io/harness/event/usagemetrics/UsageMetricsEventPublisher.java
+++ 400-rest/src/main/java/io/harness/event/usagemetrics/UsageMetricsEventPublisher.java
@@ -39,7 +39,6 @@ import com.google.inject.Singleton;
 import io.fabric8.utils.Lists;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
