0594031419614942247d874d98a49bb0efd7be71
Piyush Bhuwalka <piyush.bhuwalka@harness.io>
2022-10-31 11:36:16 +0530

    [CDS-43508]: added step info CDStepInfo class

--- 125-cd-nextgen/src/main/java/io/harness/cdng/pipeline/CDStepInfo.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/pipeline/CDStepInfo.java
@@ -23,6 +23,7 @@ import io.harness.cdng.ecs.EcsCanaryDeployStepInfo;
 import io.harness.cdng.ecs.EcsRollingDeployStepInfo;
 import io.harness.cdng.ecs.EcsRollingRollbackStepInfo;
 import io.harness.cdng.ecs.EcsRunTaskStepInfo;
+import io.harness.cdng.elastigroup.ElastigroupSetupStepInfo;
 import io.harness.cdng.gitops.CreatePRStepInfo;
 import io.harness.cdng.gitops.MergePRStepInfo;
 import io.harness.cdng.gitops.UpdateReleaseRepoStepInfo;
@@ -78,7 +79,7 @@ import io.swagger.annotations.ApiModel;
         EcsCanaryDeployStepInfo.class, EcsCanaryDeleteStepInfo.class, EcsBlueGreenCreateServiceStepInfo.class,
         EcsBlueGreenSwapTargetGroupsStepInfo.class, EcsBlueGreenRollbackStepInfo.class,
         FetchInstanceScriptStepInfo.class, ShellScriptProvisionStepInfo.class, UpdateReleaseRepoStepInfo.class,
-        EcsRunTaskStepInfo.class})
+        EcsRunTaskStepInfo.class, ElastigroupSetupStepInfo.class})
 
 @OwnedBy(HarnessTeam.CDC)
 public interface CDStepInfo extends StepSpecType, WithStepElementParameters, WithDelegateSelector {
