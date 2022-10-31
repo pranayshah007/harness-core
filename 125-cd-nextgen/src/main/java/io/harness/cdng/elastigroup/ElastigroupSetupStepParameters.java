4587e5e3c0901d91fedc9f830f5aa1e1e4b2cc0f
Piyush Bhuwalka <piyush.bhuwalka@harness.io>
2022-10-31 13:50:29 +0530

    [CDS-43508]: extra fields added for step to work

--- 125-cd-nextgen/src/main/java/io/harness/cdng/elastigroup/ElastigroupSetupStepParameters.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/elastigroup/ElastigroupSetupStepParameters.java
@@ -40,7 +40,6 @@ public class ElastigroupSetupStepParameters
   }
 
   public List<String> getCommandUnits() {
-    return Arrays.asList(ElastigroupCommandUnitConstants.fetchManifests.toString(),
-            ElastigroupCommandUnitConstants.deploy.toString());
+    return Arrays.asList(ElastigroupCommandUnitConstants.fetchStartupScript.toString());
   }
 }
