139d8c676cbc38e13058e1fec43ba2420e8889d0
Akhilesh Pandey <akhilesh.pandey@harness.io>
2022-11-02 00:59:45 +0530

    feat: [CDS-45499]: format code

--- 960-api-services/src/main/java/io/harness/delegate/clienttools/ScmVersion.java
+++ 960-api-services/src/main/java/io/harness/delegate/clienttools/ScmVersion.java
@@ -16,9 +16,7 @@ import org.apache.commons.lang3.StringUtils;
 @Getter
 @Slf4j
 public enum ScmVersion implements ClientToolVersion {
-
   DEFAULT("ff17c219");
-
 
   private final String version;
 
