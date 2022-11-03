3c18f2f8e247ec701212514d13623f21df570fa6
Naman Verma <naman.verma@harness.io>
2022-11-03 10:28:41 +0530

    remove unused import

--- pipeline-service/modules/orchestration-visualization/src/main/java/io/harness/service/impl/GraphGenerationServiceImpl.java
+++ pipeline-service/modules/orchestration-visualization/src/main/java/io/harness/service/impl/GraphGenerationServiceImpl.java
@@ -38,7 +38,6 @@ import io.harness.lock.AcquiredLock;
 import io.harness.lock.PersistentLocker;
 import io.harness.plan.NodeType;
 import io.harness.pms.contracts.execution.events.OrchestrationEventType;
-import io.harness.pms.contracts.steps.StepCategory;
 import io.harness.pms.execution.utils.StatusUtils;
 import io.harness.pms.plan.execution.ExecutionSummaryUpdateUtils;
 import io.harness.pms.plan.execution.beans.PipelineExecutionSummaryEntity;
