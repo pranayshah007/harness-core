444ab19647b9fa879cbdee7851d072ed3ccaaff8
AnshuH <anshu.singla@harness.io>
2022-11-03 13:57:33 +0530

    added file

---
+++ scripts/jenkins/check-file.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+>merge_summary.txt
+>committed_files.txt
+
+export TargetBranch=`echo ${ghprbTargetBranch}`
+export SourceBranch=`echo ${ghprbSourceBranch}`
+
+REGEX1='^.*.sh$'
+REGEX2='^.*.md$'
+
+#git diff $TargetBranch origin/$TargetBranch --name-only
+#git diff $TargetBranch origin/$TargetBranch --name-only >merge_summary.txt
+
+git diff HEAD@{0} HEAD@{1} --name-only
+git diff HEAD@{0} HEAD@{1} --name-only >merge_summary.txt
+
+while read i; do echo "${i##*/}"; done < merge_summary.txt >committed_files.txt
+while read s; do echo "${s%%/*}"; done < merge_summary.txt >committed_folders.txt
+
+sort committed_folders.txt | uniq > committed_folders_1.txt
+
+if [[ $(grep -v -f bazelignore committed_folders_1.txt) ]] ; then
+#  echo "Compilation is Required"
+  compile=true
+fi
+
+if [[ $(grep -v -f bazelignore committed_files.txt) ]]  ; then
+#  echo "Compilation is Required"
+  compile=true
+fi
+
+while read i; do
+  if [[  $i =~ $REGEX1 ]]
+  then
+#      echo "Found Shell script"
+      compile=true
+  elif [[  $i =~ $REGEX2 ]]
+  then
+#      echo "Found Readme File"
+      compile=false
+#      echo $compile
+  fi
+done < committed_files.txt
+
+echo $compile
+
+if [[ "$compile" = true ]]
+then
+  echo "Doing compilation"
+else
+  echo "Compilation is Not Required"
+
+fi
