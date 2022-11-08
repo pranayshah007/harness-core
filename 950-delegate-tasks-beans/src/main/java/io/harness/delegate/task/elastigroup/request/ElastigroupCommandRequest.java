7a391b1d521eb89448e40d2c7bdd4f4cc1a863af
Piyush Bhuwalka <piyush.bhuwalka@harness.io>
2022-11-08 11:12:00 +0530

    [CDS-43509]: delegate capability addition and ecs names removed

--- 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/elastigroup/request/ElastigroupCommandRequest.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/elastigroup/request/ElastigroupCommandRequest.java
@@ -9,16 +9,26 @@ package io.harness.delegate.task.elastigroup.request;
 
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.delegate.beans.connector.awsconnector.AwsCapabilityHelper;
+import io.harness.delegate.beans.connector.awsconnector.AwsConnectorDTO;
+import io.harness.delegate.beans.connector.spotconnector.SpotCapabilityHelper;
+import io.harness.delegate.beans.connector.spotconnector.SpotConnectorDTO;
 import io.harness.delegate.beans.executioncapability.ExecutionCapability;
 import io.harness.delegate.beans.executioncapability.ExecutionCapabilityDemander;
 import io.harness.delegate.beans.logstreaming.CommandUnitsProgress;
+import io.harness.delegate.capability.EncryptedDataDetailsCapabilityHelper;
 import io.harness.delegate.task.TaskParameters;
+import io.harness.delegate.task.ecs.EcsCommandTypeNG;
+import io.harness.delegate.task.ecs.EcsInfraConfig;
 import io.harness.delegate.task.elastigroup.response.ElastigroupCommandTypeNG;
+import io.harness.delegate.task.elastigroup.response.SpotInstConfig;
 import io.harness.expression.ExpressionEvaluator;
+import io.harness.security.encryption.EncryptedDataDetail;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import org.hibernate.validator.constraints.NotEmpty;
 
 @OwnedBy(HarnessTeam.CDP)
 public interface ElastigroupCommandRequest extends TaskParameters, ExecutionCapabilityDemander {
@@ -27,9 +37,19 @@ public interface ElastigroupCommandRequest extends TaskParameters, ExecutionCapa
   String getCommandName();
   CommandUnitsProgress getCommandUnitsProgress();
   Integer getTimeoutIntervalInMin();
+  SpotInstConfig getSpotInstConfig();
 
   @Override
   default List<ExecutionCapability> fetchRequiredExecutionCapabilities(ExpressionEvaluator maskingEvaluator) {
-    return new ArrayList<>();
+    SpotInstConfig spotInstConfig = getSpotInstConfig();
+    List<EncryptedDataDetail> spotInstConfigEncryptionDataDetails = spotInstConfig.getEncryptionDataDetails();
+
+    List<ExecutionCapability> capabilities =
+        new ArrayList<>(EncryptedDataDetailsCapabilityHelper.fetchExecutionCapabilitiesForEncryptedDataDetails(
+                spotInstConfigEncryptionDataDetails, maskingEvaluator));
+
+    SpotConnectorDTO spotConnectorDTO = spotInstConfig.getSpotConnectorDTO();
+    capabilities.addAll(SpotCapabilityHelper.fetchRequiredExecutionCapabilities(spotConnectorDTO, maskingEvaluator));
+    return capabilities;
   }
 }
