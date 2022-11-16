0dff6104c2245ae662cfafd573b5356aea8e6326
Gaurav Nanda <gaurav.nanda@harness.io>
2022-11-16 11:25:29 +0530

    Revert cron schedule

--- 900-git-sync-sdk/src/main/java/io/harness/gitsync/exceptions/GitErrorMetadataDTO.java
+++ 900-git-sync-sdk/src/main/java/io/harness/gitsync/exceptions/GitErrorMetadataDTO.java
@@ -5,27 +5,29 @@
  * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
  */
 
-package io.harness.gitsync.scm.beans;
+package io.harness.gitsync.exceptions;
 
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.exception.ngexception.ErrorMetadataConstants;
+import io.harness.exception.ngexception.ErrorMetadataDTO;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.AccessLevel;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
-@OwnedBy(HarnessTeam.PL)
-@Getter
+@Data
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class ScmGitMetaData {
-  @Setter String repoName;
-  String branchName;
-  String commitId;
-  String filePath;
-  String blobId;
-  String fileUrl;
-  String repoUrl;
+@OwnedBy(HarnessTeam.PL)
+@JsonTypeName(ErrorMetadataConstants.GIT_ERROR)
+public class GitErrorMetadataDTO implements ErrorMetadataDTO {
+  String branch;
+
+  @Override
+  public String getType() {
+    return ErrorMetadataConstants.GIT_ERROR;
+  }
 }
---
+++ pipeline-service/modules/orchestration-steps/contracts/src/main/java/io/harness/steps/plugin/PmsPluginStepInfo.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.steps.plugin;
+
+import static io.harness.yaml.schema.beans.SupportedPossibleFieldTypes.runtime;
+
+import io.harness.annotation.RecasterAlias;
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.plancreator.steps.TaskSelectorYaml;
+import io.harness.plancreator.steps.common.SpecParameters;
+import io.harness.plancreator.steps.common.WithDelegateSelector;
+import io.harness.plancreator.steps.internal.PMSStepInfo;
+import io.harness.pms.contracts.steps.StepType;
+import io.harness.pms.execution.OrchestrationFacilitatorType;
+import io.harness.pms.yaml.ParameterField;
+import io.harness.steps.StepSpecTypeConstants;
+import io.harness.walktree.visitor.SimpleVisitorHelper;
+import io.harness.walktree.visitor.Visitable;
+import io.harness.yaml.YamlSchemaTypes;
+import io.harness.yaml.core.VariableExpression;
+import io.harness.yaml.core.variables.NGVariable;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.TypeAlias;
+
+@Data
+@EqualsAndHashCode
+@JsonTypeName(StepSpecTypeConstants.PLUGIN_STEP)
+@SimpleVisitorHelper(helperClass = PmsPluginStepInfoVisitorHelper.class)
+@TypeAlias("shellScriptStepInfo")
+@OwnedBy(HarnessTeam.CDC)
+@RecasterAlias("io.harness.steps.plugin.PmsPluginStepInfo")
+public class PmsPluginStepInfo implements PMSStepInfo, Visitable, WithDelegateSelector {
+  @VariableExpression(skipVariableExpression = true) List<NGVariable> outputVariables;
+  @YamlSchemaTypes(value = {runtime}) ParameterField<List<TaskSelectorYaml>> delegateSelectors;
+
+  @Builder(builderMethodName = "infoBuilder")
+  public PmsPluginStepInfo() {}
+
+  @Override
+  @JsonIgnore
+  public StepType getStepType() {
+    return StepSpecTypeConstants.PLUGIN_STEP_TYPE;
+  }
+
+  @Override
+  @JsonIgnore
+  public String getFacilitatorType() {
+    return OrchestrationFacilitatorType.TASK_CHAIN;
+  }
+
+  @Override
+  public SpecParameters getSpecParameters() {
+    // todo(abhinav): implement
+    return null;
+  }
+
+  @Override
+  public ParameterField<List<TaskSelectorYaml>> fetchDelegateSelectors() {
+    // todo(abhinav): implement
+    return null;
+  }
+}
