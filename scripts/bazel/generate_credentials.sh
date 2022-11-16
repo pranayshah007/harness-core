fa557f99a5652e65014739862ece5252f241ecc9
Udham Singh <udham.singh@harness.io>
2022-11-16 19:06:05 +0530

    fix: [BT-2376]: Running only for Darwin machines.

--- scripts/bazel/generate_credentials.sh
+++ scripts/bazel/generate_credentials.sh
@@ -5,7 +5,7 @@
 
 scripts/bazel/testDistribute.sh
 
-CACHE_FILE='bazelrc.cache'
+CACHE_FILE='bazelrc.test'
 
 CONTENT="#Remote cache configuration\n
 build --google_credentials=platform-bazel-cache-read.json\n
@@ -14,8 +14,10 @@ build --incompatible_remote_results_ignore_disk=true\n
 build --experimental_guard_against_concurrent_changes\n
 build --incompatible_strict_action_env\n"
 
-echo "$CONTENT" > ${CACHE_FILE}
+if [ `uname -s` = 'Darwin' ]; then
+  echo "$CONTENT" > ${CACHE_FILE}
 
-[ `uname -m` = 'arm64' ] \
-&& echo "build:macos --remote_cache=https://storage.googleapis.com/harness-bazel-cache-blr-dev-m1" >> ${CACHE_FILE} \
-|| echo "build:macos --remote_cache=https://storage.googleapis.com/harness-bazel-cache-blr-dev" >> ${CACHE_FILE}
+  [ `uname -m` = 'arm64' ] \
+  && echo "build:macos --remote_cache=https://storage.googleapis.com/harness-bazel-cache-blr-dev-m1" >> ${CACHE_FILE} \
+  || echo "build:macos --remote_cache=https://storage.googleapis.com/harness-bazel-cache-blr-dev" >> ${CACHE_FILE}
+fi
