efcf785ac569d2a02488aa4cadb20c26affbcb9e
Udham Singh <udham.singh@harness.io>
2022-11-16 18:59:07 +0530

    feat: [BT-2376]: Adding bazel cache file generation script

--- scripts/bazel/generate_credentials.sh
+++ scripts/bazel/generate_credentials.sh
@@ -4,3 +4,18 @@
 # https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
 
 scripts/bazel/testDistribute.sh
+
+CACHE_FILE='bazelrc.cache'
+
+CONTENT="#Remote cache configuration\n
+build --google_credentials=platform-bazel-cache-read.json\n
+build --remote_upload_local_results=false\n
+build --incompatible_remote_results_ignore_disk=true\n
+build --experimental_guard_against_concurrent_changes\n
+build --incompatible_strict_action_env\n"
+
+echo "$CONTENT" > ${CACHE_FILE}
+
+[ `uname -m` = 'arm64' ] \
+&& echo "build:macos --remote_cache=https://storage.googleapis.com/harness-bazel-cache-blr-dev-m1" >> ${CACHE_FILE} \
+|| echo "build:macos --remote_cache=https://storage.googleapis.com/harness-bazel-cache-blr-dev" >> ${CACHE_FILE}
