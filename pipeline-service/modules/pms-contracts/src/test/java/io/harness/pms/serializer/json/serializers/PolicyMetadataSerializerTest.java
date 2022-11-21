0b2b27d1ba7571d01141ad0dc6f54bce8c8bc91d
Vivek Dixit <vivek.dixit@harness.io>
2022-11-21 11:13:51 +0530

    checkstyle

--- pipeline-service/modules/pms-contracts/src/test/java/io/harness/pms/serializer/json/serializers/PolicyMetadataSerializerTest.java
+++ pipeline-service/modules/pms-contracts/src/test/java/io/harness/pms/serializer/json/serializers/PolicyMetadataSerializerTest.java
@@ -3,14 +3,17 @@ package io.harness.pms.serializer.json.serializers;
 import static io.harness.rule.OwnerRule.VIVEK_DIXIT;
 
 import io.harness.CategoryTest;
+import io.harness.category.element.UnitTests;
 import io.harness.rule.Owner;
 
 import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 public class PolicyMetadataSerializerTest extends CategoryTest {
   @Test
   @Owner(developers = VIVEK_DIXIT)
+  @Category(UnitTests.class)
   public void testConstructor() {
     PolicyMetadataSerializerTest policyMetadataSerializerTest = new PolicyMetadataSerializerTest();
     AssertionsForClassTypes.assertThat(policyMetadataSerializerTest).isNotNull();
--- pipeline-service/modules/pms-contracts/src/test/java/io/harness/pms/serializer/json/serializers/PolicySetMetadataSerializerTest.java
+++ pipeline-service/modules/pms-contracts/src/test/java/io/harness/pms/serializer/json/serializers/PolicySetMetadataSerializerTest.java
@@ -5,15 +5,18 @@ import static io.harness.rule.OwnerRule.VIVEK_DIXIT;
 
 import io.harness.CategoryTest;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.category.element.UnitTests;
 import io.harness.rule.Owner;
 
 import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 @OwnedBy(PIPELINE)
 public class PolicySetMetadataSerializerTest extends CategoryTest {
   @Test
   @Owner(developers = VIVEK_DIXIT)
+  @Category(UnitTests.class)
   public void testConstructor() {
     PolicySetMetadataSerializer policySetMetadataSerializer = new PolicySetMetadataSerializer();
     AssertionsForClassTypes.assertThat(policySetMetadataSerializer).isNotNull();
