b027fd7e328427d5331d944c79ac9847402ff652
Sourabh Awashti <sourabh.awashti@harness.io>
2022-11-24 12:40:40 +0530

    infra changes

--- 127-cd-nextgen-entities/src/main/java/io/harness/ng/core/infrastructure/InfrastructureKind.java
+++ 127-cd-nextgen-entities/src/main/java/io/harness/ng/core/infrastructure/InfrastructureKind.java
@@ -24,5 +24,5 @@ public interface InfrastructureKind {
   String GITOPS = "GitOps";
   String CUSTOM_DEPLOYMENT = "CustomDeployment";
   String ELASTIGROUP = "Elastigroup";
-  String TAS = "TanzuApplicationService";
+  String TAS = "TAS";
 }
