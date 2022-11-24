71db3c5277315c07ba6b46874f14db7089e9a606
Abhinav Kumar Singh <abhinav.singh3@harness.io>
2022-11-24 12:35:55 +0530

    [fix]:[CCM-9492]: Added index in instanceData collection

--- 490-ce-commons/src/main/java/io/harness/ccm/commons/entities/batch/InstanceData.java
+++ 490-ce-commons/src/main/java/io/harness/ccm/commons/entities/batch/InstanceData.java
@@ -97,6 +97,14 @@ public final class InstanceData implements PersistentEntity, UuidAware, CreatedA
                  .field(InstanceDataKeys.usageStartTime)
                  .field(InstanceDataKeys.instanceType)
                  .build())
+        .add(CompoundMongoIndex.builder()
+                 .name("accountId_clusterId_activeInstanceIterator_usageStartTime_instanceType")
+                 .field(InstanceDataKeys.accountId)
+                 .field(InstanceDataKeys.clusterId)
+                 .field(InstanceDataKeys.activeInstanceIterator)
+                 .field(InstanceDataKeys.usageStartTime)
+                 .field(InstanceDataKeys.instanceType)
+                 .build())
         .build();
   }
 
