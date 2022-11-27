f86677ec63f8809470af66a66e3d3e09d8dbc9be
vivekkarnatiharness <vivek.karnati@harness.io>
2022-11-27 20:37:38 +0530

    [cds-42900]: fixed error message

--- 127-cd-nextgen-entities/src/main/java/io/harness/repositories/service/custom/ServiceRepositoryCustomImpl.java
+++ 127-cd-nextgen-entities/src/main/java/io/harness/repositories/service/custom/ServiceRepositoryCustomImpl.java
@@ -28,6 +28,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Collation;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
@@ -44,6 +45,7 @@ public class ServiceRepositoryCustomImpl implements ServiceRepositoryCustom {
   @Override
   public Page<ServiceEntity> findAll(Criteria criteria, Pageable pageable) {
     Query query = new Query(criteria).with(pageable);
+    query.collation(Collation.of("en").strength(Collation.ComparisonLevel.secondary()));
     List<ServiceEntity> projects = mongoTemplate.find(query, ServiceEntity.class);
     return PageableExecutionUtils.getPage(
         projects, pageable, () -> mongoTemplate.count(Query.of(query).limit(-1).skip(-1), ServiceEntity.class));
