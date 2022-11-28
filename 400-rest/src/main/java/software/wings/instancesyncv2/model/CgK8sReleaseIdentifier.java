a542f5ed5db13d6b30a7a458ceaeb6b68b0b6126
Naman Talaycha <naman.talaycha@harness.io>
2022-11-28 15:08:38 +0530

    Revert "feat: [CDS-46240]: Manager changes: K8s & Native Helm (CG - Direct Kubernetes Cluster) Instance sync V2 implementation Revisit"
    This reverts commit d937ddd62996a939953f8c2c3c42be1876fa8bc1.

--- 400-rest/src/main/java/software/wings/instancesyncv2/model/CgK8sReleaseIdentifier.java
+++ 400-rest/src/main/java/software/wings/instancesyncv2/model/CgK8sReleaseIdentifier.java
@@ -5,13 +5,6 @@
  * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
  */
 
-/*
- Copyright  Harness Inc. All rights reserved.
- Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
- that can be found in the licenses directory at the root of this repository, also available at
- https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
- */
-
 package software.wings.instancesyncv2.model;
 
 import static io.harness.annotations.dev.HarnessTeam.CDP;
@@ -19,12 +12,13 @@ import static io.harness.annotations.dev.HarnessTeam.CDP;
 import io.harness.annotations.dev.OwnedBy;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
 
 @Data
 @Builder
