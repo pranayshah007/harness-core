df62765745aa63afc59301377a878775da7e5336
Abhishek <abhishek.abhishek@harness.io>
2022-11-28 16:22:33 +0530

    equalAndHashCode annotation

--- 120-ng-manager/src/main/java/io/harness/ng/overview/dto/InstanceGroupedByServiceList.java
+++ 120-ng-manager/src/main/java/io/harness/ng/overview/dto/InstanceGroupedByServiceList.java
@@ -8,11 +8,7 @@
 package io.harness.ng.overview.dto;
 
 import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.Value;
+import lombok.*;
 
 @Value
 @Builder
@@ -58,42 +54,11 @@ public class InstanceGroupedByServiceList {
   @Getter
   @Setter
   @AllArgsConstructor
+  @EqualsAndHashCode
   public static class InstanceGroupedByPipelineExecution {
     Integer count;
     String lastPipelineExecutionId;
     String lastPipelineExecutionName;
     Long lastDeployedAt;
-
-    @Override
-    public boolean equals(Object obj) {
-      if (this == obj)
-        return true;
-      if (obj == null)
-        return false;
-      if (getClass() != obj.getClass())
-        return false;
-      InstanceGroupedByPipelineExecution other = (InstanceGroupedByPipelineExecution) obj;
-      if (count == null) {
-        if (other.count != null)
-          return false;
-      } else if (!count.equals(other.count))
-        return false;
-      if (lastPipelineExecutionId == null) {
-        if (other.lastPipelineExecutionId != null)
-          return false;
-      } else if (!lastPipelineExecutionId.equals(other.lastPipelineExecutionId))
-        return false;
-      if (lastPipelineExecutionName == null) {
-        if (other.lastPipelineExecutionName != null)
-          return false;
-      } else if (!lastPipelineExecutionName.equals(other.lastPipelineExecutionName))
-        return false;
-      if (lastDeployedAt == null) {
-        if (other.lastDeployedAt != null)
-          return false;
-      } else if (!lastDeployedAt.equals(other.lastDeployedAt))
-        return false;
-      return true;
-    }
   }
 }
