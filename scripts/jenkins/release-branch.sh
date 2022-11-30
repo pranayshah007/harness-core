bb63fe815219300714c5aebec063aa0c2ac40bba
devesh-harness <devesh.mishra@harness.io>
2022-11-30 16:23:30 +0530

    Fix: [BT-2485]: Changes for JIRA TAGGING

--- scripts/jenkins/release-branch.sh
+++ scripts/jenkins/release-branch.sh
@@ -123,7 +123,7 @@ check_branch_name "develop"
 
 export VERSION_FILE=build.properties
 
-#export VERSION=`cat ${VERSION_FILE} | grep 'build.number=' | sed -e 's: *build.number=::g'`
+export VERSION=`cat ${VERSION_FILE} | grep 'build.number=' | sed -e 's: *build.number=::g'`
 DV=`cat ${VERSION_FILE} | grep 'delegate.version=' | sed -e 's: *delegate.version=::g'`
 
 YEAR=$(date +%y)
@@ -141,10 +141,10 @@ export NEW_VERSION=$(( ${VERSION}+1 ))
 sed -i "s:build.number=${VERSION}00:build.number=${NEW_VERSION}00:g" ${VERSION_FILE}
 sed -i "s#${DV}#${YEAR}.${MONTH}.${NEWDELEGATEVERSION}#g" ${VERSION_FILE}
 
-#git add ${VERSION_FILE}
-#git commit -m "Branching to release/${PURPOSE}/${VERSION}xx. New version ${NEW_VERSION}xx"
-#git push origin develop
-#print_err "$?" "Pushing build.properties to develop branch failed"
+git add ${VERSION_FILE}
+git commit -m "Branching to release/${PURPOSE}/${VERSION}xx. New version ${NEW_VERSION}xx"
+git push origin develop
+print_err "$?" "Pushing build.properties to develop branch failed"
 
 # Update jira issues
 echo "STEP4: INFO: Update jira issues"
