6a4b201213622c720b7cab32b9d8e8b4dcd02046
Vikyath Harekal <vikyath.harekal@harness.io>
2022-12-01 09:08:05 +0530

    fix: [CDS-45601]: revert all instance stats collector changes for now - QA HF (#40870)
    * Revert "fix: [CDS-47225]: populateInstanceStatsData - Add empty string instead of null (#40656)"
    
    This reverts commit aa1bf83ad2034424ab629be22b4e12480b384047.
    
    * Revert "feat: [CDS-45843]: Limit instance stats backlog filling (#40596)"
    
    This reverts commit 864a3144605b9d4426dc43ece52544e94d2450d2.
    
    * Revert "fix: [CDS-45601]: fix publish instance stats log (#40364)"
    
    This reverts commit eefc2494882b3b6b3b4c192cbed10915429a29ab.
    
    * Revert "fix: [CDS-45601]: InstanceStatsCollector - Do not fetch all active instances (#40075)"
    
    This reverts commit 95ed2df298e4bc3e4511cb94754f95da44eba74f.
    
    * fix: [CDS-46488]: Remove validation of env group with deploy to all as true (#40858)
    
    * Remove validation of env group with deploy to all as true
    
    * Remove validation of env group with deploy to all as true
    
    * Remove validation of env group with deploy to all as true
    
    * Unused imports
    
    * Unused imports
    
    * fix: [CDS-45601]: build.number++
    
    Co-authored-by: Yogesh Chauhan <yogesh.chauhan@harness.io>

--- 126-instance/src/main/java/io/harness/service/instance/InstanceService.java
+++ 126-instance/src/main/java/io/harness/service/instance/InstanceService.java
@@ -14,8 +14,6 @@ import io.harness.models.ActiveServiceInstanceInfo;
 import io.harness.models.CountByServiceIdAndEnvType;
 import io.harness.models.EnvBuildInstanceCount;
 import io.harness.models.InstancesByBuildId;
-import io.harness.ng.core.entities.Project;
-import io.harness.service.stats.model.InstanceCountByServiceAndEnv;
 
 import java.util.List;
 import java.util.Optional;
@@ -41,7 +39,8 @@ public interface InstanceService {
 
   Optional<InstanceDTO> findAndReplace(InstanceDTO instanceDTO);
 
-  List<InstanceCountByServiceAndEnv> getActiveInstancesByServiceAndEnv(Project project, long timestamp);
+  List<InstanceDTO> getActiveInstancesByAccountOrgProjectAndService(String accountIdentifier, String orgIdentifier,
+      String projectIdentifier, String serviceIdentifier, long timestamp);
 
   List<InstanceDTO> getActiveInstances(
       String accountIdentifier, String orgIdentifier, String projectIdentifier, long timestampInMs);
