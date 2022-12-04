884b1d283133b592cd8c6365e74f452863e76ed4
Dhruv <dhruv@harness.io>
2022-12-04 15:28:14 +0530

    techdebt: [SRM-12762]: Adding build and release scripts.

--- srm-service/release/release-branch.sh
+++ srm-service/release/release-branch.sh
@@ -43,33 +43,32 @@ function check_file_present(){
      fi
 }
 
+echo "INFO: Step 0: Setting values for env variables PURPOSE and STATUS_ID_TO_MOVE."
 export PURPOSE=srm
 export STATUS_ID_TO_MOVE=151
-
+echo "INFO: Step 1: Setting git config."
 git config --global user.email "bot@harness.io"
 git config --global user.name "bot-harness"
-
+echo "INFO: Step 2: Setting git remote url."
 git remote set-url origin https://${DRONE_NETRC_USERNAME}:${BOT_PASSWORD}@github.com/harness/harness-core.git
-
+echo "INFO: Step 3: Fetching git repo."
 git fetch --unshallow
+echo "INFO: Step 4: Fetching all git branches."
 git fetch --all
-
-set -ex
-
+echo "INFO: Step 5: Setting shell condition - exit on error TRUE."
+set -e
+echo "INFO: Step 6: Setting shell condition - debug TRUE."
+set -x
+echo "INFO: Step 7: Fetching $BASE_BRANCH"
 git fetch origin refs/heads/${BASE_BRANCH}; git checkout ${BASE_BRANCH} && git branch
+echo "INFO: Step 8: Checking $BASE_BRANCH and current branch are the same"
 check_branch_name "${BASE_BRANCH}"
 
-# Check for not merged hot fixes
-echo "STEP1: INFO: Checking for Not Merged Hot Fixes in Master."
+echo "INFO: Step 9: Checking for Not Merged Hot Fixes in $BASE_BRANCH."
+git log --remotes=origin/release/${PURPOSE}/* --pretty=oneline --abbrev-commit | grep -iE "\[SRM-[0-9]+]:" -o | sort | uniq > release.txt
+git log --remotes=origin/${BASE_BRANCH} --pretty=oneline --abbrev-commit | grep -iE "\[(SRM-[0-9]+]:" -o | sort | uniq > develop.txt
 
-PROJFILE="jira-projects.txt"
-check_file_present $PROJFILE
-PROJECTS=$(<$PROJFILE)
-
-git log --remotes=origin/release/${PURPOSE}/* --pretty=oneline --abbrev-commit | grep -iE "\[(${PROJECTS})-[0-9]+]:" -o | sort | uniq > release.txt
-git log --remotes=origin/${BASE_BRANCH} --pretty=oneline --abbrev-commit | grep -iE "\[(${PROJECTS})-[0-9]+]:" -o | sort | uniq > develop.txt
-
-NOT_MERGED=`comm -23 release.txt develop.txt`
+NOT_MERGED=$(comm -23 release.txt develop.txt)
 
 if [ ! -z "$NOT_MERGED" ]
 then
@@ -77,38 +76,27 @@ then
     exit 1
 fi
 
-# Bumping version in build.properties in develop branch.
-echo "STEP2: INFO: Bumping version in build.properties in ${BASE_BRANCH} branch."
-
-export SHA=`git rev-parse HEAD`
+echo "INFO: Step 10: Bumping version in build.properties in ${BASE_BRANCH} branch."
+export SHA=$(git rev-parse HEAD)
 export VERSION_FILE=srm-service/build.properties
-
-export VERSION=`cat ${VERSION_FILE} | grep 'build.number=' | sed -e 's: *build.number=::g'`
+export VERSION=$(cat ${VERSION_FILE} | grep 'build.number=' | sed -e 's: *build.number=::g')
 export VERSION=${VERSION%??}
 export NEW_VERSION=$(( ${VERSION}+1 ))
-
 sed -i "s:build.number=${VERSION}00:build.number=${NEW_VERSION}00:g" ${VERSION_FILE}
+git add ${VERSION_FILE}
+git commit -m "Branching to release/${PURPOSE}/${VERSION}xx. New version ${NEW_VERSION}xx"
+git push origin ${BASE_BRANCH}
+print_err "$?" "Pushing build.properties to ${BASE_BRANCH} branch failed"
 
-#TODO: Uncomment
-#git add ${VERSION_FILE}
-#git commit -m "Branching to release/${PURPOSE}/${VERSION}xx. New version ${NEW_VERSION}xx"
-#git push origin ${BASE_BRANCH}
-#print_err "$?" "Pushing build.properties to ${BASE_BRANCH} branch failed"
-
-
-echo "STEP3: INFO: Creating a release branch for ${PURPOSE}"
-
+echo "INFO: Step 11: Creating a release branch for ${PURPOSE}."
 git checkout ${SHA}
 git checkout -b release/${PURPOSE}/${VERSION}xx
-
 sed -i "s:build.number=???00:build.number=${VERSION}00:g" ${VERSION_FILE}
-
 git add ${VERSION_FILE}
 git commit --allow-empty -m "Set the proper version branch release/${PURPOSE}/${VERSION}xx"
 git push origin release/${PURPOSE}/${VERSION}xx
 
-#creating the fix version
-#TODO: Uncomment
+echo "INFO: Step 12: Creating the fix version."
 #chmod +x srm-service/release/release-branch-create-srm-versions.sh
 #srm-service/release/release-branch-create-srm-versions.sh
 #
