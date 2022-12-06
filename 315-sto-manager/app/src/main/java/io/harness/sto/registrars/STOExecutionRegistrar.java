d99c75301780cd07b87834e59e3186c18b8c04ca
Sergey Bobrov <sergey.bobrov@harness.io>
2022-12-06 15:50:25 -0600

    fix: [STO-5294]: Try sync init step

--- 315-sto-manager/app/src/main/java/io/harness/sto/registrars/STOExecutionRegistrar.java
+++ 315-sto-manager/app/src/main/java/io/harness/sto/registrars/STOExecutionRegistrar.java
@@ -16,7 +16,6 @@ import io.harness.ci.states.RunStep;
 import io.harness.ci.states.STOSpecStep;
 import io.harness.ci.states.SecurityStageStepPMS;
 import io.harness.ci.states.SecurityStep;
-import io.harness.ci.states.V1.InitializeTaskStepV2;
 import io.harness.ci.states.codebase.CodeBaseStep;
 import io.harness.ci.states.codebase.CodeBaseTaskStep;
 import io.harness.pms.contracts.steps.StepType;
@@ -40,7 +39,7 @@ public class STOExecutionRegistrar {
   public static Map<StepType, Class<? extends Step>> getEngineSteps() {
     Map<StepType, Class<? extends Step>> engineSteps = new HashMap<>();
 
-    engineSteps.put(InitializeTaskStep.STEP_TYPE, InitializeTaskStepV2.class);
+    engineSteps.put(InitializeTaskStep.STEP_TYPE, InitializeTaskStep.class);
     engineSteps.put(CleanupStep.STEP_TYPE, CleanupStep.class);
     engineSteps.put(RunStep.STEP_TYPE, RunStep.class);
     engineSteps.put(GitCloneStep.STEP_TYPE, GitCloneStep.class);
