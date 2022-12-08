11e602ed37f150640ebedb5664a327c3250b2802
Rohit Karelia <rohit.karelia@harness.io>
2022-12-08 20:54:56 +0530

    fix: [CDS-47981]: Fix issue with MatchAll filter, remove UUID  (#41304)
    * fix: [CDS-47981]: Fix issue with MatchAll filter, remove UUID (#41264)
    
    (cherry picked from commit 7d687f6f3bdb21c5ecf329f54451665de1f91320)
    
    * fix: [CDS-47524]: Fix for DeploymentStagePMSPlanCreatorV2 AdvisorObtainments in case of Multi Deployments
    
    (cherry picked from commit 98266e1bff7308377ec2b5711e624f94870bea8a)
    
    * feat: [CDS-47981]: Update build.properties
    
    * fix: [CDS-48000]: fix for duplicate log keys
    
    (cherry picked from commit 499a13bf05d0cd149b241d9f89c918f014f39597)
    
    * fix: [CDS-47781]: Update a change
    
    * fix: [CDS-47781]: Update build.properties
    
    Co-authored-by: Yogesh Chauhan <yogesh.chauhan@harness.io>
    Co-authored-by: Vaibhav Singhal <vaibhav.si@harness.io>

--- 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/stage/DeploymentStagePMSPlanCreatorV2.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/creator/plan/stage/DeploymentStagePMSPlanCreatorV2.java
@@ -195,9 +195,7 @@ public class DeploymentStagePMSPlanCreatorV2 extends AbstractStagePlanCreator<De
     stageParameters.specConfig(getSpecParameters(specField.getNode().getUuid(), ctx, stageNode));
     String uuid = MultiDeploymentSpawnerUtils.getUuidForMultiDeployment(stageNode);
     List<AdviserObtainment> adviserObtainments = new ArrayList<>();
-    DeploymentStageConfig stageConfig = stageNode.getDeploymentStageConfig();
-    if (stageConfig.getServices() == null && stageConfig.getEnvironments() == null
-        && stageConfig.getEnvironmentGroup() == null) {
+    if (!MultiDeploymentSpawnerUtils.hasMultiDeploymentConfigured(stageNode)) {
       adviserObtainments = getAdviserObtainmentFromMetaData(ctx.getCurrentField());
     }
     // We need to swap the ids if strategy is present
@@ -224,12 +222,7 @@ public class DeploymentStagePMSPlanCreatorV2 extends AbstractStagePlanCreator<De
   }
 
   public String getIdentifierWithExpression(PlanCreationContext ctx, DeploymentStageNode node, String identifier) {
-    if (node.getDeploymentStageConfig().getGitOpsEnabled()) {
-      return StrategyUtils.getIdentifierWithExpression(ctx, identifier);
-    }
-    if (node.getDeploymentStageConfig().getServices() != null
-        || node.getDeploymentStageConfig().getEnvironments() != null
-        || node.getDeploymentStageConfig().getEnvironmentGroup() != null) {
+    if (MultiDeploymentSpawnerUtils.hasMultiDeploymentConfigured(node)) {
       return identifier + StrategyValidationUtils.STRATEGY_IDENTIFIER_POSTFIX;
     }
     return StrategyUtils.getIdentifierWithExpression(ctx, identifier);
@@ -571,7 +564,6 @@ public class DeploymentStagePMSPlanCreatorV2 extends AbstractStagePlanCreator<De
       List<EnvironmentYamlV2> envV2YamlsWithFilters) {
     List<EnvironmentYamlV2> filteredInfraList = new ArrayList<>();
     for (EnvironmentYamlV2 envYamlV2 : envV2YamlsWithFilters) {
-      // if (ParameterField.isNotNull(envYamlV2.getInfrastructureDefinitions())) {
       Set<InfrastructureEntity> infrastructureEntitySet =
           environmentInfraFilterHelper.getInfrastructureForEnvironmentList(
               accountIdentifier, orgIdentifier, projectIdentifier, envYamlV2.getEnvironmentRef().getValue());
@@ -579,7 +571,6 @@ public class DeploymentStagePMSPlanCreatorV2 extends AbstractStagePlanCreator<De
       filteredInfraList = filterInfras(
           envYamlV2.getFilters().getValue(), envYamlV2.getEnvironmentRef().getValue(), infrastructureEntitySet);
       individualEnvironmentYamlV2.addAll(filteredInfraList);
-      //}
     }
     return filteredInfraList;
   }
