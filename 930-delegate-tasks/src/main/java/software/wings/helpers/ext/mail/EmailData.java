11589d22cd294c02a15a872b2f1c38e25ead99e0
Kshitij Rastogi <kshitij.rastogi@harness.io>
2022-12-08 22:06:42 +0530

    Adding EmailDataDTO

---
+++ 930-delegate-tasks/src/main/java/software/wings/helpers/ext/mail/EmailData.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
+package software.wings.helpers.ext.mail;
+
+import static io.harness.annotations.dev.HarnessTeam.DEL;
+
+import io.harness.annotations.dev.HarnessModule;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.annotations.dev.TargetModule;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@TargetModule(HarnessModule._959_COMMON_ENTITIES)
+@OwnedBy(DEL)
+public class EmailData {
+  private String accountId;
+  @Builder.Default private List<String> to = new ArrayList<>();
+  @Builder.Default private List<String> cc = new ArrayList<>();
+  @Builder.Default private List<String> bcc = new ArrayList<>();
+  private String subject;
+  private String body;
+  private String templateName;
+  private Object templateModel;
+  @Builder.Default private boolean hasHtml = true;
+  private boolean system;
+  private String appId;
+  private String workflowExecutionId;
+}
