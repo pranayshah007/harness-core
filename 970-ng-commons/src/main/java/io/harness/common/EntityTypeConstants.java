1def5294c3d40d24d0045e84d1073c7b0ed0173d
Arvind Choudhary <arvind.choudhary@harness.io>
2022-12-13 12:56:34 +0530

    [CDS-47006]: Updated with container fields

--- 970-ng-commons/src/main/java/io/harness/common/EntityTypeConstants.java
+++ 970-ng-commons/src/main/java/io/harness/common/EntityTypeConstants.java
@@ -179,6 +179,7 @@ public interface EntityTypeConstants {
   String TERRAGRUNT_APPLY = "TerragruntApply";
   String TERRAGRUNT_DESTROY = "TerragruntDestroy";
   String TERRAGRUNT_ROLLBACK = "TerragruntRollback";
+  String CONTAINER_STEP = "Container";
   String ELASTIGROUP_BG_STAGE_SETUP = "ElastigroupBGStageSetup";
   String ELASTIGROUP_SWAP_ROUTE = "ElastigroupSwapRoute";
 }
