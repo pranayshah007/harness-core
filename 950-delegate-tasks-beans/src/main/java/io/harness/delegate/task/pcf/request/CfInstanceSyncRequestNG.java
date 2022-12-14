d0e50a4dc739a5693500a98615fe0ea3807c5a56
Anil Chowdhury <anil.chowdhury@harness.io>
2022-12-15 01:04:55 +0530

    dto changes for instance sync

--- 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/pcf/request/CfInstanceSyncRequestNG.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/pcf/request/CfInstanceSyncRequestNG.java
@@ -6,7 +6,8 @@
  */
 package io.harness.delegate.task.pcf.request;
 
-import io.harness.annotations.dev.HarnessTeam;
+import static io.harness.annotations.dev.HarnessTeam.CDP;
+
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.delegate.beans.logstreaming.CommandUnitsProgress;
 import io.harness.delegate.task.pcf.CfCommandTypeNG;
@@ -14,18 +15,23 @@ import io.harness.delegate.task.pcf.response.TasInfraConfig;
 import io.harness.pcf.model.CfCliVersion;
 
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-@Value
-@Builder
-@OwnedBy(HarnessTeam.CDP)
-public class CfInstanceSyncRequestNG implements CfCommandRequestNG {
-  String accountId;
-  CfCommandTypeNG cfCommandTypeNG;
-  CfCliVersion cfCliVersion;
-  String commandName;
-  CommandUnitsProgress commandUnitsProgress;
-  TasInfraConfig tasInfraConfig;
-  Integer timeoutIntervalInMin;
+@Data
+@OwnedBy(CDP)
+@EqualsAndHashCode(callSuper = true)
+public class CfInstanceSyncRequestNG extends AbstractTasTaskRequest {
   String applicationName;
+
+  @Builder
+  public CfInstanceSyncRequestNG(String accountId, CfCommandTypeNG cfCommandTypeNG, String commandName,
+      CommandUnitsProgress commandUnitsProgress, TasInfraConfig tasInfraConfig, boolean useCfCLI,
+      CfCliVersion cfCliVersion, Integer timeoutIntervalInMin,
+
+      String applicationName) {
+    super(timeoutIntervalInMin, accountId, commandName, cfCommandTypeNG, commandUnitsProgress, tasInfraConfig, useCfCLI,
+        cfCliVersion);
+    this.applicationName = applicationName;
+  }
 }
