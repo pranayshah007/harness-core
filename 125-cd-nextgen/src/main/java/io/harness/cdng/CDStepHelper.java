0c58614c9dec37b80dc57d8e13c41c33b454bf6f
Pratyush Goel <goel.pratyush@harness.io>
2022-12-21 00:48:16 +0530

    fix: [CDS-48995]: removed the single file condition from k8s manifest for git subset store for values override and added support for azure project type connector to fetch manifest files.

--- 125-cd-nextgen/src/main/java/io/harness/cdng/CDStepHelper.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/CDStepHelper.java
@@ -79,7 +79,6 @@ import io.harness.delegate.beans.connector.scm.ScmConnector;
 import io.harness.delegate.beans.connector.scm.adapter.ScmConnectorMapper;
 import io.harness.delegate.beans.connector.scm.azurerepo.AzureRepoApiAccessDTO;
 import io.harness.delegate.beans.connector.scm.azurerepo.AzureRepoApiAccessType;
-import io.harness.delegate.beans.connector.scm.azurerepo.AzureRepoConnectionTypeDTO;
 import io.harness.delegate.beans.connector.scm.azurerepo.AzureRepoConnectorDTO;
 import io.harness.delegate.beans.connector.scm.azurerepo.AzureRepoHttpAuthenticationType;
 import io.harness.delegate.beans.connector.scm.azurerepo.AzureRepoHttpCredentialsDTO;
