8c505d3978f313b2d4fa90e9a555fe38a674f1ac
Nishant Saini <nishant.saini@harness.io>
2022-12-28 22:09:08 +0530

    feat: [PL-30349]: Moved code to correct service class

--- audit-event-streaming/service/src/main/java/io/harness/auditevent/streaming/services/StreamingBatchService.java
+++ audit-event-streaming/service/src/main/java/io/harness/auditevent/streaming/services/StreamingBatchService.java
@@ -7,6 +7,7 @@
 
 package io.harness.auditevent.streaming.services;
 
+import io.harness.audit.entities.streaming.StreamingDestination;
 import io.harness.auditevent.streaming.entities.BatchStatus;
 import io.harness.auditevent.streaming.entities.StreamingBatch;
 
@@ -16,6 +17,20 @@ import java.util.Optional;
 public interface StreamingBatchService {
   Optional<StreamingBatch> get(
       String accountIdentifier, String streamingDestinationIdentifier, List<BatchStatus> batchStatusList);
+
   Optional<StreamingBatch> getLatest(String accountIdentifier, String streamingDestinationIdentifier);
+
   StreamingBatch update(String accountIdentifier, StreamingBatch streamingBatch);
+
+  /**
+   * Returns the last streaming batch for the given streamingDestination. If no streaming is batch found then it creates
+   * a new batch and returns it. If last batch was a {@link BatchStatus#SUCCESS SUCCESS} then it creates next batch
+   * based on previous batch and returns it. If last batch {@link BatchStatus#FAILED FAILED} then it increments the
+   * retry count and return the batch.
+   * @param streamingDestination {@link StreamingDestination StreamingDestination} for which batch is to be returned
+   * @param timestamp Uses this timestamp as {@link StreamingBatch#setEndTime(Long) endTime} when creating a new {@link
+   *     StreamingBatch StreamingBatch}
+   * @return {@link StreamingBatch StreamingBatch}
+   */
+  StreamingBatch getLastStreamingBatch(StreamingDestination streamingDestination, Long timestamp);
 }
--- audit-event-streaming/service/src/main/java/io/harness/auditevent/streaming/services/impl/StreamingBatchServiceImpl.java
+++ audit-event-streaming/service/src/main/java/io/harness/auditevent/streaming/services/impl/StreamingBatchServiceImpl.java
@@ -7,15 +7,21 @@
 
 package io.harness.auditevent.streaming.services.impl;
 
+import static io.harness.auditevent.streaming.entities.BatchStatus.IN_PROGRESS;
+import static io.harness.auditevent.streaming.entities.BatchStatus.READY;
+
+import io.harness.audit.entities.streaming.StreamingDestination;
 import io.harness.auditevent.streaming.entities.BatchStatus;
 import io.harness.auditevent.streaming.entities.StreamingBatch;
 import io.harness.auditevent.streaming.entities.StreamingBatch.StreamingBatchKeys;
 import io.harness.auditevent.streaming.repositories.StreamingBatchRepository;
 import io.harness.auditevent.streaming.services.StreamingBatchService;
 import io.harness.exception.InvalidRequestException;
+import io.harness.exception.UnknownEnumTypeException;
 
 import java.util.List;
 import java.util.Optional;
+import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -55,5 +61,52 @@ public class StreamingBatchServiceImpl implements StreamingBatchService {
               accountIdentifier, streamingBatch.getAccountIdentifier(), streamingBatch.getId()));
     }
     return streamingBatchRepository.save(streamingBatch);
+  }
+
+  @Override
+  public StreamingBatch getLastStreamingBatch(StreamingDestination streamingDestination, Long timestamp) {
+    Optional<StreamingBatch> lastStreamingBatch =
+        getLatest(streamingDestination.getAccountIdentifier(), streamingDestination.getIdentifier());
+    if (lastStreamingBatch.map(batch -> batch.getStatus().equals(IN_PROGRESS)).orElse(true)) {
+      return createInitialBatch(streamingDestination, timestamp);
+    }
+    @NotNull BatchStatus status = lastStreamingBatch.get().getStatus();
+    StreamingBatch streamingBatch;
+    switch (status) {
+      case READY:
+      case IN_PROGRESS:
+        streamingBatch = lastStreamingBatch.get();
+        break;
+      case SUCCESS:
+        streamingBatch = createNextBatch(lastStreamingBatch.get(), streamingDestination, timestamp);
+        break;
+      default:
+        throw new UnknownEnumTypeException("batch status", status.name());
+    }
+    return streamingBatch;
+  }
+  private StreamingBatch createInitialBatch(StreamingDestination streamingDestination, Long timestamp) {
+    StreamingBatch streamingBatch = newBatchBuilder(streamingDestination, timestamp)
+                                        .startTime(streamingDestination.getLastStatusChangedAt())
+                                        .build();
+    // TODO: Add total number of records
+    return update(streamingDestination.getAccountIdentifier(), streamingBatch);
+  }
+
+  private StreamingBatch createNextBatch(
+      StreamingBatch previousStreamingBatch, StreamingDestination streamingDestination, Long timestamp) {
+    StreamingBatch streamingBatch =
+        newBatchBuilder(streamingDestination, timestamp).startTime(previousStreamingBatch.getEndTime()).build();
+    // TODO: Add total number of records
+    return update(streamingDestination.getAccountIdentifier(), streamingBatch);
+  }
+
+  private StreamingBatch.StreamingBatchBuilder newBatchBuilder(
+      StreamingDestination streamingDestination, Long timestamp) {
+    return StreamingBatch.builder()
+        .accountIdentifier(streamingDestination.getAccountIdentifier())
+        .streamingDestinationIdentifier(streamingDestination.getIdentifier())
+        .status(READY)
+        .endTime(timestamp);
   }
 }
