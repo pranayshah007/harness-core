325764045933143efdbbe4a96f624b18286d9827
Jimit Gandhi <jimit.gandhi@harness.io>
2022-12-28 19:02:41 +0530

    [feat]: [PL-28848]: Changing previous UserRoleAssignmentRemovalMigration run to NoopMigration.

--- access-control/service/src/main/java/io/harness/accesscontrol/commons/migration/AccessControlMongoBackgroundMigrationDetails.java
+++ access-control/service/src/main/java/io/harness/accesscontrol/commons/migration/AccessControlMongoBackgroundMigrationDetails.java
@@ -55,7 +55,7 @@ public class AccessControlMongoBackgroundMigrationDetails implements MigrationDe
         .add(Pair.of(11, NoopMigration.class))
         .add(Pair.of(12, ACLAddBooleanFieldsMigration.class))
         .add(Pair.of(13, NoopMigration.class))
-        .add(Pair.of(14, UserRoleAssignmentRemovalMigration.class))
+        .add(Pair.of(14, NoopMigration.class))
         .add(Pair.of(15, NoopMigration.class))
         .add(Pair.of(16, AccountBasicRoleAssignmentInternalFieldMigration.class))
         .add(Pair.of(17, UserRoleAssignmentRemovalMigration.class))
