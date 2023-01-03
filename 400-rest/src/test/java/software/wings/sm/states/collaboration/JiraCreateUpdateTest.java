8f98b6bdf605eaa8901c190d05a4cb4e8f5e7d56
Lucas Sales <lucas.sales@harness.io>
2023-01-03 11:59:57 -0300

    fixing file extension

--- 400-rest/src/test/java/software/wings/sm/states/collaboration/JiraCreateUpdateTest.java
+++ 400-rest/src/test/java/software/wings/sm/states/collaboration/JiraCreateUpdateTest.java
@@ -118,7 +118,7 @@ public class JiraCreateUpdateTest extends WingsBaseTest {
   @BeforeClass
   public static void setup() throws IOException {
     JSONObject jsonObject =
-        new ObjectMapper().readValue(new File("400-rest/src/test/resources/mock_create_meta"), JSONObject.class);
+        new ObjectMapper().readValue(new File("400-rest/src/test/resources/mock_create_meta.json"), JSONObject.class);
     createMetaResponse = new JiraCreateMetaResponse(jsonObject);
     projects = new ObjectMapper().readValue(new File("400-rest/src/test/resources/mock_projects"), JSONArray.class);
     statuses = new ObjectMapper().readValue(new File("400-rest/src/test/resources/mock_statuses"), JSONArray.class);
