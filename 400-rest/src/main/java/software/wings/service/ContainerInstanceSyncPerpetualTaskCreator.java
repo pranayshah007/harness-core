fbd79b50766afae37a2e2dedb18565880e750f4e
Alexandru Bosii <alexandru.bosii@harness.io>
2023-01-05 13:37:43 +0200

    feat: [CDS-49868]: Backup deleted PTs after enabling Instance sync v2

--- 400-rest/src/main/java/software/wings/service/ContainerInstanceSyncPerpetualTaskCreator.java
+++ 400-rest/src/main/java/software/wings/service/ContainerInstanceSyncPerpetualTaskCreator.java
@@ -61,6 +61,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.AccessLevel;
@@ -162,6 +163,40 @@ public class ContainerInstanceSyncPerpetualTaskCreator extends AbstractInstanceS
         Sets.difference(newDeploymentContainersMetadata, existingContainersMetadata);
 
     return createPerpetualTasks(containersMetadataToExamine, infrastructureMapping);
+  }
+
+  @Override
+  public Optional<String> restorePerpetualTask(
+      PerpetualTaskRecord perpetualTask, List<PerpetualTaskRecord> existingPerpetualTasks) {
+    Set<ContainerMetadata> existingContainersMetadata =
+        existingPerpetualTasks.stream().map(this::getContainerMetadataFromPT).collect(Collectors.toSet());
+
+    ContainerMetadata newPTContainerMetadata = getContainerMetadataFromPT(perpetualTask);
+    if (existingContainersMetadata.contains(newPTContainerMetadata)) {
+      log.warn("Perpetual task with old uuid {} already exists with", perpetualTask.getUuid());
+      return Optional.empty();
+    }
+
+    PerpetualTaskSchedule schedule = PerpetualTaskSchedule.newBuilder()
+                                         .setInterval(Durations.fromMinutes(INTERVAL_MINUTES))
+                                         .setTimeout(Durations.fromSeconds(TIMEOUT_SECONDS))
+                                         .build();
+
+    return Optional.of(
+        perpetualTaskService.createTask(PerpetualTaskType.CONTAINER_INSTANCE_SYNC, perpetualTask.getAccountId(),
+            perpetualTask.getClientContext(), schedule, ALLOW_DUPLICATE, perpetualTask.getTaskDescription()));
+  }
+
+  private ContainerMetadata getContainerMetadataFromPT(PerpetualTaskRecord record) {
+    return ContainerMetadata.builder()
+        .containerServiceName(
+            record.getClientContext().getClientParams().get(InstanceSyncConstants.CONTAINER_SERVICE_NAME))
+        .namespace(record.getClientContext().getClientParams().get(InstanceSyncConstants.NAMESPACE))
+        .releaseName(record.getClientContext().getClientParams().get(InstanceSyncConstants.RELEASE_NAME))
+        .type(extractContainerMetadataType(
+            record.getClientContext().getClientParams().get(InstanceSyncConstants.CONTAINER_TYPE)))
+        .clusterName(record.getClientContext().getClientParams().get(CLUSTER_NAME))
+        .build();
   }
 
   private ContainerMetadataType extractContainerMetadataType(String containerTypeRecord) {
