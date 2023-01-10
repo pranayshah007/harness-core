2bfd559537e2e565d2300d156bce51f4523787ad
Naman Talaycha <naman.talaycha@harness.io>
2023-01-10 11:07:33 +0530

    feat: [CDS-49868]: Backup classes for Migration logic Instance sync V2 cg

---
+++ 400-rest/src/main/java/software/wings/service/impl/instance/backup/InstanceSyncPTInfoBackup.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package software.wings.service.impl.instance.backup;
+
+import static io.harness.annotations.dev.HarnessTeam.CDP;
+
+import io.harness.annotations.StoreIn;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.mongo.index.FdIndex;
+import io.harness.ng.DbAliases;
+import io.harness.perpetualtask.internal.PerpetualTaskRecord;
+import io.harness.persistence.AccountAccess;
+import io.harness.persistence.CreatedAtAccess;
+import io.harness.persistence.CreatedAtAware;
+import io.harness.persistence.PersistentEntity;
+import io.harness.persistence.UpdatedAtAccess;
+import io.harness.persistence.UpdatedAtAware;
+import io.harness.persistence.UuidAccess;
+import io.harness.persistence.UuidAware;
+
+import dev.morphia.annotations.Entity;
+import dev.morphia.annotations.Id;
+import java.util.List;
+import java.util.Set;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Singular;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.FieldNameConstants;
+
+@Data
+@Builder
+@OwnedBy(CDP)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@FieldNameConstants(innerTypeName = "InstanceSyncPTBackupKeys")
+@StoreIn(DbAliases.HARNESS)
+@Entity(value = "instanceSyncPerpetualTasksInfoBackup", noClassnameStored = true)
+public class InstanceSyncPTInfoBackup implements PersistentEntity, UuidAware, UuidAccess, AccountAccess,
+                                                 CreatedAtAccess, CreatedAtAware, UpdatedAtAware, UpdatedAtAccess {
+  @Id String uuid;
+  @FdIndex String accountId;
+  @FdIndex String infrastructureMappingId;
+  @Singular List<PerpetualTaskRecord> perpetualTaskRecords;
+  Set<String> perpetualTaskRecordIds;
+
+  long createdAt;
+  long lastUpdatedAt;
+}
