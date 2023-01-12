e0fd62b4a2cfa09ef26ec54adf1b187a413680a1
Abhay Ganvir <abhay.ganvir@harness.io>
2023-01-12 17:26:22 +0530

    [feat]: [BT-2761]: build file addee

--- scripts/cie/service-script/build/build_jar.sh
+++ scripts/cie/service-script/build/build_jar.sh
@@ -1,5 +1,18 @@
+#!/usr/bin/env bash
 # Copyright 2023 Harness Inc. All rights reserved.
 # Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
 # that can be found in the licenses directory at the root of this repository, also available at
 # https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+
+set -ex
+
+if [ "${PLATFORM}" == "jenkins" ]; then
+  bazelrc=--bazelrc=bazelrc.remote
+  bash scripts/bazel/testDistribute.sh
+fi
+
+BAZEL_DIRS=${HOME}/.bazel-dirs
+BAZEL_ARGUMENTS="--show_timestamps --announce_rc --experimental_convenience_symlinks=normal --symlink_prefix=${BAZEL_DIRS}/"
+SERVICE_MODULE="//${MODULE}/service:module //${MODULE}/service:module_deploy.jar"
+bazel ${bazelrc} build $SERVICE_MODULE ${BAZEL_ARGUMENTS}
 
