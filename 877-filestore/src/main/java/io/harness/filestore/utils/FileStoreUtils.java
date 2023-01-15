0dee5fe34b8ffbf4d0e513ef3e58f4f94772ea76
Ivan Mijailovic <ivan.mijailovic@harness.io>
2023-01-15 23:41:50 +0100

    [CDS-48893]: Fixed Sonar checks

--- 877-filestore/src/main/java/io/harness/filestore/utils/FileStoreUtils.java
+++ 877-filestore/src/main/java/io/harness/filestore/utils/FileStoreUtils.java
@@ -23,8 +23,6 @@ import lombok.experimental.UtilityClass;
 @OwnedBy(CDP)
 @UtilityClass
 public class FileStoreUtils {
-  public static final Pattern FILE_PATH_PATTERN = Pattern.compile("^(/)([^/\0]+(/)?)++$");
-
   public static boolean nameChanged(NGFile oldNGFile, FileDTO fileDto) {
     return !oldNGFile.getName().equals(fileDto.getName());
   }
