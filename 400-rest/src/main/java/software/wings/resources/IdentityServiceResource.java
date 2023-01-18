ea171707bf1b38535efee86cb163c6faf436a303
Bhavya Agrawal <bhavya.agrawal@harness.io>
2023-01-18 18:15:49 +0530

    [PL-30938]: fix checkstyle

--- 400-rest/src/main/java/software/wings/resources/IdentityServiceResource.java
+++ 400-rest/src/main/java/software/wings/resources/IdentityServiceResource.java
@@ -44,7 +44,15 @@ import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import javax.validation.constraints.NotNull;
-import javax.ws.rs.*;
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.validator.constraints.NotBlank;
@@ -79,7 +87,7 @@ public class IdentityServiceResource {
   @ExceptionMetered
   public RestResponse<User> loginUser(@QueryParam("email") String email) {
     User user = authenticationManager.loginUserForIdentityService(urlDecode(email));
-    if(user != null && isEmpty(user.getSupportAccounts()) {
+    if (user != null && isEmpty(user.getSupportAccounts())) {
       userService.loadSupportAccounts(user);
     }
     return new RestResponse<>(user);
