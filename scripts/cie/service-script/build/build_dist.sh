a4e8464d6311a14704325dc1088ea05075883fdc
Abhay Ganvir <abhay.ganvir@harness.io>
2023-01-18 12:28:17 +0530

    [fix]: [BT-2761]: 120-ng-manager script added

--- scripts/cie/service-script/build/build_dist.sh
+++ scripts/cie/service-script/build/build_dist.sh
@@ -17,18 +17,37 @@ then
 fi
 
 cp ${HOME}/.bazel-dirs/bin/${MODULE}/service/module_deploy.jar ${MODULE}-capsule.jar
-cp ../../../${MODULE}/config/config.yml .
-cp ../../../${MODULE}/config/keystore.jks .
-cp ../../../${MODULE}/config/key.pem .
-cp ../../../${MODULE}/config/cert.pem .
-cp ../../../${MODULE}/service/src/main/resources/redisson-jcache.yaml .
-cp ../../../${MODULE}/service/src/main/resources/enterprise-redisson-jcache.yaml .
-cp ../../../dockerization/base-images/apm/inject-onprem-apm-bins-into-dockerimage.sh .
-cp ../../../dockerization/base-images/apm/inject-saas-apm-bins-into-dockerimage.sh .
-cp ../../../${MODULE}/build/container/Dockerfile-${MODULE}-cie-jdk ./Dockerfile-cie-jdk
-cp -r ../../../${MODULE}/build/container/scripts/ .
-cp ../../../${MODULE}/config/jfr/default.jfc .
-cp ../../../${MODULE}/config/jfr/profile.jfc .
+
+if [ ${MODULE} == "120-ng-manager" ];then
+  cp ../../120-ng-manager/config.yml .
+  cp ../../keystore.jks .
+  cp ../../120-ng-manager/key.pem .
+  cp ../../120-ng-manager/cert.pem .
+  cp ../../120-ng-manager/src/main/resources/redisson-jcache.yaml .
+  cp ../../120-ng-manager/src/main/resources/enterprise-redisson-jcache.yaml .
+  cp ../../120-ng-manager/src/main/resources/jfr/default.jfc .
+  cp ../../120-ng-manager/src/main/resources/jfr/profile.jfc .
+
+  cp ../../dockerization/ng-manager/Dockerfile-ng-manager-jenkins-k8-openjdk ./Dockerfile
+  cp ../../dockerization/ng-manager/Dockerfile-ng-manager-cie-jdk ./Dockerfile-cie-jdk
+  cp ../../dockerization/ng-manager/Dockerfile-ng-manager-jenkins-k8-gcr-openjdk ./Dockerfile-gcr
+  cp -r ../../dockerization/ng-manager/scripts/ .
+  cp ../../../../protocol.info .
+else
+  cp ../../../${MODULE}/config/config.yml .
+  cp ../../../${MODULE}/config/keystore.jks .
+  cp ../../../${MODULE}/config/key.pem .
+  cp ../../../${MODULE}/config/cert.pem .
+  cp ../../../${MODULE}/config/jfr/default.jfc .
+  cp ../../../${MODULE}/config/jfr/profile.jfc .
+  cp ../../../${MODULE}/service/src/main/resources/redisson-jcache.yaml .
+  cp ../../../${MODULE}/service/src/main/resources/enterprise-redisson-jcache.yaml .
+
+  cp ../../dockerization/base-images/apm/inject-onprem-apm-bins-into-dockerimage.sh .
+  cp ../../dockerization/base-images/apm/inject-saas-apm-bins-into-dockerimage.sh .
+  cp ../../${MODULE}/build/container/Dockerfile-${MODULE}-cie-jdk ./Dockerfile-cie-jdk
+  cp -r ../../${MODULE}/build/container/scripts/ .
+fi
 
 java -jar ${MODULE}-capsule.jar scan-classpath-metadata
 cd ../..
