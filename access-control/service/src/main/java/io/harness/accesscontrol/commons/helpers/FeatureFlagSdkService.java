797f828f4f95fa178dfdaf736e0a06146be89200
Jimit Gandhi <jimit.gandhi@harness.io>
2023-01-18 12:23:49 +0530

    [feat]: [PL-30916]: Adding Job to remove user role assignments.

---
+++ access-control/service/src/main/java/io/harness/accesscontrol/commons/helpers/FeatureFlagSdkService.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.accesscontrol.commons.helpers;
+
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.beans.FeatureName;
+import io.harness.cf.client.api.CfClient;
+import io.harness.cf.client.dto.Target;
+
+import com.google.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@OwnedBy(HarnessTeam.PL)
+public class FeatureFlagSdkService {
+  @Inject CfClient cfClient;
+
+  public boolean isEnabled(FeatureName featureName, Target target) {
+    return cfClient.boolVariation(featureName.toString(), target, false);
+  }
+}
--- access-control/service/src/main/java/io/harness/accesscontrol/roleassignments/worker/UserRoleAssignmentRemovalService.java
+++ access-control/service/src/main/java/io/harness/accesscontrol/roleassignments/worker/UserRoleAssignmentRemovalService.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
 package io.harness.accesscontrol.roleassignments.worker;
 
 import io.harness.annotations.dev.HarnessTeam;
@@ -32,7 +39,7 @@ public class UserRoleAssignmentRemovalService implements Managed {
   public void start() throws Exception {
     log.info(DEBUG_MESSAGE + "started...");
     userRoleAssignmentRemovalJobFuture =
-        executorService.scheduleWithFixedDelay(userRoleAssignmentRemovalMigrationJob, 15, 1440, TimeUnit.DAYS);
+        executorService.scheduleWithFixedDelay(userRoleAssignmentRemovalMigrationJob, 1, 1440, TimeUnit.MINUTES);
   }
 
   @Override
