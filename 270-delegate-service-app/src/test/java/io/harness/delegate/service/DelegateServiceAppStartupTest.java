619e2f3ba134600207715f048c4c32da1e3e5039
Anupam Jaiswal <102655013+AnupamIO@users.noreply.github.com>
2023-01-19 17:53:58 +0530

    feat: [DEL-5765]: format

--- 270-delegate-service-app/src/test/java/io/harness/delegate/service/DelegateServiceAppStartupTest.java
+++ 270-delegate-service-app/src/test/java/io/harness/delegate/service/DelegateServiceAppStartupTest.java
@@ -36,64 +36,65 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 public class DelegateServiceAppStartupTest extends DelegateServiceAppTestBase {
-    public static MongoServer MONGO_SERVER;
-    public static DropwizardTestSupport<DelegateServiceConfig> SUPPORT;
+  public static MongoServer MONGO_SERVER;
 
-    private static MongoServer startMongoServer() {
-        final MongoServer mongoServer = new MongoServer(new MemoryBackend());
-        mongoServer.bind("localhost", 0);
-        return mongoServer;
+  public static DropwizardTestSupport<DelegateServiceConfig> SUPPORT;
+
+  private static MongoServer startMongoServer() {
+    final MongoServer mongoServer = new MongoServer(new MemoryBackend());
+    mongoServer.bind("localhost", 0);
+    return mongoServer;
+  }
+
+  private static void stopMongoServer() {
+    if (MONGO_SERVER != null) {
+      MONGO_SERVER.shutdownNow();
+    }
+  }
+
+  private static String getMongoUri() {
+    InetSocketAddress serverAddress = MONGO_SERVER.getLocalAddress();
+    final ServerAddress addr = new ServerAddress(serverAddress);
+    return String.format("mongodb://%s:%s/ng-harness", addr.getHost(), addr.getPort());
+  }
+
+  @BeforeClass
+  public static void beforeClass() throws Exception {
+    MONGO_SERVER = startMongoServer();
+    String directoryPath = Project.moduleDirectory(DelegateServiceAppStartupTest.class);
+    String configPath = Paths.get(directoryPath, "delegate-service-config.yml").toString();
+
+    // Handle sandboxed bazel path - remove path starting from /sandbox till execroot. This is because the source
+    // code is present outside the sandbox directory created by bazel.
+    if (!Files.exists(Paths.get(configPath)) && configPath.contains("/sandbox/")) {
+      configPath = configPath.substring(0, configPath.indexOf("/sandbox/"))
+          + configPath.substring(configPath.indexOf("/execroot"));
     }
 
-    private static void stopMongoServer() {
-        if (MONGO_SERVER != null) {
-            MONGO_SERVER.shutdownNow();
-        }
-    }
+    SUPPORT = new DropwizardTestSupport<DelegateServiceConfig>(DelegateServiceApplication.class,
+        String.valueOf(new File(configPath)), ConfigOverride.config("server.applicationConnectors[0].port", "0"),
+        ConfigOverride.config("server.applicationConnectors[0].type", "https"),
+        ConfigOverride.config("server.adminConnectors[0].type", "https"),
+        ConfigOverride.config("server.adminConnectors[0].port", "0"),
+        ConfigOverride.config("eventsFramework.redis.redisUrl", "dummyRedisUrl"),
+        ConfigOverride.config("mongo.uri", getMongoUri()));
+    SUPPORT.before();
+  }
 
-    private static String getMongoUri() {
-        InetSocketAddress serverAddress = MONGO_SERVER.getLocalAddress();
-        final ServerAddress addr = new ServerAddress(serverAddress);
-        return String.format("mongodb://%s:%s/ng-harness", addr.getHost(), addr.getPort());
-    }
+  @AfterClass
+  public static void afterClass() {
+    SUPPORT.after();
+    stopMongoServer();
+  }
 
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-        MONGO_SERVER = startMongoServer();
-        String directoryPath = Project.moduleDirectory(DelegateServiceAppStartupTest.class);
-        String configPath = Paths.get(directoryPath, "delegate-service-config.yml").toString();
-
-        // Handle sandboxed bazel path - remove path starting from /sandbox till execroot. This is because the source
-        // code is present outside the sandbox directory created by bazel.
-        if (!Files.exists(Paths.get(configPath)) && configPath.contains("/sandbox/")) {
-            configPath = configPath.substring(0, configPath.indexOf("/sandbox/"))
-                    + configPath.substring(configPath.indexOf("/execroot"));
-        }
-
-        SUPPORT = new DropwizardTestSupport<DelegateServiceConfig>(DelegateServiceApplication.class,
-                String.valueOf(new File(configPath)), ConfigOverride.config("server.applicationConnectors[0].port", "0"),
-                ConfigOverride.config("server.applicationConnectors[0].type", "https"),
-                ConfigOverride.config("server.adminConnectors[0].type", "https"),
-                ConfigOverride.config("server.adminConnectors[0].port", "0"),
-                ConfigOverride.config("eventsFramework.redis.redisUrl", "dummyRedisUrl"),
-                ConfigOverride.config("mongo.uri", getMongoUri()));
-        SUPPORT.before();
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        SUPPORT.after();
-        stopMongoServer();
-    }
-
-    @Test
-    @Owner(developers = XIN)
-    @Category(UnitTests.class)
-    public void testAppStartup() {
-        final Client client = new JerseyClientBuilder().sslContext(Http.getSslContext()).build();
-        final Response response =
-                client.target(String.format("https://localhost:%d/api/swagger.json", SUPPORT.getLocalPort())).request().get();
-        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
-        response.close();
-    }
+  @Test
+  @Owner(developers = XIN)
+  @Category(UnitTests.class)
+  public void testAppStartup() {
+    final Client client = new JerseyClientBuilder().sslContext(Http.getSslContext()).build();
+    final Response response =
+        client.target(String.format("https://localhost:%d/api/swagger.json", SUPPORT.getLocalPort())).request().get();
+    assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+    response.close();
+  }
 }
