e56800244acc78368fd54155691c11a4749615e5
Abhay Ganvir <abhay.ganvir@harness.io>
2023-01-19 18:50:33 +0530

    [fix]: [BT-2761]: syncronized the config folder structure for all services

--- scripts/cie/service-script/build/build_dist.sh
+++ scripts/cie/service-script/build/build_dist.sh
@@ -17,16 +17,18 @@ then
 fi
 
 cp ${HOME}/.bazel-dirs/bin/${MODULE}/service/module_deploy.jar ${MODULE}-capsule.jar
-cp ../../${MODULE}/config/config.yml .
-cp ../../${MODULE}/config/keystore.jks .
-cp ../../${MODULE}/config/key.pem .
-cp ../../${MODULE}/config/cert.pem .
-cp ../../${MODULE}/config/jfr/default.jfc .
-cp ../../${MODULE}/config/jfr/profile.jfc .
-cp -r ../../${MODULE}/build/container/scripts/ .
-cp ../../dockerization/base-images/apm/inject-onprem-apm-bins-into-dockerimage.sh .
-cp ../../dockerization/base-images/apm/inject-saas-apm-bins-into-dockerimage.sh .
-cp ../../${MODULE}/build/container/Dockerfile-${MODULE}-cie-jdk ./Dockerfile-cie-jdk
+cp ../../../${MODULE}/config/config.yml .
+cp ../../../${MODULE}/config/keystore.jks .
+cp ../../../${MODULE}/config/key.pem .
+cp ../../../${MODULE}/config/cert.pem .
+cp ../../../${MODULE}/service/src/main/resources/redisson-jcache.yaml .
+cp ../../../${MODULE}/service/src/main/resources/enterprise-redisson-jcache.yaml .
+cp ../../../dockerization/base-images/apm/inject-onprem-apm-bins-into-dockerimage.sh .
+cp ../../../dockerization/base-images/apm/inject-saas-apm-bins-into-dockerimage.sh .
+cp ../../../${MODULE}/build/container/Dockerfile-${MODULE}-cie-jdk ./Dockerfile-cie-jdk
+cp -r ../../../${MODULE}/build/container/scripts/ .
+cp ../../../${MODULE}/config/jfr/default.jfc .
+cp ../../../${MODULE}/config/jfr/profile.jfc .
 
 java -jar ${MODULE}-capsule.jar scan-classpath-metadata
 cd ../..
