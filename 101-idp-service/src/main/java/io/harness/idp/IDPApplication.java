f5478d39eefa16a411b109a0461702529f482531
Vikyath Harekal <vikyath.harekal@harness.io>
2023-01-25 13:09:26 +0530

    feat: [IDP-0]: update idp mongo db

--- 101-idp-service/src/main/java/io/harness/idp/IDPApplication.java
+++ 101-idp-service/src/main/java/io/harness/idp/IDPApplication.java
@@ -73,7 +73,7 @@ public class IDPApplication extends Application<IDPConfiguration> {
 
   @Override
   public String getName() {
-    return "Wings Application";
+    return "IDP Service";
   }
 
   @Override
