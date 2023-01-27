a5252d8f32e23827cd97177ad1dadcf467fe4234
Jamie Li <tianyi.li@harness.io>
2023-01-27 18:37:31 -0500

    fix: [CI-6844]: Disable Debug mode in Prod 1 for accounts other than Informatica (#43159)
    * [CI-6844]: Disable Debug mode in Prod 1 for accounts other than Informatica
    
    * [CI-6844]: Disable Debug mode in Prod 1 for accounts other than Informatica

--- 332-ci-manager/service/src/main/java/io/harness/ci/execution/serializer/vm/VmRunStepSerializer.java
+++ 332-ci-manager/service/src/main/java/io/harness/ci/execution/serializer/vm/VmRunStepSerializer.java
@@ -41,6 +41,8 @@ import org.apache.commons.lang3.StringUtils;
 public class VmRunStepSerializer {
   @Inject ConnectorUtils connectorUtils;
   @Inject CIExecutionServiceConfig ciExecutionServiceConfig;
+  private static List<String> INFORMATICA_ACCOUNT_IDS =
+      new ArrayList<>(List.of("0imfjG07TR2hVBcS5AZpCQ", "z40YS0M5RCCOybahmyEVgQ"));
   public VmRunStep serialize(RunStepInfo runStepInfo, Ambiance ambiance, String identifier,
       ParameterField<Timeout> parameterFieldTimeout, String stepName) {
     String command =
@@ -64,7 +66,9 @@ public class VmRunStepSerializer {
     }
 
     String earlyExitCommand = SerializerUtils.getEarlyExitCommand(runStepInfo.getShell());
-    if (ambiance.hasMetadata() && ambiance.getMetadata().getIsDebug()) {
+    NGAccess ngAccess = AmbianceUtils.getNgAccess(ambiance);
+    if (ambiance.hasMetadata() && ambiance.getMetadata().getIsDebug()
+        && INFORMATICA_ACCOUNT_IDS.contains(ngAccess.getAccountIdentifier())) {
       command = earlyExitCommand + System.lineSeparator()
           + SerializerUtils.getVmDebugCommand(ciExecutionServiceConfig.getRemoteDebugTimeout()) + System.lineSeparator()
           + command;
@@ -82,7 +86,7 @@ public class VmRunStepSerializer {
 
     ConnectorDetails connectorDetails;
     if (!StringUtils.isEmpty(image) && !StringUtils.isEmpty(connectorIdentifier)) {
-      NGAccess ngAccess = AmbianceUtils.getNgAccess(ambiance);
+      ngAccess = AmbianceUtils.getNgAccess(ambiance);
       connectorDetails = connectorUtils.getConnectorDetails(ngAccess, connectorIdentifier);
       runStepBuilder.imageConnector(connectorDetails);
     }
