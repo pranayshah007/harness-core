ea2bbf3cac821e6a910ef977d16b435a9b1e6f18
shalini-agr <shalini.agrawal@harness.io>
2023-02-06 15:54:37 +0530

    update log

--- 952-debezium-engine/src/main/java/io/harness/debezium/DebeziumUtils.java
+++ 952-debezium-engine/src/main/java/io/harness/debezium/DebeziumUtils.java
@@ -41,8 +41,9 @@ public class DebeziumUtils {
         debeziumController.stopDebeziumController();
       } else if (!success) {
         resetOffset(JsonUtils.asObject(redisConfigJson, RedisConfig.class), redisKey);
-        log.error(
-            "Offset reset for key: {} because of exception: {}, at {}", redisKey, error, System.currentTimeMillis());
+        log.error(String.format("Offset reset for key: %s because of exception: %s, at %s", redisKey, error,
+                      System.currentTimeMillis()),
+            error);
       }
     };
   }
