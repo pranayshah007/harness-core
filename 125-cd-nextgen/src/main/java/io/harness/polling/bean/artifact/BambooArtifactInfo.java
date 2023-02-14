05585515458b98c96174eb184c70d042631ecd9c
Shivam Negi <shivam.negi@harness.io>
2023-02-14 08:57:57 +0530

    feat: [CDS-51737]: Added the trigger support for bamboo

--- 125-cd-nextgen/src/main/java/io/harness/polling/bean/artifact/BambooArtifactInfo.java
+++ 125-cd-nextgen/src/main/java/io/harness/polling/bean/artifact/BambooArtifactInfo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Harness Inc. All rights reserved.
+ * Copyright 2022 Harness Inc. All rights reserved.
  * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
  * that can be found in the licenses directory at the root of this repository, also available at
  * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
@@ -10,6 +10,7 @@ package io.harness.polling.bean.artifact;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.cdng.artifact.bean.ArtifactConfig;
+import io.harness.cdng.artifact.bean.yaml.JenkinsArtifactConfig;
 import io.harness.cdng.artifact.bean.yaml.nexusartifact.BambooArtifactConfig;
 import io.harness.delegate.task.artifacts.ArtifactSourceType;
 import io.harness.pms.yaml.ParameterField;
