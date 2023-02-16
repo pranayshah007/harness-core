8d81a14e3b698a7e8539c756d4416b224c33f45a
Adithya Viswanathan <adithya.viswanathan@harness.io>
2023-02-16 10:33:22 +0530

    [feat]: [PIE-8309]: build fix

--- pipeline-service/service/src/test/java/io/harness/pms/ngpipeline/inputset/api/InputSetsApiUtilsTest.java
+++ pipeline-service/service/src/test/java/io/harness/pms/ngpipeline/inputset/api/InputSetsApiUtilsTest.java
@@ -20,6 +20,7 @@ import io.harness.annotations.dev.OwnedBy;
 import io.harness.beans.FeatureName;
 import io.harness.category.element.UnitTests;
 import io.harness.exception.InvalidRequestException;
+import io.harness.ngsettings.client.remote.NGSettingsClient;
 import io.harness.pms.ngpipeline.inputset.beans.entity.InputSetEntity;
 import io.harness.pms.yaml.PipelineVersion;
 import io.harness.rule.Owner;
@@ -28,6 +29,7 @@ import io.harness.spec.server.pipeline.v1.model.InputSetResponseBody;
 import io.harness.utils.PmsFeatureFlagHelper;
 
 import com.google.common.io.Resources;
+import com.google.inject.Inject;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
@@ -41,6 +43,8 @@ import org.mockito.MockitoAnnotations;
 public class InputSetsApiUtilsTest extends CategoryTest {
   private InputSetsApiUtils inputSetsApiUtils;
   @Mock private PmsFeatureFlagHelper pmsFeatureFlagHelper;
+
+  @Mock private NGSettingsClient ngSettingsClient;
   String identifier = randomAlphabetic(10);
   String name = randomAlphabetic(10);
   String account = "accountId";
@@ -48,7 +52,7 @@ public class InputSetsApiUtilsTest extends CategoryTest {
   @Before
   public void setup() {
     MockitoAnnotations.initMocks(this);
-    inputSetsApiUtils = new InputSetsApiUtils(pmsFeatureFlagHelper);
+    inputSetsApiUtils = new InputSetsApiUtils(pmsFeatureFlagHelper, ngSettingsClient);
   }
 
   private String readFile(String filename) {
