0915ff7fcd247182ade8c09fbc84175fa52a539f
Pratyush Goel <goel.pratyush@harness.io>
2023-02-21 18:30:19 +0530

    feat: [CDS-52512]: Removing unwanted file.

--- 260-delegate/src/test/java/io/harness/perpetualtask/K8sInstanceSyncPerpetualTaskExecutorTest.java
+++ 260-delegate/src/test/java/io/harness/perpetualtask/K8sInstanceSyncPerpetualTaskExecutorTest.java
@@ -33,6 +33,7 @@ import io.harness.k8s.model.HarnessLabels;
 import io.harness.k8s.model.K8sPod;
 import io.harness.k8s.model.KubernetesConfig;
 import io.harness.logging.CommandExecutionStatus;
+import io.harness.logging.LogCallback;
 import io.harness.managerclient.DelegateAgentManagerClient;
 import io.harness.perpetualtask.instancesync.K8sDeploymentRelease;
 import io.harness.perpetualtask.instancesync.K8sInstanceSyncPerpetualTaskParams;
@@ -91,7 +92,7 @@ public class K8sInstanceSyncPerpetualTaskExecutorTest extends DelegateTestBase {
 
     doReturn(KubernetesConfig.builder().accountId(ACCOUNT_ID).build())
         .when(containerBaseHelper)
-        .createKubernetesConfig(any(DirectK8sInfraDelegateConfig.class), );
+        .createKubernetesConfig(any(DirectK8sInfraDelegateConfig.class), any(LogCallback.class));
   }
 
   @Test
