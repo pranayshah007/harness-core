d18034d0dec6903aa2999a01a574a5809a1b5357
Naman Goenka <naman.goenka@harness.io>
2023-02-26 17:10:42 +0530

    fix: [CDS-53778]: addressed review comments

--- pipeline-service/modules/orchestration-steps/contracts/src/main/java/io/harness/steps/approval/step/harness/beans/Approvers.java
+++ pipeline-service/modules/orchestration-steps/contracts/src/main/java/io/harness/steps/approval/step/harness/beans/Approvers.java
@@ -9,7 +9,6 @@ package io.harness.steps.approval.step.harness.beans;
 
 import static io.harness.annotations.dev.HarnessTeam.CDC;
 import static io.harness.yaml.schema.beans.SupportedPossibleFieldTypes.expression;
-import static io.harness.yaml.schema.beans.SupportedPossibleFieldTypes.runtime;
 import static io.harness.yaml.schema.beans.SupportedPossibleFieldTypes.string;
 
 import io.harness.annotation.RecasterAlias;
@@ -35,7 +34,7 @@ import lombok.experimental.FieldDefaults;
 public class Approvers {
   @NotNull
   @Size(min = 1)
-  @YamlSchemaTypes(value = {runtime, expression})
+  @YamlSchemaTypes(value = {expression})
   @ApiModelProperty(dataType = SwaggerConstants.STRING_LIST_CLASSPATH)
   ParameterField<List<String>> userGroups;
 
