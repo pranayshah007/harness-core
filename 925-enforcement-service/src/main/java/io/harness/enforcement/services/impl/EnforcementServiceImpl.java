b02f51b1b2b48c436454d74494e15607162d9c82
Vikas Maddukuri <vikas.maddukuri@harness.io>
2023-02-27 22:53:10 +0530

    fix: [DEL-5999]: Added more logs

--- 925-enforcement-service/src/main/java/io/harness/enforcement/services/impl/EnforcementServiceImpl.java
+++ 925-enforcement-service/src/main/java/io/harness/enforcement/services/impl/EnforcementServiceImpl.java
@@ -111,10 +111,12 @@ public class EnforcementServiceImpl implements EnforcementService {
       throw new InvalidRequestException(String.format("Feature [%s] is not defined", featureRestrictionName));
     }
     FeatureRestriction featureRestriction = featureRestrictionMap.get(featureRestrictionName);
+    log.info("TestVikasLogs: Feature restriction is {}.", featureRestriction);
     Edition edition = getLicenseEdition(accountIdentifier, featureRestriction.getModuleType());
 
     ConversionHandler conversionHandler =
         conversionHandlerFactory.getConversionHandler(isFeatureFlagEnabled(accountIdentifier));
+    log.info("TestVikasLogs: conversionHandler is {}.", conversionHandler);
     return conversionHandler.toFeatureMetadataDTO(featureRestriction, edition, accountIdentifier);
   }
 
