84fb51e0b55fe62c4c048eab4ce450782918b0ac
Vivek Dixit <vivek.dixit@harness.io>
2023-02-27 15:41:00 +0530

    [feat]: [PIE-7218]: Classes for OutboxEvents

---
+++ pipeline-service/modules/orchestration/src/main/java/io/harness/engine/pms/events/StageStartEvent.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.engine.pms.events;
+
+import static io.harness.annotations.dev.HarnessTeam.PIPELINE;
+
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.audit.ResourceTypeConstants;
+import io.harness.ng.core.ProjectScope;
+import io.harness.ng.core.Resource;
+import io.harness.ng.core.ResourceConstants;
+import io.harness.ng.core.ResourceScope;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@OwnedBy(PIPELINE)
+@Data
+@NoArgsConstructor
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class StageStartEvent extends NodeExecutionEvent {
+  private String stageIdentifier;
+  private String stageType;
+  private Long startTs;
+  private String nodeExecutionId;
+
+  public StageStartEvent(String accountIdentifier, String orgIdentifier, String projectIdentifier,
+      String pipelineIdentifier, String planExecutionId, String stageIdentifier, String stageType, Long startTs,
+      String nodeExecutionId) {
+    super(accountIdentifier, orgIdentifier, projectIdentifier, pipelineIdentifier, planExecutionId);
+    this.stageIdentifier = stageIdentifier;
+    this.stageType = stageType;
+    this.startTs = startTs;
+    this.nodeExecutionId = nodeExecutionId;
+  }
+
+  @JsonIgnore
+  @Override
+  public ResourceScope getResourceScope() {
+    return new ProjectScope(accountIdentifier, orgIdentifier, projectIdentifier);
+  }
+
+  @JsonIgnore
+  @Override
+  public Resource getResource() {
+    Map<String, String> labels = new HashMap<>();
+    labels.put(ResourceConstants.LABEL_KEY_RESOURCE_NAME, pipelineIdentifier);
+    return Resource.builder()
+        .identifier(stageIdentifier)
+        .type(ResourceTypeConstants.NODE_EXECUTION)
+        .labels(labels)
+        .build();
+  }
+
+  @JsonIgnore
+  @Override
+  public String getEventType() {
+    return NodeExecutionOutboxEvents.STAGE_START;
+  }
+}
