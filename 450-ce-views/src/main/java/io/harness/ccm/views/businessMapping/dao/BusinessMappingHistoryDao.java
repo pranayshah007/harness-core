389eaea451cba16a657ca69eb450a6c0c48e7eca
Trunapushpa <trunapushpa.surkar@harness.io>
2023-02-28 05:30:10 +0530

    fix: [CCM-11420]: Cost Category Query Hotfix 2 (#44439)
    * fix: [CCM-9811]: fix both queries
    
    * fix: [CCM-9811]: final query fix

--- 450-ce-views/src/main/java/io/harness/ccm/views/businessMapping/dao/BusinessMappingHistoryDao.java
+++ 450-ce-views/src/main/java/io/harness/ccm/views/businessMapping/dao/BusinessMappingHistoryDao.java
@@ -56,32 +56,20 @@ public class BusinessMappingHistoryDao {
     query.or(query.and(query.criteria(BusinessMappingHistoryKeys.startAt).lessThanOrEq(endYearMonth),
                  query.criteria(BusinessMappingHistoryKeys.startAt).greaterThanOrEq(startYearMonth)),
         query.and(query.criteria(BusinessMappingHistoryKeys.startAt).lessThan(startYearMonth),
-            query.criteria(BusinessMappingHistoryKeys.endAt).greaterThan(startYearMonth)));
+            query.criteria(BusinessMappingHistoryKeys.endAt).greaterThanOrEq(startYearMonth)));
     return query.asList();
   }
 
   public List<BusinessMappingHistory> getInRange(
       String accountId, String businessMappingId, Integer startYearMonth, Integer endYearMonth) {
-    List<BusinessMappingHistory> list = hPersistence.createQuery(BusinessMappingHistory.class)
-                                            .filter(BusinessMappingHistoryKeys.accountId, accountId)
-                                            .filter(BusinessMappingHistoryKeys.businessMappingId, businessMappingId)
-                                            .field(BusinessMappingHistoryKeys.startAt)
-                                            .lessThanOrEq(endYearMonth)
-                                            .field(BusinessMappingHistoryKeys.startAt)
-                                            .greaterThanOrEq(startYearMonth)
-                                            .asList();
-    BusinessMappingHistory beginning = hPersistence.createQuery(BusinessMappingHistory.class)
-                                           .filter(BusinessMappingHistoryKeys.accountId, accountId)
-                                           .filter(BusinessMappingHistoryKeys.businessMappingId, businessMappingId)
-                                           .field(BusinessMappingHistoryKeys.startAt)
-                                           .lessThan(startYearMonth)
-                                           .field(BusinessMappingHistoryKeys.endAt)
-                                           .greaterThan(startYearMonth)
-                                           .get();
-    if (beginning != null) {
-      list.add(beginning);
-    }
-    return list;
+    Query<BusinessMappingHistory> query = hPersistence.createQuery(BusinessMappingHistory.class)
+                                              .filter(BusinessMappingHistoryKeys.accountId, accountId)
+                                              .filter(BusinessMappingHistoryKeys.businessMappingId, businessMappingId);
+    query.or(query.and(query.criteria(BusinessMappingHistoryKeys.startAt).lessThanOrEq(endYearMonth),
+                 query.criteria(BusinessMappingHistoryKeys.startAt).greaterThanOrEq(startYearMonth)),
+        query.and(query.criteria(BusinessMappingHistoryKeys.startAt).lessThan(startYearMonth),
+            query.criteria(BusinessMappingHistoryKeys.endAt).greaterThanOrEq(startYearMonth)));
+    return query.asList();
   }
 
   public BusinessMappingHistory update(BusinessMappingHistory businessMappingHistory) {
