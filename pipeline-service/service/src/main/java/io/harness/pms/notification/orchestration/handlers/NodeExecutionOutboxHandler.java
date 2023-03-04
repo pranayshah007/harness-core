fd76bd5322080a5601bcad9779fb39bdce998855
Vivek Dixit <vivek.dixit@harness.io>
2023-03-04 10:12:23 +0530

    [feat]: [PIE-8298]: Resolved Comments, added Ambiance in autoLogContext

--- pipeline-service/service/src/main/java/io/harness/pms/notification/orchestration/handlers/NodeExecutionOutboxHandler.java
+++ pipeline-service/service/src/main/java/io/harness/pms/notification/orchestration/handlers/NodeExecutionOutboxHandler.java
@@ -7,8 +7,6 @@
 
 package io.harness.pms.notification.orchestration.handlers;
 
-import static io.harness.logging.AutoLogContext.OverrideBehavior.OVERRIDE_NESTS;
-
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.engine.observers.NodeExecutionStartObserver;
@@ -18,7 +16,8 @@ import io.harness.engine.pms.audits.events.StageStartEvent;
 import io.harness.logging.AutoLogContext;
 import io.harness.outbox.api.OutboxService;
 import io.harness.pms.contracts.ambiance.Ambiance;
-import io.harness.pms.outbox.autoLog.NodeExecutionLogContext;
+import io.harness.pms.execution.utils.AmbianceUtils;
+import io.harness.pms.plan.execution.SetupAbstractionKeys;
 
 import com.google.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
@@ -26,22 +25,18 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @OwnedBy(HarnessTeam.PIPELINE)
 public class NodeExecutionOutboxHandler implements NodeExecutionStartObserver {
-  public static final String ACCOUNT_ID = "accountId";
-  public static final String ORG_IDENTIFIER = "orgIdentifier";
-  public static final String PROJECT_IDENTIFIER = "projectIdentifier";
   public static final String PIPELINE = "PIPELINE";
   public static final String STAGES = "STAGES";
   @Inject private OutboxService outboxService;
 
   @Override
   public void onNodeStart(NodeStartInfo nodeStartInfo) {
-    if (validatePresenceOfNodeGroup(nodeStartInfo))
+    if (!validatePresenceOfNodeGroup(nodeStartInfo)) {
       return;
+    }
 
-    String nodeGroup = null;
-    try (AutoLogContext ignore =
-             new NodeExecutionLogContext(nodeStartInfo.getNodeExecution().getUuid(), OVERRIDE_NESTS)) {
-      nodeGroup = nodeStartInfo.getNodeExecution().getGroup();
+    try (AutoLogContext ignore = AmbianceUtils.autoLogContext(nodeStartInfo.getNodeExecution().getAmbiance())) {
+      String nodeGroup = nodeStartInfo.getNodeExecution().getGroup();
       try {
         switch (nodeGroup) {
           case PIPELINE:
@@ -60,8 +55,8 @@ public class NodeExecutionOutboxHandler implements NodeExecutionStartObserver {
   }
 
   private boolean validatePresenceOfNodeGroup(NodeStartInfo nodeStartInfo) {
-    if (nodeStartInfo == null || nodeStartInfo.getNodeExecution() == null
-        || nodeStartInfo.getNodeExecution().getGroup() == null) {
+    if (nodeStartInfo != null && nodeStartInfo.getNodeExecution() != null
+        && nodeStartInfo.getNodeExecution().getGroup() != null) {
       log.error(String.format("Required fields to send an outBoxEvent are not populated in nodeStartInfo!"));
       return true;
     }
@@ -72,9 +67,9 @@ public class NodeExecutionOutboxHandler implements NodeExecutionStartObserver {
     Ambiance ambiance = nodeStartInfo.getNodeExecution().getAmbiance();
     StageStartEvent stageStartEvent =
         StageStartEvent.builder()
-            .accountIdentifier(ambiance.getSetupAbstractionsMap().get(ACCOUNT_ID))
-            .orgIdentifier(ambiance.getSetupAbstractionsMap().get(ORG_IDENTIFIER))
-            .projectIdentifier(ambiance.getSetupAbstractionsMap().get(PROJECT_IDENTIFIER))
+            .accountIdentifier(ambiance.getSetupAbstractionsMap().get(SetupAbstractionKeys.accountId))
+            .orgIdentifier(ambiance.getSetupAbstractionsMap().get(SetupAbstractionKeys.orgIdentifier))
+            .projectIdentifier(ambiance.getSetupAbstractionsMap().get(SetupAbstractionKeys.projectIdentifier))
             .pipelineIdentifier(ambiance.getMetadata().getPipelineIdentifier())
             .stageIdentifier(nodeStartInfo.getNodeExecution().getIdentifier())
             .planExecutionId(nodeStartInfo.getNodeExecution().getAmbiance().getPlanExecutionId())
@@ -89,9 +84,9 @@ public class NodeExecutionOutboxHandler implements NodeExecutionStartObserver {
     Ambiance ambiance = nodeStartInfo.getNodeExecution().getAmbiance();
     PipelineStartEvent pipelineStartEvent =
         PipelineStartEvent.builder()
-            .accountIdentifier(ambiance.getSetupAbstractionsMap().get(ACCOUNT_ID))
-            .orgIdentifier(ambiance.getSetupAbstractionsMap().get(ORG_IDENTIFIER))
-            .projectIdentifier(ambiance.getSetupAbstractionsMap().get(PROJECT_IDENTIFIER))
+            .accountIdentifier(ambiance.getSetupAbstractionsMap().get(SetupAbstractionKeys.accountId))
+            .orgIdentifier(ambiance.getSetupAbstractionsMap().get(SetupAbstractionKeys.orgIdentifier))
+            .projectIdentifier(ambiance.getSetupAbstractionsMap().get(SetupAbstractionKeys.projectIdentifier))
             .pipelineIdentifier(ambiance.getMetadata().getPipelineIdentifier())
             .planExecutionId(ambiance.getPlanExecutionId())
             .startTs(nodeStartInfo.getNodeExecution().getStartTs())
