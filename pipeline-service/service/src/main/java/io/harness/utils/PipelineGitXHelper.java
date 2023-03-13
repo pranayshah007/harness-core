54da610f1f4bce859fc3bb88e195aa3410b861af
Prashant Sharma <prashant.sharma@harness.io>
2023-03-13 16:14:12 +0530

    fix: [PIE-8899]: Pipeline chaining: parent pipeline execution is intermittent failing when parent and child pipeline are in non default branch (#45039)

--- pipeline-service/service/src/main/java/io/harness/utils/PipelineGitXHelper.java
+++ pipeline-service/service/src/main/java/io/harness/utils/PipelineGitXHelper.java
@@ -63,6 +63,7 @@ public class PipelineGitXHelper {
       if (isNotEmpty(entityGitDetails.getParentEntityRepoName())) {
         gitEntityInfo.setParentEntityRepoName(entityGitDetails.getParentEntityRepoName());
       }
+      GitAwareContextHelper.updateGitEntityContext(gitEntityInfo);
     }
   }
 
