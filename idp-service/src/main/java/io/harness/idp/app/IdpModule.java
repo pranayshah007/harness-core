c1599e5c11ab6124666bb20186f725125d22002e
Vikyath Harekal <vikyath.harekal@harness.io>
2023-03-15 10:31:15 +0530

    feat: [IDP-231]: Cron job to sync secrets periodically

--- idp-service/src/main/java/io/harness/idp/app/IdpModule.java
+++ idp-service/src/main/java/io/harness/idp/app/IdpModule.java
@@ -53,6 +53,7 @@ import io.harness.idp.status.k8s.PodHealthCheck;
 import io.harness.idp.status.resources.StatusInfoApiImpl;
 import io.harness.idp.status.service.StatusInfoService;
 import io.harness.idp.status.service.StatusInfoServiceImpl;
+import io.harness.manage.ManagedScheduledExecutorService;
 import io.harness.metrics.modules.MetricsModule;
 import io.harness.mongo.AbstractMongoModule;
 import io.harness.mongo.MongoConfig;
@@ -95,6 +96,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.converter.Converter;
@@ -228,6 +230,9 @@ public class IdpModule extends AbstractModule {
     bind(OnboardingService.class).to(OnboardingServiceImpl.class);
     bind(GitClientV2.class).to(GitClientV2Impl.class);
     bind(LayoutProxyApi.class).to(LayoutProxyApiImpl.class);
+    bind(ScheduledExecutorService.class)
+        .annotatedWith(Names.named("envSecretSyncer"))
+        .toInstance(new ManagedScheduledExecutorService("EnvSecretSyncer"));
   }
 
   @Provides
