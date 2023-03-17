f95910756b3c30c5b83d24271cab09fea03050e4
Mankrit Singh <mankrit.singh@harness.io>
2023-03-17 11:56:18 +0530

    spec hosted

--- 120-ng-manager/src/main/java/io/harness/ng/OasSpecFirstYamlResource.java
+++ 120-ng-manager/src/main/java/io/harness/ng/OasSpecFirstYamlResource.java
@@ -7,7 +7,12 @@
 
 package io.harness.ng;
 
+import io.swagger.v3.oas.annotations.Operation;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -15,49 +20,46 @@ import javax.ws.rs.core.Response;
 
 @Path("/spec-first")
 public class OasSpecFirstYamlResource {
-  public static final String NG_MANAGER_PATH = "120-ng-manager/contracts/openapi/v1/openapi.yaml";
+  public static final String NG_MANAGER_PATH =
+      "/Users/singhmankrit/Documents/harness-core/120-ng-manager/contracts/openapi/v1/openapi.yaml";
   public static final String CONNECTORS_PATH = "440-connector-nextgen/contracts/openapi/v1/openapi.yaml";
   public static final String COMMONS_PATH = "970-ng-commons/contracts/openapi/v1/openapi.yaml";
 
   @GET
-  @Produces("application/yaml")
+  @Produces("text/yaml")
   @Path("/ng-manager/openapi.yaml")
-  public Response getOpenApiYamlNGManager() {
-    try {
-      String yaml = String.valueOf(new File(NG_MANAGER_PATH));
-      return Response.ok(yaml, "application/yaml")
-          .header("Content-Disposition", "attachment; filename=\"openapi.yaml\"")
-          .build();
-    } catch (Exception e) {
-      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+  @Operation(hidden = true)
+  public Response getOpenApiYamlNGManager() throws IOException {
+    File file = new File(NG_MANAGER_PATH);
+    if (!file.exists()) {
+      throw new FileNotFoundException("OpenAPI spec file not found at path: " + NG_MANAGER_PATH);
     }
+    String yaml = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
+    return Response.ok().entity(yaml).header("Content-Disposition", "inline; filename=openapi.yaml").build();
   }
 
   @GET
-  @Produces("application/yaml")
+  @Produces("text/yaml")
   @Path("/connectors/openapi.yaml")
-  public Response getOpenApiYamlConnectors() {
-    try {
-      String yaml = String.valueOf(new File(CONNECTORS_PATH));
-      return Response.ok(yaml, "application/yaml")
-          .header("Content-Disposition", "attachment; filename=\"openapi.yaml\"")
-          .build();
-    } catch (Exception e) {
-      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+  @Operation(hidden = true)
+  public Response getOpenApiYamlConnectors() throws IOException {
+    File file = new File(NG_MANAGER_PATH);
+    if (!file.exists()) {
+      throw new FileNotFoundException("OpenAPI spec file not found at path: " + CONNECTORS_PATH);
     }
+    String yaml = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
+    return Response.ok().entity(yaml).header("Content-Disposition", "inline; filename=openapi.yaml").build();
   }
 
   @GET
-  @Produces("application/yaml")
+  @Produces("text/yaml")
   @Path("/commons/openapi.yaml")
-  public Response getOpenApiYamlCommons() {
-    try {
-      String yaml = String.valueOf(new File(COMMONS_PATH));
-      return Response.ok(yaml, "application/yaml")
-          .header("Content-Disposition", "attachment; filename=\"openapi.yaml\"")
-          .build();
-    } catch (Exception e) {
-      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+  @Operation(hidden = true)
+  public Response getOpenApiYamlCommons() throws IOException {
+    File file = new File(NG_MANAGER_PATH);
+    if (!file.exists()) {
+      throw new FileNotFoundException("OpenAPI spec file not found at path: " + COMMONS_PATH);
     }
+    String yaml = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
+    return Response.ok().entity(yaml).header("Content-Disposition", "inline; filename=openapi.yaml").build();
   }
-}
