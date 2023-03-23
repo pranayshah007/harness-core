0ab7078c05388be9627e0049a0f2eded2af37398
Naman Verma <naman.verma@harness.io>
2023-03-23 10:26:50 +0530

    remove sweeping output from rollback stage step

--- pipeline-service/service/src/main/java/io/harness/pms/pipelinerollback/PipelineRollbackStageStep.java
+++ pipeline-service/service/src/main/java/io/harness/pms/pipelinerollback/PipelineRollbackStageStep.java
@@ -8,12 +8,14 @@
 package io.harness.pms.pipelinerollback;
 
 import static io.harness.annotations.dev.HarnessTeam.PIPELINE;
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 
 import io.harness.OrchestrationStepTypes;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.engine.interrupts.InterruptService;
 import io.harness.exception.InvalidRequestException;
 import io.harness.execution.PlanExecution;
-import io.harness.plancreator.pipelinerollback.PipelineRollbackStageSweepingOutput;
+import io.harness.interrupts.Interrupt;
 import io.harness.pms.contracts.ambiance.Ambiance;
 import io.harness.pms.contracts.execution.AsyncExecutableResponse;
 import io.harness.pms.contracts.execution.Status;
@@ -21,16 +23,22 @@ import io.harness.pms.contracts.steps.StepCategory;
 import io.harness.pms.contracts.steps.StepType;
 import io.harness.pms.execution.utils.AmbianceUtils;
 import io.harness.pms.plan.execution.PipelineExecutor;
+import io.harness.pms.plan.execution.PlanExecutionInterruptType;
 import io.harness.pms.plan.execution.beans.PipelineExecutionSummaryEntity.PlanExecutionSummaryKeys;
+import io.harness.pms.plan.execution.service.PMSExecutionService;
 import io.harness.pms.plan.execution.service.PmsExecutionSummaryService;
-import io.harness.pms.sdk.core.resolver.outputs.ExecutionSweepingOutputService;
 import io.harness.pms.sdk.core.steps.io.EmptyStepParameters;
 import io.harness.pms.sdk.core.steps.io.StepInputPackage;
 import io.harness.pms.sdk.core.steps.io.StepResponse;
+import io.harness.pms.security.PmsSecurityContextGuardUtils;
+import io.harness.security.SecurityContextBuilder;
+import io.harness.security.SourcePrincipalContextBuilder;
+import io.harness.security.dto.Principal;
 import io.harness.steps.executable.AsyncExecutableWithRbac;
 import io.harness.tasks.ResponseData;
 
 import com.google.inject.Inject;
+import java.util.List;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.core.query.Update;
@@ -44,8 +52,9 @@ public class PipelineRollbackStageStep implements AsyncExecutableWithRbac<EmptyS
                                                .build();
 
   @Inject private PipelineExecutor pipelineExecutor;
-  @Inject private ExecutionSweepingOutputService sweepingOutputService;
   @Inject private PmsExecutionSummaryService executionSummaryService;
+  @Inject private InterruptService interruptService;
+  @Inject private PMSExecutionService executionService;
 
   @Override
   public Class<EmptyStepParameters> getStepParametersClass() {
@@ -58,34 +67,40 @@ public class PipelineRollbackStageStep implements AsyncExecutableWithRbac<EmptyS
     String accountId = AmbianceUtils.getAccountId(ambiance);
     String orgId = AmbianceUtils.getOrgIdentifier(ambiance);
     String projectId = AmbianceUtils.getProjectIdentifier(ambiance);
-    String planExecutionId = ambiance.getPlanExecutionId();
+    String currentPlanExecutionId = ambiance.getPlanExecutionId();
     log.info("Starting Pipeline Rollback");
-    PlanExecution planExecution = pipelineExecutor.startPipelineRollback(accountId, orgId, projectId, planExecutionId);
-    if (planExecution == null) {
+    PlanExecution rollbackPlanExecution =
+        pipelineExecutor.startPipelineRollback(accountId, orgId, projectId, currentPlanExecutionId);
+    if (rollbackPlanExecution == null) {
       throw new InvalidRequestException("Failed to start Pipeline Rollback");
     }
     Update update = new Update();
-    update.set(PlanExecutionSummaryKeys.rollbackModeExecutionId, planExecution.getUuid());
-    executionSummaryService.update(planExecutionId, update);
-    // saving output for handleAsyncResponse
-    sweepingOutputService.consume(ambiance, PipelineRollbackStageSweepingOutput.OUTPUT_NAME,
-        PipelineRollbackStageSweepingOutput.builder().rollbackModeExecutionId(planExecution.getUuid()).build(),
-        StepCategory.STAGE.name());
-
-    return AsyncExecutableResponse.newBuilder().addCallbackIds(planExecution.getUuid()).build();
+    update.set(PlanExecutionSummaryKeys.rollbackModeExecutionId, rollbackPlanExecution.getUuid());
+    executionSummaryService.update(currentPlanExecutionId, update);
+    return AsyncExecutableResponse.newBuilder().addCallbackIds(rollbackPlanExecution.getUuid()).build();
   }
 
   @Override
   public StepResponse handleAsyncResponse(
       Ambiance ambiance, EmptyStepParameters stepParameters, Map<String, ResponseData> responseDataMap) {
-    // todo: implement
     return StepResponse.builder().status(Status.SUCCEEDED).build();
   }
 
   @Override
   public void handleAbort(
       Ambiance ambiance, EmptyStepParameters stepParameters, AsyncExecutableResponse executableResponse) {
-    // todo: implement
+    Principal principal = PmsSecurityContextGuardUtils.getPrincipalFromAmbiance(ambiance);
+    SourcePrincipalContextBuilder.setSourcePrincipal(principal);
+    SecurityContextBuilder.setContext(principal);
+    // Setting interrupt config of parent pipeline while registering interrupt for child pipeline
+    List<Interrupt> interrupts = interruptService.fetchAbortAllPlanLevelInterrupt(ambiance.getPlanExecutionId());
+    if (isNotEmpty(interrupts)) {
+      Interrupt interrupt = interrupts.get(0);
+      if (executableResponse != null && isNotEmpty(executableResponse.getCallbackIdsList())) {
+        executionService.registerInterrupt(PlanExecutionInterruptType.ABORTALL, executableResponse.getCallbackIds(0),
+            null, interrupt.getInterruptConfig());
+      }
+    }
   }
 
   @Override
