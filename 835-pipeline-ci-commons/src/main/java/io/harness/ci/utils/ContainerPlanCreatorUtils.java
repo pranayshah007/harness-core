b900bdb8e2d755c59f1ddfa4315dce12f11c65b3
Abhinav Singh <abhinav.singh@harness.io>
2023-03-28 12:24:34 +0530

    Update ContainerPlanCreatorUtils.java

--- 835-pipeline-ci-commons/src/main/java/io/harness/ci/utils/ContainerPlanCreatorUtils.java
+++ 835-pipeline-ci-commons/src/main/java/io/harness/ci/utils/ContainerPlanCreatorUtils.java
@@ -5,90 +5,187 @@
  * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
  */
 
-package io.harness.ci.utils;
+package io.harness.plancreator.steps;
 
-import io.harness.exception.InvalidRequestException;
-import io.harness.exception.ngexception.CIStageExecutionException;
-import io.harness.plancreator.execution.ExecutionWrapperConfig;
-import io.harness.plancreator.steps.ParallelStepElementConfig;
-import io.harness.plancreator.steps.StepGroupElementConfig;
-import io.harness.pms.utils.IdentifierGeneratorUtils;
+import com.google.common.base.Preconditions;
+import com.google.inject.Inject;
+import com.google.protobuf.ByteString;
+import io.harness.advisers.nextstep.NextStepAdviserParameters;
+import io.harness.data.structure.EmptyPredicate;
+import io.harness.plancreator.strategy.StrategyUtils;
+import io.harness.pms.contracts.advisers.AdviserObtainment;
+import io.harness.pms.contracts.advisers.AdviserType;
+import io.harness.pms.contracts.facilitators.FacilitatorObtainment;
+import io.harness.pms.contracts.facilitators.FacilitatorType;
+import io.harness.pms.contracts.plan.ExecutionMode;
+import io.harness.pms.contracts.plan.PlanCreationContextValue;
+import io.harness.pms.contracts.steps.StepCategory;
+import io.harness.pms.execution.OrchestrationFacilitatorType;
+import io.harness.pms.execution.utils.SkipInfoUtils;
+import io.harness.pms.plan.creation.PlanCreatorUtils;
+import io.harness.pms.sdk.core.adviser.OrchestrationAdviserTypes;
+import io.harness.pms.sdk.core.adviser.success.OnSuccessAdviserParameters;
+import io.harness.pms.sdk.core.plan.PlanNode;
+import io.harness.pms.sdk.core.plan.creation.beans.PlanCreationContext;
+import io.harness.pms.sdk.core.plan.creation.beans.PlanCreationResponse;
+import io.harness.pms.sdk.core.plan.creation.creators.ChildrenPlanCreator;
+import io.harness.pms.sdk.core.steps.io.StepParameters;
+import io.harness.pms.yaml.DependenciesUtils;
 import io.harness.pms.yaml.YAMLFieldNameConstants;
 import io.harness.pms.yaml.YamlField;
-import io.harness.pms.yaml.YamlNode;
-import io.harness.steps.container.exception.ContainerStepExecutionException;
-import io.harness.yaml.utils.JsonPipelineUtils;
+import io.harness.pms.yaml.YamlUtils;
+import io.harness.serializer.KryoSerializer;
+import io.harness.steps.common.steps.stepgroup.StepGroupElementConfigV2;
+import io.harness.steps.common.steps.stepgroup.StepGroupStep;
+import io.harness.steps.common.steps.stepgroup.StepGroupStepParameters;
+import io.harness.when.utils.RunInfoUtils;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.Set;
 
-public abstract class ContainerPlanCreatorUtils {
-  public static List<YamlField> getStepYamlFields(YamlField yamlField) {
-    List<YamlNode> yamlNodes = Optional.of(yamlField.getNode().asArray()).orElse(Collections.emptyList());
-    return yamlNodes.stream().map(YamlField::new).collect(Collectors.toList());
-  }
+import static io.harness.pms.yaml.YAMLFieldNameConstants.PARALLEL;
+import static io.harness.pms.yaml.YAMLFieldNameConstants.ROLLBACK_STEPS;
+import static io.harness.pms.yaml.YAMLFieldNameConstants.STEPS;
+import static io.harness.pms.yaml.YAMLFieldNameConstants.STEP_GROUP;
+import static io.harness.pms.yaml.YAMLFieldNameConstants.STEP_GROUP_V2;
 
-  public static ExecutionWrapperConfig getExecutionConfig(YamlField step) {
-    return getExecutionConfig(step, true);
-  }
+public class StepGroupPMSPlanCreatorV2 extends ChildrenPlanCreator<StepGroupElementConfigV2> {
+  @Inject private KryoSerializer kryoSerializer;
+  @Override
+  public PlanNode createPlanForParentNode(
+      PlanCreationContext ctx, StepGroupElementConfigV2 config, List<String> childrenNodeIds) {
+    YamlField stepsField =
+        Preconditions.checkNotNull(ctx.getCurrentField().getNode().getField(YAMLFieldNameConstants.STEPS));
+    config.setIdentifier(StrategyUtils.getIdentifierWithExpression(ctx, config.getIdentifier()));
+    config.setName(StrategyUtils.getIdentifierWithExpression(ctx, config.getName()));
+    StepParameters stepParameters = StepGroupStepParameters.getStepParameters(config, stepsField.getNode().getUuid());
 
-  public static ExecutionWrapperConfig getExecutionConfig(YamlField step, boolean isCI) {
-    if (step.getType() == null) {
-      throw new InvalidRequestException(String.format("Type cannot be null for %s Step", isCI ? "CI" : "Container"));
+    boolean isStepGroupInsideRollback = false;
+    if (YamlUtils.findParentNode(ctx.getCurrentField().getNode(), ROLLBACK_STEPS) != null) {
+      isStepGroupInsideRollback = true;
     }
-    switch (step.getType()) {
-      case YAMLFieldNameConstants.PARALLEL:
-        List<YamlField> parallelNodes = getStepYamlFields(
-            step.getNode().getField(YAMLFieldNameConstants.SPEC).getNode().getField(YAMLFieldNameConstants.STEPS));
-        ParallelStepElementConfig parallelStepElementConfig =
-            ParallelStepElementConfig.builder()
-                .sections(parallelNodes.stream()
-                              .map(ContainerPlanCreatorUtils::getExecutionConfig)
-                              .collect(Collectors.toList()))
-                .build();
-        return ExecutionWrapperConfig.builder()
-            .uuid(step.getUuid())
-            .parallel(getJsonNode(parallelStepElementConfig, isCI))
-            .build();
-      case YAMLFieldNameConstants.GROUP:
-        List<YamlField> groupNodes = getStepYamlFields(
-            step.getNode().getField(YAMLFieldNameConstants.SPEC).getNode().getField(YAMLFieldNameConstants.STEPS));
-        StepGroupElementConfig stepGroupElementConfig =
-            StepGroupElementConfig.builder()
-                .identifier(IdentifierGeneratorUtils.getId(step.getNodeName()))
-                .name(step.getNodeName())
-                .steps(
-                    groupNodes.stream().map(ContainerPlanCreatorUtils::getExecutionConfig).collect(Collectors.toList()))
-                .build();
-        return ExecutionWrapperConfig.builder()
-            .uuid(step.getUuid())
-            .stepGroup(getJsonNode(stepGroupElementConfig, isCI))
-            .build();
-      default:
-        JsonNode node = step.getNode().getCurrJsonNode();
-        if (node != null && node.isObject() && node.get(YAMLFieldNameConstants.NAME) != null) {
-          ObjectNode objectNode = (ObjectNode) node;
-          objectNode.put(YAMLFieldNameConstants.IDENTIFIER,
-              IdentifierGeneratorUtils.getId(objectNode.get(YAMLFieldNameConstants.NAME).asText()));
-        }
-        return ExecutionWrapperConfig.builder().uuid(step.getUuid()).step(step.getNode().getCurrJsonNode()).build();
-    }
+    PlanCreationContextValue planCreationContextValue = ctx.getGlobalContext().get("metadata");
+    ExecutionMode executionMode = planCreationContextValue.getMetadata().getExecutionMode();
+    return PlanNode.builder()
+        .name(config.getName())
+        .uuid(StrategyUtils.getSwappedPlanNodeId(ctx, config.getUuid()))
+        .identifier(config.getIdentifier())
+        .stepType(StepGroupStep.STEP_TYPE)
+        .group(StepCategory.STEP_GROUP.name())
+        .skipCondition(SkipInfoUtils.getSkipCondition(config.getSkipCondition()))
+        // We Should add default when condition as StageFailure if stepGroup is inside rollback
+        .whenCondition(isStepGroupInsideRollback
+                ? RunInfoUtils.getRunConditionForRollback(config.getWhen(), executionMode)
+                : RunInfoUtils.getRunConditionForStep(config.getWhen()))
+        .stepParameters(stepParameters)
+        .facilitatorObtainment(
+            FacilitatorObtainment.newBuilder()
+                .setType(FacilitatorType.newBuilder().setType(OrchestrationFacilitatorType.CHILD).build())
+                .build())
+        .adviserObtainments(getAdviserObtainmentFromMetaData(
+            ctx.getCurrentField(), StrategyUtils.isWrappedUnderStrategy(ctx.getCurrentField())))
+        .skipExpressionChain(false)
+        .build();
   }
 
-  private static JsonNode getJsonNode(Object object, boolean isCI) {
-    try {
-      String json = JsonPipelineUtils.writeJsonString(object);
-      return JsonPipelineUtils.getMapper().readTree(json);
-    } catch (IOException e) {
-      if (isCI) {
-        throw new CIStageExecutionException("Failed to serialise node", e);
+  @Override
+  public Class<StepGroupElementConfigV2> getFieldClass() {
+    return StepGroupElementConfigV2.class;
+  }
+
+  @Override
+  public Map<String, Set<String>> getSupportedTypes() {
+    return Collections.singletonMap(STEP_GROUP_V2, Collections.singleton(PlanCreatorUtils.ANY_TYPE));
+  }
+
+  @Override
+  public LinkedHashMap<String, PlanCreationResponse> createPlanForChildrenNodes(
+      PlanCreationContext ctx, StepGroupElementConfigV2 config) {
+    List<YamlField> dependencyNodeIdsList = ctx.getStepYamlFields();
+
+    LinkedHashMap<String, PlanCreationResponse> responseMap = new LinkedHashMap<>();
+    // Add Steps Node
+    if (EmptyPredicate.isNotEmpty(dependencyNodeIdsList)) {
+      YamlField stepsField =
+          Preconditions.checkNotNull(ctx.getCurrentField().getNode().getField(YAMLFieldNameConstants.STEPS));
+      String stepsNodeId = stepsField.getNode().getUuid();
+      Map<String, YamlField> stepsYamlFieldMap = new HashMap<>();
+      stepsYamlFieldMap.put(stepsNodeId, stepsField);
+      responseMap.put(stepsNodeId,
+          PlanCreationResponse.builder()
+              .dependencies(DependenciesUtils.toDependenciesProto(stepsYamlFieldMap))
+              .build());
+    }
+    addStrategyFieldDependencyIfPresent(kryoSerializer, ctx, config.getUuid(), config.getName(), config.getIdentifier(),
+        responseMap, new HashMap<>(), getAdviserObtainmentFromMetaData(ctx.getCurrentField(), false));
+
+    return responseMap;
+  }
+
+  protected List<AdviserObtainment> getAdviserObtainmentFromMetaData(YamlField currentField, boolean checkForStrategy) {
+    List<AdviserObtainment> adviserObtainments = new ArrayList<>();
+    if (checkForStrategy) {
+      return adviserObtainments;
+    }
+
+    /*
+     * Adding OnSuccess adviser if stepGroup is inside rollback section else adding NextStep adviser for when condition
+     * to work.
+     */
+    if (currentField != null && currentField.getNode() != null) {
+      // Check if step is inside RollbackStep
+      if (YamlUtils.findParentNode(currentField.getNode(), ROLLBACK_STEPS) != null) {
+        addOnSuccessAdviser(currentField, adviserObtainments);
+      } else {
+        // Adding NextStep Adviser at last due to giving priority to Failure strategy more. DO NOT CHANGE.
+        addNextStepAdviser(currentField, adviserObtainments);
       }
-      throw new ContainerStepExecutionException("Failed to serialise node", e);
     }
+    return adviserObtainments;
+  }
+
+  private void addNextStepAdviser(YamlField currentField, List<AdviserObtainment> adviserObtainments) {
+    if (currentField.checkIfParentIsParallel(STEPS)) {
+      return;
+    }
+    YamlField siblingField = currentField.getNode().nextSiblingFromParentArray(
+            currentField.getName(), Arrays.asList(YAMLFieldNameConstants.STEP, PARALLEL, STEP_GROUP));
+    if (siblingField != null && siblingField.getNode().getUuid() != null) {
+      adviserObtainments.add(
+              AdviserObtainment.newBuilder()
+                      .setType(AdviserType.newBuilder().setType(OrchestrationAdviserTypes.NEXT_STEP.name()).build())
+                      .setParameters(ByteString.copyFrom(kryoSerializer.asBytes(
+                              NextStepAdviserParameters.builder().nextNodeId(siblingField.getNode().getUuid()).build())))
+                      .build());
+    }
+  }
+
+  private void addOnSuccessAdviser(YamlField currentField, List<AdviserObtainment> adviserObtainments) {
+    if (currentField.checkIfParentIsParallel(ROLLBACK_STEPS)) {
+      return;
+    }
+    YamlField siblingField = currentField.getNode().nextSiblingFromParentArray(
+            currentField.getName(), Arrays.asList(YAMLFieldNameConstants.STEP, PARALLEL, STEP_GROUP));
+    if (siblingField != null && siblingField.getNode().getUuid() != null) {
+      adviserObtainments.add(
+              AdviserObtainment.newBuilder()
+                      .setType(AdviserType.newBuilder().setType(OrchestrationAdviserTypes.ON_SUCCESS.name()).build())
+                      .setParameters(ByteString.copyFrom(kryoSerializer.asBytes(
+                              OnSuccessAdviserParameters.builder().nextNodeId(siblingField.getNode().getUuid()).build())))
+                      .build());
+    }
+  }
+
+  public void addStrategyFieldDependencyIfPresent(KryoSerializer kryoSerializer, PlanCreationContext ctx, String uuid,
+                                                  String name, String identifier, LinkedHashMap<String, PlanCreationResponse> responseMap,
+                                                  HashMap<Object, Object> objectObjectHashMap, List<AdviserObtainment> adviserObtainmentFromMetaData) {
+    StrategyUtils.addStrategyFieldDependencyIfPresent(kryoSerializer, ctx, uuid, name, identifier, responseMap,
+            new HashMap<>(), getAdviserObtainmentFromMetaData(ctx.getCurrentField(), false));
   }
 }
