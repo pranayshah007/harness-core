604aaabd506d73f77cdea9610578c7bced01e4ab
Shivam Negi <shivam.negi@harness.io>
2023-03-29 20:54:36 +0530

    fix: [CDS-53512]: reverted the last published changes (#45831)
    * feat: [CDS-53513]: Last Collected expression support (#44724)
    
    * feat: [CDS-48195]: Adding the console logs for jenkins failure
    
    * feat: [CDS-48195]: Adding the console logs for jenkins failure
    
    * feat: [CDS-53513]: Last Collected expression support
    
    * feat: [CDS-53513]: Last Collected expression support
    
    * feat: [CDS-53513]: Last Collected expression support
    
    * feat: [CDS-53513]: Last Collected expression support
    
    * feat: [CDS-53513]: Last Collected expression support
    
    * feat: [CDS-53513]: Last Collected expression support
    
    * feat: [CDS-53513]: Last Collected expression support
    
    * feat: [CDS-53513]: Last Collected expression support
    
    * feat: [CDS-53513]: Last Collected expression support
    
    * feat: [CDS-53513]: Code format fix
    
    * feat: [CDS-53513]: UT added
    
    * feat: [CDS-53513]: UT added
    
    * feat: [CDS-53513]: UT added
    
    * feat: [CDS-53513]: UT fix
    
    * feat: [CDS-53513]: Last Collected expression support
    
    * feat: [CDS-53513]: Last Collected expression support
    
    * fix: [CDS-53513]: Replacing * with .*?
    
    * fix: [CDS-53513]: Replacing * with .*?
    
    * fix: [CDS-53513]: Incremented the build number
    
    * Revert "feat: [CDS-53513]: Last Collected expression support (#45671)"
    
    This reverts commit 72218629f7030172e2460ba3f76ba4be88ad3db2.
    
    * fix: [CDS-53512]: Reverting the last published changes
    
    * Revert "feat: [CDS-53513]: Last Collected expression support (#44724)"
    
    This reverts commit 3646ffbf6b2506d54719d2932548dc80b2dd8066.
    
    * Revert "feat: [CDS-53513]: Last Collected expression support (#44724)"
    
    This reverts commit 3646ffbf6b2506d54719d2932548dc80b2dd8066.
    
    * fix: [CDS-53512]: Reverting the last published changes
    
    * fix: [CDS-53512]: Reverting the last published changes
    
    * fix: [CDS-53512]: Reverting the last published changes

--- 930-delegate-tasks/src/main/java/io/harness/delegate/task/artifacts/docker/DockerArtifactTaskHandler.java
+++ 930-delegate-tasks/src/main/java/io/harness/delegate/task/artifacts/docker/DockerArtifactTaskHandler.java
@@ -31,18 +31,16 @@ import lombok.AllArgsConstructor;
 public class DockerArtifactTaskHandler extends DelegateArtifactTaskHandler<DockerArtifactDelegateRequest> {
   private final DockerRegistryService dockerRegistryService;
   private final SecretDecryptionService secretDecryptionService;
-  private final String ACCEPT_ALL_REGEX = "*";
 
   @Override
   public ArtifactTaskExecutionResponse getLastSuccessfulBuild(DockerArtifactDelegateRequest attributesRequest) {
     BuildDetailsInternal lastSuccessfulBuild;
     ArtifactMetaInfo artifactMetaInfo = null;
     List<Map<String, String>> labels;
-    if (isRegex(attributesRequest) || attributesRequest.getTag().equals(ACCEPT_ALL_REGEX)) {
-      String tagRegex = isRegex(attributesRequest) ? attributesRequest.getTagRegex() : attributesRequest.getTag();
+    if (isRegex(attributesRequest)) {
       lastSuccessfulBuild = dockerRegistryService.getLastSuccessfulBuildFromRegex(
           DockerRequestResponseMapper.toDockerInternalConfig(attributesRequest), attributesRequest.getImagePath(),
-          tagRegex);
+          attributesRequest.getTagRegex());
       labels = dockerRegistryService.getLabels(DockerRequestResponseMapper.toDockerInternalConfig(attributesRequest),
           attributesRequest.getImagePath(), Collections.singletonList(lastSuccessfulBuild.getNumber()));
     } else {
