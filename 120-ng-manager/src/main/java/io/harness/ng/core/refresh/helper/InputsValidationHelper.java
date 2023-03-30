f4e76f73184febce020ed2f22f619606fe411d2f
Abhinav Hinger <abhinav.hinger@harness.io>
2023-03-30 16:27:42 +0530

    resolve service yaml before checking for template recon

--- 120-ng-manager/src/main/java/io/harness/ng/core/refresh/helper/InputsValidationHelper.java
+++ 120-ng-manager/src/main/java/io/harness/ng/core/refresh/helper/InputsValidationHelper.java
@@ -15,9 +15,13 @@ import io.harness.cdng.visitor.YamlTypes;
 import io.harness.common.NGExpressionUtils;
 import io.harness.data.structure.EmptyPredicate;
 import io.harness.exception.InvalidRequestException;
+import io.harness.exception.YamlException;
+import io.harness.expression.EngineExpressionEvaluator;
 import io.harness.ng.core.refresh.bean.EntityRefreshContext;
 import io.harness.ng.core.service.entity.ServiceEntity;
+import io.harness.ng.core.service.mappers.ServiceFilterHelper;
 import io.harness.ng.core.service.services.ServiceEntityService;
+import io.harness.ng.core.template.RefreshRequestDTO;
 import io.harness.ng.core.template.refresh.ValidateTemplateInputsResponseDTO;
 import io.harness.ng.core.template.refresh.v2.InputsValidationResponse;
 import io.harness.ng.core.yaml.CDYamlFacade;
@@ -27,10 +31,14 @@ import io.harness.pms.yaml.YamlField;
 import io.harness.pms.yaml.YamlNode;
 import io.harness.pms.yaml.YamlNodeUtils;
 import io.harness.pms.yaml.YamlUtils;
+import io.harness.remote.client.NGRestUtils;
+import io.harness.template.remote.TemplateResourceClient;
 import io.harness.template.yaml.TemplateRefHelper;
+import io.harness.utils.YamlPipelineUtils;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.inject.Inject;
 import java.io.IOException;
@@ -51,6 +59,7 @@ public class InputsValidationHelper {
   @Inject EntityFetchHelper entityFetchHelper;
   @Inject EnvironmentRefreshHelper environmentRefreshHelper;
   @Inject private CDYamlFacade cdYamlFacade;
+  @Inject private TemplateResourceClient templateResourceClient;
 
   public InputsValidationResponse validateInputsForYaml(
       String accountId, String orgId, String projectId, String yaml, String resolvedTemplatesYaml) {
@@ -170,26 +179,34 @@ public class InputsValidationHelper {
 
     String serviceYaml = serviceEntity.fetchNonEmptyYaml();
 
-    // check template inputs are updated
-    if (TemplateRefHelper.hasTemplateRef(serviceYaml)) {
-      ValidateTemplateInputsResponseDTO validateTemplateInputsResponseDTO = serviceEntityService.validateTemplateInputs(
-          context.getAccountId(), context.getOrgId(), context.getProjectId(), serviceRef, "true");
+    YamlNode primaryArtifactRefNode = YamlNodeUtils.goToPathUsingFqn(
+        entityNode, "serviceInputs.serviceDefinition.spec.artifacts.primary.primaryArtifactRef");
+
+    String serviceYamlGivenPrimaryArtifactRef = createServiceYamlGivenPrimaryArtifactRef(
+        serviceYaml, serviceRef, primaryArtifactRefNode == null ? null : primaryArtifactRefNode.asText());
+
+    if (TemplateRefHelper.hasTemplateRef(serviceYamlGivenPrimaryArtifactRef)) {
+      ValidateTemplateInputsResponseDTO validateTemplateInputsResponseDTO =
+          NGRestUtils.getResponse(templateResourceClient.validateTemplateInputsForGivenYaml(context.getAccountId(),
+              context.getOrgId(), context.getProjectId(), null, null, null, null, null, null, null, null, "true",
+              RefreshRequestDTO.builder().yaml(serviceYamlGivenPrimaryArtifactRef).build()));
+
       if (!validateTemplateInputsResponseDTO.isValidYaml()) {
         errorNodeSummary.setValid(false);
         return;
       }
     }
 
-    YamlNode primaryArtifactRefNode = YamlNodeUtils.goToPathUsingFqn(
-        entityNode, "serviceInputs.serviceDefinition.spec.artifacts.primary.primaryArtifactRef");
     String serviceRuntimeInputYaml = serviceEntityService.createServiceInputsYamlGivenPrimaryArtifactRef(
         serviceYaml, serviceRef, primaryArtifactRefNode == null ? null : primaryArtifactRefNode.asText());
+
     if (EmptyPredicate.isEmpty(serviceRuntimeInputYaml)) {
       if (EnvironmentRefreshHelper.isNodeNotNullAndNotHaveRuntimeValue(serviceInputs)) {
         errorNodeSummary.setValid(false);
       }
       return;
     }
+
     ObjectMapper mapper = new ObjectMapper();
     ObjectNode serviceInputsNode = mapper.createObjectNode();
     serviceInputsNode.set(YamlTypes.SERVICE_INPUTS, serviceInputs);
@@ -198,5 +215,88 @@ public class InputsValidationHelper {
             new HashSet<>(), new HashSet<>(keysToIgnoreInNodeToValidate))) {
       errorNodeSummary.setValid(false);
     }
+  }
+
+  private String createServiceYamlGivenPrimaryArtifactRef(
+      String serviceYaml, String serviceIdentifier, String primaryArtifactRef) {
+    try {
+      YamlField serviceYamlField = YamlUtils.readTree(serviceYaml).getNode().getField(YamlTypes.SERVICE_ENTITY);
+      Map<String, Object> updatedService = new HashMap<>();
+      if (serviceYamlField == null) {
+        throw new YamlException(
+            String.format("Yaml provided for service %s does not have service root field.", serviceIdentifier));
+      }
+
+      modifyServiceDefinitionGivenPrimaryArtifactRef(serviceYamlField, serviceIdentifier, primaryArtifactRef);
+      ObjectNode serviceNode = (ObjectNode) serviceYamlField.getNode().getCurrJsonNode();
+      updatedService.put(YamlTypes.SERVICE_ENTITY, serviceNode);
+      return YamlPipelineUtils.writeYamlString(updatedService);
+    } catch (IOException e) {
+      throw new InvalidRequestException(
+          String.format("Error occurred while creating service yaml for service %s", serviceIdentifier), e);
+    }
+  }
+
+  private void modifyServiceDefinitionGivenPrimaryArtifactRef(
+      YamlField serviceYamlField, String serviceIdentifier, String primaryArtifactRef) {
+    YamlField primaryArtifactField = ServiceFilterHelper.getPrimaryArtifactNodeFromServiceYaml(serviceYamlField);
+    if (primaryArtifactField == null) {
+      return;
+    }
+    if (!primaryArtifactField.getNode().isObject()) {
+      throw new InvalidRequestException(
+          String.format("Primary field inside service %s should be an OBJECT node but was %s", serviceIdentifier,
+              primaryArtifactField.getNode().getCurrJsonNode().getNodeType()));
+    }
+
+    YamlField primaryArtifactRefField = primaryArtifactField.getNode().getField(YamlTypes.PRIMARY_ARTIFACT_REF);
+    YamlField artifactSourcesField = primaryArtifactField.getNode().getField(YamlTypes.ARTIFACT_SOURCES);
+    if (primaryArtifactRefField == null || artifactSourcesField == null) {
+      return;
+    }
+
+    if (!artifactSourcesField.getNode().isArray()) {
+      throw new InvalidRequestException(
+          String.format("Artifact sources inside service %s should be ARRAY node but was %s", serviceIdentifier,
+              artifactSourcesField.getNode().getCurrJsonNode().getNodeType()));
+    }
+
+    String primaryArtifactRefValue = primaryArtifactRefField.getNode().asText();
+
+    ObjectNode primaryArtifactObjectNode = (ObjectNode) primaryArtifactField.getNode().getCurrJsonNode();
+    if (NGExpressionUtils.matchesInputSetPattern(primaryArtifactRefValue)) {
+      if (EmptyPredicate.isNotEmpty(primaryArtifactRef)
+          && !NGExpressionUtils.matchesInputSetPattern(primaryArtifactRef)) {
+        primaryArtifactRefValue = primaryArtifactRef;
+      } else {
+        // do we not do any validation on sources when ref in an input
+        primaryArtifactObjectNode.remove(YamlTypes.ARTIFACT_SOURCES);
+        primaryArtifactObjectNode.put(YamlTypes.ARTIFACT_SOURCES, "<+input>");
+        return;
+      }
+    }
+
+    if (EngineExpressionEvaluator.hasExpressions(primaryArtifactRefValue)) {
+      throw new InvalidRequestException(
+          String.format("Primary artifact ref cannot be an expression inside the service %s", serviceIdentifier));
+    }
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    ArrayNode filteredArtifactSourcesNode = objectMapper.createArrayNode();
+    List<YamlNode> artifactSources = artifactSourcesField.getNode().asArray();
+    for (YamlNode artifactSource : artifactSources) {
+      String sourceIdentifier = artifactSource.getIdentifier();
+      if (primaryArtifactRefValue.equals(sourceIdentifier)) {
+        filteredArtifactSourcesNode.add(artifactSource.getCurrJsonNode());
+        break;
+      }
+    }
+
+    if (EmptyPredicate.isEmpty(filteredArtifactSourcesNode)) {
+      throw new InvalidRequestException(
+          String.format("Primary artifact ref value %s provided does not exist in sources in service %s",
+              primaryArtifactRefValue, serviceIdentifier));
+    }
+    primaryArtifactObjectNode.set(YamlTypes.ARTIFACT_SOURCES, filteredArtifactSourcesNode);
   }
 }
