470b00fc87b2db789b3cfbfc05571225a407536f
Alexandru Casian <alexandru.casian@harness.io>
2023-04-05 11:58:36 +0300

    feat: [CDS-58399]: Add support for config file variables/secrets rendering (#46074)
    * feat: [CDS-58399]: Add support for config file variables/secrets rendering (#46026)
    
    * feat: [CDS-58399]: Add support for config file variables/secrets rendering
    
    * feat: [CDS-58399]: Fixed failing cheks.
    
    * feat: [CDS-58399]: Fixed remaning failing checks.
    
    * feat: [CDS-58399]: Fixed code format pr check

--- 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
+++ 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
@@ -670,7 +670,9 @@ public enum FeatureName {
       "Applies RBAC on services overview page, only displays services which the user has access to read",
       HarnessTeam.SPG),
   CDS_SERVICENOW_TICKET_TYPE_V2("FF to use servicenow ticketTypesV2 endpoint", HarnessTeam.CDC),
-  FF_ALLOW_OPTIONAL_VARIABLE("Allow Optional Variable from UI in NG.", HarnessTeam.PIPELINE);
+  FF_ALLOW_OPTIONAL_VARIABLE("Allow Optional Variable from UI in NG.", HarnessTeam.PIPELINE),
+  CDS_NG_CONFIG_FILE_EXPRESSION(
+      "Enable Harness variable rendering in Hanress config files (SSH/WinRM)", HarnessTeam.CDP);
 
   @Deprecated
   FeatureName() {
