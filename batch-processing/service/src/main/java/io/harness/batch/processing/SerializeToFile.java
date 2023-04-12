d1f3fb6c20cb2704584ac82049f9d3e60194dcb7
Hitesh Aringa <hitesh.aringa@harness.io>
2023-04-11 19:30:57 -0700

    [Fix]: Security Vulnerabilities

---
+++ batch-processing/service/src/main/java/io/harness/batch/processing/SerializeToFile.java
@@ -0,0 +1,57 @@
+package io.harness.batch.processing;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class SerializeToFile {
+
+    public static void SerializeToFile(Object obj, String filename)
+    {
+        try
+        {
+            FileOutputStream file = new FileOutputStream(filename);
+            ObjectOutputStream out = new ObjectOutputStream(file);
+
+            // Serialization of the object to file
+
+            System.out.println("Serializing " + obj.toString() + " to " + filename);
+            out.writeObject(obj);
+
+            out.close();
+            file.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Exception: " + e.toString());
+        }
+    }
+
+    // Function to deserialize an object from a file
+
+    public static Object DeserializeFromFile(String filename)
+    {
+        Object obj = new Object();
+
+        try
+        {
+            FileInputStream file = new FileInputStream(filename);
+            ObjectInputStream in = new ObjectInputStream(file);
+
+            // Deserialization of the object to file
+
+            System.out.println("Deserializing from " + filename);
+            obj = in.readObject();
+
+            in.close();
+            file.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Exception: " + e.toString());
+        }
+
+        return obj;
+    }
+}
