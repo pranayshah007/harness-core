df788592f33b66f222f1aa524b082c8fc40cf846
Abhinav Hinger <abhinav.hinger@harness.io>
2023-04-12 18:26:30 +0530

    add docs endpoint (#46348)

--- pipeline-service/service/src/main/java/io/harness/openai/OpenAiResource.java
+++ pipeline-service/service/src/main/java/io/harness/openai/OpenAiResource.java
@@ -7,6 +7,9 @@
 
 package io.harness.openai;
 
+import static io.harness.logging.CommandExecutionStatus.FAILURE;
+import static io.harness.logging.CommandExecutionStatus.SUCCESS;
+
 import io.harness.NGCommonEntityConstants;
 import io.harness.accesscontrol.AccountIdentifier;
 import io.harness.accesscontrol.OrgIdentifier;
@@ -14,6 +17,7 @@ import io.harness.accesscontrol.ProjectIdentifier;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.exception.InvalidRequestException;
+import io.harness.logging.CommandExecutionStatus;
 import io.harness.ng.core.dto.ErrorDTO;
 import io.harness.ng.core.dto.FailureDTO;
 import io.harness.ng.core.dto.ResponseDTO;
@@ -39,6 +43,13 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -104,22 +115,22 @@ public class OpenAiResource {
     OpenAiService service = new OpenAiService(OpenAiConstants.openAIKey, Duration.ofMinutes(10L));
 
     Optional<PipelineEntity> goldenPipelineEntity =
-        pmsPipelineService.getPipeline(accountId, orgId, projectId, goldenPipelineIdentifier, false, false);
+            pmsPipelineService.getPipeline(accountId, orgId, projectId, goldenPipelineIdentifier, false, false);
 
     if (goldenPipelineEntity.isEmpty()) {
       throw new InvalidRequestException("Golden Pipeline Identifier not found");
     }
 
     String queryForGPT =
-        "Given the following pipeline YAML as reference, provide verbal OPA policies that can be recommended to other similar Pipelines. \n Cover OPA policies such as manager approval, build environment, security checks, deployment type, and rollback strategy. \n"
-        + goldenPipelineEntity.get().getYaml();
+            "Given the following pipeline YAML as reference, provide verbal OPA policies that can be recommended to other similar Pipelines. \n Cover OPA policies such as manager approval, build environment, security checks, deployment type, and rollback strategy. \n"
+                    + goldenPipelineEntity.get().getYaml();
 
     System.out.println("\nCreating completion...");
 
     List<ChatMessage> queries = new ArrayList<>();
     queries.add(new ChatMessage("assistant", queryForGPT));
     ChatCompletionRequest completionRequest =
-        ChatCompletionRequest.builder().model("gpt-3.5-turbo").messages(queries).user("testing").build();
+            ChatCompletionRequest.builder().model("gpt-3.5-turbo").messages(queries).user("testing").build();
 
     ChatCompletionResult result = service.createChatCompletion(completionRequest);
 
@@ -134,5 +145,43 @@ public class OpenAiResource {
 
     service.shutdownExecutor();
     return ResponseDTO.newResponse(response);
+
+  }
+
+  @GET
+  @Path("/docs")
+  @ApiOperation(value = "Get Docs Query", nickname = "getTemplates")
+  public ResponseDTO<String> getDocs(@QueryParam("query") String query) {
+    StringBuffer response = new StringBuffer();
+    try {
+      URL url = new URL("https://flask-docs-query-khaki-rho.vercel.app/docs");
+      HttpURLConnection con = (HttpURLConnection) url.openConnection();
+      con.setRequestMethod("POST");
+      con.setDoOutput(true);
+      con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+      String encodedQuery = query.replace(" ", "+");
+
+      encodedQuery = "query=" + encodedQuery;
+      OutputStream os = con.getOutputStream();
+      os.write(encodedQuery.getBytes());
+      os.flush();
+      os.close();
+
+      int responseCode = con.getResponseCode();
+      System.out.println("Response Code: " + responseCode);
+
+      BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+      String inputLine;
+
+      while ((inputLine = in.readLine()) != null) {
+        response.append(inputLine);
+      }
+      in.close();
+    } catch (Exception ex) {
+      log.error("Error while fetching docs");
+    }
+
+    return ResponseDTO.newResponse(response.toString());
   }
 }
