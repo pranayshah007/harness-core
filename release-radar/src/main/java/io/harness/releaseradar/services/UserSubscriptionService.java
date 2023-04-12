01a9e2160255bd4b9a294004c55aab2e71bc3e7a
Yogesh Chauhan <yogesh.chauhan@harness.io>
2023-04-12 18:03:02 +0530

    find subscribers through jira Ids

--- release-radar/src/main/java/io/harness/releaseradar/services/UserSubscriptionService.java
+++ release-radar/src/main/java/io/harness/releaseradar/services/UserSubscriptionService.java
@@ -10,13 +10,13 @@ package io.harness.releaseradar.services;
 import static io.harness.data.structure.EmptyPredicate.isEmpty;
 import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 
-import io.harness.data.structure.EmptyPredicate;
 import io.harness.releaseradar.beans.EventFilter;
 import io.harness.releaseradar.entities.UserSubscription;
 import io.harness.repositories.UserSubscriptionRepository;
 
 import com.google.inject.Inject;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -43,6 +43,16 @@ public class UserSubscriptionService {
     return subscriptions;
   }
 
+  public List<UserSubscription> getAllSubscriptions(Collection<String> jiraIds) {
+    List<UserSubscription> subscriptions = new ArrayList<>();
+    repository.findAll().forEach(userSubscription -> {
+      if (isSubscribed(jiraIds, userSubscription)) {
+        subscriptions.add(userSubscription);
+      }
+    });
+    return subscriptions;
+  }
+
   private static boolean isSubscribed(EventFilter filter, UserSubscription userSubscription) {
     EventFilter userFilter = userSubscription.getFilter();
     return (isEmpty(userFilter.getServiceName())
@@ -58,5 +68,10 @@ public class UserSubscriptionService {
         && (isEmpty(userFilter.getRelease()) || userFilter.getRelease().equalsIgnoreCase(filter.getRelease()))
 
         && (isEmpty(userFilter.getJiraId()) || userFilter.getJiraId().equalsIgnoreCase(filter.getJiraId()));
+  }
+
+  private static boolean isSubscribed(Collection<String> jiraIds, UserSubscription userSubscription) {
+    EventFilter userFilter = userSubscription.getFilter();
+    return isNotEmpty(userFilter.getJiraId()) && jiraIds.contains(userFilter.getJiraId());
   }
 }
