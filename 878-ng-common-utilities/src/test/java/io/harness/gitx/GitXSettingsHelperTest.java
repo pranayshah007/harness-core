90cc4d98458aba6f47b3189c18b5758583e2f6ce
Vivek Dixit <vivek.dixit@harness.io>
2023-04-17 17:15:10 +0530

    [feat]: [PIE-9453]: Added Uts

--- 878-ng-common-utilities/src/test/java/io/harness/gitx/GitXSettingsHelperTest.java
+++ 878-ng-common-utilities/src/test/java/io/harness/gitx/GitXSettingsHelperTest.java
@@ -1,46 +1,62 @@
 package io.harness.gitx;
 
+import static io.harness.annotations.dev.HarnessTeam.PIPELINE;
+import static io.harness.rule.OwnerRule.VIVEK_DIXIT;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+
 import io.harness.CategoryTest;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.category.element.UnitTests;
 import io.harness.context.GlobalContext;
+import io.harness.exception.InvalidRequestException;
 import io.harness.gitsync.beans.StoreType;
 import io.harness.gitsync.interceptor.GitEntityInfo;
 import io.harness.gitsync.interceptor.GitSyncBranchContext;
 import io.harness.manage.GlobalContextManager;
 import io.harness.ngsettings.client.remote.NGSettingsClient;
 import io.harness.rule.Owner;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
-import static io.harness.annotations.dev.HarnessTeam.PIPELINE;
-import static io.harness.rule.OwnerRule.VIVEK_DIXIT;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 @OwnedBy(PIPELINE)
 public class GitXSettingsHelperTest extends CategoryTest {
   @Mock private NGSettingsClient ngSettingsClient;
-  @InjectMocks GitXSettingsHelper gitXSettingsHelper;
+  @Spy @InjectMocks GitXSettingsHelper gitXSettingsHelper;
 
   private final String ACCOUNT_IDENTIFIER = "accountId";
   private final String ORG_IDENTIFIER = "orgId";
   private final String PROJ_IDENTIFIER = "projId";
 
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+  }
+
   @Test
   @Owner(developers = VIVEK_DIXIT)
   @Category(UnitTests.class)
   public void testEnforceGitExperienceIfApplicable() {
-    GitEntityInfo branchInfo = GitEntityInfo.builder().storeType(StoreType.REMOTE).build();
+    GitEntityInfo branchInfo = GitEntityInfo.builder().storeType(StoreType.INLINE).build();
     if (!GlobalContextManager.isAvailable()) {
       GlobalContextManager.set(new GlobalContext());
     }
     GlobalContextManager.upsertGlobalContextRecord(GitSyncBranchContext.builder().gitBranchInfo(branchInfo).build());
-    gitXSettingsHelper.enforceGitExperienceIfApplicable(ACCOUNT_IDENTIFIER, ORG_IDENTIFIER, PROJ_IDENTIFIER);
+    doReturn(true).when(gitXSettingsHelper).isGitExperienceEnforcedInSettings(any(), any(), any());
 
-    verify(gitXSettingsHelper, times(0)).isGitExperienceEnforcedInSettings(any(), any(), any());
+    assertThatThrownBy(
+        () -> gitXSettingsHelper.enforceGitExperienceIfApplicable(ACCOUNT_IDENTIFIER, ORG_IDENTIFIER, PROJ_IDENTIFIER))
+        .isInstanceOf(InvalidRequestException.class)
+        .hasMessage(
+            "Git Experience is enforced for the current scope with accountId: accountId, orgIdentifier: orgId and projIdentifier: projId. Hence Interaction with INLINE entities is forbidden.");
+    ;
   }
 }
