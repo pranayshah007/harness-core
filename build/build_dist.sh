5575462f6d579b7ea02da99e44456680a48f0510
Vishal Bhatia <vishal.bhatia@harness.io>
2023-05-23 23:45:56 -0400

    feat:[IE-202]: adding build files

--- build/build_dist.sh
+++ build/build_dist.sh
@@ -193,21 +193,23 @@ function copy_eventsapi-monitor_jars(){
 
 function copy_template_service_jars(){
 
-  cp ${HOME}/.bazel-dirs/bin/template-service/module_deploy.jar template-service-capsule.jar
-  cp ../../template-service/config.yml .
-  cp ../../template-service/keystore.jks .
-  cp ../../template-service/key.pem .
-  cp ../../template-service/cert.pem .
-  cp ../../template-service/src/main/resources/redisson-jcache.yaml .
-  cp ../../template-service/src/main/resources/jfr/default.jfc .
-  cp ../../template-service/src/main/resources/jfr/profile.jfc .
+  cp ${BAZEL_BIN}/template-service/service/module_deploy.jar template-service-capsule.jar
+  cp ../../template-service/config/config.yml .
+  cp ../../template-service/config/keystore.jks .
+  cp ../../template-service/config/key.pem .
+  cp ../../template-service/config/cert.pem .
+  cp ../../template-service/service/src/main/resources/redisson-jcache.yaml .
+  cp ../../template-service/service/src/main/resources/jfr/default.jfc .
+  cp ../../template-service/service/src/main/resources/jfr/profile.jfc .
 
-  cp ../../dockerization/template-service/Dockerfile-template-service-cie-jdk ./Dockerfile-cie-jdk
   cp ../../dockerization/base-images/apm/inject-onprem-apm-bins-into-dockerimage.sh .
   cp ../../dockerization/base-images/apm/inject-saas-apm-bins-into-dockerimage.sh .
-  cp -r ../../dockerization/template-service/scripts/ .
-  copy_common_files
-  cd ../..
+
+  cp ../../template-service/build/container/Dockerfile-template-service-cie-jdk ./Dockerfile-cie-jdk
+  cp -r ../../template-service/build/container/scripts/ .
+
+  cp ../../protocol.info .
+  java -jar template-service-capsule.jar scan-classpath-metadata
 
 }
 
