0bf796d213b657d7bd2c19370505abafd3a067cd
Shashank Singh <shashank.singh@harness.io>
2023-05-25 13:51:34 +0530

    [feat]: [PL-31462]: Fixed bug with filtering only CG users in list CG users call
    Signed-off-by: Shashank Singh <shashank.singh@harness.io>

--- 400-rest/src/test/java/software/wings/resources/UserResourceTest.java
+++ 400-rest/src/test/java/software/wings/resources/UserResourceTest.java
@@ -167,8 +167,8 @@ public class UserResourceTest extends WingsBaseTest {
     PageRequest pageRequest = mock(PageRequest.class);
     when(pageRequest.getOffset()).thenReturn("0");
     when(pageRequest.getPageSize()).thenReturn(30);
-    when(
-        USER_SERVICE.listUsers(any(), any(), any(), anyInt(), anyInt(), anyBoolean(), anyBoolean(), anyBoolean(), true))
+    when(USER_SERVICE.listUsers(
+             any(), any(), any(), anyInt(), anyInt(), anyBoolean(), anyBoolean(), anyBoolean(), anyBoolean()))
         .thenReturn(aPageResponse().withResponse(asList(anUser().build())).build());
     userResource.list(pageRequest, UUIDGenerator.generateUuid(), null, false, false);
     verify(USER_SERVICE).getTotalUserCount(any(), anyBoolean(), true, true);
