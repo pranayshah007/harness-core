6eedc5fac582b2f74474ac77ce301f04f40222c1
Sahil Hindwani <sahil.hindwani@harness.io>
2023-05-30 23:17:09 +0530

    [CDS-0]: Fix issue try

--- 125-cd-nextgen/src/main/java/io/harness/cdng/aws/sam/DownloadManifestsStep.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/aws/sam/DownloadManifestsStep.java
@@ -1,7 +1,14 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
 package io.harness.cdng.aws.sam;
 
-import com.google.inject.Inject;
-import graphql.execution.AsyncExecutionStrategy;
+import static io.harness.ci.commonconstants.CIExecutionConstants.GIT_CLONE_STEP_ID;
+
 import io.harness.beans.steps.stepinfo.GitCloneStepInfo;
 import io.harness.cdng.manifest.ManifestType;
 import io.harness.cdng.manifest.steps.outcome.ManifestsOutcome;
@@ -11,6 +18,7 @@ import io.harness.cdng.manifest.yaml.ManifestOutcome;
 import io.harness.cdng.manifest.yaml.ValuesManifestOutcome;
 import io.harness.cdng.steps.EmptyStepParameters;
 import io.harness.cdng.stepsdependency.constants.OutcomeExpressionConstants;
+import io.harness.data.structure.UUIDGenerator;
 import io.harness.executions.steps.ExecutionNodeType;
 import io.harness.plancreator.steps.common.SpecParameters;
 import io.harness.plancreator.steps.common.StepElementParameters;
@@ -21,6 +29,7 @@ import io.harness.pms.contracts.execution.AsyncExecutableResponse;
 import io.harness.pms.contracts.execution.Status;
 import io.harness.pms.contracts.steps.StepCategory;
 import io.harness.pms.contracts.steps.StepType;
+import io.harness.pms.execution.utils.AmbianceUtils;
 import io.harness.pms.sdk.core.resolver.RefObjectUtils;
 import io.harness.pms.sdk.core.resolver.outcome.OutcomeService;
 import io.harness.pms.sdk.core.steps.io.StepInputPackage;
@@ -32,153 +41,159 @@ import io.harness.yaml.extended.ci.codebase.Build;
 import io.harness.yaml.extended.ci.codebase.BuildType;
 import io.harness.yaml.extended.ci.codebase.impl.BranchBuildSpec;
 
+import com.google.inject.Inject;
+import graphql.execution.AsyncExecutionStrategy;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import static io.harness.ci.commonconstants.CIExecutionConstants.GIT_CLONE_STEP_ID;
-
 public class DownloadManifestsStep implements AsyncExecutableWithRbac<StepElementParameters> {
+  public static final StepType STEP_TYPE = StepType.newBuilder()
+                                               .setType(ExecutionNodeType.DOWNLOAD_MANIFESTS.getYamlType())
+                                               .setStepCategory(StepCategory.STEP)
+                                               .build();
 
-    public static final StepType STEP_TYPE = StepType.newBuilder()
-            .setType(ExecutionNodeType.DOWNLOAD_MANIFESTS.getYamlType())
-            .setStepCategory(StepCategory.STEP)
+  @Inject GitCloneStep gitCloneStep;
+
+  @Inject private OutcomeService outcomeService;
+
+  @Override
+  public void validateResources(Ambiance ambiance, StepElementParameters stepParameters) {}
+
+  @Override
+  public AsyncExecutableResponse executeAsyncAfterRbac(
+      Ambiance ambiance, StepElementParameters stepParameters, StepInputPackage inputPackage) {
+    ManifestsOutcome manifestsOutcome =
+        (ManifestsOutcome) outcomeService
+            .resolveOptional(ambiance, RefObjectUtils.getOutcomeRefObject(OutcomeExpressionConstants.MANIFESTS))
+            .getOutcome();
+
+    AwsSamDirectoryManifestOutcome awsSamDirectoryManifestOutcome =
+        (AwsSamDirectoryManifestOutcome) getAwsSamDirectoryManifestOutcome(manifestsOutcome.values());
+
+    List<String> callbackIds = new ArrayList<>();
+
+    GitStoreConfig gitStoreConfig = (GitStoreConfig) awsSamDirectoryManifestOutcome.getStore();
+
+    Build build = Build.builder()
+                      .spec(BranchBuildSpec.builder().branch(gitStoreConfig.getBranch()).build())
+                      .type(BuildType.BRANCH)
+                      .build();
+
+    GitCloneStepInfo gitCloneStepInfo =
+        GitCloneStepInfo.builder()
+            .cloneDirectory(
+                ParameterField.<String>builder().value(awsSamDirectoryManifestOutcome.getIdentifier()).build())
+            .identifier(awsSamDirectoryManifestOutcome.getIdentifier())
+            .name(awsSamDirectoryManifestOutcome.getIdentifier())
+            .connectorRef(gitStoreConfig.getConnectorRef())
+            .repoName(gitStoreConfig.getRepoName())
+            .build(ParameterField.<Build>builder().value(build).build())
             .build();
 
-    @Inject
-    GitCloneStep gitCloneStep;
+    StepElementParameters stepElementParameters =
+        StepElementParameters.builder()
+            .name(awsSamDirectoryManifestOutcome.getIdentifier())
+            .spec(gitCloneStepInfo)
+            .identifier(GIT_CLONE_STEP_ID + awsSamDirectoryManifestOutcome.getIdentifier())
+            .build();
 
-    @Inject private OutcomeService outcomeService;
+    Ambiance ambiance1 =
+        buildAmbianceForGitClone(ambiance, GIT_CLONE_STEP_ID + awsSamDirectoryManifestOutcome.getIdentifier());
+    AsyncExecutableResponse samDirectoryAsyncExecutableResponse =
+        gitCloneStep.executeAsyncAfterRbac(ambiance1, stepElementParameters, inputPackage);
 
-    @Override
-    public void validateResources(Ambiance ambiance, StepElementParameters stepParameters) {
+    callbackIds.addAll(samDirectoryAsyncExecutableResponse.getCallbackIdsList());
 
+    ValuesManifestOutcome valuesManifestOutcome =
+        (ValuesManifestOutcome) getAwsSamValuesManifestOutcome(manifestsOutcome.values());
+
+    if (valuesManifestOutcome != null) {
+      GitStoreConfig valuesGitStoreConfig = (GitStoreConfig) valuesManifestOutcome.getStore();
+
+      Build valuesBuild = Build.builder()
+                              .spec(BranchBuildSpec.builder().branch(valuesGitStoreConfig.getBranch()).build())
+                              .type(BuildType.BRANCH)
+                              .build();
+
+      GitCloneStepInfo valuesGitCloneStepInfo =
+          GitCloneStepInfo.builder()
+              .cloneDirectory(ParameterField.<String>builder().value(valuesManifestOutcome.getIdentifier()).build())
+              .identifier(valuesManifestOutcome.getIdentifier())
+              .name(valuesManifestOutcome.getIdentifier())
+              .connectorRef(valuesGitStoreConfig.getConnectorRef())
+              .repoName(valuesGitStoreConfig.getRepoName())
+              .build(ParameterField.<Build>builder().value(valuesBuild).build())
+              //                .outputFilePathsContent(ParameterField.<List<String>>builder().value(Arrays.asList(getValuesPathFromValuesManifestOutcome(valuesManifestOutcome))).build())
+              .build();
+
+      StepElementParameters valuesStepElementParameters =
+          StepElementParameters.builder()
+              .name(valuesManifestOutcome.getIdentifier())
+              .spec(valuesGitCloneStepInfo)
+              .identifier(GIT_CLONE_STEP_ID + valuesManifestOutcome.getIdentifier())
+              .build();
+
+      Ambiance ambiance2 =
+          buildAmbianceForGitClone(ambiance, GIT_CLONE_STEP_ID + valuesManifestOutcome.getIdentifier());
+
+      AsyncExecutableResponse valuesAsyncExecutableResponse =
+          gitCloneStep.executeAsyncAfterRbac(ambiance2, valuesStepElementParameters, inputPackage);
+      callbackIds.addAll(valuesAsyncExecutableResponse.getCallbackIdsList());
     }
 
-    @Override
-    public AsyncExecutableResponse executeAsyncAfterRbac(Ambiance ambiance, StepElementParameters stepParameters, StepInputPackage inputPackage) {
+    return AsyncExecutableResponse.newBuilder()
+        .addAllCallbackIds(callbackIds)
+        .setStatus(samDirectoryAsyncExecutableResponse.getStatus())
+        .addAllLogKeys(samDirectoryAsyncExecutableResponse.getLogKeysList())
+        .build();
+  }
 
-        ManifestsOutcome manifestsOutcome = (ManifestsOutcome) outcomeService.resolveOptional(
-                ambiance, RefObjectUtils.getOutcomeRefObject(OutcomeExpressionConstants.MANIFESTS)).getOutcome();
+  public String getValuesPathFromValuesManifestOutcome(ValuesManifestOutcome valuesManifestOutcome) {
+    GitStoreConfig gitStoreConfig = (GitStoreConfig) valuesManifestOutcome.getStore();
+    String samDirectoryPath =
+        "/harness/" + valuesManifestOutcome.getIdentifier() + "/" + gitStoreConfig.getPaths().getValue().get(0);
+    return samDirectoryPath;
+  }
 
-        AwsSamDirectoryManifestOutcome awsSamDirectoryManifestOutcome = (AwsSamDirectoryManifestOutcome) getAwsSamDirectoryManifestOutcome(manifestsOutcome.values());
+  @Override
+  public Class<StepElementParameters> getStepParametersClass() {
+    return StepElementParameters.class;
+  }
 
-        List<String> callbackIds = new ArrayList<>();
+  @Override
+  public void handleAbort(
+      Ambiance ambiance, StepElementParameters stepParameters, AsyncExecutableResponse executableResponse) {}
 
-        GitStoreConfig gitStoreConfig = (GitStoreConfig) awsSamDirectoryManifestOutcome.getStore();
+  @Override
+  public StepResponse handleAsyncResponse(
+      Ambiance ambiance, StepElementParameters stepParameters, Map<String, ResponseData> responseDataMap) {
+    return StepResponse.builder().status(Status.SUCCEEDED).build();
+  }
 
-        Build build = Build.builder()
-                .spec(BranchBuildSpec.builder()
-                        .branch(gitStoreConfig.getBranch())
-                        .build())
-                .type(BuildType.BRANCH)
-                .build();
+  public ManifestOutcome getAwsSamDirectoryManifestOutcome(Collection<ManifestOutcome> manifestOutcomes) {
+    List<ManifestOutcome> manifestOutcomeList =
+        manifestOutcomes.stream()
+            .filter(manifestOutcome -> ManifestType.AwsSamDirectory.equals(manifestOutcome.getType()))
+            .collect(Collectors.toList());
+    return manifestOutcomeList.isEmpty() ? null : manifestOutcomeList.get(0);
+  }
 
-        GitCloneStepInfo gitCloneStepInfo = GitCloneStepInfo.builder()
-                .cloneDirectory(ParameterField.<String>builder().value(awsSamDirectoryManifestOutcome.getIdentifier()).build())
-                .identifier(awsSamDirectoryManifestOutcome.getIdentifier())
-                .name(awsSamDirectoryManifestOutcome.getIdentifier())
-                .connectorRef(gitStoreConfig.getConnectorRef())
-                .repoName(gitStoreConfig.getRepoName())
-                .build(ParameterField.<Build>builder().value(build).build())
-                .build();
+  public ManifestOutcome getAwsSamValuesManifestOutcome(Collection<ManifestOutcome> manifestOutcomes) {
+    List<ManifestOutcome> manifestOutcomeList =
+        manifestOutcomes.stream()
+            .filter(manifestOutcome -> ManifestType.VALUES.equals(manifestOutcome.getType()))
+            .collect(Collectors.toList());
+    return manifestOutcomeList.isEmpty() ? null : manifestOutcomeList.get(0);
+  }
 
-        StepElementParameters stepElementParameters = StepElementParameters.builder()
-                .name(awsSamDirectoryManifestOutcome.getIdentifier())
-                .spec(gitCloneStepInfo)
-                .identifier(GIT_CLONE_STEP_ID + awsSamDirectoryManifestOutcome.getIdentifier())
-                .build();
-
-        List<Level> samDirectoryLevels = new ArrayList<>();
-        samDirectoryLevels.add(Level.newBuilder(ambiance.getLevels(ambiance.getLevelsCount()-1)).setIdentifier(GIT_CLONE_STEP_ID + awsSamDirectoryManifestOutcome.getIdentifier()).build());
-        Ambiance ambiance1 = Ambiance.newBuilder(ambiance).addAllLevels(samDirectoryLevels).build();
-
-        AsyncExecutableResponse samDirectoryAsyncExecutableResponse =
-                gitCloneStep.executeAsyncAfterRbac(ambiance1, stepElementParameters, inputPackage);
-
-        callbackIds.addAll(samDirectoryAsyncExecutableResponse.getCallbackIdsList());
-
-        ValuesManifestOutcome valuesManifestOutcome = (ValuesManifestOutcome) getAwsSamValuesManifestOutcome(manifestsOutcome.values());
-
-        if (valuesManifestOutcome != null) {
-            GitStoreConfig valuesGitStoreConfig = (GitStoreConfig) valuesManifestOutcome.getStore();
-
-            Build valuesBuild = Build.builder()
-                    .spec(BranchBuildSpec.builder()
-                            .branch(valuesGitStoreConfig.getBranch())
-                            .build())
-                    .type(BuildType.BRANCH)
-                    .build();
-
-            GitCloneStepInfo valuesGitCloneStepInfo = GitCloneStepInfo.builder()
-                    .cloneDirectory(ParameterField.<String>builder().value(valuesManifestOutcome.getIdentifier()).build())
-                    .identifier(valuesManifestOutcome.getIdentifier())
-                    .name(valuesManifestOutcome.getIdentifier())
-                    .connectorRef(valuesGitStoreConfig.getConnectorRef())
-                    .repoName(valuesGitStoreConfig.getRepoName())
-                    .build(ParameterField.<Build>builder().value(valuesBuild).build())
-//                .outputFilePathsContent(ParameterField.<List<String>>builder().value(Arrays.asList(getValuesPathFromValuesManifestOutcome(valuesManifestOutcome))).build())
-                    .build();
-
-            StepElementParameters valuesStepElementParameters = StepElementParameters.builder()
-                    .name(valuesManifestOutcome.getIdentifier())
-                    .spec(valuesGitCloneStepInfo)
-                    .identifier(GIT_CLONE_STEP_ID + valuesManifestOutcome.getIdentifier())
-                    .build();
-
-            List<Level> valuesLevels = new ArrayList<>();
-            valuesLevels.add(Level.newBuilder(ambiance.getLevels(ambiance.getLevelsCount()-1)).setIdentifier(GIT_CLONE_STEP_ID + valuesManifestOutcome.getIdentifier()).build());
-            Ambiance ambiance2 = Ambiance.newBuilder(ambiance).addAllLevels(valuesLevels).build();
-
-            AsyncExecutableResponse valuesAsyncExecutableResponse =
-                    gitCloneStep.executeAsyncAfterRbac(ambiance2, valuesStepElementParameters, inputPackage);
-            callbackIds.addAll(valuesAsyncExecutableResponse.getCallbackIdsList());
-        }
-
-
-
-        return AsyncExecutableResponse.newBuilder().addAllCallbackIds(callbackIds)
-                .setStatus(samDirectoryAsyncExecutableResponse.getStatus())
-                .addAllLogKeys(samDirectoryAsyncExecutableResponse.getLogKeysList())
-                .build();
-    }
-
-    public String getValuesPathFromValuesManifestOutcome(ValuesManifestOutcome valuesManifestOutcome) {
-        GitStoreConfig gitStoreConfig = (GitStoreConfig) valuesManifestOutcome.getStore();
-        String samDirectoryPath = "/harness/" + valuesManifestOutcome.getIdentifier() +
-                "/" + gitStoreConfig.getPaths().getValue().get(0);
-        return samDirectoryPath;
-    }
-
-    @Override
-    public Class<StepElementParameters> getStepParametersClass() {
-        return StepElementParameters.class;
-    }
-
-    @Override
-    public void handleAbort(Ambiance ambiance, StepElementParameters stepParameters, AsyncExecutableResponse executableResponse) {
-
-    }
-
-    @Override
-    public StepResponse handleAsyncResponse(Ambiance ambiance, StepElementParameters stepParameters, Map<String, ResponseData> responseDataMap) {
-        return StepResponse.builder()
-                .status(Status.SUCCEEDED)
-                .build();
-    }
-
-    public ManifestOutcome getAwsSamDirectoryManifestOutcome(Collection<ManifestOutcome> manifestOutcomes) {
-        List<ManifestOutcome> manifestOutcomeList =
-                manifestOutcomes.stream()
-                        .filter(manifestOutcome -> ManifestType.AwsSamDirectory.equals(manifestOutcome.getType()))
-                        .collect(Collectors.toList());
-        return manifestOutcomeList.isEmpty() ? null : manifestOutcomeList.get(0);
-    }
-
-    public ManifestOutcome getAwsSamValuesManifestOutcome(Collection<ManifestOutcome> manifestOutcomes) {
-        List<ManifestOutcome> manifestOutcomeList =
-                manifestOutcomes.stream()
-                        .filter(manifestOutcome -> ManifestType.VALUES.equals(manifestOutcome.getType()))
-                        .collect(Collectors.toList());
-        return manifestOutcomeList.isEmpty() ? null : manifestOutcomeList.get(0);
-    }
+  // Todo(Sahil): Come up with a better way to pass information to child
+  private Ambiance buildAmbianceForGitClone(Ambiance ambiance, String identifier) {
+    Level level = Level.newBuilder()
+                      .setIdentifier(identifier)
+                      .setSkipExpressionChain(true)
+                      .setSetupId(UUIDGenerator.generateUuid())
+                      .setRuntimeId(UUIDGenerator.generateUuid())
+                      .build();
+    return AmbianceUtils.cloneForChild(ambiance, level);
+  }
 }
