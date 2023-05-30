e73535aa33e4f1a43785052ee56ee95ff59b23c4
Vishal Bhatia <vishal.bhatia@harness.io>
2023-05-30 11:36:13 -0400

    feat:[IE-202]: adding build files

--- build/build_jar.sh
+++ build/build_jar.sh
@@ -25,7 +25,6 @@ service_map["iacm-manager"]="310-iacm-manager/app"
 service_map["sto-manager"]="315-sto-manager/app"
 service_map["ci-manager"]="332-ci-manager/app"
 service_map["batch-processing"]="batch-processing/service"
-service_map["audit-event-streaming"]="audit-event-streaming/service"
 service_map["ce-nextgen"]="ce-nextgen/service"
 service_map["debezium-service"]="debezium-service/service"
 service_map["idp-service"]="idp-service/src/main/java/io/harness/idp/app"
@@ -36,8 +35,8 @@ service_map["template-service"]="template-service/service"
 
 
 key="${SERVICE_NAME}"
-echo $key
 for key in "${!service_map[@]}"; do
+  echo ${service_map[$key]}
   bazel ${bazelrc} build //${service_map[$key]}":module_deploy.jar" ${BAZEL_ARGUMENTS}
 done
 
