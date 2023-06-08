6b4cecfd4ead6dbc38c379d6b160824f6ae8f1d2
achyuthprakash <achyuth.prakash@harness.io>
2023-06-08 13:08:48 +0530

    fix: [CDS-71025]: removed unused method

--- 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/artifacts/ami/AMIArtifactDelegateRequest.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/artifacts/ami/AMIArtifactDelegateRequest.java
@@ -7,7 +7,6 @@
 
 package io.harness.delegate.task.artifacts.ami;
 
-import static io.harness.data.structure.EmptyPredicate.isEmpty;
 import static io.harness.delegate.beans.connector.ConnectorCapabilityBaseHelper.populateDelegateSelectorCapability;
 
 import io.harness.annotations.dev.HarnessTeam;
@@ -25,11 +24,8 @@ import io.harness.expression.ExpressionEvaluator;
 import io.harness.security.encryption.EncryptedDataDetail;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -92,10 +88,6 @@ public class AMIArtifactDelegateRequest implements ArtifactSourceDelegateRequest
    * Delegate Selectors.
    */
   List<String> delegateSelectors;
-
-  public Set<String> getDelegateSelectors() {
-    return isEmpty(delegateSelectors) ? Collections.EMPTY_SET : delegateSelectors.stream().collect(Collectors.toSet());
-  }
 
   @Override
   public List<ExecutionCapability> fetchRequiredExecutionCapabilities(ExpressionEvaluator maskingEvaluator) {
