0513fd651865402f7fa46d512dea923c61b64a94
Jenny James <jenny.james@harness.io>
2023-06-11 21:08:35 -0700

    updated

--- 400-rest/src/main/java/io/harness/delegate/queueservice/DelegateTaskQueueService.java
+++ 400-rest/src/main/java/io/harness/delegate/queueservice/DelegateTaskQueueService.java
@@ -48,7 +48,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -112,7 +111,6 @@ public class DelegateTaskQueueService implements DelegateServiceQueue<DelegateTa
                          .build())
               .filter(this::isResourceAvailableToAssignTask)
               .collect(toList());
-      log.info("Dequeue list {}", delegateTasksDequeueList);
       delegateTasksDequeueList.forEach(this::acknowledgeAndProcessDelegateTask);
       return true;
     } catch (Exception e) {
