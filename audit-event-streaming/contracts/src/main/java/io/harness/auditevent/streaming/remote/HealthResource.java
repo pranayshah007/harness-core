a5a4744537d24fbbb4b82feaa1e17f5bfd70c811
Meenakshi Raikwar <meenakshi.raikwar@harness.io>
2023-06-14 14:11:40 +0530

    feat: [PL-39021]: Added Health Resource for Audit Streaming Service

---
+++ audit-event-streaming/contracts/src/main/java/io/harness/auditevent/streaming/remote/HealthResource.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.auditevent.streaming.remote;
+
+import static io.harness.annotations.dev.HarnessTeam.PL;
+
+import io.harness.annotations.ExposeInternalException;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.ng.core.dto.ResponseDTO;
+import io.harness.security.annotations.PublicApi;
+
+import com.codahale.metrics.annotation.ExceptionMetered;
+import com.codahale.metrics.annotation.Timed;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Api("health")
+@Path("/health")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@ExposeInternalException
+@PublicApi
+@OwnedBy(PL)
+public interface HealthResource {
+  @GET
+  @Timed
+  @ExceptionMetered
+  @ApiOperation(value = "get health for Audit Streaming service", nickname = "getAuditStreamingHealthStatus")
+  ResponseDTO<String> get() throws Exception;
+}
