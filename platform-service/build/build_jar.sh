cd563464c940d7182e66bd92a7800d2e92871ffc
Vishal Bhatia <vishal.bhatia@harness.io>
2023-07-02 18:33:18 -0400

    updated docker file for platform service

--- platform-service/build/build_jar.sh
+++ platform-service/build/build_jar.sh
@@ -10,7 +10,8 @@ if [ "${PLATFORM}" == "jenkins" ]; then
   bazelrc=--bazelrc=bazelrc.remote
   bash scripts/bazel/testDistribute.sh
 fi
+bash scripts/bazel/testDistribute.sh
 BAZEL_DIRS=${HOME}/.bazel-dirs
 BAZEL_ARGUMENTS="--show_timestamps --announce_rc --experimental_convenience_symlinks=normal --remote_download_outputs=all --symlink_prefix=${BAZEL_DIRS}/"
 PLATFORM_SERVICE_MODULE="//platform-service/service:module_deploy.jar"
-bazel ${bazelrc} build $PLATFORM_SERVICE_MODULE ${BAZEL_ARGUMENTS}
+bazel --bazelrc=bazelrc.remote build $PLATFORM_SERVICE_MODULE ${BAZEL_ARGUMENTS}
