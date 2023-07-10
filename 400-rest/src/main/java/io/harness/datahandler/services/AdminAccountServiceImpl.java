9915a9ccb493b95eb2d0a3312c85c4cc15196f11
VikasMaddukuriHarness <vikas.maddukuri@harness.io>
2023-07-10 18:58:03 +0530

    fix: [PL-39879]: Remove unused import (#50350)

--- 400-rest/src/main/java/io/harness/datahandler/services/AdminAccountServiceImpl.java
+++ 400-rest/src/main/java/io/harness/datahandler/services/AdminAccountServiceImpl.java
@@ -11,8 +11,6 @@ import static io.harness.annotations.dev.HarnessModule._955_ACCOUNT_MGMT;
 import static io.harness.annotations.dev.HarnessTeam.PL;
 import static io.harness.beans.PageRequest.PageRequestBuilder.aPageRequest;
 
-import static software.wings.beans.AccountStatus.MARKED_FOR_DELETION;
-
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.annotations.dev.TargetModule;
 import io.harness.beans.FeatureFlag;
