db55654b01a336424cd6ce0f01330727360966d8
Abhinav Hinger <abhinav.hinger@harness.io>
2023-07-19 18:25:29 +0530

    fix: [CDS-73357]: update clean up script to clean up nested processes (#50684)
    * fix: [CDS-73357]: fix cleanup script to kill nested processes
    
    * use for loop for better readability
    
    * inc build

--- 400-rest/src/test/java/software/wings/core/ssh/executors/ChildProcessExecutorTest.java
+++ 400-rest/src/test/java/software/wings/core/ssh/executors/ChildProcessExecutorTest.java
@@ -10,7 +10,9 @@ package software.wings.core.ssh.executors;
 import static io.harness.annotations.dev.HarnessTeam.CDC;
 import static io.harness.rule.OwnerRule.PRABU;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.category.element.UnitTests;
@@ -19,12 +21,19 @@ import io.harness.shell.ChildProcessStopper;
 
 import software.wings.WingsBaseTest;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.mockito.Mockito;
-import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.zeroturnaround.exec.ProcessExecutor;
+import org.zeroturnaround.exec.ProcessResult;
 
 @OwnedBy(CDC)
 public class ChildProcessExecutorTest extends WingsBaseTest {
@@ -33,29 +42,47 @@ public class ChildProcessExecutorTest extends WingsBaseTest {
   private ProcessExecutor processExecutor = Mockito.mock(ProcessExecutor.class);
   private ChildProcessStopper childProcessStopper =
       new ChildProcessStopper(FILE_NAME, workingDirectory, processExecutor);
+  private ListAppender<ILoggingEvent> listAppender;
 
   @Test
   @Owner(developers = PRABU)
   @Category(UnitTests.class)
-  public void testExecuteScript() throws IllegalAccessException {
+  public void testExecuteScript() throws IllegalAccessException, IOException, InterruptedException, TimeoutException {
     Process process = mock(Process.class);
-    Logger mockLogger = mock(Logger.class);
-    setStaticFieldValue(ChildProcessStopper.class, "log", mockLogger);
+    when(processExecutor.execute()).thenReturn(new ProcessResult(0, null));
+    Logger logger = (Logger) LoggerFactory.getLogger(ChildProcessStopper.class);
+    listAppender = new ListAppender<>();
+    listAppender.start();
+    logger.addAppender(listAppender);
     childProcessStopper.stop(process);
     Mockito.verify(processExecutor).command("/bin/bash", "kill-" + FILE_NAME);
-    Mockito.verify(mockLogger)
-        .info("Kill child processes command: {}",
-            "list_descendants ()\n"
-                + "{\n"
-                + "  local children=$(ps -ef | grep -v grep | grep $1 | awk '{print $2}')\n"
-                + "  kill -9 ${children[0]}\n"
-                + "\n"
-                + "  for (( c=1; c<${#children[@]} ; c++ ))\n"
-                + "  do\n"
-                + "    list_descendants ${children[c]}\n"
-                + "  done\n"
-                + "}\n"
-                + "\n"
-                + "list_descendants $(ps -ef | grep -v grep | grep -m1 FILE | awk '{print $2}')");
+    assertThat(listAppender.list.get(0).getFormattedMessage())
+        .isEqualTo("Kill child processes command: list_descendants () {\n"
+            + "    echo \"Parent process id: $1\"\n"
+            + "    local -a children=()\n"
+            + "    read -ra children <<< \"$(ps -ef | grep -v grep | grep $1 | awk '{print $2}' | tr '\\n' ' ')\"\n"
+            + "    echo \"Number of children processes: ${#children[@]}\"\n"
+            + "    local c\n"
+            + "\n"
+            + "    for (( c=1; c<${#children[@]} ; c++ ))\n"
+            + "    do\n"
+            + "        list_descendants ${children[c]}\n"
+            + "    done\n"
+            + "\n"
+            + "    first_child=${children[0]}\n"
+            + "    if kill -0 $first_child > /dev/null 2>&1; then\n"
+            + "        if kill -9 $first_child; then\n"
+            + "            echo \"Termination successful for PID: $first_child\"\n"
+            + "        else\n"
+            + "            echo \"Failed to terminate PID: $first_child\"\n"
+            + "        fi\n"
+            + "    else\n"
+            + "        echo \"Process with PID $first_child is not running. Skipping termination.\"\n"
+            + "    fi\n"
+            + "}\n"
+            + "\n"
+            + "list_descendants $(ps -ef | grep -v grep | grep -m1 FILE | awk '{print $2}')");
+    assertThat(listAppender.list.get(0).getLevel()).isEqualTo(Level.INFO);
+    assertThat(listAppender.list.get(1).getFormattedMessage()).isEqualTo("Kill child processes command exited with: 0");
   }
 }
