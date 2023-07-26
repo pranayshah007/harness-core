5bbeee4d330f6ed181f31677b312deafd175f28d
Archit Singla <archit.singla@harness.io>
2023-07-26 21:06:28 +0530

    [fix]: [CDS-72887]: Adding FF to control behaviour of ParameterFieldExpressionProcessor in CD service (#51023)
    * [fix]: [CDS-72887]: Adding FF to control behaviour of ParameterFieldExpressionProcessor in CD service
    
    * [fix]: [CDS-72887]: Adding FF to control behaviour of ParameterFieldExpressionProcessor in CD service

--- 125-cd-nextgen/src/main/java/io/harness/cdng/gitops/UpdateReleaseRepoStep.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/gitops/UpdateReleaseRepoStep.java
@@ -265,7 +265,7 @@ public class UpdateReleaseRepoStep extends CdTaskExecutable<NGGitOpsResponse> {
 
           if (copyParameter.getValue() != null) {
             populateVariables(
-                copyParameter, flattennedVariables, variableEntry.getKey(), copyParameter.fetchFinalValue().toString());
+                copyParameter, flattennedVariables, variableEntry.getKey(), copyParameter.getValue().toString());
           }
 
           for (String key : flattennedVariables.keySet()) {
