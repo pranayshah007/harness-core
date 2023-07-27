d9cc785b6e02bb3e68d19f0b3bd3b87ab580775c
Dhruv <dhruv@harness.io>
2023-07-27 12:28:48 +0530

    feat: [OIP-529]: Adding release scripts for oip jira project

--- srm-service/release/release-branch-update-oip-jiras.sh
+++ srm-service/release/release-branch-update-oip-jiras.sh
@@ -12,18 +12,18 @@ echo $KEYS
 #FIX_OIP_VERSION value to be same as used in release-branch-create-oip-version.sh
 FIX_OIP_VERSION="OIP_""$VERSION"00
 
-#for KEY in ${KEYS}
-#  do
-#    echo "$KEY"
-#    response=$(curl -q -X PUT https://harness.atlassian.net/rest/api/2/issue/${KEY} --write-out '%{http_code}' --user ${JIRA_USERNAME}:${JIRA_PASSWORD} -H "Content-Type: application/json" -d '{
-#    "update": {
-#    "fixVersions": [
-#      {"add":
-#        {"name": "'"$FIX_OIP_VERSION"'" }
-#      }]}}')
-#    if [[ "$response" -eq 204 ]] ; then
-#      echo "$KEY fixVersion set to $FIX_OIP_VERSION"
-#    elif [[ "$response" -eq 400 ]] ; then
-#      echo "Could not set fixVersion on $KEY - field hidden for the issue type"
-#    fi
-#  done
+for KEY in ${KEYS}
+  do
+    echo "$KEY"
+    response=$(curl -q -X PUT https://harness.atlassian.net/rest/api/2/issue/${KEY} --write-out '%{http_code}' --user ${JIRA_USERNAME}:${JIRA_PASSWORD} -H "Content-Type: application/json" -d '{
+    "update": {
+    "fixVersions": [
+      {"add":
+        {"name": "'"$FIX_OIP_VERSION"'" }
+      }]}}')
+    if [[ "$response" -eq 204 ]] ; then
+      echo "$KEY fixVersion set to $FIX_OIP_VERSION"
+    elif [[ "$response" -eq 400 ]] ; then
+      echo "Could not set fixVersion on $KEY - field hidden for the issue type"
+    fi
+  done
