7fce46b048f294ad1b9b78f17d21e308474631c4
Vinícius Calasans <vinicius.calasans@harness.io>
2023-07-27 12:34:37 -0300

    fix: [CDS-50176]: Set polling trigger status to success when resetting failedCount to zero (#51041)
    * [CDS-50176]: Set polling trigger status to success when resetting failedCount to zero
    
    * [CDS-50176]: Bump build number

--- 120-ng-manager/src/main/java/io/harness/ng/webhook/polling/PollingResponseHandler.java
+++ 120-ng-manager/src/main/java/io/harness/ng/webhook/polling/PollingResponseHandler.java
@@ -154,6 +154,8 @@ public class PollingResponseHandler {
 
     if (pollingDocument.getFailedAttempts() > 0) {
       pollingService.updateFailedAttempts(accountId, pollDocId, 0);
+      pollingService.updateTriggerPollingStatus(
+          pollingDocument.getAccountId(), pollingDocument.getSignatures(), true, null, Collections.emptyList());
     }
 
     switch (pollingDocument.getPollingType()) {
