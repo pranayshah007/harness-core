fd258f055b967e367e419c2d223f998b7560fca4
Abhinav Singh <abhinav.singh2@harness.io>
2023-08-02 11:31:56 +0530

    git capability using scm

--- 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/k8s/K8sDeployRequest.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/delegate/task/k8s/K8sDeployRequest.java
@@ -21,7 +21,6 @@ import io.harness.delegate.beans.connector.gcp.GcpCapabilityHelper;
 import io.harness.delegate.beans.connector.helm.OciHelmConnectorDTO;
 import io.harness.delegate.beans.connector.k8Connector.K8sTaskCapabilityHelper;
 import io.harness.delegate.beans.connector.scm.GitCapabilityHelper;
-import io.harness.delegate.beans.connector.scm.adapter.ScmConnectorMapper;
 import io.harness.delegate.beans.executioncapability.ExecutionCapability;
 import io.harness.delegate.beans.executioncapability.ExecutionCapabilityDemander;
 import io.harness.delegate.beans.executioncapability.HelmInstallationCapability;
@@ -103,8 +102,7 @@ public interface K8sDeployRequest extends TaskParameters, ExecutionCapabilityDem
         GitStoreDelegateConfig gitStoreDelegateConfig =
             (GitStoreDelegateConfig) getManifestDelegateConfig().getStoreDelegateConfig();
         capabilities.addAll(GitCapabilityHelper.fetchRequiredExecutionCapabilities(
-            ScmConnectorMapper.toGitConfigDTO(gitStoreDelegateConfig.getGitConfigDTO()),
-            gitStoreDelegateConfig.getEncryptedDataDetails(), gitStoreDelegateConfig.getSshKeySpecDTO()));
+            gitStoreDelegateConfig, gitStoreDelegateConfig.getEncryptedDataDetails()));
         capabilities.addAll(EncryptedDataDetailsCapabilityHelper.fetchExecutionCapabilitiesForEncryptedDataDetails(
             gitStoreDelegateConfig.getEncryptedDataDetails(), maskingEvaluator));
       }
