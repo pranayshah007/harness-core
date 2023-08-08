23d1c9013140d0597bc18481cdfb91b8d60d6920
Shivam Negi <shivam.negi@harness.io>
2023-08-08 12:38:59 +0530

    feat: [CDS-74848]: Added the DTOs and entity class for Global Market Place

--- template-service/service/src/main/java/io/harness/template/services/NGTemplateSchemaServiceImpl.java
+++ template-service/service/src/main/java/io/harness/template/services/NGTemplateSchemaServiceImpl.java
@@ -167,55 +167,9 @@ public class NGTemplateSchemaServiceImpl implements NGTemplateSchemaService {
   }
 
   public void validateYamlSchemaInternal(TemplateEntity templateEntity) {
-    String accountIdentifier = templateEntity.getAccountIdentifier();
-    String projectIdentifier = templateEntity.getProjectIdentifier();
-    String orgIdentifier = templateEntity.getOrgIdentifier();
-    String templateYaml = templateEntity.getYaml();
-    long start = System.currentTimeMillis();
-    if (TemplateYamlSchemaMergeHelper.isFeatureFlagEnabled(
-            FeatureName.DISABLE_TEMPLATE_SCHEMA_VALIDATION, accountIdentifier, accountClient)) {
-      return;
-    }
-    try {
-      Scope scope = templateEntity.getTemplateScope() != null ? templateEntity.getTemplateScope()
-          : projectIdentifier != null                         ? Scope.PROJECT
-          : orgIdentifier != null                             ? Scope.ORG
-                                                              : Scope.ACCOUNT;
-
-      JsonNode schema;
-      // Use Static schema if ff is enabled.
-      if (TemplateYamlSchemaMergeHelper.isFeatureFlagEnabled(
-              FeatureName.PIE_STATIC_YAML_SCHEMA, accountIdentifier, accountClient)) {
-        schema = getStaticYamlSchema(accountIdentifier, orgIdentifier, projectIdentifier, templateEntity.getChildType(),
-            templateEntity.getTemplateEntityType(), scope, "v0");
-      } else {
-        // Use traditional way of generating schema if ff is off
-        schema = getTemplateSchema(accountIdentifier, projectIdentifier, orgIdentifier, scope,
-            templateEntity.getChildType(), templateEntity.getTemplateEntityType());
-      }
-      String schemaString = JsonPipelineUtils.writeJsonString(schema);
-      if (templateEntity.getTemplateEntityType().equals(TemplateEntityType.PIPELINE_TEMPLATE)) {
-        String pathToJsonNode = TEMPLATE_NODE + "/" + SPEC_NODE + "/" + STAGES_NODE;
-        yamlSchemaValidator.validate(templateYaml, schemaString,
-            TemplateYamlSchemaMergeHelper.isFeatureFlagEnabled(
-                FeatureName.DONT_RESTRICT_PARALLEL_STAGE_COUNT, accountIdentifier, accountClient),
-            allowedParallelStages, pathToJsonNode);
-      } else {
-        yamlSchemaValidator.validate(
-            templateYaml, schemaString, true, allowedParallelStages, PIPELINE_NODE + "/" + STAGES_NODE);
-      }
-    } catch (io.harness.yaml.validator.InvalidYamlException e) {
-      log.info("[TEMPLATE_SCHEMA] Schema validation took total time {}ms", System.currentTimeMillis() - start);
-      throw e;
-    } catch (Exception ex) {
-      log.error(ex.getMessage(), ex);
-      YamlSchemaErrorWrapperDTO errorWrapperDTO =
-          YamlSchemaErrorWrapperDTO.builder()
-              .schemaErrors(Collections.singletonList(
-                  YamlSchemaErrorDTO.builder().message(ex.getMessage()).fqn("$.pipeline").build()))
-              .build();
-      throw new io.harness.yaml.validator.InvalidYamlException(ex.getMessage(), ex, errorWrapperDTO, templateYaml);
-    }
+    validateYamlSchema(templateEntity.getAccountIdentifier(), templateEntity.getProjectIdentifier(),
+        templateEntity.getOrgIdentifier(), templateEntity.getYaml(), templateEntity.getTemplateScope(),
+        templateEntity.getChildType(), templateEntity.getTemplateEntityType());
   }
 
   public void validateYamlSchemaInternal(GlobalTemplateEntity templateEntity) {
@@ -356,34 +310,38 @@ public class NGTemplateSchemaServiceImpl implements NGTemplateSchemaService {
   }
 
   public void validateYamlSchemaInternal(GlobalTemplateEntity globalTemplateEntity) {
-    String accountIdentifier = globalTemplateEntity.getAccountIdentifier();
-    String projectIdentifier = globalTemplateEntity.getProjectIdentifier();
-    String orgIdentifier = globalTemplateEntity.getOrgIdentifier();
-    String templateYaml = globalTemplateEntity.getYaml();
+    validateYamlSchema(globalTemplateEntity.getAccountIdentifier(), globalTemplateEntity.getProjectIdentifier(),
+        globalTemplateEntity.getOrgIdentifier(), globalTemplateEntity.getYaml(),
+        globalTemplateEntity.getTemplateScope(), globalTemplateEntity.getChildType(),
+        globalTemplateEntity.getTemplateEntityType());
+  }
+
+  void validateYamlSchema(String accountIdentifier, String projectIdentifier, String orgIdentifier, String templateYaml,
+      Scope templateScope, String childType, TemplateEntityType templateEntityType) {
     long start = System.currentTimeMillis();
     if (TemplateYamlSchemaMergeHelper.isFeatureFlagEnabled(
             FeatureName.DISABLE_TEMPLATE_SCHEMA_VALIDATION, accountIdentifier, accountClient)) {
       return;
     }
     try {
-      Scope scope = globalTemplateEntity.getTemplateScope() != null ? globalTemplateEntity.getTemplateScope()
-          : projectIdentifier != null                               ? Scope.PROJECT
-          : orgIdentifier != null                                   ? Scope.ORG
-                                                                    : Scope.ACCOUNT;
+      Scope scope = templateScope != null ? templateScope
+          : projectIdentifier != null     ? Scope.PROJECT
+          : orgIdentifier != null         ? Scope.ORG
+                                          : Scope.ACCOUNT;
 
       JsonNode schema;
       // Use Static schema if ff is enabled.
       if (TemplateYamlSchemaMergeHelper.isFeatureFlagEnabled(
               FeatureName.PIE_STATIC_YAML_SCHEMA, accountIdentifier, accountClient)) {
-        schema = getStaticYamlSchema(accountIdentifier, orgIdentifier, projectIdentifier,
-            globalTemplateEntity.getChildType(), globalTemplateEntity.getTemplateEntityType(), scope, "v0");
+        schema = getStaticYamlSchema(
+            accountIdentifier, orgIdentifier, projectIdentifier, childType, templateEntityType, scope, "v0");
       } else {
         // Use traditional way of generating schema if ff is off
-        schema = getTemplateSchema(accountIdentifier, projectIdentifier, orgIdentifier, scope,
-            globalTemplateEntity.getChildType(), globalTemplateEntity.getTemplateEntityType());
+        schema = getTemplateSchema(
+            accountIdentifier, projectIdentifier, orgIdentifier, scope, childType, templateEntityType);
       }
       String schemaString = JsonPipelineUtils.writeJsonString(schema);
-      if (globalTemplateEntity.getTemplateEntityType().equals(TemplateEntityType.PIPELINE_TEMPLATE)) {
+      if (templateEntityType.equals(TemplateEntityType.PIPELINE_TEMPLATE)) {
         String pathToJsonNode = TEMPLATE_NODE + "/" + SPEC_NODE + "/" + STAGES_NODE;
         yamlSchemaValidator.validate(templateYaml, schemaString,
             TemplateYamlSchemaMergeHelper.isFeatureFlagEnabled(
