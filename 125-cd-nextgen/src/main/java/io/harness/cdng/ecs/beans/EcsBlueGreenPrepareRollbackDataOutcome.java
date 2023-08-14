b72914591574afde4555e435d9aea2073dd67594
Pragyesh Mishra <pragyesh.mishra@harness.io>
2023-08-14 21:30:19 +0530

    [fix]: [CDS-76795]: fix check

--- 125-cd-nextgen/src/main/java/io/harness/cdng/ecs/beans/EcsBlueGreenPrepareRollbackDataOutcome.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/ecs/beans/EcsBlueGreenPrepareRollbackDataOutcome.java
@@ -7,17 +7,17 @@
 
 package io.harness.cdng.ecs.beans;
 
-import com.fasterxml.jackson.annotation.JsonTypeName;
 import io.harness.annotation.RecasterAlias;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.pms.sdk.core.data.ExecutionSweepingOutput;
 import io.harness.pms.sdk.core.data.Outcome;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import java.util.List;
 import lombok.Builder;
 import lombok.Value;
 import org.springframework.data.annotation.TypeAlias;
-
-import java.util.List;
 
 @OwnedBy(HarnessTeam.CDP)
 @Value
