33c3f1257776eaa7d0976430a2cfba6b86e60631
Devesh Mishra <devesh.mishra@harness.io>
2023-08-28 20:39:42 +0530

    fix: [IDP-10]: test

--- idp-service/src/main/java/io/harness/idp/namespace/Handler.java
+++ idp-service/src/main/java/io/harness/idp/namespace/Handler.java
@@ -13,6 +13,7 @@ import static io.harness.mongo.iterator.MongoPersistenceIterator.SchedulingType.
 import static java.time.Duration.*;
 import static java.time.Duration.ofSeconds;
 
+//import io.harness.connector.entities.embedded.vaultconnector.VaultConnector;
 import io.harness.idp.namespace.beans.entity.NamespaceEntity;
 import io.harness.iterator.PersistenceIteratorFactory;
 import io.harness.mongo.iterator.MongoPersistenceIterator;
@@ -24,6 +25,8 @@ import com.google.inject.Inject;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 
+import javax.validation.constraints.NotNull;
+
 public class Handler implements MongoPersistenceIterator.Handler<NamespaceEntity> {
   @Inject private PersistenceIteratorFactory persistenceIteratorFactory;
   @Inject private MongoTemplate mongoTemplate;
@@ -33,33 +36,52 @@ public class Handler implements MongoPersistenceIterator.Handler<NamespaceEntity
   @Override
   public void handle(NamespaceEntity namespaceEntity) {
     System.out.println(
-        "Hello******" + namespaceEntity.getNextIteration() + "****" + namespaceEntity.getAccountIdentifier());
+        "Hello******" + namespaceEntity.getNextIteration() + "****" + namespaceEntity.getAccountIdentifier()+"****"+namespaceEntity.getUuid());
   }
 
+//  public void registerIterators() {
+//    persistenceIteratorFactory.createRedisBatchIteratorWithDedicatedThreadPool(
+//        PersistenceIteratorFactory.RedisBatchExecutorOptions.builder()
+//            .name("IDPTriggerProcessor")
+//            .poolSize(5)
+//            .batchSize(20)
+//            .interval(ofSeconds(5))
+//            .build(),
+//        Handler.class,
+//        MongoPersistenceIterator.<NamespaceEntity, SpringFilterExpander>builder()
+//            .clazz(NamespaceEntity.class)
+//            .fieldName(NamespaceEntity.NamespaceKeys.nextIteration)
+//            .targetInterval(ofSeconds(40))
+//            .acceptableExecutionTime(ofMinutes(1))
+//            .acceptableNoAlertDelay(ofSeconds(80))
+//            .maximumDelayForCheck(ofSeconds(30))
+//            .handler(this)
+//            //                        .filterExpander(query
+//            //                                -> query.addCriteria(new Criteria()
+//            //                                .and(NamespaceEntity.NamespaceKeys.nextIteration)
+//            //                                .exists(true)))
+//            .schedulingType(REGULAR)
+//            .persistenceProvider(new SpringPersistenceProvider<>(mongoTemplate)).redistribute(true));
+//  }
+
   public void registerIterators() {
-    persistenceIteratorFactory.createRedisBatchIteratorWithDedicatedThreadPool(
-        PersistenceIteratorFactory.RedisBatchExecutorOptions.builder()
-            .name("IDPTriggerProcessor")
-            .poolSize(1)
-            .batchSize(1)
-            .interval(ofSeconds(10))
-            .build(),
-        Handler.class,
-        MongoPersistenceIterator.<NamespaceEntity, SpringFilterExpander>builder()
-            .clazz(NamespaceEntity.class)
-            .fieldName(NamespaceEntity.NamespaceKeys.nextIteration)
-            .targetInterval(ofSeconds(10))
-            .acceptableExecutionTime(ofMinutes(1))
-            .acceptableNoAlertDelay(ofSeconds(30))
-            .maximumDelayForCheck(ofSeconds(30))
-            .redisModeBatchSize(5)
-            .handler(this)
-            //                        .filterExpander(query
-            //                                -> query.addCriteria(new Criteria()
-            //                                .and(NamespaceEntity.NamespaceKeys.nextIteration)
-            //                                .exists(true)))
-            .schedulingType(REGULAR)
-            .persistenceProvider(new SpringPersistenceProvider<>(mongoTemplate)));
-    //                        .redistribute(true));
+    persistenceIteratorFactory.createPumpIteratorWithDedicatedThreadPool(
+            PersistenceIteratorFactory.PumpExecutorOptions.builder()
+                    .name("IDPTriggerProcessor")
+                    .poolSize(5)
+                    .interval(ofSeconds(5))
+                    .build(),
+            Handler.class,
+            MongoPersistenceIterator.<NamespaceEntity, SpringFilterExpander>builder()
+                    .clazz(NamespaceEntity.class)
+                    .fieldName(NamespaceEntity.NamespaceKeys.nextIteration)
+                    .targetInterval(ofSeconds(31))
+                    .acceptableExecutionTime(ofSeconds(31))
+                    .acceptableNoAlertDelay(ofSeconds(62))
+                    .handler(this)
+                    .schedulingType(REGULAR)
+                    .persistenceProvider(new SpringPersistenceProvider<>(mongoTemplate))
+                    .redistribute(true));
   }
+
 }
