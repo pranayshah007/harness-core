289f75ea240b4dd82265be8ed6a620e094d3ab18
Ayushi Tiwari <ayushi.tiwari@harness.com>
2023-08-29 15:54:23 +0530

    feat: [CDS-78055]: Improve coverage pipeline-service : YamlField

--- 953-yaml-commons/src/test/java/io/harness/pms/yaml/YamlFieldTest.java
+++ 953-yaml-commons/src/test/java/io/harness/pms/yaml/YamlFieldTest.java
@@ -7,22 +7,60 @@
 
 package io.harness.pms.yaml;
 
+import static io.harness.rule.OwnerRule.AYUSHI_TIWARI;
 import static io.harness.rule.OwnerRule.SAHIL;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.harness.CategoryTest;
 import io.harness.category.element.UnitTests;
+import io.harness.pms.contracts.plan.YamlFieldBlob;
 import io.harness.rule.Owner;
+import io.harness.serializer.JsonUtils;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
+import com.google.protobuf.ByteString;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import javax.validation.constraints.NotNull;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 public class YamlFieldTest extends CategoryTest {
+  @NotNull YamlNode node;
+  private static final Charset CHARSET = Charset.forName(StandardCharsets.UTF_8.name());
+
+  @Test
+  @Owner(developers = AYUSHI_TIWARI)
+  @Category(UnitTests.class)
+  public void testGetName() throws IOException {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("pipeline.yaml");
+    String yamlContent = Resources.toString(testFile, Charsets.UTF_8);
+    YamlField yamlField = YamlUtils.readTree(YamlUtils.injectUuid(yamlContent));
+    String name = yamlField.getNode().getField("pipeline").getNode().getName();
+    // String stagesNode = pipelineNode.getField("stages").getUuid();
+
+    assertThat(name).isEqualTo("Manager Service Deployment");
+  }
+
+  @Test
+  @Owner(developers = AYUSHI_TIWARI)
+  @Category(UnitTests.class)
+  public void testToFieldBlob() throws IOException {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("pipeline.yaml");
+    String yamlContent = Resources.toString(testFile, Charsets.UTF_8);
+    YamlField yamlField = YamlUtils.readTree(YamlUtils.injectUuid(yamlContent));
+    YamlFieldBlob yaml = yamlField.toFieldBlob();
+    YamlFieldBlob expectedResult =
+        YamlFieldBlob.newBuilder().setBlob(ByteString.copyFrom(JsonUtils.asJson(yamlField), CHARSET)).build();
+    assertThat(expectedResult).isEqualTo(yaml);
+  }
+
   @Test
   @Owner(developers = SAHIL)
   @Category(UnitTests.class)
@@ -96,5 +134,105 @@ public class YamlFieldTest extends CategoryTest {
                                                       .getField("step");
     assertThat(normalStepFieldUnderParallelStage.checkIfParentIsParallel("stages")).isTrue();
     assertThat(normalStepFieldUnderParallelStage.checkIfParentIsParallel("steps")).isFalse();
+  }
+
+  @Test
+  @Owner(developers = AYUSHI_TIWARI)
+  @Category(UnitTests.class)
+  public void testFromFieldBlob() throws IOException {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("pipeline.yaml");
+    String yamlContent = Resources.toString(testFile, Charsets.UTF_8);
+    YamlField yamlField = YamlUtils.readTree(YamlUtils.injectUuid(yamlContent));
+    YamlFieldBlob result =
+        YamlFieldBlob.newBuilder().setBlob(ByteString.copyFrom(JsonUtils.asJson(yamlField), CHARSET)).build();
+    YamlField yaml = yamlField.fromFieldBlob(result);
+    YamlField expectedResult = JsonUtils.asObject(result.getBlob().toString(CHARSET), YamlField.class);
+    assertThat(expectedResult).isEqualTo(yaml);
+  }
+
+  @Test
+  @Owner(developers = AYUSHI_TIWARI)
+  @Category(UnitTests.class)
+  public void testGetYamlPath() throws IOException {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("pipeline.yaml");
+    String yamlContent = Resources.toString(testFile, Charsets.UTF_8);
+    YamlField yamlField = YamlUtils.readTree(YamlUtils.injectUuid(yamlContent));
+    String path = yamlField.getNode().getField("pipeline").getNode().asArray().get(1).getYamlPath();
+    assertThat(path).isEqualTo("pipeline/[1]");
+  }
+
+  @Test
+  @Owner(developers = AYUSHI_TIWARI)
+  @Category(UnitTests.class)
+  public void testGetType() throws IOException {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("pipeline.yaml");
+    String yamlContent = Resources.toString(testFile, Charsets.UTF_8);
+    YamlField yamlField = YamlUtils.readTree(YamlUtils.injectUuid(yamlContent));
+    YamlNode pipelineNode = yamlField.getNode().getField("pipeline").getNode();
+    YamlField stagesNode = pipelineNode.getField("stages");
+    String stage1Field = stagesNode.getNode().asArray().get(1).getField("stage").getType();
+
+    assertThat(stage1Field).isEqualTo("Deployment");
+  }
+
+  @Test
+  @Owner(developers = AYUSHI_TIWARI)
+  @Category(UnitTests.class)
+  public void testGetNodeName() throws IOException {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("pipeline.yaml");
+    String yamlContent = Resources.toString(testFile, Charsets.UTF_8);
+    YamlField yamlField = YamlUtils.readTree(YamlUtils.injectUuid(yamlContent));
+    YamlNode pipelineNode = yamlField.getNode().getField("pipeline").getNode();
+    YamlField stagesNode = pipelineNode.getField("stages");
+    String stage1Field = stagesNode.getNode().asArray().get(1).getField("stage").getNodeName();
+
+    assertThat(stage1Field).isEqualTo("prod stage");
+  }
+
+  @Test
+  @Owner(developers = AYUSHI_TIWARI)
+  @Category(UnitTests.class)
+  public void testGetId() throws IOException {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("pipeline.yaml");
+    String yamlContent = Resources.toString(testFile, Charsets.UTF_8);
+    YamlField yamlField = YamlUtils.readTree(YamlUtils.injectUuid(yamlContent));
+    YamlNode pipelineNode = yamlField.getNode().getField("pipeline").getNode();
+    YamlField stagesNode = pipelineNode.getField("stages");
+    String stage1Field = stagesNode.getNode().asArray().get(1).getField("stage").getId();
+
+    assertThat(stage1Field).isEqualTo("Deployment");
+  }
+
+  @Test
+  @Owner(developers = AYUSHI_TIWARI)
+  @Category(UnitTests.class)
+  public void testFromYamlPath() throws IOException {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("pipeline.yaml");
+    String yamlContent = Resources.toString(testFile, Charsets.UTF_8);
+    YamlField yamlField = YamlUtils.readTree(YamlUtils.injectUuid(yamlContent));
+    YamlField pipeline = yamlField.getNode().getField("pipeline").fromYamlPath("stages/[1]/stage");
+    String name = pipeline.getNodeName();
+
+    assertThat(name).isEqualTo("prod stage");
+  }
+
+  @Test
+  @Owner(developers = AYUSHI_TIWARI)
+  @Category(UnitTests.class)
+  public void testGetUuid() throws IOException {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    final URL testFile = classLoader.getResource("pipeline.yaml");
+    String yamlContent = Resources.toString(testFile, Charsets.UTF_8);
+    YamlField yamlField = YamlUtils.readTree(YamlUtils.injectUuid(yamlContent));
+    YamlNode pipelineNode = yamlField.getNode().getField("pipeline").getNode();
+    String stagesNode = pipelineNode.getField("stages").getUuid();
+
+    assertThat(stagesNode).isNotNull();
   }
 }
