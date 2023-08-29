ed2e31cb84f09bec003ffb0b31f23b16270b0f04
Rishabh Gupta <rishabh.gupta@harness.io>
2023-08-29 16:13:57 +0530

    fix: [CDS-75504]: TAS Rolling create application without rolling strategy if it doesn't exist (#52171)
    * fix: [CDS-75504]: TAS Rolling create application without rolling strategy if it doesn't exist
    
    * fix: [CDS-75504]: increase build no.

--- 930-delegate-tasks/src/main/java/io/harness/delegate/pcf/CfRollingDeployCommandTaskHandlerNG.java
+++ 930-delegate-tasks/src/main/java/io/harness/delegate/pcf/CfRollingDeployCommandTaskHandlerNG.java
@@ -129,13 +129,16 @@ public class CfRollingDeployCommandTaskHandlerNG extends CfCommandTaskNGHandler 
 
     File artifactFile = null;
     File workingDirectory = null;
+    File trailingLogsDirectory = null;
     TasApplicationInfo currentProdInfo = null;
     CfRollingDeployResponseNGBuilder cfRollingDeployResponseNGBuilder = CfRollingDeployResponseNG.builder();
     try {
       List<ApplicationSummary> previousReleases = cfDeploymentManager.getPreviousReleasesForRolling(
           cfRequestConfig, ((CfRollingDeployRequestNG) cfCommandRequestNG).getApplicationName());
       workingDirectory = generateWorkingDirectoryOnDelegate(cfRollingDeployRequestNG);
+      trailingLogsDirectory = generateWorkingDirectoryOnDelegate(cfRollingDeployRequestNG);
       cfRequestConfig.setCfHomeDirPath(workingDirectory.getAbsolutePath());
+      cfRequestConfig.setTrailingLogsDirPath(trailingLogsDirectory.getAbsolutePath());
       currentProdInfo = getCurrentProdInfo(previousReleases, clonePcfRequestConfig(cfRequestConfig).build(),
           workingDirectory, ((CfRollingDeployRequestNG) cfCommandRequestNG).getTimeoutIntervalInMin(), logCallback);
       cfRollingDeployResponseNGBuilder.currentProdInfo(currentProdInfo);
@@ -163,9 +166,10 @@ public class CfRollingDeployCommandTaskHandlerNG extends CfCommandTaskNGHandler 
               .pcfManifestFileData(pcfManifestFileData)
               .varsYmlFilePresent(varsYmlPresent)
               .dockerBasedDeployment(isDockerArtifact(cfRollingDeployRequestNG.getTasArtifactConfig()))
-              .strategy("rolling")
               .build();
-
+      if (!isEmpty(previousReleases)) {
+        requestData.setStrategy("rolling");
+      }
       requestData.setFinalManifestYaml(generateManifestYamlForPush(cfRollingDeployRequestNG, requestData));
       // Create manifest.yaml file
       prepareManifestYamlFile(requestData);
@@ -475,6 +479,7 @@ public class CfRollingDeployCommandTaskHandlerNG extends CfCommandTaskNGHandler 
         .cfCliPath(cfRequestConfig.getCfCliPath())
         .cfCliVersion(cfRequestConfig.getCfCliVersion())
         .cfHomeDirPath(cfRequestConfig.getCfHomeDirPath())
+        .trailingLogsDirPath(cfRequestConfig.getTrailingLogsDirPath())
         .loggedin(cfRequestConfig.isLoggedin())
         .limitPcfThreads(cfRequestConfig.isLimitPcfThreads())
         .useNumbering(cfRequestConfig.isUseNumbering())
