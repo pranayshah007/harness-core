3dc7ad32543f4c77e5ff9d53babd6951aff32e1b
Raghavendra Murali <raghavendra.murali@harness.io>
2023-08-31 11:56:58 -0700

    feat: [temp]: Reference code to use the new Bijou APIs

--- pipeline-service/modules/orchestration/src/main/java/io/harness/engine/pms/tasks/NgDelegate2TaskExecutor.java
+++ pipeline-service/modules/orchestration/src/main/java/io/harness/engine/pms/tasks/NgDelegate2TaskExecutor.java
@@ -7,6 +7,11 @@
 
 package io.harness.engine.pms.tasks;
 
+import static io.harness.data.structure.UUIDGenerator.generateUuid;
+import static io.harness.threading.Morpheus.sleep;
+
+import static software.wings.beans.TaskType.SHELL_SCRIPT_TASK_NG;
+
 import static java.lang.System.currentTimeMillis;
 
 import io.harness.annotations.dev.HarnessTeam;
@@ -15,24 +20,42 @@ import io.harness.callback.DelegateCallbackToken;
 import io.harness.delegate.AccountId;
 import io.harness.delegate.CancelTaskRequest;
 import io.harness.delegate.CancelTaskResponse;
+import io.harness.delegate.ComputingResource;
+import io.harness.delegate.ContainerSpec;
 import io.harness.delegate.DelegateServiceGrpc.DelegateServiceBlockingStub;
+import io.harness.delegate.Execution;
+import io.harness.delegate.ExecutionInfrastructure;
+import io.harness.delegate.ExecutionInput;
+import io.harness.delegate.K8sInfraSpec;
+import io.harness.delegate.LogConfig;
+import io.harness.delegate.ScheduleTaskRequest;
+import io.harness.delegate.SchedulingConfig;
+import io.harness.delegate.SetupExecutionInfrastructureRequest;
 import io.harness.delegate.SubmitTaskRequest;
 import io.harness.delegate.SubmitTaskResponse;
 import io.harness.delegate.TaskId;
 import io.harness.delegate.TaskMode;
+import io.harness.delegate.beans.DelegateTaskPackage;
+import io.harness.delegate.beans.RunnerType;
+import io.harness.delegate.beans.TaskData;
 import io.harness.exception.InvalidRequestException;
 import io.harness.grpc.utils.HTimestamps;
 import io.harness.pms.contracts.execution.tasks.TaskRequest;
 import io.harness.pms.contracts.execution.tasks.TaskRequest.RequestCase;
 import io.harness.pms.plan.execution.SetupAbstractionKeys;
 import io.harness.pms.utils.PmsGrpcClientUtils;
+import io.harness.serializer.KryoSerializer;
 import io.harness.service.intfc.DelegateAsyncService;
 import io.harness.service.intfc.DelegateSyncService;
 import io.harness.tasks.ResponseData;
 
 import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import com.google.protobuf.ByteString;
 import com.google.protobuf.util.Timestamps;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 import lombok.Builder;
@@ -47,6 +70,8 @@ public class NgDelegate2TaskExecutor implements TaskExecutor {
   @Inject private DelegateSyncService delegateSyncService;
   @Inject private DelegateAsyncService delegateAsyncService;
   @Inject private Supplier<DelegateCallbackToken> tokenSupplier;
+  @Inject @Named("referenceFalseKryoSerializer") private KryoSerializer referenceFalseKryoSerializer;
+  private static String ExecutionInfraID = generateUuid();
 
   @Override
   public String queueTask(Map<String, String> setupAbstractions, TaskRequest taskRequest, Duration holdFor) {
@@ -55,12 +80,93 @@ public class NgDelegate2TaskExecutor implements TaskExecutor {
       throw new InvalidRequestException(check.getMessage());
     }
 
-    SubmitTaskResponse submitTaskResponse =
-        PmsGrpcClientUtils.retryAndProcessException(delegateServiceBlockingStub::submitTaskV2,
+    SubmitTaskResponse submitTaskResponse;
+    if (taskRequest.getUseReferenceFalseKryoSerializer()) {
+      // TODO: define new proto to replace TaskRequest
+      if (taskRequest.getDelegateTaskRequest().getRequest().getDetails().getType().getType().equals(
+              SHELL_SCRIPT_TASK_NG.name())) {
+        var submitRequest = taskRequest.getDelegateTaskRequest().getRequest();
+
+        submitTaskResponse = PmsGrpcClientUtils.retryAndProcessException(
+            delegateServiceBlockingStub::initTask, buildK8sInfraReq(submitRequest));
+        sleep(Duration.ofSeconds(30));
+        submitTaskResponse = PmsGrpcClientUtils.retryAndProcessException(
+            delegateServiceBlockingStub::executeTask, buildK8sExecReq(submitRequest, "566698"));
+      } else {
+        submitTaskResponse = PmsGrpcClientUtils.retryAndProcessException(delegateServiceBlockingStub::submitTaskV2,
             buildTaskRequestWithToken(taskRequest.getDelegateTaskRequest().getRequest()));
+      }
+    } else {
+      submitTaskResponse = PmsGrpcClientUtils.retryAndProcessException(delegateServiceBlockingStub::submitTaskV2,
+          buildTaskRequestWithToken(taskRequest.getDelegateTaskRequest().getRequest()));
+    }
     delegateAsyncService.setupTimeoutForTask(submitTaskResponse.getTaskId().getId(),
         Timestamps.toMillis(submitTaskResponse.getTotalExpiry()), currentTimeMillis() + holdFor.toMillis());
+
     return submitTaskResponse.getTaskId().getId();
+  }
+
+  private SchedulingConfig buildK8sSchedulingConfig(SubmitTaskRequest submitTaskRequest) {
+    SchedulingConfig schedulingConfig = SchedulingConfig.newBuilder()
+                                            .addAllSelectors(submitTaskRequest.getSelectorsList())
+                                            .setSetupAbstractions(submitTaskRequest.getSetupAbstractions())
+                                            .setRunnerType(RunnerType.RUNNER_TYPE_K8S)
+                                            .setAccountId(submitTaskRequest.getAccountId().getId())
+                                            .setExecutionTimeout(submitTaskRequest.getDetails().getExecutionTimeout())
+                                            .setSelectionTrackingLogEnabled(true)
+                                            .setCallbackToken(tokenSupplier.get())
+                                            .build();
+
+    return schedulingConfig;
+  }
+
+  private ScheduleTaskRequest buildK8sExecReq(SubmitTaskRequest submitTaskRequest, String infraRefId) {
+    SchedulingConfig schedulingConfig = buildK8sSchedulingConfig(submitTaskRequest);
+
+    Object params =
+        referenceFalseKryoSerializer.asInflatedObject(submitTaskRequest.getDetails().getKryoParameters().toByteArray());
+    TaskData taskData =
+        TaskData.builder().parameters(new Object[] {params}).taskType(SHELL_SCRIPT_TASK_NG.name()).async(true).build();
+    DelegateTaskPackage delegateTaskPackage =
+        DelegateTaskPackage.builder().accountId(submitTaskRequest.getAccountId().getId()).data(taskData).build();
+    byte[] taskPackageBytes = referenceFalseKryoSerializer.asDeflatedBytes(delegateTaskPackage);
+    ExecutionInput executionInput = ExecutionInput.newBuilder().setData(ByteString.copyFrom(taskPackageBytes)).build();
+    Execution execution = Execution.newBuilder().setInfraRefId(infraRefId).setInput(executionInput).build();
+
+    return ScheduleTaskRequest.newBuilder().setExecution(execution).setConfig(schedulingConfig).build();
+  }
+
+  private SetupExecutionInfrastructureRequest buildK8sInfraReq(SubmitTaskRequest submitTaskRequest) {
+    SchedulingConfig schedulingConfig = buildK8sSchedulingConfig(submitTaskRequest);
+
+    LogConfig logConfig = LogConfig.newBuilder().setLogKey("asdsadasd").setToken("dfgdghdgh").build();
+
+    ComputingResource computingResource = ComputingResource.newBuilder().setCpu("100m").setMemory("100Mi").build();
+
+    List<Long> ports = new ArrayList<>();
+    ports.add(Long.valueOf(20002));
+    ContainerSpec containerSpec = ContainerSpec
+                                      .newBuilder()
+                                      //.setImage("us.gcr.io/gcr-play/delegate-plugin:shell")
+                                      .setImage("raghavendramurali/shell-task-ng:1.0")
+                                      .addAllPort(ports)
+                                      .setResource(computingResource)
+                                      .build();
+
+    List<ContainerSpec> tasks = new ArrayList<>();
+    tasks.add(containerSpec);
+    K8sInfraSpec k8sInfraSpec = K8sInfraSpec.newBuilder().addAllTasks(tasks).build();
+
+    ExecutionInfrastructure executionInfrastructure =
+        ExecutionInfrastructure.newBuilder().setLogConfig(logConfig).setK8Infraspec(k8sInfraSpec).build();
+
+    SetupExecutionInfrastructureRequest setupExecutionInfrastructureRequest =
+        SetupExecutionInfrastructureRequest.newBuilder()
+            .setConfig(schedulingConfig)
+            .setInfra(executionInfrastructure)
+            .build();
+
+    return setupExecutionInfrastructureRequest;
   }
 
   @Override
