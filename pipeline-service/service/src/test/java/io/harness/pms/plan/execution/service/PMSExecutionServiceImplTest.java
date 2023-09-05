a97b6a46d95eb3955c6ed0e35a248fad0bf4f2c5
Prashant Sharma <prashant.sharma@harness.io>
2023-09-05 16:22:42 +0530

    feat: [CDS-78726]: Create a mongo index for plan Execution summary entity

--- pipeline-service/service/src/test/java/io/harness/pms/plan/execution/service/PMSExecutionServiceImplTest.java
+++ pipeline-service/service/src/test/java/io/harness/pms/plan/execution/service/PMSExecutionServiceImplTest.java
@@ -196,11 +196,8 @@ public class PMSExecutionServiceImplTest extends CategoryTest {
     childCriteria.orOperator(Criteria.where(PlanExecutionSummaryKeys.parentStageInfo).exists(false),
         Criteria.where(PlanExecutionSummaryKeys.isChildPipeline).is(false));
 
-    List<Boolean> inChildList =
-        (List<Boolean>) ((Document) form.getCriteriaObject().get(PlanExecutionSummaryKeys.isChildPipeline)).get("$in");
-    assertThat(inChildList.size()).isEqualTo(2);
-    assertThat(inChildList.get(0)).isNull();
-    assertThat(inChildList.get(1)).isEqualTo(false);
+    Boolean inChildList = (Boolean) form.getCriteriaObject().get(PlanExecutionSummaryKeys.isChildPipeline);
+    assertThat(inChildList).isEqualTo(false);
   }
 
   @Test
