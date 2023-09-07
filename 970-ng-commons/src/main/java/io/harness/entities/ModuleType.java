09be8db30ccce114e40bce1d5ed663b8402f20ea
ashish-levelops <ashish.duthade@harness.io>
2023-09-08 02:13:37 +0530

    [feat]: [SEI-2562]: - BE - SEI tenant creation on Harness and licensing

--- 970-ng-commons/src/main/java/io/harness/entities/ModuleType.java
+++ 970-ng-commons/src/main/java/io/harness/entities/ModuleType.java
@@ -46,7 +46,7 @@ public enum ModuleType {
   @JsonProperty("GOVERNANCE") GOVERNANCE("Governance", INTERNAL, ONBOARDING_IN_PROGRESS),
   @JsonProperty("IDP") IDP("Internal Developer Portal", INTERNAL, ONBOARDING_IN_PROGRESS),
   // moving SEI to Onboarded for qa
-  @JsonProperty("SEI") SEI("Software Engineering Insights", INTERNAL, ONBOARDED);
+  @JsonProperty("SEI") SEI("Software Engineering Insights", PUBLIC, ONBOARDED);
 
   String displayName;
   ModuleVisibility visibility;
