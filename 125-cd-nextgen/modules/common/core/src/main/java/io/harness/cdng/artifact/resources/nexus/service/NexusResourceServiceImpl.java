8613b6d5c4cf416a9a0024c801c71c8c40851cdd
Rakshit Agarwal <rakshit.agarwal@harness.io>
2023-09-27 10:31:53 +0530

    [fix]:[CDS-68938]:Added Validation before Delegate Creation

--- 125-cd-nextgen/modules/common/core/src/main/java/io/harness/cdng/artifact/resources/nexus/service/NexusResourceServiceImpl.java
+++ 125-cd-nextgen/modules/common/core/src/main/java/io/harness/cdng/artifact/resources/nexus/service/NexusResourceServiceImpl.java
@@ -55,6 +55,8 @@ import io.harness.security.encryption.EncryptedDataDetail;
 import io.harness.service.DelegateGrpcClientWrapper;
 
 import software.wings.helpers.ext.nexus.NexusRepositories;
+import software.wings.utils.RepositoryFormat;
+import software.wings.utils.RepositoryType;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.inject.Inject;
@@ -94,6 +96,23 @@ public class NexusResourceServiceImpl implements NexusResourceService {
       String artifactPath, String repositoryFormat, String artifactRepositoryUrl, String orgIdentifier,
       String projectIdentifier, String groupId, String artifactId, String extension, String classifier,
       String packageName, String group) {
+    ArtifactUtils.validateIfAllValuesAssigned(MutablePair.of(NGArtifactConstants.REPOSITORY, repositoryName));
+    if (RepositoryFormat.docker.name().equals(repositoryFormat)) {
+      ArtifactUtils.validateIfAllValuesAssigned(MutablePair.of(NGArtifactConstants.ARTIFACT_PATH, artifactPath));
+      ArtifactUtils.validateIfAnyValueAssigned(
+          MutablePair.of(NGArtifactConstants.REPOSITORY_URL, artifactRepositoryUrl),
+          MutablePair.of(NGArtifactConstants.REPOSITORY_PORT, repositoryPort));
+    } else if (RepositoryFormat.maven.name().equals(repositoryFormat)) {
+      ArtifactUtils.validateIfAllValuesAssigned(MutablePair.of(NGArtifactConstants.GROUP_ID, groupId),
+          MutablePair.of(NGArtifactConstants.ARTIFACT_ID, artifactId));
+    } else if (RepositoryFormat.npm.name().equals(repositoryFormat)) {
+      ArtifactUtils.validateIfAllValuesAssigned(MutablePair.of(NGArtifactConstants.PACKAGE_NAME, packageName));
+    } else if (RepositoryFormat.nuget.name().equals(repositoryFormat)) {
+      ArtifactUtils.validateIfAllValuesAssigned(MutablePair.of(NGArtifactConstants.PACKAGE_NAME, packageName));
+    } else if (RepositoryFormat.raw.name().equals(repositoryFormat)) {
+      ArtifactUtils.validateIfAllValuesAssigned(MutablePair.of(NGArtifactConstants.GROUP, group));
+    }
+
     NexusConnectorDTO connector = getConnector(nexusConnectorRef);
     BaseNGAccess baseNGAccess =
         getBaseNGAccess(nexusConnectorRef.getAccountIdentifier(), orgIdentifier, projectIdentifier);
@@ -308,7 +327,7 @@ public class NexusResourceServiceImpl implements NexusResourceService {
     } else if (responseData instanceof RemoteMethodReturnValueData) {
       RemoteMethodReturnValueData remoteMethodReturnValueData = (RemoteMethodReturnValueData) responseData;
       if (remoteMethodReturnValueData.getException() instanceof InvalidRequestException) {
-        throw(InvalidRequestException)(remoteMethodReturnValueData.getException());
+        throw (InvalidRequestException) (remoteMethodReturnValueData.getException());
       } else {
         throw new NexusRegistryException(
             "Unexpected error during authentication to nexus server " + remoteMethodReturnValueData.getReturnValue());
