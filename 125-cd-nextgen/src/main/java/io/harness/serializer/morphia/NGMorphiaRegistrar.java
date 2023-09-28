f3939490560286f5d996c14c27eb88b39ce36c4a
Lucas Sales <lucas.sales@harness.io>
2023-09-28 10:40:09 -0300

    fixing tests

--- 125-cd-nextgen/src/main/java/io/harness/serializer/morphia/NGMorphiaRegistrar.java
+++ 125-cd-nextgen/src/main/java/io/harness/serializer/morphia/NGMorphiaRegistrar.java
@@ -33,6 +33,7 @@ import io.harness.cdng.provision.terraformcloud.dal.TerraformCloudConfig;
 import io.harness.cdng.provision.terragrunt.TerragruntConfig;
 import io.harness.cdng.service.steps.ServiceStepParameters;
 import io.harness.cdng.usage.task.CDLicenseReportAccounts;
+import io.harness.gitopsprovider.entity.GithubRestraintInstance;
 import io.harness.morphia.MorphiaRegistrar;
 import io.harness.morphia.MorphiaRegistrarHelperPut;
 import io.harness.polling.bean.PollingDocument;
@@ -65,6 +66,7 @@ public class NGMorphiaRegistrar implements MorphiaRegistrar {
     set.add(StageExecutionInstanceInfo.class);
     set.add(CDLicenseReportAccounts.class);
     set.add(AwsCdkConfig.class);
+    set.add(GithubRestraintInstance.class);
   }
 
   @Override
