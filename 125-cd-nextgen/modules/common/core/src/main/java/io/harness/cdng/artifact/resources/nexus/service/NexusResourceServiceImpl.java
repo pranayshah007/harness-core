2c025da3413e06f519401aeea43f749fbb6a9fa7
Rakshit Agarwal <rakshit.agarwal@harness.io>
2023-09-28 18:14:20 +0530

    [fix]:[CDS-68938]:formatting issue resolved

--- 125-cd-nextgen/modules/common/core/src/main/java/io/harness/cdng/artifact/resources/nexus/service/NexusResourceServiceImpl.java
+++ 125-cd-nextgen/modules/common/core/src/main/java/io/harness/cdng/artifact/resources/nexus/service/NexusResourceServiceImpl.java
@@ -56,6 +56,7 @@ import io.harness.service.DelegateGrpcClientWrapper;
 
 import software.wings.helpers.ext.nexus.NexusRepositories;
 import software.wings.utils.RepositoryFormat;
+
 import com.google.common.annotations.VisibleForTesting;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -335,7 +336,7 @@ public class NexusResourceServiceImpl implements NexusResourceService {
     } else if (responseData instanceof RemoteMethodReturnValueData) {
       RemoteMethodReturnValueData remoteMethodReturnValueData = (RemoteMethodReturnValueData) responseData;
       if (remoteMethodReturnValueData.getException() instanceof InvalidRequestException) {
-        throw(InvalidRequestException)(remoteMethodReturnValueData.getException());
+        throw (InvalidRequestException) (remoteMethodReturnValueData.getException());
       } else {
         throw new NexusRegistryException(
             "Unexpected error during authentication to nexus server " + remoteMethodReturnValueData.getReturnValue());
