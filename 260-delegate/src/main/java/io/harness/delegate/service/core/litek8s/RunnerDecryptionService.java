ad83bdcbbb05ce5e607bcf431fae67a1b0d80830
Vikas Maddukuri <vikas.maddukuri@harness.io>
2023-09-29 21:41:59 +0530

    fix: bijuo-delegateside-secret-decryption-for-locallyEncryptedSecrets: Initial commit

--- 260-delegate/src/main/java/io/harness/delegate/service/core/litek8s/RunnerDecryptionService.java
+++ 260-delegate/src/main/java/io/harness/delegate/service/core/litek8s/RunnerDecryptionService.java
@@ -7,14 +7,20 @@
 
 package io.harness.delegate.service.core.litek8s;
 
+import io.harness.delegate.core.beans.EncryptedDataRecord;
 import io.harness.delegate.core.beans.Secret;
+import io.harness.delegate.service.secret.EncryptedDataRecordProtoPojoMapper;
+import io.harness.delegate.service.secret.EncryptionConfigProtoPojoMapper;
 import io.harness.security.encryption.DelegateDecryptionService;
 import io.harness.security.encryption.EncryptedRecord;
+import io.harness.security.encryption.EncryptedRecordData;
 import io.harness.security.encryption.EncryptionConfig;
 import io.harness.serializer.KryoSerializer;
 
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
+import com.google.protobuf.InvalidProtocolBufferException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -32,6 +38,21 @@ public class RunnerDecryptionService {
     final List<EncryptedRecord> kryoSecrets =
         (List<EncryptedRecord>) kryoSerializer.asInflatedObject(secret.getSecrets().getBinaryData().toByteArray());
     final var decrypt = decryptionService.decrypt(Map.of(secretConfig, kryoSecrets));
+    log.info("Decrypted secrets are: {}", decrypt);
+    return decrypt;
+  }
+
+  public Map<String, char[]> decryptByteArray(final Secret secret) throws InvalidProtocolBufferException {
+    final EncryptedDataRecord encryptedDataRecord =
+        EncryptedDataRecord.parseFrom(secret.getSecrets().getBinaryData().toByteArray());
+    EncryptedRecordData mappedRecordData = EncryptedDataRecordProtoPojoMapper.map(encryptedDataRecord);
+    final io.harness.delegate.core.beans.EncryptionConfig encryptionConfig =
+        io.harness.delegate.core.beans.EncryptionConfig.parseFrom(secret.getConfig().getBinaryData().toByteArray());
+    EncryptionConfig mappedEncryptionConfig = EncryptionConfigProtoPojoMapper.map(encryptionConfig);
+    List<EncryptedRecord> encryptedRecordList = new ArrayList<>();
+    encryptedRecordList.add(mappedRecordData);
+    assert mappedEncryptionConfig != null;
+    final var decrypt = decryptionService.decrypt(Map.of(mappedEncryptionConfig, encryptedRecordList));
     log.info("Decrypted secrets are: {}", decrypt);
     return decrypt;
   }
