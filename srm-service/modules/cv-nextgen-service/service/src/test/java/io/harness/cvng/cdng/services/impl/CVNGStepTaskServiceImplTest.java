501a2482b9d75886acbc2ccc5efadc5afb39138a
Anshika Anand <anshika.anand@harness.io>
2023-10-03 14:23:25 +0530

    feat: [CDS-80035]: Adding tests coverage for CvStepParametersUtils.java Also working on increasing coverage for CVNGStepTaskServiceImplTest.java

--- srm-service/modules/cv-nextgen-service/service/src/test/java/io/harness/cvng/cdng/services/impl/CVNGStepTaskServiceImplTest.java
+++ srm-service/modules/cv-nextgen-service/service/src/test/java/io/harness/cvng/cdng/services/impl/CVNGStepTaskServiceImplTest.java
@@ -193,6 +193,7 @@ public class CVNGStepTaskServiceImplTest extends CvNextGenTestBase {
   @Owner(developers = ANSHIKA)
   @Ignore
   @Category(UnitTests.class)
+  // Working on it.
   public void testGetDeploymentActivityLogAnalysisClusters() {
     String accountId = generateUuid();
     String callbackId = generateUuid();
