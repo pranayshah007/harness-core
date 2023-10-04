90cb34cd200cfd6327218e0b9608ec0a916a2c87
Shubhendu Patidar <shubhendu.patidar@harness.io>
2023-10-04 11:25:35 +0530

    fix: [CDS-80049]: Added NullChecks; Added BE validations for analysisDTO for CVNG

--- srm-service/modules/cv-nextgen-service/service/src/main/java/io/harness/cvng/core/validators/AnalysisDTOValidator.java
+++ srm-service/modules/cv-nextgen-service/service/src/main/java/io/harness/cvng/core/validators/AnalysisDTOValidator.java
@@ -8,10 +8,14 @@
 package io.harness.cvng.core.validators;
 
 import io.harness.cvng.core.beans.HealthSourceMetricDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+
 public class AnalysisDTOValidator implements ConstraintValidator<AnalysisDTOCheck, Object> {
+  private static final Logger logger = LoggerFactory.getLogger(AnalysisDTOValidator.class);
   @Override
   public void initialize(AnalysisDTOCheck constraintAnnotation) {
     ConstraintValidator.super.initialize(constraintAnnotation);
@@ -23,13 +27,29 @@ public class AnalysisDTOValidator implements ConstraintValidator<AnalysisDTOChec
       return false;
     }
     try {
+      if (!(o instanceof HealthSourceMetricDefinition.AnalysisDTO)) {
+        return false;
+      }
       HealthSourceMetricDefinition.AnalysisDTO analysisDTO = (HealthSourceMetricDefinition.AnalysisDTO) o;
+      if (analysisDTO.getLiveMonitoring() == null){
+        logger.error("Live Monitoring config object is NULL, analysisDTO: {}", analysisDTO);
+        return false;
+      }
+      if (analysisDTO.getDeploymentVerification() == null){
+        logger.error("Continuous Verification config object is NULL, analysisDTO: {}", analysisDTO);
+        return false;
+      }
+      if (analysisDTO.getRiskProfile() == null){
+        logger.error("Risk Profile config object is NULL, analysisDTO: {}", analysisDTO);
+        return false;
+      }
       if (!analysisDTO.getLiveMonitoring().getEnabled() && !analysisDTO.getDeploymentVerification().getEnabled()) {
         return true;
       }
       return analysisDTO.getRiskProfile().getThresholdTypes() != null
           && !analysisDTO.getRiskProfile().getThresholdTypes().isEmpty();
     } catch (Exception e) {
+      logger.error("Unknown Exception caught while validating analysisDTO object for Monitored-Service", e);
       return false;
     }
   }
