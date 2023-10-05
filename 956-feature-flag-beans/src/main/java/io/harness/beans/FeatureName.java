26f23b22a1ea9294298b8a15cb9f61a1dda40f3d
Pratyush Goel <goel.pratyush@harness.io>
2023-10-05 18:56:03 +0530

    feat: [CDS-80876]: HF: adding FF to remove the ECR config from UI. (#53778)

--- 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
+++ 956-feature-flag-beans/src/main/java/io/harness/beans/FeatureName.java
@@ -147,6 +147,8 @@ public enum FeatureName {
       "Makes authenticated custom webhook calls use service principal instead of the principal inherited from the API key",
       HarnessTeam.CDC),
   CDS_NG_TRIGGER_MULTI_ARTIFACTS("Allows creation of multi-region artifact triggers", HarnessTeam.CDC),
+  CDS_OCI_HELM_ECR_CONFIG_SUPPORT_NG(
+      "This flag enables the support ECR config in OCI Helm store type.", HarnessTeam.CDP),
   CDS_OrgAccountLevelServiceEnvEnvGroup(
       "Support Creation and Use of Org and Account level Services and Environments", HarnessTeam.CDC),
   CDS_PAUSE_JIRA_APPROVAL_CG("Pause CG Jira Approval", HarnessTeam.CDC),
