a39464a1371e69504f1b591b2dc2e5392d3afe02
Tejas Tiwari <tejas.tiwari@harness.io>
2023-10-06 13:55:01 +0530

    added PEM reader

--- 970-ng-commons/src/main/java/io/harness/oidc/RSAKeyPairGenerator.java
+++ 970-ng-commons/src/main/java/io/harness/oidc/RSAKeyPairGenerator.java
@@ -10,6 +10,8 @@ package io.harness.oidc;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.StringReader;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
@@ -17,20 +19,28 @@ import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Security;
+import java.security.interfaces.RSAKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import lombok.extern.slf4j.Slf4j;
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMException;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.bouncycastle.util.io.pem.PemObject;
 import org.bouncycastle.util.io.pem.PemObjectGenerator;
 import org.bouncycastle.util.io.pem.PemWriter;
 
+@Slf4j
 public class RSAKeyPairGenerator {
   public KeyPair generateKeyPair() {
     Security.addProvider(new BouncyCastleProvider());
     KeyPairGenerator keyPairGenerator = null;
     try {
       keyPairGenerator = KeyPairGenerator.getInstance("RSA", "BC");
-    } catch (NoSuchAlgorithmException e) {
-      throw new RuntimeException(e);
-    } catch (NoSuchProviderException e) {
+    } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
       throw new RuntimeException(e);
     }
     keyPairGenerator.initialize(2048);
@@ -59,9 +69,39 @@ public class RSAKeyPairGenerator {
       String publicKeyPEM = byteArrayOutputStream.toString();
       pemWriter.close();
       byteArrayOutputStream.close();
+      readPemFile(publicKeyPEM);
+      readPemFile(privateKeyPEM);
       return RSAKeyPairPEM.builder().privateKeyPem(privateKeyPEM).publicKeyPem(publicKeyPEM).build();
     } catch (IOException e) {
       throw new RuntimeException(e);
+    }
+  }
+
+  public RSAKey readPemFile(String pemFile) {
+    Reader reader = new StringReader(pemFile);
+    PEMParser pemParser = new PEMParser(reader);
+    Object pemObject = null;
+    try {
+      pemObject = pemParser.readObject();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    if (pemObject instanceof SubjectPublicKeyInfo) {
+      try {
+        return (RSAPublicKey) new JcaPEMKeyConverter().getPublicKey((SubjectPublicKeyInfo) pemObject);
+      } catch (PEMException e) {
+        throw new RuntimeException(e);
+      }
+    } else if (pemObject instanceof PrivateKeyInfo) {
+      try {
+        return (RSAPrivateKey) new JcaPEMKeyConverter().getPrivateKey((PrivateKeyInfo) pemObject);
+      } catch (PEMException e) {
+        throw new RuntimeException(e);
+      }
+    } else {
+      log.error("Invalid PEM file format");
+      throw new RuntimeException("Invalid PEM file format");
     }
   }
 }
