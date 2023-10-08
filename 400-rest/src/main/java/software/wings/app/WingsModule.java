aa3cb1f4b7edbea8a40c124f9582434db2c149fe
Sahiba Khan <sahiba.khan@harness.io>
2023-10-08 19:42:53 +0530

    Revert "feat: [PL-41680]: Delete accounts MARKED-FOR-DELETION through Scheduler Job  (#53857)" (#53869)
    This reverts commit 6424686bb026391b07804d2f783e70df2b39a9e6.

--- 400-rest/src/main/java/software/wings/app/WingsModule.java
+++ 400-rest/src/main/java/software/wings/app/WingsModule.java
@@ -864,8 +864,6 @@ import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
@@ -995,14 +993,6 @@ public class WingsModule extends AbstractModule implements ServersModule {
     boolean isFreeCluster = StringUtils.equals(clusterType, "freemium");
 
     return new CdnStorageUrlGenerator(configuration.getCdnConfig(), isFreeCluster);
-  }
-
-  @Provides
-  @Singleton
-  @Named("cgJobExecutor")
-  public ScheduledExecutorService cgJobExecutor() {
-    return new ScheduledThreadPoolExecutor(
-        2, new ThreadFactoryBuilder().setNameFormat("cg-job-%d").setPriority(Thread.MAX_PRIORITY).build());
   }
 
   @Override
--- 400-rest/src/main/java/software/wings/service/intfc/AccountService.java
+++ 400-rest/src/main/java/software/wings/service/intfc/AccountService.java
@@ -305,6 +305,4 @@ public interface AccountService {
   void setPublicAccessEnabled(String accountId, boolean publicAccessEnabled);
 
   Account updateDefaultExperience(String accountIdentifier, DefaultExperience defaultExperience);
-
-  List<Account> listAccountsMarkedForDeletion(int limit);
 }
