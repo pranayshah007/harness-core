d7d8e600027437445989b4086c40a1c7757df50a
Vasile Lica <vasile.lica@harness.io>
2023-10-11 14:48:56 +0300

    feat: [CDS-79096]: revert azure upgrade saas

--- 400-rest/src/test/java/software/wings/helpers/ext/azure/AzureHelperServiceTest.java
+++ 400-rest/src/test/java/software/wings/helpers/ext/azure/AzureHelperServiceTest.java
@@ -91,13 +91,11 @@ import com.azure.resourcemanager.compute.models.GalleryImageVersionPublishingPro
 import com.azure.resourcemanager.compute.models.GalleryImageVersionStorageProfile;
 import com.azure.resourcemanager.compute.models.GalleryImageVersions;
 import com.azure.resourcemanager.compute.models.GalleryImages;
-import com.azure.resourcemanager.compute.models.HyperVGeneration;
 import com.azure.resourcemanager.compute.models.ImagePurchasePlan;
 import com.azure.resourcemanager.compute.models.OperatingSystemStateTypes;
 import com.azure.resourcemanager.compute.models.OperatingSystemTypes;
 import com.azure.resourcemanager.compute.models.RecommendedMachineConfiguration;
 import com.azure.resourcemanager.compute.models.ReplicationStatus;
-import com.azure.resourcemanager.compute.models.SecurityTypes;
 import com.azure.resourcemanager.compute.models.TargetRegion;
 import com.azure.resourcemanager.compute.models.VirtualMachine;
 import com.azure.resourcemanager.compute.models.VirtualMachines;
@@ -1420,16 +1418,6 @@ public class AzureHelperServiceTest extends WingsBaseTest {
 
       @Override
       public String releaseNoteUri() {
-        return null;
-      }
-
-      @Override
-      public HyperVGeneration hyperVGeneration() {
-        return null;
-      }
-
-      @Override
-      public SecurityTypes securityType() {
         return null;
       }
 
