b1fdae2bfea344952a1b58255b303f73a517c84b
Anmol Maheshwari <anmol.maheshwari@harness.io>
2023-10-17 19:21:50 +0530

    [feat]: [CCM-14676]: Migration Support for OOTB policies

---
+++ 450-ce-views/src/main/java/io/harness/ccm/governance/migration/GovernanceMigrationRules.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.ccm.governance.migration;
+
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+
+import java.util.Set;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Value;
+
+@OwnedBy(HarnessTeam.CE)
+@Value
+@Builder
+public class GovernanceMigrationRules {
+  @NotNull String name;
+  @NotNull int version;
+  @NotEmpty Set<GovernanceMigrationRule> governanceMigrationRules;
+}
