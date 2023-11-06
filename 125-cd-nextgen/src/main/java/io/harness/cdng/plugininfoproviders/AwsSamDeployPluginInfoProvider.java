9d6a2fe5fa541a4b7018e50c1205b659122628c4
Piyush Bhuwalka <piyush.bhuwalka@harness.io>
2023-11-06 16:23:02 +0530

    feat:[CDS-83465]: Moving env variables during runtime

--- 125-cd-nextgen/src/main/java/io/harness/cdng/plugininfoproviders/AwsSamDeployPluginInfoProvider.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/plugininfoproviders/AwsSamDeployPluginInfoProvider.java
@@ -7,7 +7,6 @@
 
 package io.harness.cdng.plugininfoproviders;
 
-import static io.harness.connector.ConnectorModule.DEFAULT_CONNECTOR_SERVICE;
 import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 
 import io.harness.annotations.dev.CodePulse;
@@ -15,27 +14,11 @@ import io.harness.annotations.dev.HarnessModuleComponent;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.annotations.dev.ProductModule;
-import io.harness.beans.IdentifierRef;
 import io.harness.cdng.aws.sam.AwsSamDeployStepInfo;
 import io.harness.cdng.aws.sam.AwsSamStepHelper;
-import io.harness.cdng.expressions.CDExpressionResolver;
-import io.harness.cdng.infra.beans.AwsSamInfrastructureOutcome;
-import io.harness.cdng.infra.beans.InfrastructureOutcome;
-import io.harness.cdng.manifest.steps.outcome.ManifestsOutcome;
-import io.harness.cdng.manifest.yaml.AwsSamDirectoryManifestOutcome;
 import io.harness.cdng.pipeline.executions.CDPluginInfoProvider;
 import io.harness.cdng.pipeline.steps.CdAbstractStepNode;
-import io.harness.cdng.stepsdependency.constants.OutcomeExpressionConstants;
-import io.harness.connector.ConnectorInfoDTO;
-import io.harness.connector.ConnectorResponseDTO;
-import io.harness.connector.services.ConnectorService;
-import io.harness.delegate.beans.connector.ConnectorConfigDTO;
-import io.harness.delegate.beans.connector.awsconnector.AwsConnectorDTO;
-import io.harness.delegate.beans.connector.awsconnector.AwsCredentialDTO;
-import io.harness.delegate.beans.connector.awsconnector.AwsCredentialSpecDTO;
-import io.harness.delegate.beans.connector.awsconnector.AwsManualConfigSpecDTO;
 import io.harness.executions.steps.StepSpecTypeConstants;
-import io.harness.ng.core.NGAccess;
 import io.harness.pms.contracts.ambiance.Ambiance;
 import io.harness.pms.contracts.plan.ImageDetails;
 import io.harness.pms.contracts.plan.PluginCreationRequest;
@@ -43,35 +26,20 @@ import io.harness.pms.contracts.plan.PluginCreationResponse;
 import io.harness.pms.contracts.plan.PluginCreationResponseWrapper;
 import io.harness.pms.contracts.plan.PluginDetails;
 import io.harness.pms.contracts.plan.StepInfoProto;
-import io.harness.pms.execution.utils.AmbianceUtils;
 import io.harness.pms.sdk.core.plugin.ContainerPluginParseException;
-import io.harness.pms.sdk.core.resolver.RefObjectUtils;
-import io.harness.pms.sdk.core.resolver.outcome.OutcomeService;
 import io.harness.pms.yaml.ParameterField;
 import io.harness.pms.yaml.YamlUtils;
 import io.harness.steps.container.execution.plugin.StepImageConfig;
-import io.harness.utils.IdentifierRefHelper;
-import io.harness.yaml.utils.NGVariablesUtils;
 
 import com.google.inject.Inject;
-import com.google.inject.name.Named;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
-import org.jooq.tools.StringUtils;
 
 @CodePulse(module = ProductModule.CDS, unitCoverageRequired = true, components = {HarnessModuleComponent.CDS_ECS})
 @OwnedBy(HarnessTeam.CDP)
 public class AwsSamDeployPluginInfoProvider implements CDPluginInfoProvider {
-  @Inject private CDExpressionResolver cdExpressionResolver;
-  @Inject private OutcomeService outcomeService;
-
   @Inject PluginExecutionConfig pluginExecutionConfig;
-
-  @Named(DEFAULT_CONNECTOR_SERVICE) @Inject private ConnectorService connectorService;
 
   @Inject private AwsSamStepHelper awsSamStepHelper;
 
@@ -107,7 +75,7 @@ public class AwsSamDeployPluginInfoProvider implements CDPluginInfoProvider {
 
     pluginDetailsBuilder.setImageDetails(imageDetails);
 
-    pluginDetailsBuilder.putAllEnvVariables(getEnvironmentVariables(ambiance, awsSamDeployStepInfo));
+    pluginDetailsBuilder.putAllEnvVariables(new HashMap<>());
     PluginCreationResponse response =
         PluginCreationResponse.newBuilder().setPluginDetails(pluginDetailsBuilder.build()).build();
     StepInfoProto stepInfoProto = StepInfoProto.newBuilder()
@@ -124,112 +92,5 @@ public class AwsSamDeployPluginInfoProvider implements CDPluginInfoProvider {
       return true;
     }
     return false;
-  }
-
-  private Map<String, String> getEnvironmentVariables(Ambiance ambiance, AwsSamDeployStepInfo awsSamDeployStepInfo) {
-    ParameterField<Map<String, String>> envVariables = awsSamDeployStepInfo.getEnvVariables();
-    ParameterField<List<String>> deployCommandOptions = awsSamDeployStepInfo.getDeployCommandOptions();
-    ParameterField<String> stackName = awsSamDeployStepInfo.getStackName();
-
-    // Resolve Expressions
-    cdExpressionResolver.updateExpressions(ambiance, deployCommandOptions);
-
-    // todo: Fetch from infrastructure outcome
-    InfrastructureOutcome infrastructureOutcome = (InfrastructureOutcome) outcomeService.resolve(
-        ambiance, RefObjectUtils.getOutcomeRefObject(OutcomeExpressionConstants.INFRASTRUCTURE_OUTCOME));
-
-    AwsSamInfrastructureOutcome awsSamInfrastructureOutcome = (AwsSamInfrastructureOutcome) infrastructureOutcome;
-
-    String awsConnectorRef = awsSamInfrastructureOutcome.getConnectorRef();
-    String crossAccountRoleArn = null;
-    String externalId = null;
-
-    String awsAccessKey = null;
-    String awsSecretKey = null;
-    String region = ((AwsSamInfrastructureOutcome) infrastructureOutcome).getRegion();
-
-    if (awsConnectorRef != null) {
-      NGAccess ngAccess = AmbianceUtils.getNgAccess(ambiance);
-
-      IdentifierRef identifierRef = IdentifierRefHelper.getIdentifierRef(awsConnectorRef,
-          ngAccess.getAccountIdentifier(), ngAccess.getOrgIdentifier(), ngAccess.getProjectIdentifier());
-
-      Optional<ConnectorResponseDTO> connectorDTO = connectorService.get(identifierRef.getAccountIdentifier(),
-          identifierRef.getOrgIdentifier(), identifierRef.getProjectIdentifier(), identifierRef.getIdentifier());
-
-      ConnectorInfoDTO connectorInfoDTO = connectorDTO.get().getConnector();
-      ConnectorConfigDTO connectorConfigDTO = connectorInfoDTO.getConnectorConfig();
-      AwsConnectorDTO awsConnectorDTO = (AwsConnectorDTO) connectorConfigDTO;
-      AwsCredentialDTO awsCredentialDTO = awsConnectorDTO.getCredential();
-      AwsCredentialSpecDTO awsCredentialSpecDTO = awsCredentialDTO.getConfig();
-
-      if (awsCredentialSpecDTO instanceof AwsManualConfigSpecDTO) {
-        AwsManualConfigSpecDTO awsManualConfigSpecDTO = (AwsManualConfigSpecDTO) awsCredentialSpecDTO;
-
-        if (!StringUtils.isEmpty(awsManualConfigSpecDTO.getAccessKey())) {
-          awsAccessKey = awsManualConfigSpecDTO.getAccessKey();
-        } else {
-          awsAccessKey = NGVariablesUtils.fetchSecretExpressionWithExpressionToken(
-              awsManualConfigSpecDTO.getAccessKeyRef().toSecretRefStringValue(), ambiance.getExpressionFunctorToken());
-        }
-
-        awsSecretKey = NGVariablesUtils.fetchSecretExpressionWithExpressionToken(
-            awsManualConfigSpecDTO.getSecretKeyRef().toSecretRefStringValue(), ambiance.getExpressionFunctorToken());
-      }
-
-      if (awsCredentialDTO.getCrossAccountAccess() != null) {
-        crossAccountRoleArn = awsCredentialDTO.getCrossAccountAccess().getCrossAccountRoleArn();
-        externalId = awsCredentialDTO.getCrossAccountAccess().getExternalId();
-      }
-    }
-
-    HashMap<String, String> samDeployEnvironmentVariablesMap = new HashMap<>();
-
-    ManifestsOutcome manifestsOutcome =
-        (ManifestsOutcome) outcomeService
-            .resolveOptional(ambiance, RefObjectUtils.getOutcomeRefObject(OutcomeExpressionConstants.MANIFESTS))
-            .getOutcome();
-    // @todo(hinger) render manifests here
-
-    AwsSamDirectoryManifestOutcome awsSamDirectoryManifestOutcome =
-        (AwsSamDirectoryManifestOutcome) awsSamStepHelper.getAwsSamDirectoryManifestOutcome(manifestsOutcome.values());
-
-    String samDir =
-        awsSamStepHelper.getSamDirectoryPathFromAwsSamDirectoryManifestOutcome(awsSamDirectoryManifestOutcome);
-
-    samDeployEnvironmentVariablesMap.put("PLUGIN_SAM_DIR", samDir);
-
-    if (ParameterField.isNotNull(deployCommandOptions)) {
-      samDeployEnvironmentVariablesMap.put(
-          "PLUGIN_DEPLOY_COMMAND_OPTIONS", String.join(" ", deployCommandOptions.getValue()));
-    }
-
-    samDeployEnvironmentVariablesMap.put("PLUGIN_STACK_NAME", stackName.getValue());
-
-    if (awsAccessKey != null) {
-      samDeployEnvironmentVariablesMap.put("PLUGIN_AWS_ACCESS_KEY", awsAccessKey);
-    }
-
-    if (awsSecretKey != null) {
-      samDeployEnvironmentVariablesMap.put("PLUGIN_AWS_SECRET_KEY", awsSecretKey);
-    }
-
-    if (crossAccountRoleArn != null) {
-      samDeployEnvironmentVariablesMap.put("PLUGIN_AWS_ROLE_ARN", crossAccountRoleArn);
-    }
-
-    if (externalId != null) {
-      samDeployEnvironmentVariablesMap.put("PLUGIN_AWS_STS_EXTERNAL_ID", externalId);
-    }
-
-    if (region != null) {
-      samDeployEnvironmentVariablesMap.put("PLUGIN_REGION", region);
-    }
-
-    if (envVariables != null && envVariables.getValue() != null) {
-      samDeployEnvironmentVariablesMap.putAll(envVariables.getValue());
-    }
-
-    return samDeployEnvironmentVariablesMap;
   }
 }
