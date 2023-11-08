aa6096213b3b670b3f358b05cccdc2bc1bd5157d
Dhruv <dhruv@harness.io>
2023-11-08 23:37:28 +0530

    feat:[SSCA-869]: Formatting

--- ssca-manager/build/container/scripts/start_process.sh
+++ ssca-manager/build/container/scripts/start_process.sh
@@ -11,7 +11,6 @@ if [[ -v "{hostname}" ]]; then
    export HOSTNAME=$(hostname)
 fi
 
-export MEMORY_MIN=1024m
 if [[ -z "$MEMORY" ]]; then
    export MEMORY=4096m
 fi
@@ -28,7 +27,7 @@ fi
 
 export GC_PARAMS=" -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=40 -XX:MaxGCPauseMillis=1000 -Dfile.encoding=UTF-8"
 
-export JAVA_OPTS="-Xms${MEMORY_MIN} -Xmx${MEMORY} -XX:+HeapDumpOnOutOfMemoryError -Xloggc:mygclogfilename.gc $GC_PARAMS $JAVA_ADVANCED_FLAGS $JAVA_17_FLAGS"
+export JAVA_OPTS="-Xms${MEMORY} -Xmx${MEMORY} -XX:+HeapDumpOnOutOfMemoryError -Xloggc:mygclogfilename.gc $GC_PARAMS $JAVA_ADVANCED_FLAGS $JAVA_17_FLAGS"
 
 if [[ "${ENABLE_APPDYNAMICS}" == "true" ]]; then
     mkdir /opt/harness/AppServerAgent && unzip AppServerAgent.zip -d /opt/harness/AppServerAgent
