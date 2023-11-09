5c8f68d312d77af5e36fd467d91f08f2d1cdeb74
Vishal Bhatia <vishal.bhatia@harness.io>
2023-11-09 16:07:31 -0500

    feat:[IE-492]

--- srm-service/build/container/scripts/start_process.sh
+++ srm-service/build/container/scripts/start_process.sh
@@ -28,13 +28,13 @@ fi
 
 export JAVA_OPTS="-Xms${MEMORY}m -Xmx${MEMORY}m -XX:+HeapDumpOnOutOfMemoryError -Xloggc:mygclogfilename.gc -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=40 -XX:MaxGCPauseMillis=500 -Dfile.encoding=UTF-8 $JAVA_ADVANCED_FLAGS $JAVA_17_FLAGS"
 
-if [[ "${ENABLE_APPDYNAMICS}" == "true" ]]; then
-    mkdir /opt/harness/AppServerAgent && unzip AppServerAgent.zip -d /opt/harness/AppServerAgent
-    node_name="-Dappdynamics.agent.nodeName=$(hostname)"
-    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/AppServerAgent/javaagent.jar -Dappdynamics.jvm.shutdown.mark.node.as.historical=true"
-    JAVA_OPTS="$JAVA_OPTS $node_name"
-    echo "Using Appdynamics java agent"
-fi
+#if [[ "${ENABLE_APPDYNAMICS}" == "true" ]]; then
+#    mkdir /opt/harness/AppServerAgent && unzip AppServerAgent.zip -d /opt/harness/AppServerAgent
+#    node_name="-Dappdynamics.agent.nodeName=$(hostname)"
+#    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/AppServerAgent/javaagent.jar -Dappdynamics.jvm.shutdown.mark.node.as.historical=true"
+#    JAVA_OPTS="$JAVA_OPTS $node_name"
+#    echo "Using Appdynamics java agent"
+#fi
 
 if [[ "${ENABLE_MONITORING}" == "true" ]] ; then
     echo "Monitoring  is enabled"
@@ -42,25 +42,25 @@ if [[ "${ENABLE_MONITORING}" == "true" ]] ; then
     echo "Using inspectIT Java Agent"
 fi
 
-if [[ "${ENABLE_COVERAGE}" == "true" ]] ; then
-    echo "functional code coverage is enabled"
-    mkdir /opt/harness/jacoco-0.8.7 && unzip jacoco-0.8.7.zip -d /opt/harness/jacoco-0.8.7
-    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/jacoco-0.8.7/lib/jacocoagent.jar=port=6300,address=0.0.0.0,append=true,output=tcpserver,destfile=jacoco-remote.exec"
-    echo "Using Jacoco Java Agent"
-fi
+#if [[ "${ENABLE_COVERAGE}" == "true" ]] ; then
+#    echo "functional code coverage is enabled"
+#    mkdir /opt/harness/jacoco-0.8.7 && unzip jacoco-0.8.7.zip -d /opt/harness/jacoco-0.8.7
+#    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/jacoco-0.8.7/lib/jacocoagent.jar=port=6300,address=0.0.0.0,append=true,output=tcpserver,destfile=jacoco-remote.exec"
+#    echo "Using Jacoco Java Agent"
+#fi
 
-if [[ "${ENABLE_OPENTELEMETRY}" == "true" ]] ; then
-    echo "OpenTelemetry is enabled"
-    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/opentelemetry-javaagent.jar -Dotel.service.name=${OTEL_SERVICE_NAME:-cv-nextgen}"
-
-    if [ -n "$OTEL_EXPORTER_OTLP_ENDPOINT" ]; then
-        JAVA_OPTS=$JAVA_OPTS" -Dotel.exporter.otlp.endpoint=$OTEL_EXPORTER_OTLP_ENDPOINT "
-    else
-        echo "OpenTelemetry export is disabled"
-        JAVA_OPTS=$JAVA_OPTS" -Dotel.traces.exporter=none -Dotel.metrics.exporter=none "
-    fi
-    echo "Using OpenTelemetry Java Agent"
-fi
+#if [[ "${ENABLE_OPENTELEMETRY}" == "true" ]] ; then
+#    echo "OpenTelemetry is enabled"
+#    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/opentelemetry-javaagent.jar -Dotel.service.name=${OTEL_SERVICE_NAME:-cv-nextgen}"
+#
+#    if [ -n "$OTEL_EXPORTER_OTLP_ENDPOINT" ]; then
+#        JAVA_OPTS=$JAVA_OPTS" -Dotel.exporter.otlp.endpoint=$OTEL_EXPORTER_OTLP_ENDPOINT "
+#    else
+#        echo "OpenTelemetry export is disabled"
+#        JAVA_OPTS=$JAVA_OPTS" -Dotel.traces.exporter=none -Dotel.metrics.exporter=none "
+#    fi
+#    echo "Using OpenTelemetry Java Agent"
+#fi
 
 if [[ "${DEPLOY_MODE}" == "KUBERNETES" ]] || [[ "${DEPLOY_MODE}" == "KUBERNETES_ONPREM" ]]; then
     java $JAVA_OPTS -jar $CAPSULE_JAR $COMMAND /opt/harness/cv-nextgen-config.yml
--- template-service/build/container/scripts/start_process.sh
+++ template-service/build/container/scripts/start_process.sh
@@ -27,33 +27,33 @@ export GC_PARAMS=" -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=40 -XX:MaxGCP
 
 export JAVA_OPTS="-Xms${MEMORY} -Xmx${MEMORY} -XX:+HeapDumpOnOutOfMemoryError -Xloggc:mygclogfilename.gc $GC_PARAMS $JAVA_ADVANCED_FLAGS $JAVA_17_FLAGS"
 
-if [[ "${ENABLE_APPDYNAMICS}" == "true" ]]; then
-    mkdir /opt/harness/AppServerAgent && unzip AppServerAgent.zip -d /opt/harness/AppServerAgent
-    node_name="-Dappdynamics.agent.nodeName=$(hostname)"
-    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/AppServerAgent/javaagent.jar -Dappdynamics.jvm.shutdown.mark.node.as.historical=true"
-    JAVA_OPTS="$JAVA_OPTS $node_name"
-    echo "Using Appdynamics java agent"
-fi
+#if [[ "${ENABLE_APPDYNAMICS}" == "true" ]]; then
+#    mkdir /opt/harness/AppServerAgent && unzip AppServerAgent.zip -d /opt/harness/AppServerAgent
+#    node_name="-Dappdynamics.agent.nodeName=$(hostname)"
+#    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/AppServerAgent/javaagent.jar -Dappdynamics.jvm.shutdown.mark.node.as.historical=true"
+#    JAVA_OPTS="$JAVA_OPTS $node_name"
+#    echo "Using Appdynamics java agent"
+#fi
 
-if [[ "${ENABLE_OPENTELEMETRY}" == "true" ]] ; then
-    echo "OpenTelemetry is enabled"
-    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/opentelemetry-javaagent.jar -Dotel.service.name=${OTEL_SERVICE_NAME:-template-service}"
+#if [[ "${ENABLE_OPENTELEMETRY}" == "true" ]] ; then
+#    echo "OpenTelemetry is enabled"
+#    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/opentelemetry-javaagent.jar -Dotel.service.name=${OTEL_SERVICE_NAME:-template-service}"
+#
+#    if [ -n "$OTEL_EXPORTER_OTLP_ENDPOINT" ]; then
+#        JAVA_OPTS=$JAVA_OPTS" -Dotel.exporter.otlp.endpoint=$OTEL_EXPORTER_OTLP_ENDPOINT "
+#    else
+#        echo "OpenTelemetry export is disabled"
+#        JAVA_OPTS=$JAVA_OPTS" -Dotel.traces.exporter=none -Dotel.metrics.exporter=none "
+#    fi
+#    echo "Using OpenTelemetry Java Agent"
+#fi
 
-    if [ -n "$OTEL_EXPORTER_OTLP_ENDPOINT" ]; then
-        JAVA_OPTS=$JAVA_OPTS" -Dotel.exporter.otlp.endpoint=$OTEL_EXPORTER_OTLP_ENDPOINT "
-    else
-        echo "OpenTelemetry export is disabled"
-        JAVA_OPTS=$JAVA_OPTS" -Dotel.traces.exporter=none -Dotel.metrics.exporter=none "
-    fi
-    echo "Using OpenTelemetry Java Agent"
-fi
-
-if [[ "${ENABLE_COVERAGE}" == "true" ]] ; then
-    echo "functional code coverage is enabled"
-    mkdir /opt/harness/jacoco-0.8.7 && unzip jacoco-0.8.7.zip -d /opt/harness/jacoco-0.8.7
-    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/jacoco-0.8.7/lib/jacocoagent.jar=port=6300,address=0.0.0.0,append=true,output=tcpserver,destfile=jacoco-remote.exec"
-    echo "Using Jacoco Java Agent"
-fi
+#if [[ "${ENABLE_COVERAGE}" == "true" ]] ; then
+#    echo "functional code coverage is enabled"
+#    mkdir /opt/harness/jacoco-0.8.7 && unzip jacoco-0.8.7.zip -d /opt/harness/jacoco-0.8.7
+#    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/jacoco-0.8.7/lib/jacocoagent.jar=port=6300,address=0.0.0.0,append=true,output=tcpserver,destfile=jacoco-remote.exec"
+#    echo "Using Jacoco Java Agent"
+#fi
 
 if [[ "${DEPLOY_MODE}" == "KUBERNETES" || "${DEPLOY_MODE}" == "KUBERNETES_ONPREM" || "${DEPLOY_VERSION}" == "COMMUNITY" ]]; then
     java $JAVA_OPTS -jar $CAPSULE_JAR $COMMAND /opt/harness/config.yml
