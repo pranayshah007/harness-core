d57c6cf4c020295f509a60837587732c6afb1b52
Naman Talaycha <naman.talaycha@harness.io>
2023-11-09 13:21:32 +0530

    feat: [CDS-82817]: Manager side changes : Adding new delegate task for Artifact bundle support in TAS deployment

--- 950-delegate-tasks-beans/src/main/java/io/harness/serializer/kryo/DelegateTasksBeansKryoRegister.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/serializer/kryo/DelegateTasksBeansKryoRegister.java
@@ -435,6 +435,8 @@ import io.harness.delegate.exception.HelmNGException;
 import io.harness.delegate.exception.ServerlessNGException;
 import io.harness.delegate.exception.TaskNGDataException;
 import io.harness.delegate.task.ListNotifyResponseData;
+import io.harness.delegate.task.artifactBundle.ArtifactBundleFetchRequest;
+import io.harness.delegate.task.artifactBundle.response.ArtifactBundleFetchResponse;
 import io.harness.delegate.task.artifacts.ArtifactSourceType;
 import io.harness.delegate.task.artifacts.ArtifactTaskType;
 import io.harness.delegate.task.artifacts.S3ArtifactDelegateResponse;
@@ -2581,5 +2583,7 @@ public class DelegateTasksBeansKryoRegister implements KryoRegistrar {
     kryo.register(HelmChartManifest.class, 20000619);
     kryo.register(ReleaseMetadata.class, 20001002);
     kryo.register(AtomicBoolean.class, 20001003);
+    kryo.register(ArtifactBundleFetchRequest.class, 20001004);
+    kryo.register(ArtifactBundleFetchResponse.class, 20001005);
   }
 }
