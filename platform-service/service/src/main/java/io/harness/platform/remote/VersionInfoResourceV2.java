e589057b4335642c5355122b09b08fae080878b1
Vishal Bhatia <vishal.bhatia@harness.io>
2023-11-14 13:31:53 -0500

    feat:[IE-494]: debugging

--- platform-service/service/src/main/java/io/harness/platform/remote/VersionInfoResourceV2.java
+++ platform-service/service/src/main/java/io/harness/platform/remote/VersionInfoResourceV2.java
@@ -18,6 +18,7 @@ import io.swagger.annotations.ApiOperation;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
 import java.util.Map;
