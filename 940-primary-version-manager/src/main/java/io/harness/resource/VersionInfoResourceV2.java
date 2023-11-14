3ccf5c50d8b63b8c9216794e2ec61401cd3430aa
Vishal Bhatia <vishal.bhatia@harness.io>
2023-11-14 07:38:47 -0500

    feat:[IE-494]: debugging

---
+++ 940-primary-version-manager/src/main/java/io/harness/resource/VersionInfoResourceV2.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2021 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
+package io.harness.resource;
+
+import io.harness.security.annotations.PublicApi;
+import io.harness.version.VersionInfoManagerV2;
+import io.harness.version.VersionInfoV2;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+import java.util.Map;
+
+import io.dropwizard.jersey.errors.ErrorMessage;
+
+@PublicApi
+@Path("/version")
+@Produces(MediaType.APPLICATION_JSON)
+public class VersionInfoResourceV2 {
+  @Inject
+  private VersionInfoManagerV2 versionInfoManager;
+
+  public VersionInfoResourceV2(VersionInfoManagerV2 versionInfoManager) {
+    this.versionInfoManager = versionInfoManager;
+  }
+
+  @GET
+  public VersionInfoV2 getVersionInfo() {
+    try {
+      return versionInfoManager.getVersionInfo();
+    } catch (Exception e) {
+      ErrorMessage errorMessage = new ErrorMessage(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), "Failed to retrieve version info: " + e.getMessage());
+      throw new WebApplicationException(Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(errorMessage).build());
+    }
+  }
+}
