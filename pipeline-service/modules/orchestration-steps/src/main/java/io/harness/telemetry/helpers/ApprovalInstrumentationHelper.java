5797d8ab544c8868d2e98bb95cd46003a4d3c84d
Sarthak Kasat <sarthak.kasat@harness.io>
2023-11-17 14:04:13 +0530

    feat: [CDS-83759]: Move InstrumentationHelper to Telemetry module

--- pipeline-service/modules/orchestration-steps/src/main/java/io/harness/telemetry/helpers/ApprovalInstrumentationHelper.java
+++ pipeline-service/modules/orchestration-steps/src/main/java/io/harness/telemetry/helpers/ApprovalInstrumentationHelper.java
@@ -12,27 +12,20 @@ import static io.harness.telemetry.helpers.InstrumentationConstants.APPROVAL_CRI
 import static io.harness.telemetry.helpers.InstrumentationConstants.APPROVAL_STEP;
 import static io.harness.telemetry.helpers.InstrumentationConstants.APPROVAL_TYPE;
 import static io.harness.telemetry.helpers.InstrumentationConstants.AUTO_APPROVAL;
-import static io.harness.telemetry.helpers.InstrumentationConstants.GLOBAL_ACCOUNT_ID;
 import static io.harness.telemetry.helpers.InstrumentationConstants.ORG;
+import static io.harness.telemetry.helpers.InstrumentationConstants.PIPELINE_ID;
 import static io.harness.telemetry.helpers.InstrumentationConstants.PROJECT;
 import static io.harness.telemetry.helpers.InstrumentationConstants.REJECTION_CRITERIA_SPEC_TYPE;
 import static io.harness.telemetry.helpers.InstrumentationConstants.RETRY_INTERNAL;
 
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.data.structure.EmptyPredicate;
 import io.harness.steps.approval.step.custom.entities.CustomApprovalInstance;
 import io.harness.steps.approval.step.entities.ApprovalInstance;
 import io.harness.steps.approval.step.harness.entities.HarnessApprovalInstance;
 import io.harness.steps.approval.step.jira.entities.JiraApprovalInstance;
 import io.harness.steps.approval.step.servicenow.entities.ServiceNowApprovalInstance;
-import io.harness.telemetry.Category;
-import io.harness.telemetry.Destination;
-import io.harness.telemetry.TelemetryOption;
-import io.harness.telemetry.TelemetryReporter;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.util.HashMap;
 import java.util.concurrent.CompletableFuture;
@@ -42,14 +35,13 @@ import lombok.extern.slf4j.Slf4j;
 @Singleton
 @OwnedBy(HarnessTeam.CDC)
 public class ApprovalInstrumentationHelper extends InstrumentationHelper {
-  @Inject TelemetryReporter telemetryReporter;
-
   public CompletableFuture<Void> sendApprovalEvent(ApprovalInstance approvalInstance) {
     HashMap<String, Object> eventPropertiesMap = new HashMap<>();
     eventPropertiesMap.put(ACCOUNT, approvalInstance.getAccountId());
     eventPropertiesMap.put(ORG, approvalInstance.getProjectIdentifier());
     eventPropertiesMap.put(PROJECT, approvalInstance.getOrgIdentifier());
     eventPropertiesMap.put(APPROVAL_TYPE, approvalInstance.getType());
+    eventPropertiesMap.put(PIPELINE_ID, approvalInstance.getPipelineIdentifier());
     switch (approvalInstance.getType()) {
       case JIRA_APPROVAL:
         return publishJiraApprovalInfo((JiraApprovalInstance) approvalInstance, APPROVAL_STEP, eventPropertiesMap);
@@ -97,27 +89,5 @@ public class ApprovalInstrumentationHelper extends InstrumentationHelper {
     eventPropertiesMap.put(REJECTION_CRITERIA_SPEC_TYPE, approvalInstance.getRejectionCriteria().getType());
     eventPropertiesMap.put(APPROVAL_CRITERIA_SPEC_TYPE, approvalInstance.getApprovalCriteria().getType());
     return sendEvent(eventName, accountId, eventPropertiesMap);
-  }
-
-  private CompletableFuture<Void> sendEvent(
-      String eventName, String accountId, HashMap<String, Object> eventPropertiesMap) {
-    try {
-      if (EmptyPredicate.isNotEmpty(accountId) || !accountId.equals(GLOBAL_ACCOUNT_ID)) {
-        String userId = getUserId();
-        return CompletableFuture.runAsync(
-            ()
-                -> telemetryReporter.sendTrackEvent(eventName, userId, accountId, eventPropertiesMap,
-                    ImmutableMap.<Destination, Boolean>builder()
-                        .put(Destination.AMPLITUDE, true)
-                        .put(Destination.ALL, false)
-                        .build(),
-                    Category.PLATFORM, TelemetryOption.builder().sendForCommunity(true).build()));
-      } else {
-        log.info("There is no account found for account ID = " + accountId + "!. Cannot send " + eventName + " event.");
-      }
-    } catch (Exception e) {
-      log.error(eventName + " event failed for accountID= " + accountId, e);
-    }
-    return null;
   }
 }
