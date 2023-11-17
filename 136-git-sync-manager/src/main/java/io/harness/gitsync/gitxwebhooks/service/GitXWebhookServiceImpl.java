25f295476f9891a8d4cfdd7049fd6e1b02183590
Adithya Viswanathan <adithya.viswanathan@harness.io>
2023-11-17 10:01:54 +0530

    [feat]: [CDS-84609]: checkstyle fix

--- 136-git-sync-manager/src/main/java/io/harness/gitsync/gitxwebhooks/service/GitXWebhookServiceImpl.java
+++ 136-git-sync-manager/src/main/java/io/harness/gitsync/gitxwebhooks/service/GitXWebhookServiceImpl.java
@@ -52,8 +52,6 @@ import io.harness.repositories.gitxwebhook.GitXWebhookRepository;
 
 import com.google.inject.Inject;
 import com.mongodb.client.result.DeleteResult;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
