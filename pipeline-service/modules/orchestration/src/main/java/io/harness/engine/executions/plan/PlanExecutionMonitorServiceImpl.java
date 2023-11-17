8b9052219587ffef986831d2e6703f00fbb89170
Yuvraj Singhal <yuvraj.singhal@harness.io>
2023-11-17 13:23:33 +0530

    [CDS-83924]: Change query to facet operation

--- pipeline-service/modules/orchestration/src/main/java/io/harness/engine/executions/plan/PlanExecutionMonitorServiceImpl.java
+++ pipeline-service/modules/orchestration/src/main/java/io/harness/engine/executions/plan/PlanExecutionMonitorServiceImpl.java
@@ -6,7 +6,6 @@
  */
 
 package io.harness.engine.executions.plan;
-
 import io.harness.annotations.dev.CodePulse;
 import io.harness.annotations.dev.HarnessModuleComponent;
 import io.harness.annotations.dev.HarnessTeam;
