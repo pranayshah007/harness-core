6fcd6488c7d94c0237a234c850578afc7bd7cc36
Alexandru Bosii <alexandru.bosii@harness.io>
2023-11-20 15:37:45 +0200

    revert local changes

--- 960-api-services/src/main/java/io/harness/process/LocalProcessRunner.java
+++ 960-api-services/src/main/java/io/harness/process/LocalProcessRunner.java
@@ -1,10 +1,6 @@
 package io.harness.process;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
-import java.util.concurrent.atomic.AtomicInteger;
-import org.zeroturnaround.exec.ProcessExecutor;
-import org.zeroturnaround.exec.ProcessResult;
 
 public class LocalProcessRunner extends AbstractProcessRunner {
   private final ExecutorService executorService;
@@ -15,19 +11,6 @@ public class LocalProcessRunner extends AbstractProcessRunner {
 
   @Override
   protected ProcessRef execute(String processKey, ProcessExecutorFactory processFactory) {
-    Future<ProcessResult> resultFuture = executorService.submit(() -> {
-      ProcessExecutor executor = processFactory.create();
-      return executor.execute();
-    });
-
-    return new SharedProcessRef(resultFuture, new AtomicInteger(1), createClosable(resultFuture));
-  }
-
-  private Runnable createClosable(final Future<ProcessResult> future) {
-    return () -> {
-      if (!future.isDone() && !future.isCancelled()) {
-        future.cancel(true);
-      }
-    };
+    return new LocalProcessRef(processFactory);
   }
 }
