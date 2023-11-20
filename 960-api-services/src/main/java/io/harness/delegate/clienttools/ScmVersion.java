8c2927ea43bff01fb417b0bf9a4ce2f99cbb604a
abhay084 <abhay.ganvir@harness.io>
2023-11-20 18:17:20 +0530

    fix: [CI-10298]: scm grpc version upgrade

--- 960-api-services/src/main/java/io/harness/delegate/clienttools/ScmVersion.java
+++ 960-api-services/src/main/java/io/harness/delegate/clienttools/ScmVersion.java
@@ -16,7 +16,7 @@ import org.apache.commons.lang3.StringUtils;
 @Getter
 @Slf4j
 public enum ScmVersion implements ClientToolVersion {
-  DEFAULT("0b5de953");
+  DEFAULT("d6b5dabd");
 
   private final String version;
 
